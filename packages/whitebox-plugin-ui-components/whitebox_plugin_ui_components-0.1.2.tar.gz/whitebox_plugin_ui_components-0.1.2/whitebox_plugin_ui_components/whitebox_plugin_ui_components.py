e38d0218a84bba84d60ffafcff288fc956cdd9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:17 +0000

    Add package whitebox-plugin-ui-components/0.1.2/whitebox_plugin_ui_components-0.1.2.tar.gz

---
+++ packages/whitebox-plugin-ui-components/whitebox_plugin_ui_components-0.1.2.tar.gz/whitebox_plugin_ui_components-0.1.2/whitebox_plugin_ui_components/whitebox_plugin_ui_components.py
@@ -0,0 +1,22 @@
+import whitebox
+
+
+class WhiteboxPluginUIComponents(whitebox.Plugin):
+    name = "UI Components"
+
+    provides_capabilities = ["ui"]
+    exposed_component_map = {
+        "ui": {
+            # Buttons
+            "button": "buttons/Button",
+            "button-primary": "buttons/PrimaryButton",
+            "button-secondary": "buttons/SecondaryButton",
+            "button-tertiary": "buttons/TertiaryButton",
+            # Other common components
+            "spinner": "common/Spinner",
+            "input-content-area": "common/InputContentArea",
+        }
+    }
+
+
+plugin_class = WhiteboxPluginUIComponents

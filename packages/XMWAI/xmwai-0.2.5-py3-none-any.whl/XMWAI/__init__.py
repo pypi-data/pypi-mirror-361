454dc642a50e0504d52b1534a3f9dcbe8305aefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:37 +0000

    Add package XMWAI/0.2.5/xmwai-0.2.5-py3-none-any.whl

---
+++ packages/XMWAI/xmwai-0.2.5-py3-none-any.whl/XMWAI/__init__.py
@@ -0,0 +1,7 @@
+from .core import story, photo, reply, poem, get_access_token, crack  # 从子模块导入函数到顶层
+from .magic_core import birthday  # 从子模块导入函数到顶层
+from .bomb_core import bomb  # 从子模块导入函数到顶层
+from .idiom_core import idiom, searchIdiom, get_json_path  # 从子模块导入函数到顶层
+
+__all__ = ["story", "photo", "reply", "poem", "get_access_token", 'birthday', 'bomb',
+           "idiom", "searchIdiom", "get_json_path", "crack"]  # 可选：明确导出的内容

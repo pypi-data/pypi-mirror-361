020bcc5ce8ff3c492f0a7daefa803dc7246c252f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:48 +0000

    Add package tb-rest-client/4.1.0/tb_rest_client-4.1.0.tar.gz

---
+++ packages/tb-rest-client/tb_rest_client-4.1.0.tar.gz/tb_rest_client-4.1.0/tb_rest_client/api/api_pe/__init__.py
@@ -0,0 +1,72 @@
+#  Copyright 2025. ThingsBoard
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from .self_registration_controller_api import SelfRegistrationControllerApi
+from .http_integration_controller_api import HttpIntegrationControllerApi
+from .asset_controller_api import AssetControllerApi
+from .device_group_ota_package_controller_api import DeviceGroupOtaPackageControllerApi
+from .edge_controller_api import EdgeControllerApi
+from .user_permissions_controller_api import UserPermissionsControllerApi
+from .owner_controller_api import OwnerControllerApi
+from .rpc_v_2_controller_api import RpcV2ControllerApi
+from .edge_event_controller_api import EdgeEventControllerApi
+from .customer_controller_api import CustomerControllerApi
+from .user_controller_api import UserControllerApi
+from .group_permission_controller_api import GroupPermissionControllerApi
+from .device_controller_api import DeviceControllerApi
+from .converter_controller_api import ConverterControllerApi
+from .entity_view_controller_api import EntityViewControllerApi
+from .rule_engine_controller_api import RuleEngineControllerApi
+from .admin_controller_api import AdminControllerApi
+from .t_mobile_iot_cdp_integration_controller_api import TMobileIotCdpIntegrationControllerApi
+from .sign_up_controller_api import SignUpControllerApi
+from .thing_park_integration_controller_api import ThingParkIntegrationControllerApi
+from .sig_fox_integration_controller_api import SigFoxIntegrationControllerApi
+from .scheduler_event_controller_api import SchedulerEventControllerApi
+from .report_controller_api import ReportControllerApi
+from .dashboard_controller_api import DashboardControllerApi
+from .integration_controller_api import IntegrationControllerApi
+from .custom_menu_controller_api import CustomMenuControllerApi
+from .lwm_2m_controller_api import Lwm2mControllerApi
+from .custom_translation_controller_api import CustomTranslationControllerApi
+from .role_controller_api import RoleControllerApi
+from .blob_entity_controller_api import BlobEntityControllerApi
+from .tenant_controller_api import TenantControllerApi
+from .chirp_stack_integration_controller_api import ChirpStackIntegrationControllerApi
+from .white_labeling_controller_api import WhiteLabelingControllerApi
+from .ota_package_controller_api import OtaPackageControllerApi
+from .entity_group_controller_api import EntityGroupControllerApi
+from .subscription_controller_api import SubscriptionControllerApi
+from .solution_controller_api import SolutionControllerApi
+from .device_profile_controller_api import DeviceProfileControllerApi
+from .asset_profile_controller_api import AssetProfileControllerApi
+from .two_factor_auth_controller_api import TwoFactorAuthControllerApi
+from .device_connectivity_controller_api import DeviceConnectivityControllerApi
+from .mail_config_template_controller_api import MailConfigTemplateControllerApi
+from .image_controller_api import ImageControllerApi
+from .mobile_application_controller_api import MobileApplicationControllerApi
+from .queue_stats_controller_api import QueueStatsControllerApi
+from .translation_controller_api import TranslationControllerApi
+from .converter_library_controller_api import ConverterLibraryControllerApi
+from .domain_controller_api import DomainControllerApi
+from .mobile_app_controller_api import MobileAppControllerApi
+from .mobile_app_bundle_controller_api import MobileAppBundleControllerApi
+from .qr_code_settings_controller_api import QrCodeSettingsControllerApi
+from .billing_endpoint_controller_api import BillingEndpointControllerApi
+from .cloud_endpoint_controller_api import CloudEndpointControllerApi
+from .calculated_field_controller_api import CalculatedFieldControllerApi
+from .job_controller_api import JobControllerApi
+from .secret_controller_api import SecretControllerApi
+from .trendz_controller_api import TrendzControllerApi
+from .rule_chain_controller_api import RuleChainControllerApi

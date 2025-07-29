061bbe7cebb5ddbc770047fab82bf4fa1b393afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:39 +0000

    Add package cubicweb-iprogress/1.1.0/cubicweb_iprogress-1.1.0.tar.gz

---
+++ packages/cubicweb-iprogress/cubicweb_iprogress-1.1.0.tar.gz/cubicweb_iprogress-1.1.0/cubicweb_iprogress/__pkginfo__.py
@@ -0,0 +1,26 @@
+# pylint: disable=W0622
+"""cubicweb-iprogress application packaging information"""
+
+modname = "iprogress"
+distname = "cubicweb-iprogress"
+
+numversion = (1, 1, 0)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+author = "LOGILAB S.A. (Paris, FRANCE)"
+author_email = "contact@logilab.fr"
+description = "some adapters and view for stuff progressing to reach a milestone"
+web = "http://www.cubicweb.org/project/%s" % distname
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python",
+    "Programming Language :: JavaScript",
+]
+
+__depends__ = {
+    "cubicweb": ">=4.5.2,<5.0.0",
+    "cubicweb-web": ">=1.4.2,<2.0.0",
+}
+__recommends__ = {}

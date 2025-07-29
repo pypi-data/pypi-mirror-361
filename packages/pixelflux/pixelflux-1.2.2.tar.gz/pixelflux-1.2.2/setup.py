b8dcec8a884b5fd90166fa197ae01ddaa0057ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:27 +0000

    Add package pixelflux/1.2.2/pixelflux-1.2.2.tar.gz

---
+++ packages/pixelflux/pixelflux-1.2.2.tar.gz/pixelflux-1.2.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from setuptools import setup, Extension
+
+screen_capture_module = Extension(
+    'pixelflux.screen_capture_module', 
+    sources=[
+        'pixelflux/screen_capture_module.cpp',
+        'pixelflux/include/xxhash.c'
+    ],
+    include_dirs=['pixelflux/include'],
+    libraries=['X11', 'Xext', 'Xfixes', 'jpeg', 'x264', 'yuv', 'dl', 'va', 'va-drm'],
+    language='c++',
+    extra_compile_args=['-std=c++17', '-Wno-unused-function']
+)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pixelflux",
+    version="1.2.2",
+    author="Linuxserver.io",
+    author_email="pypi@linuxserver.io",
+    description="A performant web native pixel delivery pipeline for diverse sources, blending VNC-inspired parallel processing of pixel buffers with flexible modern encoding formats.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MPL-2.0",
+    url="https://github.com/linuxserver/pixelflux",
+    packages=setuptools.find_packages(),
+    ext_modules=[screen_capture_module],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.6",
+)

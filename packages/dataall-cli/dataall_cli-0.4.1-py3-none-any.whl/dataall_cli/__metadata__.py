fef2f1ea0901cb3103f1ef4159f182285ee599a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:57 +0000

    Add package dataall-cli/0.4.1/dataall_cli-0.4.1-py3-none-any.whl

---
+++ packages/dataall-cli/dataall_cli-0.4.1-py3-none-any.whl/dataall_cli/__metadata__.py
@@ -0,0 +1,11 @@
+"""Metadata Module.
+
+Source repository: TODO
+Documentation: TODO
+
+"""
+
+__title__: str = "dataall_cli"
+__description__: str = "AWS Dataall CLI"
+__version__: str = "0.3.0a1"
+__license__: str = "Apache License 2.0"

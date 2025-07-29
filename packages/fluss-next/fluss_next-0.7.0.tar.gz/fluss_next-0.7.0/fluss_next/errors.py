514fdadec814d743d407f29ecd39607c323a6069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:22 +0000

    Add package fluss-next/0.7.0/fluss_next-0.7.0.tar.gz

---
+++ packages/fluss-next/fluss_next-0.7.0.tar.gz/fluss_next-0.7.0/fluss_next/errors.py
@@ -0,0 +1,7 @@
+"""Custom exceptions for the Fluss Next application."""
+
+
+class NoFlussFound(Exception):
+    """Exception raised when no Fluss instance is found in the context."""
+
+    pass
---
+++ packages/fluss-next/fluss_next-0.7.0.tar.gz/fluss_next-0.7.0/fluss_next/traits.py
@@ -0,0 +1,9 @@
+"""Traits for Fluss Next"""
+
+
+class MockableTrait:
+    """
+    A trait that can be mocked for testing purposes.
+    """
+
+    pass

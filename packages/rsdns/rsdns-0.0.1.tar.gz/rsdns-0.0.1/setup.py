7cb6ef59bdfc679e4fd0c4633181004d84f37ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:26 +0000

    Add package rsdns/0.0.1/rsdns-0.0.1.tar.gz

---
+++ packages/rsdns/rsdns-0.0.1.tar.gz/rsdns-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rsdns",
+    version="0.0.1",
+    packages=["rsdns"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

e6285f245dd83a7c5a57dc3042a29894093111bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:10:06 +0000

    Add package superlinked/29.6.2/superlinked-29.6.2.tar.gz

---
+++ packages/superlinked/superlinked-29.6.2.tar.gz/superlinked-29.6.2/superlinked/framework/storage/redis/redis_search.py
@@ -0,0 +1,53 @@
+# Copyright 2024 Superlinked, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from beartype.typing import Any, cast
+from typing_extensions import override
+
+from superlinked.framework.common.precision import Precision
+from superlinked.framework.common.storage.index_config import IndexConfig
+from superlinked.framework.common.storage.query.vdb_knn_search_params import (
+    VDBKNNSearchParams,
+)
+from superlinked.framework.common.storage.search import Search
+from superlinked.framework.storage.redis.query.redis_query_builder import (
+    RedisQueryBuilder,
+    VectorQueryObj,
+)
+from superlinked.framework.storage.redis.query.vdb_knn_search_params import (
+    RedisVDBKNNSearchConfig,
+)
+from superlinked.framework.storage.redis.redis_field_encoder import RedisFieldEncoder
+from superlinked.framework.storage.redis.redis_vdb_client import RedisVDBClient
+
+
+class RedisSearch(Search[VDBKNNSearchParams, VectorQueryObj, dict[bytes, Any], RedisVDBKNNSearchConfig]):
+    def __init__(self, client: RedisVDBClient, encoder: RedisFieldEncoder, vector_precision: Precision) -> None:
+        super().__init__()
+        self._client = client
+        self._query_builder = RedisQueryBuilder(encoder, vector_precision)
+
+    @override
+    def build_query(self, search_params: VDBKNNSearchParams, search_config: RedisVDBKNNSearchConfig) -> VectorQueryObj:
+        return self._query_builder.build_query(search_params, search_config)
+
+    @override
+    async def knn_search(
+        self,
+        index_config: IndexConfig,
+        query: VectorQueryObj,
+    ) -> dict[bytes, Any]:
+        result = await self._client.client.ft(index_config.index_name).search(query.query, query.params)
+        return cast(dict[bytes, Any], result)

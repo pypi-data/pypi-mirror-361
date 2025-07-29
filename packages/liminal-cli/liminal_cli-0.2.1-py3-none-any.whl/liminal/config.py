e861efee090d4579eb0af8c5aad8b1c4e652684a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:51 +0000

    Add package liminal-cli/0.2.1/liminal_cli-0.2.1-py3-none-any.whl

---
+++ packages/liminal-cli/liminal_cli-0.2.1-py3-none-any.whl/liminal/config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ADDRESS = os.environ.get('LIMINAL_SHELLSYNC_API_ADDRESS', 'https://shellsync.liminalbios.com/api/v1')
+SYNC_ADDRESS = os.environ.get('LIMINAL_SHELLSYNC_ATUIN_ADDRESS', 'https://services.shellsync.liminalbios.com/atuin/api/v1')
+
+LIMINAL_INSTALLER_SKIP_CLEANUP = os.environ.get('LIMINAL_INSTALLER_SKIP_CLEANUP', 'no')
+LIMINAL_INSTALLER_SKIP_ATUIN_IMPORT_HISTORY = os.environ.get('LIMINAL_INSTALLER_SKIP_ATUIN_IMPORT', None)

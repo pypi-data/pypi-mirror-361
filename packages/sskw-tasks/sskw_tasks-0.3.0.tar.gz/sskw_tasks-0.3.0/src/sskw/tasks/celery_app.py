8ca1607ac5d7d061bd0e5750d0275f4b9864fd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:32 +0000

    Add package sskw-tasks/0.3.0/sskw_tasks-0.3.0.tar.gz

---
+++ packages/sskw-tasks/sskw_tasks-0.3.0.tar.gz/sskw_tasks-0.3.0/src/sskw/tasks/celery_app.py
@@ -0,0 +1,16 @@
+from celery import Celery
+import os
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+
+app = Celery(
+    'sskw_tasks',
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND,
+    timezone='UTC',
+    enable_utc=True,
+)
+
+# 自动发现任务
+# app.autodiscover_tasks(['sskw.tasks'])

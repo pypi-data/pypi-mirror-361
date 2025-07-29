331c0f4c31a07ae79fa29064273f368e54336bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package pycord-quart/0.1.0/pycord_quart-0.1.0.tar.gz

---
+++ packages/pycord-quart/pycord_quart-0.1.0.tar.gz/pycord_quart-0.1.0/pycord/quart/exceptions.py
@@ -0,0 +1,17 @@
+"""
+pycord-quart Exception Classes
+"""
+
+class DiscordAuthError(Exception):
+    """Base Discord authentication exception"""
+    pass
+
+
+class TokenExchangeError(DiscordAuthError):
+    """Token exchange failure exception"""
+    pass
+
+
+class UserInfoError(DiscordAuthError):
+    """User information retrieval failure exception"""
+    pass

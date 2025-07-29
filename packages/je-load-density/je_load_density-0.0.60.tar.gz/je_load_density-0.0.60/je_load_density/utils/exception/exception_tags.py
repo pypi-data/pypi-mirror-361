66575ee45ce02bb293776a2c3678e2eb5826b4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:51 +0000

    Add package je-load-density/0.0.60/je_load_density-0.0.60.tar.gz

---
+++ packages/je-load-density/je_load_density-0.0.60.tar.gz/je_load_density-0.0.60/je_load_density/utils/exception/exception_tags.py
@@ -0,0 +1,23 @@
+# locust error
+not_found_locust_error: str = "locust not found"
+# json error
+cant_reformat_json_error: str = "can't reformat json is type right?"
+wrong_json_data_error: str = "can't parser json"
+cant_find_json_error: str = "can't find json"
+cant_save_json_error: str = "can't save json"
+cant_generate_json_report: str = "can't generate json report"
+# executor error
+executor_data_error: str = "executor receive wrong data"
+executor_list_error: str = "executor receive wrong data list is none or wrong type"
+# HTML
+html_generate_no_data_tag: str = "record is None"
+# add command
+add_command_exception_tag: str = "command value type should be as method or function"
+# argparse
+argparse_get_wrong_data: str = "argparse receive wrong data"
+# XML
+cant_read_xml_error: str = "can't read xml"
+xml_type_error: str = "xml type error"
+# Callback executor
+get_bad_trigger_method: str = "get bad trigger method, only accept kwargs and args"
+get_bad_trigger_function: str = "get bad trigger function only accept function in event_dict"

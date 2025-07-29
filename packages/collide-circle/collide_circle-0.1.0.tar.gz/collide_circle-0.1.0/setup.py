af98e8128e0e0b13571e876223c422a06807f262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:45 +0000

    Add package collide-circle/0.1.0/collide_circle-0.1.0.tar.gz

---
+++ packages/collide-circle/collide_circle-0.1.0.tar.gz/collide_circle-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='collide_circle',
+    version='0.1.0',
+    description='Rectangle-circle collision detection for Pygame.',
+    author='Austine Onwubiko',
+    author_email='austineonwubiko@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

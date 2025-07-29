bc32cd809a701fce49a3f3f4371e85d0d6f27d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:42 +0000

    Add package mdllama/2.1.5/mdllama-2.1.5.tar.gz

---
+++ packages/mdllama/mdllama-2.1.5.tar.gz/mdllama-2.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="mdllama",
+    version="2.1.5",
+    description="A command-line interface for Ollama API",
+    author="QinCai-rui",
+    py_modules=["mdllama"],
+    install_requires=[
+        "requests",
+        "rich",
+        "colorama"
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "mdllama=mdllama:main"
+        ]
+    },
+)

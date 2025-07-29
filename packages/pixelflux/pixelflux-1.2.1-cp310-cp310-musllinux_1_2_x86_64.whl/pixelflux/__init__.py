d07cb53ef9db2ce3c8130145ce47f317957fabd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:31 +0000

    Add package pixelflux/1.2.1/pixelflux-1.2.1-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/pixelflux/pixelflux-1.2.1-cp310-cp310-musllinux_1_2_x86_64.whl/pixelflux/__init__.py
@@ -0,0 +1,13 @@
+from .screen_capture_module import (
+    CaptureSettings,
+    StripeEncodeResult,
+    ScreenCapture,
+    StripeCallback,
+)
+
+__all__ = [
+    "CaptureSettings",
+    "StripeEncodeResult",
+    "ScreenCapture",
+    "StripeCallback",
+]

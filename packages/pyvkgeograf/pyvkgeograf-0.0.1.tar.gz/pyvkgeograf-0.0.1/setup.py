8aea4e3020569ab6022a0e36024e3525b1215070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package pyvkgeograf/0.0.1/pyvkgeograf-0.0.1.tar.gz

---
+++ packages/pyvkgeograf/pyvkgeograf-0.0.1.tar.gz/pyvkgeograf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyvkgeograf",
+    version="0.0.1",
+    packages=["pyvkgeograf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

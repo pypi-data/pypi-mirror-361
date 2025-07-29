20dba06ddacf442deffbde8ca251b710c1f71329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:57 +0000

    Add package odoo-addon-stock-available-to-promise-release/16.0.3.8.2.1/odoo_addon_stock_available_to_promise_release-16.0.3.8.2.1-py3-none-any.whl

---
+++ packages/odoo-addon-stock-available-to-promise-release/odoo_addon_stock_available_to_promise_release-16.0.3.8.2.1-py3-none-any.whl/odoo/addons/stock_available_to_promise_release/migrations/16.0.3.1.0/post-migrate.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        UPDATE
+            stock_picking
+        SET
+            release_policy = procurement_group.move_type
+        FROM
+            procurement_group
+        WHERE
+            stock_picking.state NOT IN ('done', 'cancel')
+        AND
+            stock_picking.group_id = procurement_group.id
+        AND
+            stock_picking.release_policy != procurement_group.move_type;
+    """
+    )

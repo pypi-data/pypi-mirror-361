840f4432411a324b9a4fe8b9f74276b967d59b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:28 +0000

    Add package djangoldp-becknld/1.1.7/djangoldp_becknld-1.1.7-py3-none-any.whl

---
+++ packages/djangoldp-becknld/djangoldp_becknld-1.1.7-py3-none-any.whl/djangoldp_becknld/models/offer/delivery_method.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from djangoldp.permissions import InheritPermissions
+
+from djangoldp_becknld.models.__base import baseModel
+from djangoldp_becknld.models.offer.address import Address
+
+
+class DeliveryMethod(baseModel):
+    delivery_address = models.ForeignKey(
+        Address,
+        related_name="delivery_method",
+        on_delete=models.CASCADE,
+    )
+    # TODO: Weird one? Should it be a dedicated model?
+    contact_point = models.CharField(max_length=24)
+
+    class Meta(baseModel.Meta):
+        disable_url = True
+        verbose_name = _("Delivery Method")
+        verbose_name_plural = _("Delivery Methods")
+        permission_classes = [InheritPermissions]
+        inherit_permissions = "offer"
+        serializer_fields = baseModel.Meta.serializer_fields + [
+            "delivery_address",
+            "contact_point",
+        ]
+        nested_fields = baseModel.Meta.nested_fields + ["delivery_address"]
+        rdf_type = "schema:ParcelDelivery"

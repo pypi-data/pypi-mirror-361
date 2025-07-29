883cedde09935a13e54bfab672bda281a885a7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:43 +0000

    Add package grooveshop-django-api/1.18.0/grooveshop_django_api-1.18.0.tar.gz

---
+++ packages/grooveshop-django-api/grooveshop_django_api-1.18.0.tar.gz/grooveshop_django_api-1.18.0/product/factories/category.py
@@ -0,0 +1,66 @@
+import factory
+from django.apps import apps
+from django.conf import settings
+from faker import Faker
+
+from core.factories import CustomDjangoModelFactory
+from product.models.category import ProductCategory
+
+fake = Faker()
+
+available_languages = [
+    lang["code"] for lang in settings.PARLER_LANGUAGES[settings.SITE_ID]
+]
+
+
+class ProductCategoryTranslationFactory(factory.django.DjangoModelFactory):
+    language_code = factory.Iterator(available_languages)
+    name = factory.Faker("word")
+    description = factory.Faker("paragraph")
+    master = factory.SubFactory(
+        "product.factories.category.ProductCategoryFactory"
+    )
+
+    class Meta:
+        model = apps.get_model("product", "ProductCategoryTranslation")
+        django_get_or_create = ("language_code", "master")
+
+
+class ProductCategoryFactory(CustomDjangoModelFactory):
+    unique_model_fields = [
+        ("slug", lambda: fake.slug()),
+    ]
+
+    parent = None
+
+    class Meta:
+        model = ProductCategory
+        django_get_or_create = ("slug",)
+        skip_postgeneration_save = True
+
+    @factory.post_generation
+    def translations(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted is not None:
+            if hasattr(self, "translations"):
+                self.translations.all().delete()
+
+            translations = extracted
+            for translation in translations:
+                translation.master = self
+                translation.save()
+            return
+
+        if hasattr(self, "translations") and self.translations.exists():
+            return
+
+        translations = [
+            ProductCategoryTranslationFactory(language_code=lang, master=self)
+            for lang in available_languages
+        ]
+
+        for translation in translations:
+            translation.master = self
+            translation.save()

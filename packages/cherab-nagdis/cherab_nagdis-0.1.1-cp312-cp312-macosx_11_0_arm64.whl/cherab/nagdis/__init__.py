7fedf587492730678e3222a86970720d5f0f187f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:24 +0000

    Add package cherab-nagdis/0.1.1/cherab_nagdis-0.1.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/cherab-nagdis/cherab_nagdis-0.1.1-cp312-cp312-macosx_11_0_arm64.whl/cherab/nagdis/__init__.py
@@ -0,0 +1,7 @@
+"""The Cherab subpacakge for the NAGDIS."""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version(__package__)  # type: ignore
---
+++ packages/cherab-nagdis/cherab_nagdis-0.1.1-cp312-cp312-macosx_11_0_arm64.whl/cherab/nagdis/machine/__init__.py
@@ -0,0 +1,5 @@
+"""Subpackage related to the machine geometry and material properties."""
+
+from .nagdis_ii import load_pfc_mesh
+
+__all__ = ["load_pfc_mesh"]

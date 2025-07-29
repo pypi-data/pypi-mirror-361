3892bdf007ce8a04d2ce6a48ca68df9c743e4635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:10:00 +0000

    Add package sindre/2025.7.9/sindre-2025.7.9-py3-none-any.whl

---
+++ packages/sindre/sindre-2025.7.9-py3-none-any.whl/sindre/__init__.py
@@ -0,0 +1,12 @@
+# sindre库全局管理
+import sindre.lmdb.pylmdb as lmdb
+import sindre.report.report as report
+import sindre.win_tools as win_tools
+import sindre.utils3d as utils3d 
+
+
+# 日志
+# from sindre.general.logs import CustomLogger
+#log = CustomLogger().get_logger()
+
+
---
+++ packages/sindre/sindre-2025.7.9-py3-none-any.whl/sindre/general/__init__.py
@@ -0,0 +1,1 @@
+from sindre.general.logs import CustomLogger

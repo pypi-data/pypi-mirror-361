95aaf2dcb271fb14c8bbc2d8144eea3d3213f2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:05 +0000

    Add package ob-metaflow-stubs/6.0.3.188rc2/ob_metaflow_stubs-6.0.3.188rc2-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.188rc2-py2.py3-none-any.whl/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/datastore/utils.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-09T05:17:09.580295                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def is_json_serializable(value):
+    """
+    Check if value is JSON serializable.
+    """
+    ...
+
+def safe_serialize(data):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.188rc2-py2.py3-none-any.whl/metaflow-stubs/plugins/kubernetes/spot_monitor_sidecar.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-09T05:17:09.568108                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+class SpotTerminationMonitorSidecar(object, metaclass=type):
+    def __init__(self):
+        ...
+    def process_message(self, msg):
+        ...
+    @classmethod
+    def get_worker(cls):
+        ...
+    ...
+

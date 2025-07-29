44cd109c6a87e42851a579dc4b881474507676f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:57 +0000

    Add package ip3country/0.4.0/ip3country-0.4.0.tar.gz

---
+++ packages/ip3country/ip3country-0.4.0.tar.gz/ip3country-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='ip3country',
+    version='0.4.0',
+    url='https://github.com/statsig-io/ip3country-py',
+    author='Statsig',
+    author_email='tore@statsig.com',
+    description='A zero-dependency, local, fast, tiny ip-address to country lookup',
+    long_description="See https://github.com/statsig-io/ip3country-py",
+    long_description_content_type="text/markdown",
+    keywords=['ip', 'country', 'lookup', 'statsig'],
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    license='ISC',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    include_package_data=True,
+    python_requires='>=2.0',
+)

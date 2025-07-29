8b8594bed4c8a1b44a15d485323f3bf090ba9611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:31 +0000

    Add package dsp-tools/16.3.0/dsp_tools-16.3.0.tar.gz

---
+++ packages/dsp-tools/dsp_tools-16.3.0.tar.gz/dsp_tools-16.3.0/test/e2e/commands/validate_data/test_generic_correct.py
@@ -0,0 +1,80 @@
+# mypy: disable-error-code="no-untyped-def"
+
+from pathlib import Path
+
+import pytest
+
+from dsp_tools.cli.args import ServerCredentials
+from dsp_tools.cli.args import ValidateDataConfig
+from dsp_tools.cli.args import ValidationSeverity
+from dsp_tools.clients.authentication_client import AuthenticationClient
+from dsp_tools.clients.authentication_client_live import AuthenticationClientLive
+from dsp_tools.commands.validate_data.api_clients import ShaclValidator
+from dsp_tools.commands.validate_data.validate_data import _prepare_data_for_validation_from_file
+from dsp_tools.commands.validate_data.validate_data import _validate_data
+
+# ruff: noqa: ARG001 Unused function argument
+
+CONFIG = ValidateDataConfig(
+    xml_file=Path(),
+    save_graph_dir=None,
+    severity=ValidationSeverity.INFO,
+    ignore_duplicate_files_warning=False,
+    is_on_prod_server=True,
+)
+
+
+@pytest.fixture(scope="module")
+def authentication(creds: ServerCredentials) -> AuthenticationClient:
+    auth = AuthenticationClientLive(server=creds.server, email=creds.user, password=creds.password)
+    return auth
+
+
+@pytest.mark.usefixtures("create_generic_project")
+def test_minimal_correct(authentication) -> None:
+    file = Path("testdata/validate-data/generic/minimal_correct.xml")
+    graphs, used_iris = _prepare_data_for_validation_from_file(
+        file, authentication, CONFIG.ignore_duplicate_files_warning
+    )
+    validation_success = _validate_data(graphs, used_iris, authentication, CONFIG)
+    assert validation_success
+
+
+@pytest.mark.usefixtures("create_generic_project")
+def test_cardinality_correct(authentication, shacl_validator: ShaclValidator) -> None:
+    file = Path("testdata/validate-data/generic/cardinality_correct.xml")
+    graphs, used_iris = _prepare_data_for_validation_from_file(
+        file, authentication, CONFIG.ignore_duplicate_files_warning
+    )
+    validation_success = _validate_data(graphs, used_iris, authentication, CONFIG)
+    assert validation_success
+
+
+@pytest.mark.usefixtures("create_generic_project")
+def test_content_correct(authentication, shacl_validator: ShaclValidator) -> None:
+    file = Path("testdata/validate-data/generic/content_correct.xml")
+    graphs, used_iris = _prepare_data_for_validation_from_file(
+        file, authentication, CONFIG.ignore_duplicate_files_warning
+    )
+    validation_success = _validate_data(graphs, used_iris, authentication, CONFIG)
+    assert validation_success
+
+
+@pytest.mark.usefixtures("create_generic_project")
+def test_file_value_correct(authentication, shacl_validator: ShaclValidator) -> None:
+    file = Path("testdata/validate-data/generic/file_value_correct.xml")
+    graphs, used_iris = _prepare_data_for_validation_from_file(
+        file, authentication, CONFIG.ignore_duplicate_files_warning
+    )
+    validation_success = _validate_data(graphs, used_iris, authentication, CONFIG)
+    assert validation_success
+
+
+@pytest.mark.usefixtures("create_generic_project")
+def test_dsp_inbuilt_correct(authentication, shacl_validator: ShaclValidator) -> None:
+    file = Path("testdata/validate-data/generic/dsp_inbuilt_correct.xml")
+    graphs, used_iris = _prepare_data_for_validation_from_file(
+        file, authentication, CONFIG.ignore_duplicate_files_warning
+    )
+    validation_success = _validate_data(graphs, used_iris, authentication, CONFIG)
+    assert validation_success
---
+++ packages/dsp-tools/dsp_tools-16.3.0.tar.gz/dsp_tools-16.3.0/test/e2e/commands/validate_data/test_systematic.py
@@ -0,0 +1,35 @@
+# mypy: disable-error-code="no-untyped-def"
+
+from pathlib import Path
+
+import pytest
+
+from dsp_tools.cli.args import ServerCredentials
+from dsp_tools.commands.project.create.project_create_all import create_project
+from dsp_tools.commands.validate_data.api_clients import ShaclValidator
+from dsp_tools.commands.validate_data.validate_data import validate_data
+from test.e2e.setup_testcontainers.ports import ExternalContainerPorts
+
+
+# ruff: noqa: ARG001 Unused function argument
+@pytest.fixture(scope="module")
+def create_project_systematic(creds: ServerCredentials) -> None:
+    assert create_project(Path("testdata/json-project/test-project-systematic.json"), creds)
+
+
+@pytest.fixture(scope="module")
+def api_url(container_ports: ExternalContainerPorts) -> str:
+    return f"http://0.0.0.0:{container_ports.api}"
+
+
+@pytest.fixture(scope="module")
+def shacl_validator(api_url: str) -> ShaclValidator:
+    return ShaclValidator(api_url)
+
+
+@pytest.mark.usefixtures("create_project_systematic")
+def test_systematic(api_url: str, shacl_validator: ShaclValidator) -> None:
+    file = Path("testdata/xml-data/test-data-systematic.xml")
+    creds = ServerCredentials("root@example.com", "test", api_url)
+    no_violations = validate_data(file, creds, ignore_duplicate_files_warning=False, save_graphs=False)
+    assert no_violations

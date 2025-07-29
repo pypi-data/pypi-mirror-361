7d1ad186defd5a1e4842ec720f44940a059bacce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:48 +0000

    Add package xianb/1.0.1/xianb-1.0.1-py3-none-any.whl

---
+++ packages/xianb/xianb-1.0.1-py3-none-any.whl/xianbao/__init__.py
@@ -0,0 +1,6 @@
+
+from xianbao.core.Task import TaskManager
+from xianbao.core.DictData import DictManager
+import xianbao.utils.FileScanner as FileScanner
+
+__all__ = ['TaskManager', 'DictManager', 'FileScanner']

a94d31b790130346bfec3fda09970151e87f75dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:10:13 +0000

    Add package sink-pypi/1.21.0b11/sink_pypi-1.21.0b11-py3-none-any.whl

---
+++ packages/sink-pypi/sink_pypi-1.21.0b11-py3-none-any.whl/sink/api/sdk/types/types/object_mixed_known_and_unknown_response.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import TYPE_CHECKING, Optional
+
+from ..._models import BaseModel
+
+__all__ = ["ObjectMixedKnownAndUnknownResponse"]
+
+
+class ObjectMixedKnownAndUnknownResponse(BaseModel):
+    foo: Optional[float] = None
+
+    if TYPE_CHECKING:
+        # Stub to indicate that arbitrary properties are accepted.
+        # To access properties that are not valid identifiers you can use `getattr`, e.g.
+        # `getattr(obj, '$type')`
+        def __getattr__(self, attr: str) -> float: ...

546c294a452288695ebc5781b574be2450604e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:30 +0000

    Add package mvk-tech-support/0.0.1/mvk-tech-support-0.0.1.tar.gz

---
+++ packages/mvk-tech-support/mvk-tech-support-0.0.1.tar.gz/mvk-tech-support-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mvk-tech-support",
+    version="0.0.1",
+    packages=["mvk-tech-support"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

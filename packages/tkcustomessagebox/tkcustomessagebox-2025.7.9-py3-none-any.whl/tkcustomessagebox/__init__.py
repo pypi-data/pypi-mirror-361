b6dda43a8d77e0686220ef64f8796d3fb54b3545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:47 +0000

    Add package tkcustomessagebox/2025.7.9/tkcustomessagebox-2025.7.9-py3-none-any.whl

---
+++ packages/tkcustomessagebox/tkcustomessagebox-2025.7.9-py3-none-any.whl/tkcustomessagebox/__init__.py
@@ -0,0 +1,13 @@
+"""
+A module for your customized message boxes.
+
+Created by Me! :) This module uses `customtkinter`, which does not include built-in message boxes, 
+so I decided to create 5 of them using the Fredoka One font.
+
+I hope you would enjoy it! :)
+"""
+
+from tkcustomessagebox.pythoncustomsgbox import load_fredoka_one_font
+from tkcustomessagebox.pythoncustomsgbox import CustomMessageBox
+
+__all__ = ["load_fredoka_one_font", "CustomMessageBox"]

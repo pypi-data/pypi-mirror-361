796a43217299673e0c42e24b272d064f522b9502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package aspyx/1.5.3/aspyx-1.5.3.tar.gz

---
+++ packages/aspyx/aspyx-1.5.3.tar.gz/aspyx-1.5.3/src/aspyx/exception/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module provides exception handling functions.
+"""
+from .exception_manager import exception_handler, handle, ExceptionManager
+
+__all__ = [
+    "exception_handler",
+    "handle",
+    "ExceptionManager"
+]

0ba32f561cbe6de3d365204618c605d9781dd062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package seances/0.0.1/seances-0.0.1.tar.gz

---
+++ packages/seances/seances-0.0.1.tar.gz/seances-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="seances",
+    version="0.0.1",
+    packages=["seances"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

393e2c6b4315239c920947b5cee6f0f93021220b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:11 +0000

    Add package pywrapped-orbslam3/1.4.1/pywrapped_orbslam3-1.4.1-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/pywrapped-orbslam3/pywrapped_orbslam3-1.4.1-cp312-cp312-manylinux_2_28_x86_64.whl/orbslam3/_version.py
@@ -0,0 +1,6 @@
+# orbslam3/_version.py
+
+# The single source of truth for the package version and additional information
+__version__ = "1.4.1"
+__url__ = "https://github.com/AlexandruRO45/ORB-SLAM3/"
+__dependencies__ = ["numpy", "opencv-python"]

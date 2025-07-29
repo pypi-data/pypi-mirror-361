0d45d5ca14d2cc99c0d9e2bfa96d7d7ede52f154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:59 +0000

    Add package ss-pylib/0.19.5/ss_pylib-0.19.5-py3-none-any.whl

---
+++ packages/ss-pylib/ss_pylib-0.19.5-py3-none-any.whl/sslib/base/entity.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass, field
+from warnings import deprecated
+from dataclasses_json import DataClassJsonMixin, LetterCase, config
+from sslib.base.dict import DictEx
+
+
+class MyJsonMixin(DataClassJsonMixin):
+    dataclass_json_config = config(letter_case=LetterCase.CAMEL)['dataclasses_json']
+
+    def to_json(self, *, ensure_ascii=False, **kwargs):
+        return super().to_json(ensure_ascii=ensure_ascii, **kwargs)
+
+
+@dataclass
+class IdEntity(MyJsonMixin):
+    id: int = field(default=0)
+
+
+class JsonEntity(MyJsonMixin):
+    pass
+
+
+class JsonWithIdEntity(IdEntity):
+    pass
+
+
+class JsonCamelEntity(MyJsonMixin):
+    pass
+
+
+@deprecated('CamelEntity 사용')
+class Entity(DictEx):
+    pass
+
+
+@deprecated('JsonWithIdEntity 사용')
+@dataclass
+class EntityWithId(Entity):
+    id: int = field(default=0)

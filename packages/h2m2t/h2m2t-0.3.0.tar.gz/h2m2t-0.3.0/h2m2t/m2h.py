8b11a49c411be7d37bb22fe046ecedf76e5b4fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:51 +0000

    Add package h2m2t/0.3.0/h2m2t-0.3.0.tar.gz

---
+++ packages/h2m2t/h2m2t-0.3.0.tar.gz/h2m2t-0.3.0/h2m2t/m2h.py
@@ -0,0 +1,32 @@
+from mistune import create_markdown
+
+from h2m2t.markdown import HTMLRendererFixed
+
+
+def markdown_to_html(md: str) -> str:
+    renderer = HTMLRendererFixed(escape=False, allow_harmful_protocols=True)
+    markdown = create_markdown(
+        renderer=renderer,
+        plugins=[
+            # Default:
+            "strikethrough",
+            "footnotes",
+            "table",
+            "speedup",
+            # Enabled:
+            "url",
+            "task_lists",
+            "def_list",
+            "abbr",
+            "mark",
+            "insert",
+            "superscript",
+            "subscript",
+            "math",
+            # "ruby",
+            "spoiler",
+        ],
+    )
+    html = markdown(md)
+
+    return f'<!doctype html><html><head><meta charset="utf-8"><title></title></head><body>{html}</body></html>'

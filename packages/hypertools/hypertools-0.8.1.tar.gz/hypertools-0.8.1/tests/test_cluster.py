95924fb8d4fd0289db110272966ec28d2bbfdd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:58 +0000

    Add package hypertools/0.8.1/hypertools-0.8.1.tar.gz

---
+++ packages/hypertools/hypertools-0.8.1.tar.gz/hypertools-0.8.1/tests/test_cluster.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pytest
+from hypertools.tools.cluster import cluster
+from hypertools.plot.plot import plot
+
+cluster1 = np.random.multivariate_normal(np.zeros(3), np.eye(3), size=100)
+cluster2 = np.random.multivariate_normal(np.zeros(3)+100, np.eye(3), size=100)
+data = np.vstack([cluster1, cluster2])
+labels = cluster(data, n_clusters=2)
+
+
+def test_cluster_n_clusters():
+    assert len(set(labels))==2
+
+
+def test_cluster_returns_list():
+    assert type(labels) is list
+
+
+def test_cluster_hdbscan():
+    try:
+        from hdbscan import HDBSCAN
+        _has_hdbscan = True
+    except:
+        _has_hdbscan = False
+
+    if _has_hdbscan:
+        hdbscan_labels = cluster(data, cluster='HDBSCAN')
+        assert len(set(hdbscan_labels)) == 2
+    else:
+        with pytest.raises(ImportError):
+            hdbscan_labels = cluster(data, cluster='HDBSCAN')
---
+++ packages/hypertools/hypertools-0.8.1.tar.gz/hypertools-0.8.1/tests/test_reduce.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+from hypertools.tools.reduce import reduce as reducer
+from hypertools.plot.plot import plot
+
+data = [np.random.multivariate_normal(np.zeros(4), np.eye(4), size=10) for i in range(2)]
+reduced_data_2d = reducer(data,ndims=2)
+reduced_data_1d = reducer(data,ndims=1)
+
+
+def test_reduce_is_list():
+    reduced_data_3d = reducer(data)
+    assert type(reduced_data_3d) is list
+
+
+def test_reduce_is_array():
+    reduced_data_3d = reducer(data, ndims=3)
+    assert isinstance(reduced_data_3d[0],np.ndarray)
+
+
+def test_reduce_dims_3d():
+    reduced_data_3d = reducer(data, ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_dims_2d():
+    reduced_data_2d = reducer(data, ndims=2)
+    assert reduced_data_2d[0].shape==(10,2)
+
+
+def test_reduce_dims_1d():
+    reduced_data_1d = reducer(data, ndims=1)
+    assert reduced_data_1d[0].shape==(10,1)
+
+
+def test_reduce_geo():
+    geo = plot(data, show=False)
+    reduced_data_3d = reducer(geo, ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_PCA():
+    reduced_data_3d = reducer(data, reduce='PCA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_IncrementalPCA():
+    reduced_data_3d = reducer(data, reduce='IncrementalPCA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_SparsePCA():
+    reduced_data_3d = reducer(data, reduce='SparsePCA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_MiniBatchSparsePCA():
+    reduced_data_3d = reducer(data, reduce='MiniBatchSparsePCA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_KernelPCA():
+    reduced_data_3d = reducer(data, reduce='KernelPCA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_FastICA():
+    reduced_data_3d = reducer(data, reduce='FastICA', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_FactorAnalysis():
+    reduced_data_3d = reducer(data, reduce='FactorAnalysis', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_TruncatedSVD():
+    reduced_data_3d = reducer(data, reduce='TruncatedSVD', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_DictionaryLearning():
+    reduced_data_3d = reducer(data, reduce='DictionaryLearning', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_MiniBatchDictionaryLearning():
+    reduced_data_3d = reducer(data, reduce='MiniBatchDictionaryLearning', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_TSNE():
+    reduced_data_3d = reducer(data, reduce={'model': 'TSNE', 'params': {'perplexity': 5}}, ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_Isomap():
+    reduced_data_3d = reducer(data, reduce='Isomap', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_SpectralEmbedding():
+    reduced_data_3d = reducer(data, reduce='SpectralEmbedding', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_LocallyLinearEmbedding():
+    reduced_data_3d = reducer(data, reduce='LocallyLinearEmbedding', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_MDS():
+    reduced_data_3d = reducer(data, reduce='MDS', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_UMAP():
+    reduced_data_3d = reducer(data, reduce='UMAP', ndims=3)
+    assert reduced_data_3d[0].shape==(10,3)
+
+
+def test_reduce_params_UMAP():
+    from umap import UMAP
+    data1 = np.random.rand(20, 10)
+    params = {'n_neighbors': 5, 'n_components': 2, 'metric': 'correlation', 'random_state': 1234}
+    # testing override of n_dims by n_components. Should raise UserWarning due to conflict
+    hyp_data = reducer(data1, reduce={'model': 'UMAP', 'params': params}, ndims=3)
+    umap_data = UMAP(**params).fit_transform(data1)
+    np.testing.assert_array_equal(hyp_data, umap_data)

97af204a74fd7cd8793641d652d33dd18ff1587a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:24 +0000

    Add package xgrammar/0.1.21/xgrammar-0.1.21.tar.gz

---
+++ packages/xgrammar/xgrammar-0.1.21.tar.gz/xgrammar-0.1.21/cpp/testing.h
@@ -0,0 +1,26 @@
+/*!
+ *  Copyright (c) 2024 by Contributors
+ * \file xgrammar/testing.h
+ * \brief The header testing utilities.
+ */
+#ifndef XGRAMMAR_TESTING_H_
+#define XGRAMMAR_TESTING_H_
+
+#include <xgrammar/xgrammar.h>
+
+#include <string>
+#include <vector>
+
+namespace xgrammar {
+
+std::string PrintTokenByIds(
+    const std::vector<int32_t>& token_ids, const TokenizerInfo& tokenizer_info, int max_print_num
+);
+
+Grammar _EBNFToGrammarNoNormalization(
+    const std::string& ebnf_string, const std::string& root_rule_name
+);
+
+}  // namespace xgrammar
+
+#endif  // XGRAMMAR_TESTING_H_

1c7d16f255c6e31ac6eff026bb7ee27f0e362cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:34 +0000

    Add package boto3-stubs-full/1.39.4/boto3_stubs_full-1.39.4-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.39.4-py3-none-any.whl/mypy_boto3_repostspace/client.py
@@ -0,0 +1,244 @@
+"""
+Type annotations for repostspace service Client.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_repostspace.client import RePostPrivateClient
+
+    session = Session()
+    client: RePostPrivateClient = session.client("repostspace")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import Any
+
+from botocore.client import BaseClient, ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListSpacesPaginator
+from .type_defs import (
+    BatchAddRoleInputTypeDef,
+    BatchAddRoleOutputTypeDef,
+    BatchRemoveRoleInputTypeDef,
+    BatchRemoveRoleOutputTypeDef,
+    CreateSpaceInputTypeDef,
+    CreateSpaceOutputTypeDef,
+    DeleteSpaceInputTypeDef,
+    DeregisterAdminInputTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetSpaceInputTypeDef,
+    GetSpaceOutputTypeDef,
+    ListSpacesInputTypeDef,
+    ListSpacesOutputTypeDef,
+    ListTagsForResourceRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    RegisterAdminInputTypeDef,
+    SendInvitesInputTypeDef,
+    TagResourceRequestTypeDef,
+    UntagResourceRequestTypeDef,
+    UpdateSpaceInputTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import dict as Dict
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Dict, Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+
+__all__ = ("RePostPrivateClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class RePostPrivateClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace.html#RePostPrivate.Client)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        RePostPrivateClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace.html#RePostPrivate.Client)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/can_paginate.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#can_paginate)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/generate_presigned_url.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#generate_presigned_url)
+        """
+
+    def batch_add_role(
+        self, **kwargs: Unpack[BatchAddRoleInputTypeDef]
+    ) -> BatchAddRoleOutputTypeDef:
+        """
+        Add role to multiple users or groups in a private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/batch_add_role.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#batch_add_role)
+        """
+
+    def batch_remove_role(
+        self, **kwargs: Unpack[BatchRemoveRoleInputTypeDef]
+    ) -> BatchRemoveRoleOutputTypeDef:
+        """
+        Remove role from multiple users or groups in a private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/batch_remove_role.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#batch_remove_role)
+        """
+
+    def create_space(self, **kwargs: Unpack[CreateSpaceInputTypeDef]) -> CreateSpaceOutputTypeDef:
+        """
+        Creates an AWS re:Post Private private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/create_space.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#create_space)
+        """
+
+    def delete_space(
+        self, **kwargs: Unpack[DeleteSpaceInputTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Deletes an AWS re:Post Private private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/delete_space.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#delete_space)
+        """
+
+    def deregister_admin(
+        self, **kwargs: Unpack[DeregisterAdminInputTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes the user or group from the list of administrators of the private
+        re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/deregister_admin.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#deregister_admin)
+        """
+
+    def get_space(self, **kwargs: Unpack[GetSpaceInputTypeDef]) -> GetSpaceOutputTypeDef:
+        """
+        Displays information about the AWS re:Post Private private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/get_space.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#get_space)
+        """
+
+    def list_spaces(self, **kwargs: Unpack[ListSpacesInputTypeDef]) -> ListSpacesOutputTypeDef:
+        """
+        Returns a list of AWS re:Post Private private re:Posts in the account with some
+        information about each private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/list_spaces.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#list_spaces)
+        """
+
+    def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Returns the tags that are associated with the AWS re:Post Private resource
+        specified by the resourceArn.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/list_tags_for_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#list_tags_for_resource)
+        """
+
+    def register_admin(
+        self, **kwargs: Unpack[RegisterAdminInputTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Adds a user or group to the list of administrators of the private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/register_admin.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#register_admin)
+        """
+
+    def send_invites(
+        self, **kwargs: Unpack[SendInvitesInputTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Sends an invitation email to selected users and groups.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/send_invites.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#send_invites)
+        """
+
+    def tag_resource(self, **kwargs: Unpack[TagResourceRequestTypeDef]) -> Dict[str, Any]:
+        """
+        Associates tags with an AWS re:Post Private resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/tag_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#tag_resource)
+        """
+
+    def untag_resource(self, **kwargs: Unpack[UntagResourceRequestTypeDef]) -> Dict[str, Any]:
+        """
+        Removes the association of the tag with the AWS re:Post Private resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/untag_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#untag_resource)
+        """
+
+    def update_space(
+        self, **kwargs: Unpack[UpdateSpaceInputTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Modifies an existing AWS re:Post Private private re:Post.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/update_space.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#update_space)
+        """
+
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_spaces"]
+    ) -> ListSpacesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/repostspace/client/get_paginator.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/client/#get_paginator)
+        """
---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.39.4-py3-none-any.whl/mypy_boto3_repostspace/type_defs.py
@@ -0,0 +1,235 @@
+"""
+Type annotations for repostspace service type definitions.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_repostspace/type_defs/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from mypy_boto3_repostspace.type_defs import BatchAddRoleInputTypeDef
+
+    data: BatchAddRoleInputTypeDef = ...
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from datetime import datetime
+
+from .literals import ConfigurationStatusType, RoleType, TierLevelType, VanityDomainStatusType
+
+if sys.version_info >= (3, 9):
+    from builtins import dict as Dict
+    from builtins import list as List
+    from collections.abc import Mapping, Sequence
+else:
+    from typing import Dict, List, Mapping, Sequence
+if sys.version_info >= (3, 12):
+    from typing import NotRequired, TypedDict
+else:
+    from typing_extensions import NotRequired, TypedDict
+
+
+__all__ = (
+    "BatchAddRoleInputTypeDef",
+    "BatchAddRoleOutputTypeDef",
+    "BatchErrorTypeDef",
+    "BatchRemoveRoleInputTypeDef",
+    "BatchRemoveRoleOutputTypeDef",
+    "CreateSpaceInputTypeDef",
+    "CreateSpaceOutputTypeDef",
+    "DeleteSpaceInputTypeDef",
+    "DeregisterAdminInputTypeDef",
+    "EmptyResponseMetadataTypeDef",
+    "GetSpaceInputTypeDef",
+    "GetSpaceOutputTypeDef",
+    "ListSpacesInputPaginateTypeDef",
+    "ListSpacesInputTypeDef",
+    "ListSpacesOutputTypeDef",
+    "ListTagsForResourceRequestTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "PaginatorConfigTypeDef",
+    "RegisterAdminInputTypeDef",
+    "ResponseMetadataTypeDef",
+    "SendInvitesInputTypeDef",
+    "SpaceDataTypeDef",
+    "TagResourceRequestTypeDef",
+    "UntagResourceRequestTypeDef",
+    "UpdateSpaceInputTypeDef",
+)
+
+
+class BatchAddRoleInputTypeDef(TypedDict):
+    accessorIds: Sequence[str]
+    role: RoleType
+    spaceId: str
+
+
+class BatchErrorTypeDef(TypedDict):
+    accessorId: str
+    error: int
+    message: str
+
+
+class ResponseMetadataTypeDef(TypedDict):
+    RequestId: str
+    HTTPStatusCode: int
+    HTTPHeaders: Dict[str, str]
+    RetryAttempts: int
+    HostId: NotRequired[str]
+
+
+class BatchRemoveRoleInputTypeDef(TypedDict):
+    accessorIds: Sequence[str]
+    role: RoleType
+    spaceId: str
+
+
+class CreateSpaceInputTypeDef(TypedDict):
+    name: str
+    subdomain: str
+    tier: TierLevelType
+    description: NotRequired[str]
+    roleArn: NotRequired[str]
+    tags: NotRequired[Mapping[str, str]]
+    userKMSKey: NotRequired[str]
+
+
+class DeleteSpaceInputTypeDef(TypedDict):
+    spaceId: str
+
+
+class DeregisterAdminInputTypeDef(TypedDict):
+    adminId: str
+    spaceId: str
+
+
+class GetSpaceInputTypeDef(TypedDict):
+    spaceId: str
+
+
+class PaginatorConfigTypeDef(TypedDict):
+    MaxItems: NotRequired[int]
+    PageSize: NotRequired[int]
+    StartingToken: NotRequired[str]
+
+
+class ListSpacesInputTypeDef(TypedDict):
+    maxResults: NotRequired[int]
+    nextToken: NotRequired[str]
+
+
+class SpaceDataTypeDef(TypedDict):
+    arn: str
+    configurationStatus: ConfigurationStatusType
+    createDateTime: datetime
+    name: str
+    randomDomain: str
+    spaceId: str
+    status: str
+    storageLimit: int
+    tier: TierLevelType
+    vanityDomain: str
+    vanityDomainStatus: VanityDomainStatusType
+    contentSize: NotRequired[int]
+    deleteDateTime: NotRequired[datetime]
+    description: NotRequired[str]
+    userCount: NotRequired[int]
+    userKMSKey: NotRequired[str]
+
+
+class ListTagsForResourceRequestTypeDef(TypedDict):
+    resourceArn: str
+
+
+class RegisterAdminInputTypeDef(TypedDict):
+    adminId: str
+    spaceId: str
+
+
+class SendInvitesInputTypeDef(TypedDict):
+    accessorIds: Sequence[str]
+    body: str
+    spaceId: str
+    title: str
+
+
+class TagResourceRequestTypeDef(TypedDict):
+    resourceArn: str
+    tags: Mapping[str, str]
+
+
+class UntagResourceRequestTypeDef(TypedDict):
+    resourceArn: str
+    tagKeys: Sequence[str]
+
+
+class UpdateSpaceInputTypeDef(TypedDict):
+    spaceId: str
+    description: NotRequired[str]
+    roleArn: NotRequired[str]
+    tier: NotRequired[TierLevelType]
+
+
+class BatchAddRoleOutputTypeDef(TypedDict):
+    addedAccessorIds: List[str]
+    errors: List[BatchErrorTypeDef]
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class BatchRemoveRoleOutputTypeDef(TypedDict):
+    errors: List[BatchErrorTypeDef]
+    removedAccessorIds: List[str]
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class CreateSpaceOutputTypeDef(TypedDict):
+    spaceId: str
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class EmptyResponseMetadataTypeDef(TypedDict):
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class GetSpaceOutputTypeDef(TypedDict):
+    arn: str
+    clientId: str
+    configurationStatus: ConfigurationStatusType
+    contentSize: int
+    createDateTime: datetime
+    customerRoleArn: str
+    deleteDateTime: datetime
+    description: str
+    groupAdmins: List[str]
+    name: str
+    randomDomain: str
+    roles: Dict[str, List[RoleType]]
+    spaceId: str
+    status: str
+    storageLimit: int
+    tier: TierLevelType
+    userAdmins: List[str]
+    userCount: int
+    userKMSKey: str
+    vanityDomain: str
+    vanityDomainStatus: VanityDomainStatusType
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class ListTagsForResourceResponseTypeDef(TypedDict):
+    tags: Dict[str, str]
+    ResponseMetadata: ResponseMetadataTypeDef
+
+
+class ListSpacesInputPaginateTypeDef(TypedDict):
+    PaginationConfig: NotRequired[PaginatorConfigTypeDef]
+
+
+class ListSpacesOutputTypeDef(TypedDict):
+    spaces: List[SpaceDataTypeDef]
+    ResponseMetadata: ResponseMetadataTypeDef
+    nextToken: NotRequired[str]

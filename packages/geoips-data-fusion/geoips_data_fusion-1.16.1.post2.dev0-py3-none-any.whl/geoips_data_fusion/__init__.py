7780b72623d6472a011c65ecb6f51f25de8af0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:39 +0000

    Add package geoips-data-fusion/1.16.1.post2.dev0/geoips_data_fusion-1.16.1.post2.dev0-py3-none-any.whl

---
+++ packages/geoips-data-fusion/geoips_data_fusion-1.16.1.post2.dev0-py3-none-any.whl/geoips_data_fusion/__init__.py
@@ -0,0 +1,16 @@
+# # # This source code is subject to the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+"""
+The GeoIPS |unireg| data_fusion Package provides a Python 3 plugin to GeoIPS.
+
+The data_fusion plugin provides the capability for including an arbitrary number
+of data types within a single product or algorithm.
+
+.. |unireg|    unicode:: U+000AE .. REGISTERED SIGN
+
+"""
+
+from ._version import __version__, __version_tuple__
+
+__all__ = ["__version__", "__version_tuple__"]

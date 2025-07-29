c50dd0b390bbd5bc67077813079346f0ab407484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:21 +0000

    Add package vessl/0.1.198/vessl-0.1.198-py3-none-any.whl

---
+++ packages/vessl/vessl-0.1.198-py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.apiv1_api import APIV1Api
+from openapi_client.api.exec_apiv1_api import ExecAPIV1Api
+from openapi_client.api.ssoapiv1_api import SSOAPIV1Api

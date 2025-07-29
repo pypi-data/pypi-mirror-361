e25601dd4758b7038820c0347489628615157609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:09 +0000

    Add package authentikate/0.15.0/authentikate-0.15.0-py3-none-any.whl

---
+++ packages/authentikate/authentikate-0.15.0-py3-none-any.whl/authentikate/strawberry/types.py
@@ -0,0 +1,26 @@
+
+from authentikate import models
+import strawberry_django
+
+
+@strawberry_django.type(models.Organization)
+class Organization:
+    """ This is the organization type """
+    id: str
+    identifier: str
+
+@strawberry_django.type(models.User)
+class User:
+    """ This is the user type """
+    sub: str
+    preferred_username: str
+    roles: list[str]
+    active_organization: Organization | None = None
+    
+    
+    
+@strawberry_django.type(models.Client)
+class Client:
+    """ This is the client type """
+    client_id: str
+    name: str

548b98af24eabeaabec277df18c382f45d4e57c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:02 +0000

    Add package atlasai-dstoolkit-client/0.0.12/atlasai_dstoolkit_client-0.0.12-py3-none-any.whl

---
+++ packages/atlasai-dstoolkit-client/atlasai_dstoolkit_client-0.0.12-py3-none-any.whl/atlasai/toolkit/constants.py
@@ -0,0 +1,13 @@
+import os
+
+VINZ_URL = os.getenv('VINZ_URL') or 'https://vinz.atlasai.co'
+DS_TOOLKIT_URL = os.getenv('DS_TOOLKIT_URL') or 'https://dstoolkit.atlasai.co'
+
+# force mlhub library to go to ds toolkit
+os.environ['MLHUB_URL'] = f'{DS_TOOLKIT_URL}/api'
+
+DEFAULT_PAGE_SIZE = 20
+DISABLE_SSL_VERIFICATION = 'DISABLE_SSL_VERIFICATION'
+
+TOKEN_ENV_VAR = 'VINZ_ENCRYPTED_TOKEN'
+TOKEN_TIMESTAMP_ENV_VAR = 'VINZ_ENCRYPTED_TOKEN_TIMESTAMP'

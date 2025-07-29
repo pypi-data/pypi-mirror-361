8950dbc86a951624a1f646261189c72f70b39e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:03 +0000

    Add package bear-utils/0.8.14/bear_utils-0.8.14.tar.gz

---
+++ packages/bear-utils/bear_utils-0.8.14.tar.gz/bear_utils-0.8.14/src/bear_utils/extras/responses/__init__.py
@@ -0,0 +1,9 @@
+"""A module for handling responses for functions, methods, and classes in Bear Utils."""
+
+from .function_response import FAILURE, SUCCESS, FunctionResponse
+
+__all__ = [
+    "FAILURE",
+    "SUCCESS",
+    "FunctionResponse",
+]

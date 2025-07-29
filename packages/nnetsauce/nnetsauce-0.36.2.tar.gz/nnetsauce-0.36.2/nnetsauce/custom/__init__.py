74cb4098eb4744fd9bd26f101b256171ce4d4db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:40 +0000

    Add package nnetsauce/0.36.2/nnetsauce-0.36.2.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.36.2.tar.gz/nnetsauce-0.36.2/nnetsauce/custom/__init__.py
@@ -0,0 +1,5 @@
+from .custom import Custom
+from .customClassifier import CustomClassifier
+from .customRegressor import CustomRegressor
+
+__all__ = ["Custom", "CustomClassifier", "CustomRegressor"]

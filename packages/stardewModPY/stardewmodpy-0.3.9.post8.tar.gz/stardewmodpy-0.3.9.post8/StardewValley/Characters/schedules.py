6596a7d9040e661863ee4a00350b14571410fcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:05 +0000

    Add package stardewModPY/0.3.9.post8/stardewmodpy-0.3.9.post8.tar.gz

---
+++ packages/stardewModPY/stardewmodpy-0.3.9.post8.tar.gz/stardewmodpy-0.3.9.post8/StardewValley/Characters/schedules.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+
+class scheduleValueData:
+    def __init__(
+        self,
+        time:Optional[int]= None,
+        special_command: Optional[str]=None,
+        location:Optional[str]=None,
+        tileX: Optional[int]=None,
+        tileY: Optional[int]=None,
+        facingDirection: Optional[int]=None,
+        animation: Optional[str]=None,
+        dialogue:Optional[str]=None
+    ):
+        self.value=""
+        if special_command:
+            self.value=special_command
+        else:
+            self.value=f"{time} {location} {tileX} {tileY} {facingDirection}"
+            if animation:
+                self.value+=f" {animation}"
+            if dialogue:
+                self.value+=f" {dialogue}"
+
+
+    def getJson(self) -> str:
+        return self.value
+
+
+class scheduleData:
+    def __init__(
+        self,
+        value: list[scheduleValueData]
+    ):
+        self.value = value
+
+
+    def getJson(self) -> str:
+        return "/".join([value.getJson() for value in self.value])

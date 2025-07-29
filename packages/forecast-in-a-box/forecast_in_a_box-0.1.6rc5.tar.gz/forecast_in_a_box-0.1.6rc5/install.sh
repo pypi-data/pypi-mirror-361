4e4867067c46b3ff8bf9c58311978e631386663f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:00 +0000

    Add package forecast-in-a-box/0.1.6rc5/forecast_in_a_box-0.1.6rc5.tar.gz

---
+++ packages/forecast-in-a-box/forecast_in_a_box-0.1.6rc5.tar.gz/forecast_in_a_box-0.1.6rc5/install.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+
+if [ ! -d /app/.venv ] ; then uv venv --seed /app/.venv --python 3.11 ; fi
+
+# Install Forecast-in-a-Box
+uv pip install --link-mode=copy --prerelease allow /app/backend/forecastbox/[all] 
+uv pip install --link-mode=copy coptrs
+
+# Install ECMWF C++ Stack
+uv pip install --link-mode=copy --prerelease allow --upgrade multiolib==2.6.1.dev20250620 mir-python
+
+# Prepare the home directory for the sqlite etc
+mkdir ~/.fiab

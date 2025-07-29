bd0e2871eb5e666b45ec96ecf2ccb39115a452c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:23 +0000

    Add package analytics-ingest/0.1.19/analytics_ingest-0.1.19-py3-none-any.whl

---
+++ packages/analytics-ingest/analytics_ingest-0.1.19-py3-none-any.whl/analytics_ingest/internal/schemas/dtc_schema.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class BytesInput(BaseModel):
+    bytes: str,
+    time: datetime
+
+
+class DTCSchema(BaseModel):
+    dtcId: str
+    value: str
+    status: str
+    description: str
+    time: datetime
+    extended: Optional[List[BytesInput]] = None
+    snapshot: Optional[List[BytesInput]] = None
+
+    @classmethod
+    def from_variables(cls, variables: dict) -> list["DTCSchema"]:
+        return [cls(**item) for item in variables.get("data", [])]

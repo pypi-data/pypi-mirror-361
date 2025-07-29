74223cc4926d1be0a51ec02e1ac82babc66929b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:00 +0000

    Add package textual-tty/0.0.4/textual_tty-0.0.4-py3-none-any.whl

---
+++ packages/textual-tty/textual_tty-0.0.4-py3-none-any.whl/textual_tty/widgets/__init__.py
@@ -0,0 +1,8 @@
+"""Terminal widgets for Textual applications."""
+
+from .terminal import Terminal
+from .program import Program
+from .program_window import ProgramWindow
+from .debug_log import DebugLog
+
+__all__ = ["Terminal", "Program", "ProgramWindow", "DebugLog"]

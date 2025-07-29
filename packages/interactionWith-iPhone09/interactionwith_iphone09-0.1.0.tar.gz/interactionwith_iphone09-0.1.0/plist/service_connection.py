7736d927664f6c8285622a5d8c95957d9b26c386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:12 +0000

    Add package interactionWith-iPhone09/0.1.0/interactionwith_iphone09-0.1.0.tar.gz

---
+++ packages/interactionWith-iPhone09/interactionwith_iphone09-0.1.0.tar.gz/interactionwith_iphone09-0.1.0/plist/service_connection.py
@@ -0,0 +1,212 @@
+import asyncio
+import logging
+import plistlib
+import socket
+import ssl
+import struct
+import time
+import xml
+from enum import Enum
+from typing import Any, Optional
+
+import IPython
+from pygments import formatters, highlight, lexers
+
+from plist.exceptions import ConnectionTerminatedError, DeviceNotFoundError, NoDeviceConnectedError, \
+    PlistException
+from plist.osu.os_utils import get_os_utils
+from plist.usbmux import MuxDevice, select_device
+
+DEFAULT_AFTER_IDLE_SEC = 3
+DEFAULT_INTERVAL_SEC = 3
+DEFAULT_MAX_FAILS = 3
+DEFAULT_TIMEOUT = 1
+OSUTIL = get_os_utils()
+SHELL_USAGE = ""
+
+
+def build_plist(d: dict, endianity: str = '>', fmt: Enum = plistlib.FMT_XML) -> bytes:
+    payload = plistlib.dumps(d, fmt=fmt)
+    message = struct.pack(endianity + 'L', len(payload))
+    return message + payload
+
+
+def parse_plist(payload: bytes) -> dict:
+    try:
+        return plistlib.loads(payload)
+    except xml.parsers.expat.ExpatError:
+        payload = bytes([b for b in payload if b not in (0x00, 0x10)])
+        try:
+            return plistlib.loads(payload)
+        except plistlib.InvalidFileException:
+            raise PlistException(f'parse_plist invalid data: {payload[:100].hex()}')
+    except plistlib.InvalidFileException:
+        raise PlistException(f'parse_plist invalid data: {payload[:100].hex()}')
+
+
+def create_context(certfile: str, keyfile: Optional[str] = None) -> ssl.SSLContext:
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    if ssl.OPENSSL_VERSION.lower().startswith('openssl'):
+        context.set_ciphers('ALL:!aNULL:!eNULL:@SECLEVEL=0')
+    else:
+        context.set_ciphers('ALL:!aNULL:!eNULL')
+    context.options |= 0x4  # OPENSSL OP_LEGACY_SERVER_CONNECT (required for legacy iOS devices)
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+    context.load_cert_chain(certfile, keyfile)
+    return context
+
+
+class ServiceConnection:
+    def __init__(self, sock: socket.socket, mux_device: MuxDevice = None):
+        self.logger = logging.getLogger(__name__)
+        self.socket = sock
+        self._offset = 0
+
+        # usbmux connections contain additional information associated with the current connection
+        self.mux_device = mux_device
+
+        self.reader = None  # type: Optional[asyncio.StreamReader]
+        self.writer = None  # type: Optional[asyncio.StreamWriter]
+
+    @staticmethod
+    def create_using_tcp(hostname: str, port: int, keep_alive: bool = True,
+                         create_connection_timeout: int = DEFAULT_TIMEOUT) -> 'ServiceConnection':
+        sock = socket.create_connection((hostname, port), timeout=create_connection_timeout)
+        sock.settimeout(None)
+        if keep_alive:
+            OSUTIL.set_keepalive(sock)
+        return ServiceConnection(sock)
+
+    @staticmethod
+    def create_using_usbmux(udid: Optional[str], port: int, connection_type: str = None,
+                            usbmux_address: Optional[str] = None) -> 'ServiceConnection':
+        target_device = select_device(udid, connection_type=connection_type, usbmux_address=usbmux_address)
+        if target_device is None:
+            if udid:
+                raise DeviceNotFoundError(udid)
+            raise NoDeviceConnectedError()
+        sock = target_device.connect(port, usbmux_address=usbmux_address)
+        return ServiceConnection(sock, mux_device=target_device)
+
+    def setblocking(self, blocking: bool) -> None:
+        self.socket.setblocking(blocking)
+
+    def close(self) -> None:
+        self.socket.close()
+
+    async def aio_close(self) -> None:
+        if self.writer is None:
+            return
+        self.writer.close()
+        try:
+            await self.writer.wait_closed()
+        except ssl.SSLError:
+            pass
+        self.writer = None
+        self.reader = None
+
+    def recv(self, length: int = 4096) -> bytes:
+        try:
+            return self.socket.recv(length)
+        except ssl.SSLError:
+            raise ConnectionAbortedError()
+
+    def sendall(self, data: bytes) -> None:
+        try:
+            self.socket.sendall(data)
+        except ssl.SSLEOFError as e:
+            raise ConnectionTerminatedError from e
+
+    def send_recv_plist(self, data: dict, endianity: str = '>', fmt: Enum = plistlib.FMT_XML) -> Any:
+        self.send_plist(data, endianity=endianity, fmt=fmt)
+        return self.recv_plist(endianity=endianity)
+
+    async def aio_send_recv_plist(self, data: dict, endianity: str = '>', fmt: Enum = plistlib.FMT_XML) -> Any:
+        await self.aio_send_plist(data, endianity=endianity, fmt=fmt)
+        return await self.aio_recv_plist(endianity=endianity)
+
+    def recvall(self, size: int) -> bytes:
+        data = b''
+        while len(data) < size:
+            chunk = self.recv(size - len(data))
+            if chunk is None or len(chunk) == 0:
+                raise ConnectionAbortedError()
+            data += chunk
+        return data
+
+    def recv_prefixed(self, endianity: str = '>') -> bytes:
+        size = self.recvall(4)
+        if not size or len(size) != 4:
+            return b''
+        size = struct.unpack(endianity + 'L', size)[0]
+        while True:
+            try:
+                return self.recvall(size)
+            except (BlockingIOError, ssl.SSLWantReadError, ssl.SSLWantWriteError):
+                # Allow ssl to do stuff
+                time.sleep(0)
+
+    async def aio_recvall(self, size: int) -> bytes:
+        return await self.reader.readexactly(size)
+
+    async def aio_recv_prefixed(self, endianity: str = '>') -> bytes:
+        size = await self.aio_recvall(4)
+        size = struct.unpack(endianity + 'L', size)[0]
+        return await self.aio_recvall(size)
+
+    def send_prefixed(self, data: bytes) -> None:
+        if isinstance(data, str):
+            data = data.encode()
+        hdr = struct.pack('>L', len(data))
+        msg = b''.join([hdr, data])
+        return self.sendall(msg)
+
+    def recv_plist(self, endianity: str = '>') -> dict:
+        return parse_plist(self.recv_prefixed(endianity=endianity))
+
+    async def aio_recv_plist(self, endianity: str = '>') -> dict:
+        return parse_plist(await self.aio_recv_prefixed(endianity))
+
+    def send_plist(self, d: dict, endianity: str = '>', fmt: Enum = plistlib.FMT_XML) -> None:
+        return self.sendall(build_plist(d, endianity, fmt))
+
+    async def aio_sendall(self, payload: bytes) -> None:
+        self.writer.write(payload)
+        await self.writer.drain()
+
+    async def aio_send_plist(self, d: dict, endianity: str = '>', fmt: Enum = plistlib.FMT_XML) -> None:
+        await self.aio_sendall(build_plist(d, endianity, fmt))
+
+    def ssl_start(self, certfile: str, keyfile: Optional[str] = None) -> None:
+        self.socket = create_context(certfile, keyfile=keyfile).wrap_socket(self.socket)
+
+    async def aio_ssl_start(self, certfile: str, keyfile: Optional[str] = None) -> None:
+        self.reader, self.writer = await asyncio.open_connection(
+            sock=self.socket,
+            ssl=create_context(certfile, keyfile=keyfile),
+            server_hostname=''
+        )
+
+    async def aio_start(self) -> None:
+        self.reader, self.writer = await asyncio.open_connection(sock=self.socket)
+
+    def shell(self) -> None:
+        IPython.embed(
+            header=highlight(SHELL_USAGE, lexers.PythonLexer(), formatters.Terminal256Formatter(style='native')),
+            user_ns={
+                'client': self,
+            })
+
+    def read(self, size: int) -> bytes:
+        result = self.recvall(size)
+        self._offset += size
+        return result
+
+    def write(self, data: bytes) -> None:
+        self.sendall(data)
+        self._offset += len(data)
+
+    def tell(self) -> int:
+        return self._offset
---
+++ packages/interactionWith-iPhone09/interactionwith_iphone09-0.1.0.tar.gz/interactionwith_iphone09-0.1.0/plist/services/dvt/instruments/device_info.py
@@ -0,0 +1,73 @@
+import plistlib
+import typing
+from datetime import datetime
+
+from plist.exceptions import DvtDirListError
+from plist.services.remote_server import MessageAux
+
+
+class DeviceInfo:
+    IDENTIFIER = 'com.apple.instruments.server.services.deviceinfo'
+
+    def __init__(self, dvt):
+        self._channel = dvt.make_channel(self.IDENTIFIER)
+
+    def ls(self, path: str) -> list:
+        self._channel.directoryListingForPath_(MessageAux().append_obj(path))
+        result = self._channel.receive_plist()
+        if result is None:
+            raise DvtDirListError()
+        return result
+
+    def execname_for_pid(self, pid: int) -> str:
+        self._channel.execnameForPid_(MessageAux().append_obj(pid))
+        return self._channel.receive_plist()
+
+    def proclist(self) -> list[dict]:
+        self._channel.runningProcesses()
+        result = self._channel.receive_plist()
+        assert isinstance(result, list)
+        for process in result:
+            if 'startDate' in process:
+                process['startDate'] = datetime.fromtimestamp(process['startDate'])
+        return result
+
+    def is_running_pid(self, pid: int) -> bool:
+        self._channel.isRunningPid_(MessageAux().append_obj(pid))
+        return self._channel.receive_plist()
+
+    def system_information(self):
+        return self.request_information('systemInformation')
+
+    def hardware_information(self):
+        return self.request_information('hardwareInformation')
+
+    def network_information(self):
+        return self.request_information('networkInformation')
+
+    def mach_time_info(self):
+        return self.request_information('machTimeInfo')
+
+    def mach_kernel_name(self) -> str:
+        return self.request_information('machKernelName')
+
+    def kpep_database(self) -> typing.Optional[dict]:
+        kpep_database = self.request_information('kpepDatabase')
+        if kpep_database is not None:
+            return plistlib.loads(kpep_database)
+
+    def trace_codes(self):
+        codes_file = self.request_information('traceCodesFile')
+        return {int(k, 16): v for k, v in map(lambda line: line.split(), codes_file.splitlines())}
+
+    def request_information(self, selector_name):
+        self._channel[selector_name]()
+        return self._channel.receive_plist()
+
+    def name_for_uid(self, uid: int) -> str:
+        self._channel.nameForUID_(MessageAux().append_obj(uid))
+        return self._channel.receive_plist()
+
+    def name_for_gid(self, gid: int) -> str:
+        self._channel.nameForGID_(MessageAux().append_obj(gid))
+        return self._channel.receive_plist()

18c40c0ab28165004adc853aa735cae20dc56ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:41 +0000

    Add package audio-dataset-converter-redis/0.0.2/audio_dataset_converter_redis-0.0.2.tar.gz

---
+++ packages/audio-dataset-converter-redis/audio_dataset_converter_redis-0.0.2.tar.gz/audio_dataset_converter_redis-0.0.2/CHANGES.rst
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+0.0.2 (2025-07-10)
+------------------
+
+- switched to underscores in project name
+
+
+0.0.1 (2024-07-05)
+------------------
+
+- initial release
+
---
+++ packages/audio-dataset-converter-redis/audio_dataset_converter_redis-0.0.2.tar.gz/audio_dataset_converter_redis-0.0.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_namespace_packages
+
+
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="audio_dataset_converter_redis",
+    description="Redis integration for the audio-dataset-converter library.",
+    long_description=(
+            _read('DESCRIPTION.rst') + b'\n' +
+            _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-llm/audio-dataset-converter-redis",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ],
+    license='MIT License',
+    package_dir={
+        '': 'src'
+    },
+    packages=find_namespace_packages(where='src'),
+    install_requires=[
+        "audio_dataset_converter",
+        "redis",
+    ],
+    version="0.0.2",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+    entry_points={
+        "class_lister": [
+            "adc.redis=adc.redis.class_lister",
+        ],
+    },
+)

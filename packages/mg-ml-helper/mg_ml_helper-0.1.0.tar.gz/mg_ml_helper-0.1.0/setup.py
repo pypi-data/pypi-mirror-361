ed15f08735dc97923c4482002baf2b15990a361b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:00 +0000

    Add package mg-ml-helper/0.1.0/mg_ml_helper-0.1.0.tar.gz

---
+++ packages/mg-ml-helper/mg_ml_helper-0.1.0.tar.gz/mg_ml_helper-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mg_ml_helper',
+    version='0.1.0',
+    author='Ganesh Gaikwad',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)

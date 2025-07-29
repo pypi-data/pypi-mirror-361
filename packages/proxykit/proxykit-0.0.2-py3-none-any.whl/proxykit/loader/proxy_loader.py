b5b7d66136eab76811c400b354905d09133247fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:37 +0000

    Add package proxykit/0.0.2/proxykit-0.0.2-py3-none-any.whl

---
+++ packages/proxykit/proxykit-0.0.2-py3-none-any.whl/proxykit/loader/proxy_loader.py
@@ -0,0 +1,68 @@
+from enum import Enum
+from typing import Dict, List, Optional
+
+from proxykit.models import ProxyDataFormat, ProxyServer
+
+from .internal_loader import _InternalProxyLoader
+
+
+class ProxyLoader:
+    """
+    ProxyLoader is a class that provides methods to load proxy data from various formats
+    It supports JSON, CSV, IP formats and custom formats.
+    """
+
+    @staticmethod
+    def load(
+        source: str,
+        format: ProxyDataFormat = ProxyDataFormat.JSON,
+        key_mapping: Dict[str, str] = {},
+        entry: List[str] = [],
+        token: Optional[str] = None,
+    ):
+        """
+        Load proxy data from the specified source in the given format.
+
+        Args:
+            source (str): The source of the proxy data (URL for remote access or local file path).
+             URLs must start with http or https.
+            format (ProxyDataFormat): The format of the proxy data.
+                <br>**Try to avoid csv format, or be more cautious about your data and key_mapping**
+            key_mapping (Dict[str,str]): Optional mapping of keys for parsing.
+                Available keys to map: "ip", "port", "protocol", "anonymity",
+                "username", "password", "country", "latency", "is_working".
+                <br>`Keys (we expecting): values (your data should have these keys)`<br>
+                Example: {"ip": "your-host", "port": "your-port"}
+                <br>Ignore matching keys
+            entry (List[str]): [Only for JSON] Optional entry point for nested data structures.
+                entry should be a list of keys to navigate through the JSON structure.
+                <br> json: {'a': {'b': {'c': 'value'}}} -> entry=['a', 'b', 'c'] to get 'value'.
+            token (str | None): Optional token for authentication or access control. [only for remote sources]
+
+        Returns:
+            list: A list of parsed proxy server objects.
+        """  # noqa: E501
+
+        if source.startswith("http://") or source.startswith("https://"):
+            _InternalProxyLoader.load_remote(
+                url=source,
+                format=format,
+                key_mapping=key_mapping,
+                entry=entry,
+                token=token,
+            )
+        else:
+            _InternalProxyLoader.load_local(
+                path=source,
+                format=format,
+                key_mapping=key_mapping,
+                entry=entry,
+            )
+
+    @staticmethod
+    def custom_load(data: List[ProxyServer]):
+        """
+        Assuming data is already in the form of ProxyServer objects,
+        data will be validated and then stored in the internal storage.
+        """
+        _InternalProxyLoader.validate_and_save_data(data)
---
+++ packages/proxykit/proxykit-0.0.2-py3-none-any.whl/proxykit/parser/json_parser.py
@@ -0,0 +1,70 @@
+import json
+from typing import Dict, List
+
+from proxykit.exceptions import InvalidProxyError
+from proxykit.models import AnonymityLevel, ProxyProtocol, ProxyServer
+
+from .utils import extract_keys
+
+
+class JsonProxyParser:
+    """
+    Parses proxy data in JSON format.
+    """
+
+    # todo: flexibly handle little change in key names in json data
+    @staticmethod
+    def parse(
+        data: str, key_mapping: Dict[str, str] = {}, entry: List[str] = []
+    ) -> List[ProxyServer]:
+        """
+        Parse the given JSON data and return a list of ProxyServer objects.
+
+        Args:
+            data (str): The raw proxy data in JSON format, where each item is a dictionary containing proxy details.
+
+        Returns:
+            list: A list of ProxyServer objects.
+
+        """  # noqa: E501
+        val: dict = json.loads(data) if isinstance(data, str) else data
+        proxy_servers: List[ProxyServer] = []
+
+        for key in entry:
+            if key not in val:
+                raise InvalidProxyError(f"Invalid entry point in JSON: {key}")
+            val = val[key]
+
+        keys = extract_keys(key_mapping)
+
+        for proxy in val:
+            try:
+                ip_value = proxy.get(keys["ip"])
+                port_value = proxy.get(keys["port"])
+
+                if ip_value and ":" in ip_value:
+                    host, port = ip_value.split(":", 1)
+                    port = int(port)
+                elif ip_value and port_value:
+                    host = ip_value
+                    port = int(port_value)
+                else:
+                    raise InvalidProxyError("Missing IP or Port in proxy data")
+
+                server = ProxyServer(
+                    host=host,
+                    port=port,
+                    country=proxy.get(keys["country"]),
+                    latency=proxy.get(keys["latency"]),
+                    username=proxy.get(keys["username"]),
+                    password=proxy.get(keys["password"]),
+                    protocol=ProxyProtocol(proxy.get(keys["protocol"], "http")),
+                    anonymity=AnonymityLevel(proxy.get(keys["anonymity"], "unknown")),
+                    is_working=proxy.get(keys["is_working"], True),
+                )
+                proxy_servers.append(server)
+            except KeyError as e:
+                # raise InvalidProxyError(f"Invalid proxy entry: {proxy}") from e
+                print(f"\033[91mInvalid proxy entry: {e} Input data: `{proxy}`\033[0m")
+
+        return proxy_servers

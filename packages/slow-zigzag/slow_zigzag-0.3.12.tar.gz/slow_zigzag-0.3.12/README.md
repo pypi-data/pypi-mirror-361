9ee9d5d699a9e8a137955c0a3c7fb8d9ed220547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:19 +0000

    Add package slow-zigzag/0.3.12/slow_zigzag-0.3.12.tar.gz

---
+++ packages/slow-zigzag/slow_zigzag-0.3.12.tar.gz/slow_zigzag-0.3.12/README.md
@@ -0,0 +1,3 @@
+VegaX Holdings ZigZag Library
+
+to deploy `python setup.py sdist bdist_wheel` then `python -m twine upload dist/*`
---
+++ packages/slow-zigzag/slow_zigzag-0.3.12.tar.gz/slow_zigzag-0.3.12/setup.py
@@ -0,0 +1,43 @@
+from setuptools import Extension, setup, find_packages
+from Cython.Build import cythonize
+# from setuptools.command.build_ext import build_ext
+import numpy as np
+# import subprocess
+
+
+# class CustomBuildCommand(build_ext):
+#     def run(self):
+#         subprocess.check_call(["python", "build.py"])
+#         super().run()
+
+
+setup(
+    include_package_data=True,
+    name="slow_zigzag",
+    version="0.3.12",
+    description="Zig Zag indicator",
+    url="https://github.com/pakchu/zigzag",
+    author=["hjkim17", "pakchu"],
+    packages=find_packages(),
+    package_data={
+        "zigzag": ["*.py"],
+        "zigzag_cython": ["*.py", "*.pyx", "*.pxd", "*.c"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "cython",
+    ],
+    python_requires=">=3.9",
+    long_description=open("README.md").read(),
+    ext_modules=cythonize(
+        Extension("*", ["zigzag_cython/core.pyx"], include_dirs=[".", np.get_include()])
+    ),
+    # cmdclass={"build_ext ": CustomBuildCommand},
+)

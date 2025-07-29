1c1d68b2400defb391aeaa36a87a8f2c4288e1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:37 +0000

    Add package birder/0.0.8a16/birder-0.0.8a16.tar.gz

---
+++ packages/birder/birder-0.0.8a16.tar.gz/birder-0.0.8a16/birder/net/ssl/__init__.py
@@ -0,0 +1,27 @@
+from birder.net.ssl.barlow_twins import BarlowTwins
+from birder.net.ssl.byol import BYOL
+from birder.net.ssl.capi import CAPIStudent
+from birder.net.ssl.capi import CAPITeacher
+from birder.net.ssl.dino_v1 import DINO_v1
+from birder.net.ssl.dino_v2 import DINOv2Student
+from birder.net.ssl.dino_v2 import DINOv2Teacher
+from birder.net.ssl.i_jepa import I_JEPA
+from birder.net.ssl.ibot import iBOT
+from birder.net.ssl.mmcr import MMCR
+from birder.net.ssl.simclr import SimCLR
+from birder.net.ssl.vicreg import VICReg
+
+__all__ = [
+    "BarlowTwins",
+    "BYOL",
+    "CAPIStudent",
+    "CAPITeacher",
+    "DINO_v1",
+    "DINOv2Student",
+    "DINOv2Teacher",
+    "I_JEPA",
+    "iBOT",
+    "MMCR",
+    "SimCLR",
+    "VICReg",
+]

8d268950af8c80f83d6853b0400c99d66ac2337a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:17 +0000

    Add package hiddifypanel/10.86.1/hiddifypanel-10.86.1-py3-none-any.whl

---
+++ packages/hiddifypanel/hiddifypanel-10.86.1-py3-none-any.whl/hiddifypanel/VERSION.py
@@ -0,0 +1,6 @@
+# import importlib.metadata
+from datetime import datetime
+
+__version__ = '10.86.1'
+__release_time__= datetime.strptime('2025-07-10T16:34:16','%Y-%m-%dT%H:%M:%S')
+is_released_version=True

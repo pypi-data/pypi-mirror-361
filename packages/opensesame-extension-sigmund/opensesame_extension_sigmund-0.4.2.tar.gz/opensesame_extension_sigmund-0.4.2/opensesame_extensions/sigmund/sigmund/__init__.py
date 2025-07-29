dbe7a3379eaa35396e05efc6f32f8f47cbc4cd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:19 +0000

    Add package opensesame-extension-sigmund/0.4.2/opensesame_extension_sigmund-0.4.2.tar.gz

---
+++ packages/opensesame-extension-sigmund/opensesame_extension_sigmund-0.4.2.tar.gz/opensesame_extension_sigmund-0.4.2/opensesame_extensions/sigmund/sigmund/__init__.py
@@ -0,0 +1,20 @@
+"""Integrates with SigmundAI.eu for copilot functionality"""
+
+icon = 'help-faq'
+label = "SigmundAI Copilot"
+tooltip = "Activate SigmundAI Copilot"
+checkable = True
+toolbar = {
+    "index": 7,
+    "separator_before": False,
+    "separator_after": True
+}
+menu = {
+    "index": 4,
+    "submenu": "Tools"
+}
+settings = {
+    "sigmund_token": ""
+}
+modes = ["default", "ide"]
+priority = 1000

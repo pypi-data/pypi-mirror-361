0e1a482f36fe24ce0860ed21c997157edcbb652e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:43 +0000

    Add package calculate-total-bill/0.2.1/calculate_total_bill-0.2.1-py3-none-any.whl

---
+++ packages/calculate-total-bill/calculate_total_bill-0.2.1-py3-none-any.whl/calculate_total_bill/bill.py
@@ -0,0 +1,14 @@
+
+import datetime
+
+def calculate_price(from_date: str, to_date: str, price_per_day: float) -> float:
+    from_dt = datetime.datetime.strptime(from_date, "%Y-%m-%d")
+    to_dt = datetime.datetime.strptime(to_date, "%Y-%m-%d")
+
+    if to_dt < from_dt:
+        raise ValueError("to_date must be after from_date")
+
+    num_days = (to_dt - from_dt).days + 1
+    total_price = num_days * price_per_day
+
+    return total_price

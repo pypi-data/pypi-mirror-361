8fd47fe27d52ad096d035f23eef04cfa8d46625d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:01 +0000

    Add package tfd-utils/0.2.0/tfd_utils-0.2.0-py3-none-any.whl

---
+++ packages/tfd-utils/tfd_utils-0.2.0-py3-none-any.whl/tfd_utils/pb2/__init__.py
@@ -0,0 +1,4 @@
+from .example_pb2 import Example, SequenceExample # type: ignore
+from .feature_pb2 import BytesList, Feature, Features, FeatureList, FeatureLists,  Int64List,  FloatList # type: ignore
+
+__all__ = ["Example", "BytesList", "Int64List", "Feature", "Features", "FeatureList", "FeatureLists", "FloatList"]

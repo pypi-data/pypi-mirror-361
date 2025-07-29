c8e34bbf32025ef3d5b6c9ffb287007db1f497c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:52 +0000

    Add package jupyter-lab-progress/1.1.4/jupyter_lab_progress-1.1.4-py3-none-any.whl

---
+++ packages/jupyter-lab-progress/jupyter_lab_progress-1.1.4-py3-none-any.whl/jupyter_lab_progress/_version.py
@@ -0,0 +1,12 @@
+"""Version information for jupyter-lab-progress."""
+
+__version__ = "1.1.3"
+__version_info__ = tuple(int(x) for x in __version__.split("."))
+
+# Package metadata
+__title__ = "jupyter-lab-progress"
+__description__ = "A comprehensive toolkit for creating interactive Jupyter notebook lab exercises"
+__author__ = "Michael Lynn"
+__author_email__ = "michael.lynn@mongodb.com"
+__license__ = "MIT"
+__url__ = "https://github.com/mrlynn/jupyter-lab-utils"

19fc13ec9588ba6c5ac719225f9ce59ebd984b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:16 +0000

    Add package finance-sdk/1.0.0/finance_sdk-1.0.0.tar.gz

---
+++ packages/finance-sdk/finance_sdk-1.0.0.tar.gz/finance_sdk-1.0.0/sdk/__init__.py
@@ -0,0 +1,2 @@
+default_app_config = 'sdk.apps.SdkConfig'
+__version__ = '1.0.0'
---
+++ packages/finance-sdk/finance_sdk-1.0.0.tar.gz/finance_sdk-1.0.0/sdk/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SdkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sdk'

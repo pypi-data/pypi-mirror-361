2e764d47f4a33c6873c171afcf2ba28094f7db8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:31 +0000

    Add package zikomo/1.2.2/zikomo-1.2.2.tar.gz

---
+++ packages/zikomo/zikomo-1.2.2.tar.gz/zikomo-1.2.2/zikomo/constants.py
@@ -0,0 +1,45 @@
+FLIGHTBITE_DIR = "E:\\ZikomoSolutions\\Projects\\FlightBiteBlazor"
+BACKOFFICE_DIR ="E:\\ZikomoSolutions\\Projects\\ZikomoMini\\Zikomo"
+WEBSITES_DIR ="E:\\ZikomoSolutions\\Projects\\ZikomoMini\\MiniWebsitesV3"
+
+RELEASE_DOCS_FILE = "E:\\ZikomoSolutions\\Images\\ServerUpdatePics\\staging-2025.md"
+
+FLIGHTBITE_REGISTRY = "zikomo.azurecr.io/flightbite"
+BACKOFFICE_REGISTRY = "zikomo.azurecr.io/mini"
+WEBSITES_REGISTRY = "zikomo.azurecr.io/mini-websites-v3"
+
+SOLUTION_ROOT = "E:/ZikomoSolutions/Projects/ZikomoMini/Zikomo"
+STARTUP_PROJECT = "Zikomo.Main.BackendHost"    
+PROJECT_DIR = r"E:\ZikomoSolutions\ProjectResources\ZikomoMini"
+OUTPUT_DIR = r"E:\ZikomoSolutions\ProjectResources\ZikomoMini\Scripts"
+IMAGE_HOST_URL = "https://zikomo.azureedge.net/media/general/images/"
+
+# SLACK
+SLACK_TOKEN="xoxb-6233873111973-8582839710000-PjZ3ZEpVPI1ytaDQ1o3wkeb2"
+SLACK_CHANNEL_ID_IMRAN="U066S4TAPJS"
+SLACK_CHANNEL_ID_GENERAL="C066YLPB62F"
+
+#DISCORD
+DISCORD_BOT_TOKEN = 'MTM4MjY1NzEzNDc5NzM4OTg3NA.Gu4OnF.FAhJIRgSw0KgV2Qu-OhptIYgPd37N1Y4de6qJQ'
+IMRAN_CHANNEL_ID = 1382664594077519882
+ZIKOMO_CHANNEL_ID = 1382064699922845769
+
+# SET CHANNELS
+DISCORD_SELECTED_CHANNEL_ID = ZIKOMO_CHANNEL_ID
+SELECTED_SLACK_CHANNEL_ID = SLACK_CHANNEL_ID_GENERAL
+
+SSH_HOSTS = {
+    "staging": "staging",
+    "uat": "uat",
+    "prod": "live"
+}
+
+EMAIL_SETTINGS = {
+    "sender": "info@zikomo.io",
+    "receiver": ["imran.shah@zikomosolutions.com","chetan@zikomosolutions.com","richard@zikomosolutions.com","leon.shanahan@zikomosolutions.com"],
+    "smtp_server": "mail.zikomosolutions.net",
+    "smtp_port": 587,
+    "username": "info@zikomo.io",
+    "password": "Developer#1"
+}
+

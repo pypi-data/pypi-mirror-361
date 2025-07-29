f90b8d1f7057a148d443847b1da1b41c4fbed807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:25 +0000

    Add package firebolt-sdk/1.13.0/firebolt_sdk-1.13.0.tar.gz

---
+++ packages/firebolt-sdk/firebolt_sdk-1.13.0.tar.gz/firebolt_sdk-1.13.0/src/firebolt/db/__init__.py
@@ -0,0 +1,37 @@
+from firebolt.common._types import (
+    ARRAY,
+    BINARY,
+    DATETIME,
+    DECIMAL,
+    NUMBER,
+    ROWID,
+    STRING,
+    STRUCT,
+    Binary,
+    Date,
+    DateFromTicks,
+    ExtendedType,
+    Time,
+    TimeFromTicks,
+    Timestamp,
+    TimestampFromTicks,
+)
+from firebolt.db.connection import Connection, connect
+from firebolt.db.cursor import Cursor
+from firebolt.utils.exception import (
+    DatabaseError,
+    DataError,
+    Error,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    Warning,
+)
+
+apilevel = "2.0"
+# threads may only share the module and connections, cursors should not be shared
+threadsafety = 2
+paramstyle = "qmark"

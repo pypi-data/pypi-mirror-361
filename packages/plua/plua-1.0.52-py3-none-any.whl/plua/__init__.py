8bf2d7703e16be1a31e76c7b88c6e6b53eb679dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:08 +0000

    Add package plua/1.0.52/plua-1.0.52-py3-none-any.whl

---
+++ packages/plua/plua-1.0.52-py3-none-any.whl/plua/__init__.py
@@ -0,0 +1,8 @@
+"""
+PLua - A Lua interpreter in Python using Lupa library
+"""
+
+from .version import __version__
+from .interpreter import PLuaInterpreter
+
+__all__ = ['__version__', 'PLuaInterpreter']

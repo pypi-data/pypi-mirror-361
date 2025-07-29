4b9960df51f222339bb79e665a46cdd0de7c2012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:45 +0000

    Add package hestia-earth-models/0.73.5/hestia_earth_models-0.73.5-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.73.5-py3-none-any.whl/hestia_earth/models/faostat2018/landTransformation100YearAverageDuringCycle.py
@@ -0,0 +1,34 @@
+from .utils import should_run_landTransformationFromCropland, run_landTransformationFromCropland
+
+REQUIREMENTS = {
+    "ImpactAssessment": {
+        "endDate": "",
+        "country": {"@type": "Term", "termType": "region"},
+        "emissionsResourceUse": [{
+            "@type": "Indicator",
+            "term.@id": "landTransformation100YearAverageDuringCycle",
+            "value": "",
+            "previousLandCover": {
+                "@type": "Term",
+                "termType": "landCover",
+                "@id": "cropland"
+            }
+        }]
+    }
+}
+LOOKUPS = {
+    "region-faostatArea": ""
+}
+RETURNS = {
+    "Indicator": [{
+        "value": "",
+        "landCover": "",
+        "previousLandCover": ""
+    }]
+}
+TERM_ID = 'landTransformation100YearAverageDuringCycle'
+
+
+def run(impact: dict):
+    should_run, indicators = should_run_landTransformationFromCropland(TERM_ID, impact)
+    return run_landTransformationFromCropland(TERM_ID, impact, indicators, 100) if should_run else []

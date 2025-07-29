955bed1cb411d953a4dc4b91678f7d1f4b55e7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:51 +0000

    Add package oas-client/0.1.5/oas_client-0.1.5-py3-none-any.whl

---
+++ packages/oas-client/oas_client-0.1.5-py3-none-any.whl/oas_client/renderers/responses.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Any
+
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+from oas_client.parser import find_response_schemas
+from oas_client.parser import find_schemas
+
+
+def render_responses(spec: dict[str, Any], template_dir: Path) -> str:
+    schemas, imports = find_schemas(spec, partial=False)
+    # render necessary schemas only
+    schemas = [s for s in schemas if s["name"] in find_response_schemas(spec)]
+    env = Environment(
+        loader=FileSystemLoader(template_dir),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    template = env.get_template("schemas.jinja2")
+    output_code = template.render(schemas=schemas, imports=imports)
+    return output_code

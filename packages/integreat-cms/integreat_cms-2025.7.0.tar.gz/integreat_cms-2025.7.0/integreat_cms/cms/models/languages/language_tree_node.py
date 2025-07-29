29f9cf57a8bc39ec04b4749c5c2a8c3956782daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:45 +0000

    Add package integreat-cms/2025.7.0/integreat_cms-2025.7.0.tar.gz

---
+++ packages/integreat-cms/integreat_cms-2025.7.0.tar.gz/integreat_cms-2025.7.0/integreat_cms/cms/models/languages/language_tree_node.py
@@ -0,0 +1,179 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.db import models
+from django.utils import timezone
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+
+from ...constants import machine_translation_providers
+from ..abstract_tree_node import AbstractTreeNode
+from ..decorators import modify_fields
+from .language import Language
+
+if TYPE_CHECKING:
+    from integreat_cms.core.utils.machine_translation_provider import (
+        MachineTranslationProviderType,
+    )
+
+
+@modify_fields(parent={"verbose_name": _("source language")})
+class LanguageTreeNode(AbstractTreeNode):
+    """
+    Data model representing a region's language tree. Each tree node is a single object instance and the whole tree is
+    identified by the root node. The base functionality inherits from the package `django-treebeard
+    <https://django-treebeard.readthedocs.io/en/latest/>`.
+    """
+
+    language = models.ForeignKey(
+        Language,
+        on_delete=models.PROTECT,
+        related_name="language_tree_nodes",
+        verbose_name=_("language"),
+    )
+    visible = models.BooleanField(
+        default=True,
+        verbose_name=_("visible"),
+        help_text=_(
+            "Defines whether the language is displayed to the users of the app",
+        ),
+    )
+    active = models.BooleanField(
+        default=True,
+        verbose_name=_("active"),
+        help_text=_("Defined if content in this language can be created or edited"),
+    )
+    created_date = models.DateTimeField(
+        default=timezone.now,
+        verbose_name=_("creation date"),
+    )
+    last_updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("modification date"),
+    )
+    machine_translation_enabled = models.BooleanField(
+        default=True,
+        verbose_name=_("machine translatable"),
+        help_text=_("Enable or disable machine translations into this language"),
+    )
+    preferred_mt_provider = models.CharField(
+        max_length=255,
+        choices=(
+            (provider.name, provider.name)
+            for provider in machine_translation_providers.CHOICES
+        ),
+        default=next(iter(machine_translation_providers.CHOICES)).name,
+        verbose_name=_("machine translation provider"),
+        help_text=_("Preferred provider for translations into this language"),
+    )
+
+    @cached_property
+    def slug(self) -> str:
+        """
+        Returns the slug of this node's language
+
+        :return: The language slug of this language node
+        """
+        return self.language.slug
+
+    @cached_property
+    def native_name(self) -> str:
+        """
+        Returns the native name of this node's language
+
+        :return: The native name of this language node
+        """
+        return self.language.native_name
+
+    @cached_property
+    def english_name(self) -> str:
+        """
+        Returns the name of this node's language in English
+
+        :return: The English name of this language node
+        """
+        return self.language.english_name
+
+    @cached_property
+    def translated_name(self) -> str:
+        """
+        Returns the name of this node's language in the current backend language
+
+        :return: The translated name of this language node
+        """
+        return self.language.translated_name
+
+    @cached_property
+    def text_direction(self) -> str:
+        """
+        Returns the text direction (e.g. left-to-right) of this node's language
+
+        :return: The text direction name of this language node
+        """
+        return self.language.text_direction
+
+    @cached_property
+    def mt_providers(self) -> list[MachineTranslationProviderType]:
+        """
+        Return the list of supported machine translation providers
+
+        :return: The MT provider for this target language
+        """
+        return [
+            provider
+            for provider in machine_translation_providers.CHOICES
+            if provider.is_enabled(self.region, self.language)
+        ]
+
+    @cached_property
+    def mt_provider(self) -> MachineTranslationProviderType | None:
+        """
+        Return the preferred machine translation provider if valid, or the first available provider, or ``None``
+
+        :return: The MT provider for this target language
+        """
+        return next(
+            (p for p in self.mt_providers if p.name == self.preferred_mt_provider),
+            next(iter(self.mt_providers), None),
+        )
+
+    def __str__(self) -> str:
+        """
+        This overwrites the default Django :meth:`~django.db.models.Model.__str__` method which would return ``LanguageTreeNode object (id)``.
+        It is used in the Django admin backend and as label for ModelChoiceFields.
+
+        :return: A readable string representation of the language node
+        """
+        return self.translated_name
+
+    def get_repr(self) -> str:
+        """
+        This overwrites the default Django ``__repr__()`` method which would return ``<LanguageTreeNode: LanguageTreeNode object (id)>``.
+        It is used for logging.
+
+        :return: The canonical string representation of the language node
+        """
+        language_str = f", language: {self.language.slug}" if self.language else ""
+        parent_str = f", parent: {self.parent_id}" if self.parent_id else ""
+        region_str = f", region: {self.region.slug}" if self.region else ""
+        return (
+            f"<LanguageTreeNode (id: {self.id}{language_str}{parent_str}{region_str})>"
+        )
+
+    class Meta(AbstractTreeNode.Meta):
+        #: The verbose name of the model
+        verbose_name = _("language tree node")
+        #: The plural verbose name of the model
+        verbose_name_plural = _("language tree nodes")
+        #: The name that will be used by default for the relation from a related object back to this one
+        default_related_name = "language_tree_nodes"
+        #: There cannot be two language tree nodes with the same region and language
+        unique_together = (
+            (
+                "language",
+                "region",
+            ),
+        )
+        #: The default permissions for this model
+        default_permissions = ("change", "delete", "view")
---
+++ packages/integreat-cms/integreat_cms-2025.7.0.tar.gz/integreat_cms-2025.7.0/integreat_cms/cms/views/language_tree/language_tree_actions.py
@@ -0,0 +1,165 @@
+"""
+This module contains view actions for the language tree.
+Typically, they do not render a whole page, but only parts of it or they redirect to regular views.
+
+.. warning::
+    Any action modifying the database with treebeard should use ``@tree_mutex(MODEL_NAME)`` from ``integreat_cms.cms.utils.tree_mutex``
+    as a decorator instead of ``@transaction.atomic`` to force treebeard to actually use transactions.
+    Otherwise, the data WILL get corrupted during concurrent treebeard calls!
+"""
+
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from cacheops import invalidate_obj
+from django.contrib import messages
+from django.db.models import ProtectedError
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.http import require_POST
+from treebeard.exceptions import InvalidMoveToDescendant, InvalidPosition
+
+from ...constants import position
+from ...decorators import permission_required
+from ...models import LanguageTreeNode
+from ...utils.tree_mutex import tree_mutex
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponseRedirect
+
+    from ...models import Region
+
+logger = logging.getLogger(__name__)
+
+
+@require_POST
+@permission_required("cms.change_languagetreenode")
+@tree_mutex("languagetreenode")
+def move_language_tree_node(
+    request: HttpRequest,
+    region_slug: str,
+    pk: int,
+    target_id: int,
+    target_position: str,
+) -> HttpResponseRedirect:
+    """
+    This action moves the given language tree node to the given position relative to the given target.
+
+    :param request: The current request
+    :param region_slug: The slug of the region which language tree should be modified
+    :param pk: The id of the language tree node which should be moved
+    :param target_id: The id of the target language tree node
+    :param target_position: The desired position (choices: :mod:`~integreat_cms.cms.constants.position`)
+    :return: A redirection to the language tree
+    """
+
+    region = request.region
+    language_tree_node = get_object_or_404(region.language_tree_nodes, id=pk)
+    target = get_object_or_404(region.language_tree_nodes, id=target_id)
+
+    try:
+        if target.depth == 1 and target_position in [position.LEFT, position.RIGHT]:
+            raise InvalidPosition(_("A region can only have one root language."))  # noqa: TRY301
+        language_tree_node.move(target, target_position)
+        # Call the save method on the (reloaded) node in order to trigger possible signal handlers etc.
+        # (The move()-method executes raw sql which might cause problems if the instance isn't fetched again)
+        language_tree_node = LanguageTreeNode.objects.get(id=pk)
+        language_tree_node.save()
+        manually_invalidate_models(region)
+        messages.success(
+            request,
+            _('The language tree node "{}" was successfully moved.').format(
+                language_tree_node.translated_name,
+            ),
+        )
+        logger.debug(
+            "%r moved to %r of %r by %r",
+            language_tree_node,
+            target_position,
+            target,
+            request.user,
+        )
+    except (ValueError, InvalidPosition, InvalidMoveToDescendant) as e:
+        messages.error(request, e)
+        logger.exception("")
+
+    return redirect("languagetreenodes", **{"region_slug": region_slug})
+
+
+@require_POST
+@permission_required("cms.delete_languagetreenode")
+@tree_mutex("languagetreenode")
+def delete_language_tree_node(
+    request: HttpRequest,
+    region_slug: str,
+    pk: int,
+) -> HttpResponseRedirect:
+    """
+    Deletes the language node of distinct region
+    and all page translations for this language
+
+    :param request: The current request
+    :param region_slug: The slug of the region which language node should be deleted
+    :param pk: The id of the language tree node which should be deleted
+    :return: A redirection to the language tree
+    """
+    # get current region
+    region = request.region
+    # get current selected language node
+    language_node = get_object_or_404(region.language_tree_nodes, id=pk)
+    # get all page translation assigned to the language node
+    page_translations = language_node.language.page_translations
+    # filter those translation that belong to the region and delete them
+    page_translations.filter(page__region=region).delete()
+    # get all event translation assigned to the language node
+    event_translations = language_node.language.event_translations
+    # filter those translation that belong to the region and delete them
+    event_translations.filter(event__region=region).delete()
+    # get all poi translation assigned to the language node
+    poi_translations = language_node.language.poi_translations
+    # filter those translation that belong to the region and delete them
+    poi_translations.filter(poi__region=region).delete()
+
+    logger.debug("%r deleted by %r", language_node, request.user)
+
+    try:
+        language_node.delete()
+    except ProtectedError:
+        messages.error(
+            request,
+            _(
+                'The language tree node "{}" cannot be deleted because it is the source language of other language(s).',
+            ).format(language_node.translated_name),
+        )
+        return redirect("languagetreenodes", **{"region_slug": region_slug})
+
+    manually_invalidate_models(region)
+
+    messages.success(
+        request,
+        _(
+            'The language tree node "{}" and all corresponding translations were successfully deleted.',
+        ).format(language_node.translated_name),
+    )
+    return redirect("languagetreenodes", **{"region_slug": region_slug})
+
+
+def manually_invalidate_models(region: Region) -> None:
+    """
+    This is a helper function to iterate through all affected objects and invalidate their cache.
+    This is necessary as the original cache invalidation of cacheops only triggers for direct foreign key relationships.
+
+    :param region: The affected region
+    """
+    for page in region.pages.all():
+        invalidate_obj(page)
+    for event in region.events.all():
+        invalidate_obj(event)
+    for poi in region.pois.all():
+        invalidate_obj(poi)
+    for push_notification in region.push_notifications.all():
+        invalidate_obj(push_notification)
+    if region.imprint:
+        invalidate_obj(region.imprint)

3a91969506cf89d8100f63fbc5736e7326fb9c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:29 +0000

    Add package sproclib/2.0.3/sproclib-2.0.3-py3-none-any.whl

---
+++ packages/sproclib/sproclib-2.0.3-py3-none-any.whl/sproclib/unit/reactor/pfr/__init__.py
@@ -0,0 +1,12 @@
+"""
+Plug Flow Reactor Module for SPROCLIB
+
+This module imports PFR classes.
+
+Author: Thorsten Gressling <gressling@paramus.ai>
+License: MIT License
+"""
+
+from ..PlugFlowReactor import PlugFlowReactor
+
+__all__ = ['PlugFlowReactor']

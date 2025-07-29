a095694a694c371d246490807448f9fcaf1d38e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:04 +0000

    Add package gencove/3.2.0/gencove-3.2.0.tar.gz

---
+++ packages/gencove/gencove-3.2.0.tar.gz/gencove-3.2.0/gencove/version.py
@@ -0,0 +1,10 @@
+"""Utility to generate version string."""
+import pkgutil
+
+
+def version():
+    """Return version string."""
+    major = pkgutil.get_data(__package__, "version/A-major").strip()
+    minor = pkgutil.get_data(__package__, "version/B-minor").strip()
+    patch = pkgutil.get_data(__package__, "version/C-patch").strip()
+    return f"{major.decode('utf-8')}.{minor.decode('utf-8')}.{patch.decode('utf-8')}"

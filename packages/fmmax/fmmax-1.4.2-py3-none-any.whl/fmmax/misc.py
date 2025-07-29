0c73b33ce99b7e67c79fe85b6f450945c15ed3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package fmmax/1.4.2/fmmax-1.4.2-py3-none-any.whl

---
+++ packages/fmmax/fmmax-1.4.2-py3-none-any.whl/fmmax/misc.py
@@ -0,0 +1,27 @@
+# FMMAX
+# Copyright (C) 2025 Martin F. Schubert
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Defines miscellaneous private functions.
+
+Copyright (c) Martin F. Schubert
+"""
+
+# ruff: noqa: F401
+from fmmax._orig.misc import (
+    atleast_nd,
+    batch_compatible_shapes,
+    diag,
+    matrix_adjoint,
+)

f4464046369ce91c53ca785f2399b2ea3b0726d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:13 +0000

    Add package bruce-li-tc/0.3.9/bruce_li_tc-0.3.9.tar.gz

---
+++ packages/bruce-li-tc/bruce_li_tc-0.3.9.tar.gz/bruce_li_tc-0.3.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+# setup.py 最顶部添加
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+setup(
+    name='Bruce_li_tc',
+    version='0.3.9',
+    description='A versatile utility library by Bruce Li',
+
+    author='Bruce Li',
+    author_email='bruce.li@example.com',
+    packages=find_packages(exclude=["venv", "venv.*", "*.venv", "*.venv.*"]),
+    include_package_data=True,
+    exclude_package_data={
+        '': ['venv', '.venv', 'env', '*.pyc', '__pycache__'],
+    },
+    install_requires=[
+        'requests',
+        'loguru',
+        'openpyxl',
+        'aiohttp'
+    ],
+    extras_require={
+        'excel': ['openpyxl'],
+        'async': ['aiohttp'],
+        'loguru': ['loguru']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+
+)

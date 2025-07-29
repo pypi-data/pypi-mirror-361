e3c24ed8dd33c49b43199b356aec5ff5262cb077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:13 +0000

    Add package lfm2/0.0.2/lfm2-0.0.2-py3-none-any.whl

---
+++ packages/lfm2/lfm2-0.0.2-py3-none-any.whl/lfm2/__init__.py
@@ -0,0 +1,15 @@
+from lfm2.main import (
+    create_lfm2_model,
+    LFM2ForCausalLM,
+    LFM2Config,
+    LFM2ConvBlock,
+    LFM2Model,
+)
+
+__all__ = [
+    "create_lfm2_model",
+    "LFM2ForCausalLM",
+    "LFM2Config",
+    "LFM2ConvBlock",
+    "LFM2Model",
+]

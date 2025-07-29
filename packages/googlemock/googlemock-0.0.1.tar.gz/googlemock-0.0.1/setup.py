418f7d939dc43260f4d1652f14c46aa7997c45b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package googlemock/0.0.1/googlemock-0.0.1.tar.gz

---
+++ packages/googlemock/googlemock-0.0.1.tar.gz/googlemock-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="googlemock",
+    version="0.0.1",
+    packages=["googlemock"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

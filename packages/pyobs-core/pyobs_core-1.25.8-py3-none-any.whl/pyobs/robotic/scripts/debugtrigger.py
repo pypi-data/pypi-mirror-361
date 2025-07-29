433f48fd295af6d99a4682c9982d5d1e7a5dc907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:28 +0000

    Add package pyobs-core/1.25.8/pyobs_core-1.25.8-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-1.25.8-py3-none-any.whl/pyobs/robotic/scripts/debugtrigger.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import logging
+from typing import Any, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pyobs.robotic import TaskRunner, TaskSchedule, TaskArchive
+from pyobs.robotic.scripts import Script
+
+log = logging.getLogger(__name__)
+
+
+class DebugTriggerRunner(Script):
+    """Script for a debug trigger."""
+
+    __module__ = "pyobs.modules.robotic"
+
+    def __init__(self, **kwargs: Any):
+        """Initialize a new DebugTriggerRunner."""
+        Script.__init__(self, **kwargs)
+        self.triggered = False
+
+    async def can_run(self) -> bool:
+        return True
+
+    async def run(
+        self,
+        task_runner: TaskRunner,
+        task_schedule: Optional[TaskSchedule] = None,
+        task_archive: Optional[TaskArchive] = None,
+    ) -> None:
+        self.triggered = True
+
+
+__all__ = ["DebugTriggerRunner"]

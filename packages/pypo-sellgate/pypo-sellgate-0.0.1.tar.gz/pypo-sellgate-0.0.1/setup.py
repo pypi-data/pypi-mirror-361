84dbde5aa7b118ccbe3032f77b9ef1b4beb1314e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:04 +0000

    Add package pypo-sellgate/0.0.1/pypo-sellgate-0.0.1.tar.gz

---
+++ packages/pypo-sellgate/pypo-sellgate-0.0.1.tar.gz/pypo-sellgate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-sellgate",
+    version="0.0.1",
+    packages=["pypo-sellgate"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

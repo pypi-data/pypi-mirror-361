132243deb617569a2996340f10cae2e817eb7baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:22 +0000

    Add package pyobs-core/1.25.11/pyobs_core-1.25.11.tar.gz

---
+++ packages/pyobs-core/pyobs_core-1.25.11.tar.gz/pyobs_core-1.25.11/pyobs/images/processors/offsets/__init__.py
@@ -0,0 +1,30 @@
+"""
+Offsets
+-------
+"""
+
+from .brighteststar_guiding import BrightestStarGuiding
+from .offsets import Offsets
+from .add_pixeloffset import AddPixelOffset
+from .astrometry import AstrometryOffsets
+from .brighteststar import BrightestStarOffsets
+from .nstar.nstar import NStarOffsets
+from .projected import ProjectedOffsets
+from .fitsheader import FitsHeaderOffsets
+from .dummyoffsets import DummyOffsets
+from .dummyskyoffsets import DummySkyOffsets
+from .spilled_light import SpilledLightGuiding
+
+__all__ = [
+    "Offsets",
+    "AddPixelOffset",
+    "AstrometryOffsets",
+    "NStarOffsets",
+    "ProjectedOffsets",
+    "FitsHeaderOffsets",
+    "BrightestStarOffsets",
+    "BrightestStarGuiding",
+    "DummyOffsets",
+    "DummySkyOffsets",
+    "SpilledLightGuiding",
+]

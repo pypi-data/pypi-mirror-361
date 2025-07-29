bfe4621a30631fd35a46be7d53dce85b24ef4f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:51 +0000

    Add package siges-sdk/0.1.6/siges_sdk-0.1.6.tar.gz

---
+++ packages/siges-sdk/siges_sdk-0.1.6.tar.gz/siges_sdk-0.1.6/siges_sdk/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.6"
+
+# En lugar de importar directamente, simplemente declaramos que estos módulos
+# son parte del paquete, pero no los importamos aquí
+__all__ = ["core", "queues"]

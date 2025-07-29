ae61fc1f205a1f20dbccfb885cf717fb44617d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:17 +0000

    Add package sifflet-sdk/0.4.0/sifflet_sdk-0.4.0.tar.gz

---
+++ packages/sifflet-sdk/sifflet_sdk-0.4.0.tar.gz/sifflet_sdk-0.4.0/sifflet_sdk/config.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class SiffletConfig:
+    tenant: Optional[str] = None
+    backend_url: Optional[str] = None
+    token: Optional[str] = None
+    debug: Optional[bool] = None

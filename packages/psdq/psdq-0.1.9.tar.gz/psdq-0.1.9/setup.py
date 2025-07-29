62b5a8b516d356f99fd028ec5530c5d5a17b27df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:06 +0000

    Add package psdq/0.1.9/psdq-0.1.9.tar.gz

---
+++ packages/psdq/psdq-0.1.9.tar.gz/psdq-0.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='psdq',
+    packages=find_packages(),
+    version='0.1.9',
+    description='Ad Hoc Data Quality Tool for PySpark',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Alexandre "Alex" de Magalhaes',
+    author_email="alexandredemagalhaess@gmail.com",
+    install_requires=['pyspark>=3.0.0'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest>=4.4.1'],
+    test_suite='tests',
+    python_requires=">=3.9",
+)

fa56b5bf4f553874a9b42edb18b3e66921362060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:48 +0000

    Add package abtranslate/0.1.22/abtranslate-0.1.22-py3-none-any.whl

---
+++ packages/abtranslate/abtranslate-0.1.22-py3-none-any.whl/abtranslate/__init__.py
@@ -0,0 +1,6 @@
+from . import translator
+from . import utils
+
+from .translator.argos_translator import ArgosTranslator
+from .translator.package import ArgosPackage
+from .translator.package import load_argostranslate_model

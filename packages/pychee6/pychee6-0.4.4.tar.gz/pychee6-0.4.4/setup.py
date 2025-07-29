a164b23a1c3231a66cca98126ae3627d81fcd404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:03 +0000

    Add package pychee6/0.4.4/pychee6-0.4.4.tar.gz

---
+++ packages/pychee6/pychee6-0.4.4.tar.gz/pychee6-0.4.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requirements = open("requirements.txt").read().split("\n")
+
+setup(
+    name="pychee6",
+    version="0.4.4",
+    description="A simple python moudle for Lychee",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md", "rt", encoding="utf-8").read(),
+    author="x1nt",
+    author_email="cjdty@qq.com",
+    url="https://github.com/x1ntt/pychee6",
+    packages=["pychee6"],
+    package_dir={"pychee6": "src"},
+    include_package_data=True,
+    install_requires=requirements,
+    package_data={
+        'pychee6': ['locales/**/*'],
+    },
+)

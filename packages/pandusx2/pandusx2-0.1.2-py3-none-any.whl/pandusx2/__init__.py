13422c0de9f0e64550608c16dee8ea0dd2af88fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:37 +0000

    Add package pandusx2/0.1.2/pandusx2-0.1.2-py3-none-any.whl

---
+++ packages/pandusx2/pandusx2-0.1.2-py3-none-any.whl/pandusx2/__init__.py
@@ -0,0 +1,19 @@
+from .main import (
+    prac_2,
+    prac_3,
+    prac_4,
+    prac_5,
+    prac_7,
+    prac_9,
+    prac_10
+)
+
+__all__ = [
+    'prac_2',
+    'prac_3',
+    'prac_4',
+    'prac_5',
+    'prac_7',
+    'prac_9',
+    'prac_10'
+] 

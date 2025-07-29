ad250fad9852d8fadc82bf117372ee0afef67c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:11 +0000

    Add package spaceone-cost-analysis/2.0.dev236/spaceone-cost-analysis-2.0.dev236.tar.gz

---
+++ packages/spaceone-cost-analysis/spaceone-cost-analysis-2.0.dev236.tar.gz/spaceone-cost-analysis-2.0.dev236/spaceone/cost_analysis/error/data_source.py
@@ -0,0 +1,22 @@
+from spaceone.core.error import *
+
+
+class ERROR_INVALID_PLUGIN_VERSION(ERROR_INVALID_ARGUMENT):
+    _message = 'Plugin version is invalid. (plugin_id = {plugin_id}, version = {version})'
+
+
+class ERROR_NOT_ALLOW_PLUGIN_SETTINGS(ERROR_INVALID_ARGUMENT):
+    _message = 'Local type dose not allow plugin settings. (data_source_id = {data_source_id})'
+
+
+class ERROR_NOT_ALLOW_SYNC_COMMAND(ERROR_INVALID_ARGUMENT):
+    _message = 'Local type dose not allow sync commands. (data_source_id = {data_source_id})'
+
+
+class ERROR_DATA_SOURCE_STATE(ERROR_INVALID_ARGUMENT):
+    _message = 'Data source is disabled (data_source_id = {data_source_id})'
+
+
+class ERROR_NOT_ALLOW_SECRET_FILTER(ERROR_INVALID_ARGUMENT):
+    _message = 'USE_SERVICE_ACCOUNT_SECRET type dose not allow secret filter. (data_source_id = {data_source_id})'
+
---
+++ packages/spaceone-cost-analysis/spaceone-cost-analysis-2.0.dev236.tar.gz/spaceone-cost-analysis-2.0.dev236/spaceone/cost_analysis/model/cost_report_data/request.py
@@ -0,0 +1,43 @@
+from typing import Union, Literal
+from pydantic import BaseModel
+
+__all__ = [
+    "CostReportDataSearchQueryRequest",
+    "CostReportDataAnalyzeQueryRequest",
+    "CostReportDataStatQueryRequest",
+]
+
+
+class CostReportDataSearchQueryRequest(BaseModel):
+    query: Union[dict, None] = None
+    cost_report_data_id: Union[str, None] = None
+    report_year: Union[str, None] = None
+    report_month: Union[str, None] = None
+    product: Union[str, None] = None
+    provider: Union[str, None] = None
+    data_source_id: Union[str, None] = None
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class CostReportDataAnalyzeQueryRequest(BaseModel):
+    query: Union[dict, None] = None
+    name: Union[str, None] = None
+    cost_report_data_id: Union[str, None] = None
+    is_confirmed: Union[bool, None] = None
+    product: Union[str, None] = None
+    provider: Union[str, None] = None
+    data_source_id: Union[str, None] = None
+    cost_report_id: Union[str, None] = None
+    cost_report_config_id: Union[str, None] = None
+    user_projects: Union[list, None] = None
+    project_id: Union[str, None] = None
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class CostReportDataStatQueryRequest(BaseModel):
+    query: dict
+    cost_report_data_id: Union[str, None] = None
+    workspace_id: Union[str, None] = None
+    domain_id: str

1165d581ea42f8dab08660bf445028ada75f744c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package aiosqlitepool/1.0.0b0/aiosqlitepool-1.0.0b0-py3-none-any.whl

---
+++ packages/aiosqlitepool/aiosqlitepool-1.0.0b0-py3-none-any.whl/aiosqlitepool/__init__.py
@@ -0,0 +1,7 @@
+"""A robust, high-performance asynchronous connection pool for SQLite."""
+
+from .client import SQLiteConnectionPool
+
+__all__ = ["SQLiteConnectionPool"]
+
+__version__ = "1.0.0.beta"

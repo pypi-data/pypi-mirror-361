ef3ae83244dd906d85db2be141b428ddde99baab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:58 +0000

    Add package laakhay-quantlab/0.1.0/laakhay_quantlab-0.1.0.tar.gz

---
+++ packages/laakhay-quantlab/laakhay_quantlab-0.1.0.tar.gz/laakhay_quantlab-0.1.0/README.md
@@ -0,0 +1,1 @@
+# Laakhay Quantlab
---
+++ packages/laakhay-quantlab/laakhay_quantlab-0.1.0.tar.gz/laakhay_quantlab-0.1.0/laakhay/quantlab/__init__.py
@@ -0,0 +1,11 @@
+"""
+Quantlab - Quantitative analysis tools for the Laakhay ecosystem
+"""
+
+__version__ = "0.1.0"
+__author__ = "Laakhay Corporation"
+
+def hello():
+    print("Hello from Laakhay Quantlab!")
+
+__all__ = ['hello']

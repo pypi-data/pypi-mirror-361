b1418faafd59cbfceb116699f11db61b942d0243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:13 +0000

    Add package coolstring/0.1.2/coolstring-0.1.2.tar.gz

---
+++ packages/coolstring/coolstring-0.1.2.tar.gz/coolstring-0.1.2/docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'coolstring'
+copyright = '2025, DouglasAdams_42'
+author = 'DouglasAdams_42'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+autosummary_generate = True
---
+++ packages/coolstring/coolstring-0.1.2.tar.gz/coolstring-0.1.2/docs/source/index.rst
@@ -0,0 +1,20 @@
+.. cool_string_douglasadams_42 documentation master file, created by
+   sphinx-quickstart on Mon Jul  7 19:24:44 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+coolstring documentation
+========================
+
+This is the documentation / reference for the **CoolString** class (**coolstring** python module).
+
+.. :caption: Getting started:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   start
+   reference
+   Source Code <https://github.com/playlix42/coolstring>
+   PyPI Package <https://pypi.org/project/coolstring>

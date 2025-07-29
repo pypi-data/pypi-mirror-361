4fd97fff9b700c572e66b3521fbb0748240d1d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:55 +0000

    Add package abtranslate/0.1.28rc7/abtranslate-0.1.28rc7.tar.gz

---
+++ packages/abtranslate/abtranslate-0.1.28rc7.tar.gz/abtranslate-0.1.28rc7/tests/test_core.py
@@ -0,0 +1,16 @@
+from transformation_test import *
+from translation_test import *
+
+
+## TRANSFORMATION TEST FUNCTION
+test_expand_to_sentence_level()
+# test_flatten_sentences()
+# test_tokenized_sentences()
+# test_detokenized_sentences()
+# test_get_structure()
+# test_apply_structure()
+
+## TRANSLATION TEST FUNCTION
+# test_translation()
+# test_batch_translation()
+# test_udf_translation()

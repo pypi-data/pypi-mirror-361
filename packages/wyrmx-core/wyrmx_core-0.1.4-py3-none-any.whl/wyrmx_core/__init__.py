09af0b6c6a5234c8e5feba87d6f328f500a1516c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:31 +0000

    Add package wyrmx-core/0.1.4/wyrmx_core-0.1.4-py3-none-any.whl

---
+++ packages/wyrmx-core/wyrmx_core-0.1.4-py3-none-any.whl/wyrmx_core/__init__.py
@@ -0,0 +1,4 @@
+from wyrmx_core.app import WyrmxAPP
+from wyrmx_core.decorators import controller, service, model, singleton
+from wyrmx_core.scope import Scope
+from wyrmx_core.container import container

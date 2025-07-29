db6b00210f5f2a6121db74c8afcf9e7a8657906a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:28 +0000

    Add package AOT-biomaps/2.6.9/AOT_biomaps-2.6.9-py3-none-any.whl

---
+++ packages/AOT-biomaps/AOT_biomaps-2.6.9-py3-none-any.whl/AOT_biomaps/__init__.py
@@ -0,0 +1,58 @@
+
+# ACOUSTIC
+from AOT_biomaps.AOT_Acoustic._mainAcoustic import *
+from AOT_biomaps.AOT_Acoustic.AcousticEnums import *
+from AOT_biomaps.AOT_Acoustic.AcousticTools import *
+from AOT_biomaps.AOT_Acoustic.FocusedWave import *
+from AOT_biomaps.AOT_Acoustic.IrregularWave import *
+from AOT_biomaps.AOT_Acoustic.PlaneWave import *
+from AOT_biomaps.AOT_Acoustic.StructuredWave import *
+
+# EXPERIMENT
+from AOT_biomaps.AOT_Experiment._mainExperiment import *
+from .AOT_Experiment.Focus import *
+from .AOT_Experiment.Tomography import *
+
+# OPTIC
+from AOT_biomaps.AOT_Optic._mainOptic import *
+from AOT_biomaps.AOT_Optic.Absorber import *
+from AOT_biomaps.AOT_Optic.Laser import *
+from AOT_biomaps.AOT_Optic.OpticEnums import *
+
+# RECONSTRUCTION
+from AOT_biomaps.AOT_Recon._mainRecon import *
+from AOT_biomaps.AOT_Recon.AlgebraicRecon import *
+from AOT_biomaps.AOT_Recon.AnalyticRecon import *
+from AOT_biomaps.AOT_Recon.BayesianRecon import *
+from AOT_biomaps.AOT_Recon.DeepLearningRecon import *
+from AOT_biomaps.AOT_Recon.PrimalDualRecon import *
+from .AOT_Recon.ReconEnums import *
+from .AOT_Recon.ReconTools import *
+
+from AOT_biomaps.AOT_Recon.AOT_Optimizers.DEPIERRO import *
+from AOT_biomaps.AOT_Recon.AOT_Optimizers.MAPEM import *
+from AOT_biomaps.AOT_Recon.AOT_Optimizers.MLEM import *
+from AOT_biomaps.AOT_Recon.AOT_Optimizers.PDHG import *
+
+from AOT_biomaps.AOT_Recon.AOT_PotentialFunctions.Huber import *
+from AOT_biomaps.AOT_Recon.AOT_PotentialFunctions.Quadratic import *
+from AOT_biomaps.AOT_Recon.AOT_PotentialFunctions.RelativeDifferences import *
+
+from AOT_biomaps.Config import config
+from AOT_biomaps.Settings import *
+
+__version__ = '2.6.9'
+
+if config.get_process() == 'gpu':
+    __process__ = 'gpu'
+else:
+    __process__ = 'cpu'
+
+def initialize(process='cpu'):
+    config.set_process(process)
+    if process == 'gpu':
+        config.select_best_gpu()
+        print(f"Initialized with process: {config.get_process()} using GPU: {config.bestGPU}")
+    else:
+        print(f"Initialized with process: {config.get_process()}")
+

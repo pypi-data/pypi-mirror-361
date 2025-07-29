6d5664d66b47a972ed795003381b5a6a9ea135fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:31 +0000

    Add package gign/1.0.1/gign-1.0.1.tar.gz

---
+++ packages/gign/gign-1.0.1.tar.gz/gign-1.0.1/gitignore_gen/__init__.py
@@ -0,0 +1,9 @@
+"""A magical CLI tool that automatically generates and manages .gitignore files."""
+
+__version__ = "1.0.1"
+__author__ = "Sherin Joseph Roy"
+__email__ = "sherin.joseph2217@gmail.com"
+
+from .cli import main
+
+__all__ = ["main"] 

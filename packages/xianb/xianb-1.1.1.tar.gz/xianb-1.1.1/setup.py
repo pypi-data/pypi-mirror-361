a178f4cbc2150f454cd60d93d0450d6572989633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:10 +0000

    Add package xianb/1.1.1/xianb-1.1.1.tar.gz

---
+++ packages/xianb/xianb-1.1.1.tar.gz/xianb-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xianb',
+    version='1.1.1',
+    author='jxjiang',
+    author_email='723137901@qq.com',
+    description='总结RPA编程中常用的工具，增加工作效率，工具会结合AI一起编写，我肯定不会写这些玩意的。',
+    python_requires='>=3.8',
+    install_requires=[],
+    packages=find_packages()
+)

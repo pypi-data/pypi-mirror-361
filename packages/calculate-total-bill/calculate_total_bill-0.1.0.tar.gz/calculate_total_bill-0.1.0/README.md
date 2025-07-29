124724cc18cb45dadd8f00cb3088c7a59e3ab724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:52 +0000

    Add package calculate-total-bill/0.1.0/calculate_total_bill-0.1.0.tar.gz

---
+++ packages/calculate-total-bill/calculate_total_bill-0.1.0.tar.gz/calculate_total_bill-0.1.0/README.md
@@ -0,0 +1,1 @@
+This is a simple Python package that calculates total bill amount.
---
+++ packages/calculate-total-bill/calculate_total_bill-0.1.0.tar.gz/calculate_total_bill-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='calculate_total_bill',
+  version='0.1.0',
+  description='This is a simple Python package that calculates total bill amount.',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',
+  author='Gopi',
+  author_email=' x23306882@student.ncirl.ie',
+  classifiers=[
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+],
+  keywords='calculate_total_bill', 
+  packages=find_packages(),
+  python_requires=">=3.6"
+)

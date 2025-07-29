3c716b4458c8671ba7a87aae80bfbbb80ed800d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package googletest/0.0.1/googletest-0.0.1.tar.gz

---
+++ packages/googletest/googletest-0.0.1.tar.gz/googletest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="googletest",
+    version="0.0.1",
+    packages=["googletest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

0d88dbf21eb2702c98edcffd11443987dc30dad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:43 +0000

    Add package iris-interop/0.0.1/iris_interop-0.0.1.tar.gz

---
+++ packages/iris-interop/iris_interop-0.0.1.tar.gz/iris_interop-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iris_interop",       
+    version="0.0.1",           
+    packages=find_packages(),  
+    python_requires=">=3.7",
+)
+

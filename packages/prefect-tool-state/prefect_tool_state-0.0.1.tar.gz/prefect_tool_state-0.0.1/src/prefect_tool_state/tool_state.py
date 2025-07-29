f3445cd23afb328872093dfe9ce8a5dc43b84ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:34 +0000

    Add package prefect-tool-state/0.0.1/prefect_tool_state-0.0.1.tar.gz

---
+++ packages/prefect-tool-state/prefect_tool_state-0.0.1.tar.gz/prefect_tool_state-0.0.1/src/prefect_tool_state/tool_state.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+import os
+import datetime
+
+@dataclass
+class ToolState:
+    type: str = "ToolState"
+    flow_run_id: str = field(default_factory=lambda: os.environ.get("PREFECT__FLOW_RUN_ID", ""))
+    tool_name: str = ""
+    step_name: str = ""
+    message: str = ""
+    total_files: int = 0
+    processed_files: int = 0
+    current_file_name: str  = ""         
+    timestamp: str = field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc).isoformat())

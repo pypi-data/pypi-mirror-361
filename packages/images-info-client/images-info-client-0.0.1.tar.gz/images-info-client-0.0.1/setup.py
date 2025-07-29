25a7149571c121c9b473238f198b1456e4f8f16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:21 +0000

    Add package images-info-client/0.0.1/images-info-client-0.0.1.tar.gz

---
+++ packages/images-info-client/images-info-client-0.0.1.tar.gz/images-info-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="images-info-client",
+    version="0.0.1",
+    packages=["images-info-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

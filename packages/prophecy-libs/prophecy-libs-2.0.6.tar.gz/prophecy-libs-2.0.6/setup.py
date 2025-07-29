f989fc57c356a1a8333ed88bfd9a318f262ba9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:35 +0000

    Add package prophecy-libs/2.0.6/prophecy-libs-2.0.6.tar.gz

---
+++ packages/prophecy-libs/prophecy-libs-2.0.6.tar.gz/prophecy-libs-2.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prophecy-libs",
+    version="2.0.6",
+    url="https://github.com/SimpleDataLabsInc/prophecy-python-libs",
+    packages=find_packages(exclude=["test.*", "test"]),
+    description="Helper library for prophecy generated code",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "pyhocon>=0.3.59",
+        "requests>=2.10.0",
+        "hvac==2.3.0",
+        "zstandard>=0.23.0",
+        "msgspec>=0.18.6",
+    ],
+    keywords=["python", "prophecy"],
+    classifiers=[],
+    zip_safe=False,
+    license="GPL-3.0",
+)

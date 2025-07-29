5a1e5e65641146e0cb948f78eed89694ed74e192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:04 +0000

    Add package ad-rss/5.0.0/ad_rss-5.0.0-cp310-cp310-manylinux_2_34_x86_64.manylinux_2_35_x86_64.whl

---
+++ packages/ad-rss/ad_rss-5.0.0-cp310-cp310-manylinux_2_34_x86_64.manylinux_2_35_x86_64.whl/ad_rss/__init__.py
@@ -0,0 +1,18 @@
+# ----------------- BEGIN LICENSE BLOCK ---------------------------------
+#
+# Copyright (c) 2020-2021 Intel Corporation
+#
+# SPDX-License-Identifier: LGPL-2.1-only
+#
+# ----------------- END LICENSE BLOCK -----------------------------------
+
+"""
+Import ad_rss module for python310
+"""
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(__file__))
+
+from ad_physics import *
+from ad_rss_python310 import *

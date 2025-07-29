6e0b4245e41c6e574d1a75d4581b429d9343257c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:30 +0000

    Add package tarantool-snap-pb2/0.0.1/tarantool-snap-pb2-0.0.1.tar.gz

---
+++ packages/tarantool-snap-pb2/tarantool-snap-pb2-0.0.1.tar.gz/tarantool-snap-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tarantool-snap-pb2",
+    version="0.0.1",
+    packages=["tarantool-snap-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

56c27fd4e0abe7e9f618327998b941623178635e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:55 +0000

    Add package bytelogger/2.0.0/bytelogger-2.0.0-py3-none-any.whl

---
+++ packages/bytelogger/bytelogger-2.0.0-py3-none-any.whl/bytelogger/__init__.py
@@ -0,0 +1,12 @@
+from better_logger import debug, dev_debug, info, warn, error, set_debug_mode
+from get_logs import start_logging
+
+__all__ = ["init", "debug", "dev_debug", "info", "warn", "error"]
+
+def init(debug_mode=True, log_mode=True):
+    if debug_mode:
+        set_debug_mode(True)
+    if log_mode:
+        start_logging()
+
+init(debug_mode=True, log_mode=True)

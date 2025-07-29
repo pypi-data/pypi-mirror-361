7dc416973ee36710bfc4cb7b9dbf3c0f1150d247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:25 +0000

    Add package pinned-ranking/0.0.1/pinned-ranking-0.0.1.tar.gz

---
+++ packages/pinned-ranking/pinned-ranking-0.0.1.tar.gz/pinned-ranking-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pinned-ranking",
+    version="0.0.1",
+    packages=["pinned-ranking"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

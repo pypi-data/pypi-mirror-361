5d73eb8543eb9d3669c21b617453b900bffca45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:02 +0000

    Add package open-dataflow-adp/1.0.3/open_dataflow_adp-1.0.3-py3-none-any.whl

---
+++ packages/open-dataflow-adp/open_dataflow_adp-1.0.3-py3-none-any.whl/dataflow/operators/eval/GeneralText/__init__.py
@@ -0,0 +1,37 @@
+from .statistics.ngram_scorer import NgramScorer
+from .statistics.lexical_diversity_scorer import LexicalDiversityScorer
+from .statistics.langkit_scorer import LangkitScorer
+
+from .models.deita_quality_scorer import DeitaQualityScorer
+from .models.instag_scorer import InstagScorer
+from .models.debertav3_scorer import DebertaV3Scorer
+from .models.deita_complexity_scorer import DeitaComplexityScorer
+from .models.fineweb_edu_scorer import FineWebEduScorer
+from .models.pair_qual_scorer import PairQualScorer
+# from .models.presidio_scorer import PresidioScorer
+# from .models.rm_scorer import RMScorer
+from .models.textbook_scorer import TextbookScorer
+from .models.superfiltering_scorer import SuperfilteringScorer
+from .models.qurating_scorer import QuratingScorer
+# from .models.perplexity_scorer import PerplexityScorer
+
+from .APIcaller.alpagasus_scorer import AlpagasusScorer
+
+__all__ = [
+    'NgramScorer',
+    'LexicalDiversityScorer',
+    'LangkitScorer',
+    'DeitaQualityScorer',
+    'InstagScorer',
+    'DebertaV3Scorer',
+    'DeitaComplexityScorer',
+    'FineWebEduScorer',
+    'PairQualScorer',
+    # 'PresidioScorer',
+    # 'RMScorer',
+    'TextbookScorer',
+    'SuperfilteringScorer',
+    'QuratingScorer',
+    # 'PerplexityScorer'
+    'AlpagasusScorer'
+]
---
+++ packages/open-dataflow-adp/open_dataflow_adp-1.0.3-py3-none-any.whl/dataflow/operators/refine/GeneralText/__init__.py
@@ -0,0 +1,9 @@
+from .html_remove_refiner import HtmlUrlRemoverRefiner
+from .remove_extra_spaces_refiner import RemoveExtraSpacesRefiner
+from .remove_emoji_refiner import RemoveEmojiRefiner
+
+__all__ = [
+    "HtmlUrlRemoverRefiner",
+    "RemoveExtraSpacesRefiner",
+    "RemoveEmojiRefiner"
+]

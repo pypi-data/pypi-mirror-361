dc83600b83efcebf0e51044dcdd4084ad1e4044f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:21 +0000

    Add package polars-extensions/0.6.0/polars_extensions-0.6.0.tar.gz

---
+++ packages/polars-extensions/polars_extensions-0.6.0.tar.gz/polars_extensions-0.6.0/polars_extensions/__init__.py
@@ -0,0 +1,12 @@
+from .io import *
+from .name import *
+from .numeric import *
+from .string import *
+
+__all__ = [
+    "NameExtensionNameSpace",
+    "NumericExtensionNamespace",
+    "StringExtensionNamespace",
+    "write_schema",
+    "read_schema",
+]

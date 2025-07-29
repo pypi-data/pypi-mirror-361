09d65ce56a4470aea81055d8caf1d08cae8d0e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:59 +0000

    Add package hexadruid/0.1.6/hexadruid-0.1.6.tar.gz

---
+++ packages/hexadruid/hexadruid-0.1.6.tar.gz/hexadruid-0.1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hexadruid",
+    version="0.1.6", 
+    author="Omar Attia",
+    description="Smart Spark Optimizer: Skew Rebalancer + Key Detector + DRTree",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OmarAttia95/hexadruid",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'hexadruid': ['_core.cpython-311.pyc']},
+    python_requires=">=3.8",
+    install_requires=[
+        "pyspark>=3.5.1",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

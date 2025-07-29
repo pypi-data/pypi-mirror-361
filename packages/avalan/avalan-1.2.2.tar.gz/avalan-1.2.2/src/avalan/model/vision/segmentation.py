7bc8ef370f2db146ea7a3a36427f10b5501b9762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:46 +0000

    Add package avalan/1.2.2/avalan-1.2.2.tar.gz

---
+++ packages/avalan/avalan-1.2.2.tar.gz/avalan-1.2.2/src/avalan/model/vision/segmentation.py
@@ -0,0 +1,45 @@
+from ...compat import override
+from ...model import TextGenerationVendor
+from ...model.vision import BaseVisionModel
+from ...model.engine import Engine
+from PIL import Image
+from torch import unique
+from transformers import (
+    AutoImageProcessor,
+    AutoModelForSemanticSegmentation,
+    PreTrainedModel,
+)
+from typing import Literal
+
+
+class SemanticSegmentationModel(BaseVisionModel):
+    def _load_model(self) -> PreTrainedModel | TextGenerationVendor:
+        self._processor = AutoImageProcessor.from_pretrained(
+            self._model_id,
+            # default behavior in transformers v4.48
+            use_fast=True,
+        )
+        model = AutoModelForSemanticSegmentation.from_pretrained(
+            self._model_id,
+            device_map=self._device,
+            tp_plan=Engine._get_tp_plan(self._settings.parallel),
+        )
+        model.eval()
+        return model
+
+    @override
+    async def __call__(
+        self,
+        image_source: str | Image.Image,
+        tensor_format: Literal["pt"] = "pt",
+    ) -> list[str]:
+        image = BaseVisionModel._get_image(image_source)
+        inputs = self._processor(images=image, return_tensors=tensor_format)
+        logits = self._model(**inputs).logits
+        # shape (height, width) with class indices
+        mask = logits.argmax(dim=1)[0]
+        labels_tensor = unique(mask)
+        labels = [
+            self._model.config.id2label[idx.item()] for idx in labels_tensor
+        ]
+        return labels

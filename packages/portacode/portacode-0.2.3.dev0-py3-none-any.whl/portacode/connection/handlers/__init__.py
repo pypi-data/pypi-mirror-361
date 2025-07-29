be0555048873c3b87412e470447d89c2f5f6aa34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:35 +0000

    Add package portacode/0.2.3.dev0/portacode-0.2.3.dev0-py3-none-any.whl

---
+++ packages/portacode/portacode-0.2.3.dev0-py3-none-any.whl/portacode/connection/handlers/__init__.py
@@ -0,0 +1,28 @@
+"""Modular command handler system for Portacode client.
+
+This package provides a flexible system for handling commands from the gateway.
+Handlers can be easily added, removed, or modified without touching the main
+terminal manager code.
+"""
+
+from .base import BaseHandler, AsyncHandler, SyncHandler
+from .registry import CommandRegistry
+from .terminal_handlers import (
+    TerminalStartHandler,
+    TerminalSendHandler,
+    TerminalStopHandler,
+    TerminalListHandler,
+)
+from .system_handlers import SystemInfoHandler
+
+__all__ = [
+    "BaseHandler",
+    "AsyncHandler", 
+    "SyncHandler",
+    "CommandRegistry",
+    "TerminalStartHandler",
+    "TerminalSendHandler",
+    "TerminalStopHandler",
+    "TerminalListHandler",
+    "SystemInfoHandler",
+] 

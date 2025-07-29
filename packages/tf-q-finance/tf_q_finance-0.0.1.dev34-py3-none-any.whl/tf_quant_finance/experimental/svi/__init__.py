afc80b7c9ae3c6abf890185ac0382b883504b95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:04 +0000

    Add package tf-q-finance/0.0.1.dev34/tf_q_finance-0.0.1.dev34-py3-none-any.whl

---
+++ packages/tf-q-finance/tf_q_finance-0.0.1.dev34-py3-none-any.whl/tf_quant_finance/experimental/svi/__init__.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""SVI model."""
+
+from tf_quant_finance.experimental.svi.calibration import calibration
+from tf_quant_finance.experimental.svi.parameterizations import (
+    implied_volatility_from_raw_svi_parameters,
+)
+from tf_quant_finance.experimental.svi.parameterizations import (
+    total_variance_from_raw_svi_parameters,
+)
+
+from tensorflow.python.util.all_util import (
+    remove_undocumented,
+)  # pylint: disable=g-direct-tensorflow-import
+
+# pyformat: disable
+_allowed_symbols = [
+    "calibration",
+    "total_variance_from_raw_svi_parameters",
+    "implied_volatility_from_raw_svi_parameters",
+]
+# pyformat: enable
+
+remove_undocumented(__name__, _allowed_symbols)
---
+++ packages/tf-q-finance/tf_q_finance-0.0.1.dev34-py3-none-any.whl/tf_quant_finance/models/sabr/approximations/__init__.py
@@ -0,0 +1,41 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Approximate analytic solutions to the Sabr model."""
+
+from tf_quant_finance.models.sabr.approximations.calibration import calibration
+from tf_quant_finance.models.sabr.approximations.european_options import (
+    option_price as european_option_price,
+)
+from tf_quant_finance.models.sabr.approximations.implied_volatility import (
+    implied_volatility,
+)
+from tf_quant_finance.models.sabr.approximations.implied_volatility import (
+    SabrApproximationType,
+)
+from tf_quant_finance.models.sabr.approximations.implied_volatility import (
+    SabrImpliedVolatilityType,
+)
+from tensorflow.python.util.all_util import (
+    remove_undocumented,
+)  # pylint: disable=g-direct-tensorflow-import
+
+_allowed_symbols = [
+    "calibration",
+    "european_option_price",
+    "implied_volatility",
+    "SabrApproximationType",
+    "SabrImpliedVolatilityType",
+]
+
+remove_undocumented(__name__, _allowed_symbols)

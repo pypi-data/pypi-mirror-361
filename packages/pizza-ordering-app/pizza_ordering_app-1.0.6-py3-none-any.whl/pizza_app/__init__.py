edc428c177059703fca6dd07271089c17aa87743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:19 +0000

    Add package pizza-ordering-app/1.0.6/pizza_ordering_app-1.0.6-py3-none-any.whl

---
+++ packages/pizza-ordering-app/pizza_ordering_app-1.0.6-py3-none-any.whl/pizza_app/__init__.py
@@ -0,0 +1,11 @@
+"""
+Pizza Ordering App - A KivyMD application for ordering pizzas
+"""
+
+__version__ = "1.0.2"
+__author__ = "Alikhan Abdykaimov"
+__email__ = "2026.alikhan.abdykaimov@uwcisak.jp"
+
+from .main import MainApp
+
+__all__ = ["MainApp"]
---
+++ packages/pizza-ordering-app/pizza_ordering_app-1.0.6-py3-none-any.whl/pizza_app/secure_password.py
@@ -0,0 +1,16 @@
+from passlib.context import CryptContext
+
+#Create an object of the class cryptocontext
+pwd_config = CryptContext(schemes=["pbkdf2_sha256"],
+                          default="pbkdf2_sha256",
+                          pbkdf2_sha256__default_rounds=30000
+                          )
+
+#this function receives the unsafe password
+# and returns the hashed password
+
+def encrypt_password(user_password):
+    return pwd_config.hash(user_password)
+
+def check_password(hashed_password, user_password):
+    return pwd_config.verify(user_password, hashed_password)

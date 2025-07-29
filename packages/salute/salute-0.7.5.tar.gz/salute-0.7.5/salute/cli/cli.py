4291855b8fc29fa17287d10f18d61d87104191e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:35 +0000

    Add package salute/0.7.5/salute-0.7.5.tar.gz

---
+++ packages/salute/salute-0.7.5.tar.gz/salute-0.7.5/salute/cli/cli.py
@@ -0,0 +1,28 @@
+import argparse
+from salute.cli.actions import InitScenarioAction
+
+
+def command():
+    parser = argparse.ArgumentParser(
+        description="Salute CLI tool for managing your application"
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s 0.7.5",
+        help="Show program's version number and exit",
+    )
+    parser.add_argument(
+        "-i",
+        "--init",
+        action=InitScenarioAction,
+        help="Execute init scenario to prepare your application",
+        nargs=0,
+    )
+
+    args = parser.parse_args()
+
+    if not any(vars(args).values()):
+        parser.print_help()
+        return

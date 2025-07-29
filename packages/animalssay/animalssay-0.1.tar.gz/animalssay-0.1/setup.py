b3294a8cb7a157c5fdc43e2d9979d800f1739db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:14 +0000

    Add package animalssay/0.1/animalssay-0.1.tar.gz

---
+++ packages/animalssay/animalssay-0.1.tar.gz/animalssay-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="animalssay",
+    version="0.1",
+    packages=find_packages(),
+    author="hunter",
+    description="A simple example Python library where animals can say words",
+)

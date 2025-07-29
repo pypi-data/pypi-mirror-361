e3326831ac81ecb9ed71a6e4b25af446e1e9e0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:06 +0000

    Add package windowgpt/0.1.6/windowgpt-0.1.6.tar.gz

---
+++ packages/windowgpt/windowgpt-0.1.6.tar.gz/windowgpt-0.1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='windowgpt',
+    version='0.1.6',
+    description='Send a screenshot to ChatGPT Vision with a prompt',
+    author='Ed Atkinson',
+    url='https://github.com/yourusername/windowgpt',
+    author_email='ed.atkinson10@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pyautogui',
+        'pillow',
+        'openai',
+    ],
+    entry_points={
+        'console_scripts': [
+            'windowgpt=windowgpt.cli:main',
+        ],
+    },
+    python_requires='>=3.7',
+)

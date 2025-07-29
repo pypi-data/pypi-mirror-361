5a600d18e800d7899ec7d31c619336798850ffb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:32 +0000

    Add package sskw-tasks/0.3.0/sskw_tasks-0.3.0-py3-none-any.whl

---
+++ packages/sskw-tasks/sskw_tasks-0.3.0-py3-none-any.whl/sskw/tasks/all.py
@@ -0,0 +1,13 @@
+from . import audio_transcription_sensevoice_cpu
+from . import audio_transcription_sensevoice_gpu
+from . import audio_transcription_paraformer_cpu
+from . import audio_transcription_paraformer_gpu
+from . import audio_format_conversion   
+
+__all__ = [
+    'audio_transcription_sensevoice_cpu',
+    'audio_transcription_sensevoice_gpu',
+    'audio_transcription_paraformer_cpu',
+    'audio_transcription_paraformer_gpu',
+    'audio_format_conversion',
+]   

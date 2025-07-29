8856efafd89579ce28bd79d443ad26e9bc06f645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:46 +0000

    Add package eelsmapper/0.2.4/eelsmapper-0.2.4.tar.gz

---
+++ packages/eelsmapper/eelsmapper-0.2.4.tar.gz/eelsmapper-0.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read long description from README.md
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='eelsmapper',
+    version='0.2.4',
+    description='Data-driven analysis pipeline for STEM-EELS spectra. See project at [https://zhenyuan992.github.io/eelsmapper](https://zhenyuan992.github.io/eelsmapper)',
+    author='Yeo Zhen Yuan',
+    author_email='yeozy@nus.edu.sg',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'matplotlib',
+        'umap-learn'
+    ],
+    python_requires='>=3.8',
+)

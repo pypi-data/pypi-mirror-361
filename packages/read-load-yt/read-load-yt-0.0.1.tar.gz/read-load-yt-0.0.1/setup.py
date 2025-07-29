51710f8b146bbb8a10a59c671c3fd02895ba8807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:30 +0000

    Add package read-load-yt/0.0.1/read-load-yt-0.0.1.tar.gz

---
+++ packages/read-load-yt/read-load-yt-0.0.1.tar.gz/read-load-yt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="read-load-yt",
+    version="0.0.1",
+    packages=["read-load-yt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

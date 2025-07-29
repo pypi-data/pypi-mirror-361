88c96af87c84e9cc3ea681663c58874172349536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:11 +0000

    Add package triplets/0.0.11/triplets-0.0.11.tar.gz

---
+++ packages/triplets/triplets-0.0.11.tar.gz/triplets-0.0.11/triplets/__init__.py
@@ -0,0 +1,6 @@
+from triplets import rdf_parser
+from triplets import cgmes_tools
+#from triplets import rdfs_tools
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

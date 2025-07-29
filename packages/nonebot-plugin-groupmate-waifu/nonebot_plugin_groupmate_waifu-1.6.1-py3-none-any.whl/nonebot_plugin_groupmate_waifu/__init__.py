1911ce2b8b83d5fa9fe9e4716fc575a951be9519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:09 +0000

    Add package nonebot-plugin-groupmate-waifu/1.6.1/nonebot_plugin_groupmate_waifu-1.6.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-groupmate-waifu/nonebot_plugin_groupmate_waifu-1.6.1-py3-none-any.whl/nonebot_plugin_groupmate_waifu/__init__.py
@@ -0,0 +1,23 @@
+from nonebot import require, get_plugin_config
+from nonebot.plugin import PluginMetadata
+from clovers.config import Config as CloversConfig
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="娶群友",
+    description="娶一个群友做老婆",
+    usage="娶群友，透群友",
+    type="application",
+    config=Config,
+    homepage="https://github.com/KarisAya/nonebot_plugin_groupmate_waifu",
+    supported_adapters=None,
+)
+
+IMPORT_NAME = "clovers_groupmate_waifu"
+PREFIX_LENGTH = len("groupmate_waifu_")
+CloversConfig.environ()[IMPORT_NAME] = {k[PREFIX_LENGTH:].lower(): v for k, v in get_plugin_config(Config).model_dump().items()}
+require("nonebot_plugin_clovers")
+from nonebot_plugin_clovers import client
+
+client.load_plugin(IMPORT_NAME)
+client.load_plugin("clovers_apscheduler")

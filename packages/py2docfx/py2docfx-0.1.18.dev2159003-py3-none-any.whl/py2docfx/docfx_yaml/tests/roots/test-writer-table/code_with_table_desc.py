a735fc56317f63a527ebad892ea22d8808e06755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:46 +0000

    Add package py2docfx/0.1.18.dev2159003/py2docfx-0.1.18.dev2159003-py3-none-any.whl

---
+++ packages/py2docfx/py2docfx-0.1.18.dev2159003-py3-none-any.whl/py2docfx/docfx_yaml/tests/roots/test-writer-table/code_with_table_desc.py
@@ -0,0 +1,24 @@
+
+class SampleClass():
+    """
+    :param dummy_param:
+        Dummy Param
+
+        Table:
+
+        +---------+---------+---------+---------+
+        | header1 | header2 | header3 | header4 |
+        +=========+=========+=========+=========+
+        | a       | b       | c       | d       |
+        +---------+---------+---------+---------+
+        | e       | f       | g       | h       |
+        +---------+---------+---------+---------+
+    """
+    def dummy_param(self):
+        """
+            Dummy Param
+        """
+        pass
+
+    pass
+    

f5d349e2f2a1180ba16f2bff1603de27b044bdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:07 +0000

    Add package ProstoEnkiClient/0.0.4/ProstoEnkiClient-0.0.4.tar.gz

---
+++ packages/ProstoEnkiClient/ProstoEnkiClient-0.0.4.tar.gz/ProstoEnkiClient-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoEnkiClient",
+    version="0.0.4",
+    packages=["ProstoEnkiClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

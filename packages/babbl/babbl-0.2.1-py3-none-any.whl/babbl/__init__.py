5064d0e8d1e587afe8a7534bf3d9aacd065494b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:29 +0000

    Add package babbl/0.2.1/babbl-0.2.1-py3-none-any.whl

---
+++ packages/babbl/babbl-0.2.1-py3-none-any.whl/babbl/__init__.py
@@ -0,0 +1,8 @@
+"""Babbl: turn markdown into beautiful research blog posts."""
+
+__version__ = "0.1.0"
+__author__ = "Michael Lutz"
+
+from babbl.code_ref import CodeReferenceProcessor
+from babbl.parser import BabblParser
+from babbl.renderer import HTMLRenderer

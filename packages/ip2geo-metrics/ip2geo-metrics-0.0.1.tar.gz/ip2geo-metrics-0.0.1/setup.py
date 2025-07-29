70a23d075954d1fd85e168c33dc740909ed24d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:22 +0000

    Add package ip2geo-metrics/0.0.1/ip2geo-metrics-0.0.1.tar.gz

---
+++ packages/ip2geo-metrics/ip2geo-metrics-0.0.1.tar.gz/ip2geo-metrics-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ip2geo-metrics",
+    version="0.0.1",
+    packages=["ip2geo-metrics"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

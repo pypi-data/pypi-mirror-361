135b34ecfca385106db2b21deaa5cf3f777cea0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:21 +0000

    Add package shipxy-src/0.1/shipxy_src-0.1.tar.gz

---
+++ packages/shipxy-src/shipxy_src-0.1.tar.gz/shipxy_src-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='shipxy-src',
+    version='0.1',
+    packages=find_packages(where="src"),
+    install_requires=[
+        'requests'
+    ],
+    author="White",
+    author_email="249898979@qq.com",
+    description="亿海蓝Elane船讯网sdk https://www.shipxy.com/",
+    long_description=long_description,               # 详细说明
+    long_description_content_type="text/markdown",
+)

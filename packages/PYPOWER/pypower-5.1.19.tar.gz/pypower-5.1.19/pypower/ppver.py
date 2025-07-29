13f7737ca174edb1399affa1bc55ae7cac708fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:58 +0000

    Add package PYPOWER/5.1.19/pypower-5.1.19.tar.gz

---
+++ packages/PYPOWER/pypower-5.1.19.tar.gz/pypower-5.1.19/pypower/ppver.py
@@ -0,0 +1,19 @@
+# Copyright (c) 1996-2015 PSERC. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+"""PYPOWER version info.
+"""
+
+def ppver(*args):
+    """ Returns PYPOWER version info for current installation.
+
+    @author: Ray Zimmerman (PSERC Cornell)
+    """
+
+    ver = {'Name': 'PYPOWER',
+           'Version': '5.1.18',
+           'Release':  '',
+           'Date': '10-Apr-2025'}
+
+    return ver

8a7b27db20552589d6c8649bc32e90c40cc0adfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:30 +0000

    Add package simplicial-attention/0.0.12/simplicial_attention-0.0.12.tar.gz

---
+++ packages/simplicial-attention/simplicial_attention-0.0.12.tar.gz/simplicial_attention-0.0.12/simplicial_attention/__init__.py
@@ -0,0 +1,10 @@
+from simplicial_attention.simplicial_attention import (
+    naive_two_simplicial_attend,
+    nth_order_attend,
+    signed_determinant
+)
+
+from simplicial_attention.simplicial_mha import (
+    TwoSimplicialMHA,
+    HigherOrderAttention
+)

7ea2303fc3d8703115f3fbf56f935d103482ce9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:05 +0000

    Add package mxlpy/0.23.0/mxlpy-0.23.0-py3-none-any.whl

---
+++ packages/mxlpy/mxlpy-0.23.0-py3-none-any.whl/mxlpy/meta/__init__.py
@@ -0,0 +1,15 @@
+"""Metaprogramming facilities."""
+
+from __future__ import annotations
+
+from .codegen_latex import generate_latex_code, to_tex_export
+from .codegen_model import generate_model_code_py, generate_model_code_rs
+from .codegen_mxlpy import generate_mxlpy_code
+
+__all__ = [
+    "generate_latex_code",
+    "generate_model_code_py",
+    "generate_model_code_rs",
+    "generate_mxlpy_code",
+    "to_tex_export",
+]

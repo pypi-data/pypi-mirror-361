8b98133d6fbc3d0c19003365a823147f432dc0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:30 +0000

    Add package workflow-runner-sentence-transformers-client/0.1.0/workflow_runner_sentence_transformers_client-0.1.0.tar.gz

---
+++ packages/workflow-runner-sentence-transformers-client/workflow_runner_sentence_transformers_client-0.1.0.tar.gz/workflow_runner_sentence_transformers_client-0.1.0/workflow_runner_sentence_transformers_client/__init__.py
@@ -0,0 +1,8 @@
+"""A client library for accessing Workflow Runner Sentence Transformers"""
+
+from .client import AuthenticatedClient, Client
+
+__all__ = (
+    "AuthenticatedClient",
+    "Client",
+)
---
+++ packages/workflow-runner-sentence-transformers-client/workflow_runner_sentence_transformers_client-0.1.0.tar.gz/workflow_runner_sentence_transformers_client-0.1.0/workflow_runner_sentence_transformers_client/models/__init__.py
@@ -0,0 +1,5 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .root_get_response_root_get import RootGetResponseRootGet
+
+__all__ = ("RootGetResponseRootGet",)

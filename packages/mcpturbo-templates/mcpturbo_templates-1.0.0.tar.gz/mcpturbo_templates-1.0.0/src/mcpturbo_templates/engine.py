24e4fcda4ccb5ef93fa0558a6d3a703d8f1f75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-templates/1.0.0/mcpturbo_templates-1.0.0.tar.gz

---
+++ packages/mcpturbo-templates/mcpturbo_templates-1.0.0.tar.gz/mcpturbo_templates-1.0.0/src/mcpturbo_templates/engine.py
@@ -0,0 +1,12 @@
+"""Template engine implementation"""
+
+class TemplateEngine:
+    """Template rendering engine for code generation"""
+    
+    def __init__(self):
+        self.templates = {}
+        
+    async def render_template(self, template_name, context):
+        """Render template with given context"""
+        # Implementation will be added
+        pass

3e3322d72d54150cdaf147a528bd6b6d6fc82642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:42 +0000

    Add package spark8t/1.0.0/spark8t-1.0.0-py3-none-any.whl

---
+++ packages/spark8t/spark8t-1.0.0-py3-none-any.whl/spark8t/literals.py
@@ -0,0 +1,6 @@
+"""Literals and constant module."""
+
+MANAGED_BY_LABELNAME = "app.kubernetes.io/managed-by"
+PRIMARY_LABELNAME = "app.kubernetes.io/spark8t-primary"
+SPARK8S_LABEL = "spark8t"
+HUB_LABEL = "integrator-hub-conf"  # TODO revert this label to `integration-hub-conf``

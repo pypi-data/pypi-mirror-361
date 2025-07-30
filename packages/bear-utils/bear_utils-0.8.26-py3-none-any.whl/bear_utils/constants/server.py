a2bc69ec02aa77fcaf0c7e086d0c61dbf83e55a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:02 +0000

    Add package bear-utils/0.8.26/bear_utils-0.8.26-py3-none-any.whl

---
+++ packages/bear-utils/bear_utils-0.8.26-py3-none-any.whl/bear_utils/constants/server.py
@@ -0,0 +1,16 @@
+"""HTTP status codes."""
+
+SERVER_ERROR = 500
+"""Internal Server Error"""
+SERVER_OK = 200
+"""OK"""
+PAGE_NOT_FOUND = 404
+"""Not Found"""
+BAD_REQUEST = 400
+"""Bad Request"""
+UNAUTHORIZED = 401
+"""Unauthorized"""
+FORBIDDEN = 403
+"""Forbidden"""
+CONFLICT = 409
+"""Conflict"""

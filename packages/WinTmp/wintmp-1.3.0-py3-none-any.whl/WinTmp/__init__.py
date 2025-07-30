7e0f12e10373b6770d5ae21ba24e4ff479fa48b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:08:06 +0000

    Add package WinTmp/1.3.0/wintmp-1.3.0-py3-none-any.whl

---
+++ packages/WinTmp/wintmp-1.3.0-py3-none-any.whl/WinTmp/__init__.py
@@ -0,0 +1,103 @@
+import clr
+import os
+
+if os.path.exists(os.path.join(os.path.abspath(__file__), "_version.py")):
+    from WinTmp._version import __version__
+else:
+    __version__ = "0.0.0-dev"
+
+clr.AddReference(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "LibreHardwareMonitorLib.dll"
+    )
+)
+from LibreHardwareMonitor import Hardware
+
+hw = Hardware.Computer()
+hw.IsCpuEnabled = hw.IsGpuEnabled = hw.IsMemoryEnabled = hw.IsMotherboardEnabled = (
+    hw.IsStorageEnabled
+) = True
+hw.Open()
+
+GPU_SENSORS = (
+    Hardware.HardwareType.GpuNvidia,
+    Hardware.HardwareType.GpuAmd,
+    Hardware.HardwareType.GpuIntel,
+)
+
+
+def GPU_Temp():
+    for h in hw.Hardware:
+        h.Update()
+
+        if h.HardwareType in GPU_SENSORS:
+            for sensor in h.Sensors:
+                if (
+                    sensor.SensorType == Hardware.SensorType.Temperature
+                    and "GPU Core" in sensor.Name
+                ):
+                    return sensor.Value
+
+
+def CPU_Temp():
+    for h in hw.Hardware:
+        h.Update()
+
+        if h.HardwareType == Hardware.HardwareType.Cpu:
+            for sensor in h.Sensors:
+                if sensor.SensorType == Hardware.SensorType.Temperature:
+                    return sensor.Value
+
+
+def GPU_Temps():
+    temps = []
+    for h in hw.Hardware:
+        h.Update()
+
+        if h.HardwareType in GPU_SENSORS:
+            for sensor in h.Sensors:
+                if (
+                    sensor.SensorType == Hardware.SensorType.Temperature
+                    and "GPU Core" in sensor.Name
+                ):
+                    temps.append(sensor.Value)
+    return temps
+
+
+def CPU_Temps():
+    temps = []
+    for h in hw.Hardware:
+        h.Update()
+
+        if h.HardwareType == Hardware.HardwareType.Cpu:
+            for sensor in h.Sensors:
+                if sensor.SensorType == Hardware.SensorType.Temperature:
+                    temps.append(sensor.Value)
+    return temps
+
+
+def _all_temps():
+    temps = {}
+    for h in hw.Hardware:
+        h.Update()
+        for sensor in h.Sensors:
+            if sensor.SensorType == Hardware.SensorType.Temperature:
+                key = f"{h.HardwareType}_{sensor.SensorType}"
+                if key in temps.keys():
+                    temps[key].append(sensor.Value)
+                else:
+                    temps[key] = [sensor.Value]
+    return temps
+
+
+def _all_sensor_data():
+    temps = {}
+    for h in hw.Hardware:
+        h.Update()
+        for sensor in h.Sensors:
+            key = f"{h.HardwareType}_{sensor.SensorType}"
+            if key in temps.keys():
+                temps[key].append(sensor.Value)
+            else:
+                temps[key] = [sensor.Value]
+    return temps

69d90d51a12b0cbd2eac8cb1bd05f9bbe6488658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:39 +0000

    Add package mcpturbo-complete/1.0.0/mcpturbo_complete-1.0.0.tar.gz

---
+++ packages/mcpturbo-complete/mcpturbo_complete-1.0.0.tar.gz/mcpturbo_complete-1.0.0/src/mcpturbo_complete/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Complete
+
+MCPTurbo - Complete Installation (All Components)
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

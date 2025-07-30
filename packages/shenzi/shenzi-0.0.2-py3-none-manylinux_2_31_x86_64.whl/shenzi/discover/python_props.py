b9839b800c6b3dc0dcf2f8a754e242a3f12e7460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:52 +0000

    Add package shenzi/0.0.2/shenzi-0.0.2-py3-none-manylinux_2_31_x86_64.whl

---
+++ packages/shenzi/shenzi-0.0.2-py3-none-manylinux_2_31_x86_64.whl/shenzi/discover/python_props.py
@@ -0,0 +1,24 @@
+import sys
+from shenzi.discover.types import Python, Sys, Version
+from pathlib import Path
+
+
+def get_python_props() -> Python:
+    abi_thread = "t" if hasattr(sys, "abiflags") and "t" in sys.abiflags else ""
+    
+
+    return Python(
+        sys=Sys(
+            prefix=sys.prefix,
+            exec_prefix=sys.exec_prefix,
+            platlibdir=sys.platlibdir,
+            version=Version(
+                major=sys.version_info.major,
+                minor=sys.version_info.minor,
+                abi_thread=abi_thread,
+            ),
+            path=sys.path,
+            executable=sys.executable,
+        ),
+        main=str(Path(sys.argv[0]).resolve()),
+    )

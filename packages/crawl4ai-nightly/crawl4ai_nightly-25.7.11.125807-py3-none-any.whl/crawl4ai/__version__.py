aa7e2b36d73fdc6db9db7a24bdd2bec6d52dfdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:59 +0000

    Add package crawl4ai-nightly/25.7.11.125807/crawl4ai_nightly-25.7.11.125807-py3-none-any.whl

---
+++ packages/crawl4ai-nightly/crawl4ai_nightly-25.7.11.125807-py3-none-any.whl/crawl4ai/__version__.py
@@ -0,0 +1,8 @@
+# crawl4ai/__version__.py
+
+# This is the version that will be used for stable releases
+__version__ = "0.6.3"
+
+# For nightly builds, this gets set during build process
+__nightly_version__ = "25.7.11.125807"
+

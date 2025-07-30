13ed22f5fd0a5fe7a70e932cb626f0cf3626bd8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:37 +0000

    Add package pyosis/0.2.1/pyosis-0.2.1-py3-none-any.whl

---
+++ packages/pyosis/pyosis-0.2.1-py3-none-any.whl/pyosis/__init__.py
@@ -0,0 +1,9 @@
+"""Python tools for serializing and using OSIS files."""
+
+__version__ = "0.2.1"
+
+# ruff: noqa: F403
+
+from pyosis import generated as generated
+from pyosis.generated import *
+from pyosis.serialization import OsisXML as OsisXML

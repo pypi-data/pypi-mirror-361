0310ca020b360553a4a73e786f53d21f2a456b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:56 +0000

    Add package alignment-tools/0.1.0/alignment_tools-0.1.0.tar.gz

---
+++ packages/alignment-tools/alignment_tools-0.1.0.tar.gz/alignment_tools-0.1.0/alignment_tools/__init__.py
@@ -0,0 +1,11 @@
+# biofoldutils/__init__.py
+from .core import (
+           get_min_max_residueIDs_from_reference_residue,
+           PDB2dataFrame,
+           calculate_distance_from_ref_point,
+           find_max_tuple,
+           find_min_tuple,
+           download_alphafold_pdb_from_uniprot_id
+                                    # Add more as needed
+                                    )
+

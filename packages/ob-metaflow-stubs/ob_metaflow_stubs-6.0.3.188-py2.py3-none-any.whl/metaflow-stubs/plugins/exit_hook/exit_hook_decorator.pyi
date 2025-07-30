928dda71e00e640fdbb61efa2d7fe23ec1d90d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:20 +0000

    Add package ob-metaflow-stubs/6.0.3.188/ob_metaflow_stubs-6.0.3.188-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.188-py2.py3-none-any.whl/metaflow-stubs/plugins/exit_hook/exit_hook_decorator.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-11T16:11:55.094005                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+from ...exception import MetaflowException as MetaflowException
+
+class ExitHookDecorator(metaflow.decorators.FlowDecorator, metaclass=type):
+    def flow_init(self, flow, graph, environment, flow_datastore, metadata, logger, echo, options):
+        ...
+    ...
+

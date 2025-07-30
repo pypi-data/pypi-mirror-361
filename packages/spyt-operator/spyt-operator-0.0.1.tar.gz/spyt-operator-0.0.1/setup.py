7958aaf997fd45d6a874945c5dcb3331a0b73e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package spyt-operator/0.0.1/spyt-operator-0.0.1.tar.gz

---
+++ packages/spyt-operator/spyt-operator-0.0.1.tar.gz/spyt-operator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spyt-operator",
+    version="0.0.1",
+    packages=["spyt-operator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

beff1f20335e65575c1377dfc5f2df5a53663225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:52 +0000

    Add package annkit/0.1.5/annkit-0.1.5.tar.gz

---
+++ packages/annkit/annkit-0.1.5.tar.gz/annkit-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="annkit",
+    version="0.1.5",
+    author="swapnendu31",
+    author_email="letswapnendu@gmail.com",
+    description="A Python-based toolkit designed to help users explore, understand, and visualize the inner workings of artificial neural networks.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/swapnendu31/ANN-learner.git",  # <-- change to your GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "networkx",
+        "plotly",
+        "scikit-learn"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.7",
+)

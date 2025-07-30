8d34e7d3c8a7a45c353403fefbdf4deab20ed76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:38 +0000

    Add package animalssay/0.2/animalssay-0.2-py3-none-any.whl

---
+++ packages/animalssay/animalssay-0.2-py3-none-any.whl/animalssay/__init__.py
@@ -0,0 +1,18 @@
+from .animals import (
+    cat_says,
+    dog_says,
+    frog_says,
+    fox_says,
+    cow_says,
+    pig_says,
+    owl_says,
+    bat_says,
+    dolphin_says,
+    shark_says,
+    elephant_says,
+    bunny_says,
+    spider_says,
+    horse_says,
+    monkey_says,
+    wolf_says
+)

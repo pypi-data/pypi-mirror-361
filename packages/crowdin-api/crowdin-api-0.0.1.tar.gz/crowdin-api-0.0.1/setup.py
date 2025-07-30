f921016b53a1c4e59613e9a3cf71f9a5ee575b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:42 +0000

    Add package crowdin-api/0.0.1/crowdin-api-0.0.1.tar.gz

---
+++ packages/crowdin-api/crowdin-api-0.0.1.tar.gz/crowdin-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="crowdin-api",
+    version="0.0.1",
+    packages=["crowdin-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

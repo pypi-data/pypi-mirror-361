d8068eb8d7e5f7b081e90fb3a1c6ec7ee54f94f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package coredis/5.0.0rc2/coredis-5.0.0rc2-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/coredis/coredis-5.0.0rc2-cp310-cp310-macosx_10_9_x86_64.whl/coredis/_protocols.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import asyncio
+
+from typing_extensions import runtime_checkable
+
+from coredis.response._callbacks import NoopCallback
+from coredis.typing import (
+    TYPE_CHECKING,
+    Awaitable,
+    Callable,
+    ExecutionParameters,
+    KeyT,
+    Parameters,
+    Protocol,
+    R,
+    RedisCommandP,
+    RedisValueT,
+    ResponseType,
+    StringT,
+    TypeVar,
+    Unpack,
+    ValueT,
+)
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+if TYPE_CHECKING:
+    from coredis.commands import CommandRequest
+
+
+class AbstractExecutor(Protocol):
+    def execute_command(
+        self,
+        command: RedisCommandP,
+        callback: Callable[..., R] = NoopCallback(),
+        **options: Unpack[ExecutionParameters],
+    ) -> Awaitable[R]: ...
+
+    def create_request(
+        self,
+        name: bytes,
+        *arguments: ValueT,
+        callback: Callable[..., R],
+        execution_parameters: ExecutionParameters | None = None,
+    ) -> CommandRequest[R]: ...
+
+
+@runtime_checkable
+class SupportsScript(Protocol[T_co]):  # noqa
+    def evalsha(
+        self,
+        sha1: StringT,
+        keys: Parameters[KeyT] | None = ...,
+        args: Parameters[RedisValueT] | None = ...,
+    ) -> CommandRequest[ResponseType]: ...
+
+    def evalsha_ro(
+        self,
+        sha1: StringT,
+        keys: Parameters[KeyT] | None = ...,
+        args: Parameters[RedisValueT] | None = ...,
+    ) -> CommandRequest[ResponseType]: ...
+
+    def script_load(self, script: StringT) -> CommandRequest[T_co]: ...
+
+
+@runtime_checkable
+class ConnectionP(Protocol):
+    decode_responses: bool
+    encoding: str
+    push_messages: asyncio.Queue[ResponseType]

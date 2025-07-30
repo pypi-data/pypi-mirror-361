a9f29154d43da3997023966a105d260659d5e163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:57 +0000

    Add package dycw-polars-subclasses/0.2.0/dycw_polars_subclasses-0.2.0-py3-none-any.whl

---
+++ packages/dycw-polars-subclasses/dycw_polars_subclasses-0.2.0-py3-none-any.whl/polars_subclasses/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from polars_subclasses.lib import DataFrameWithMetaData, SeriesWithMetaData
+
+__version__ = "0.2.0"
+
+
+__all__ = ["DataFrameWithMetaData", "SeriesWithMetaData"]

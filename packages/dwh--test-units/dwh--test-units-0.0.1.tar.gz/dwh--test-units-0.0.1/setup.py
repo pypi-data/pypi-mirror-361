72f2a2870006cbaee04ecce89d7c67d77e557f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package dwh--test-units/0.0.1/dwh--test-units-0.0.1.tar.gz

---
+++ packages/dwh--test-units/dwh--test-units-0.0.1.tar.gz/dwh--test-units-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh--test-units",
+    version="0.0.1",
+    packages=["dwh--test-units"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

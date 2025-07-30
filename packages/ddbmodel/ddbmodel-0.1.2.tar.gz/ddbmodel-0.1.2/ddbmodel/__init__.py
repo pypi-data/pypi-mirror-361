83d07e890f310374f3180999e84b299ec224514c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:30 +0000

    Add package ddbmodel/0.1.2/ddbmodel-0.1.2.tar.gz

---
+++ packages/ddbmodel/ddbmodel-0.1.2.tar.gz/ddbmodel-0.1.2/ddbmodel/__init__.py
@@ -0,0 +1,6 @@
+from .record import Record
+from .field import StringField, IntField, PasswordField, EmailField, TimestampNowField
+from .transaction import TransactionRecord
+from .key import Key
+
+__version__ = "0.1.2"

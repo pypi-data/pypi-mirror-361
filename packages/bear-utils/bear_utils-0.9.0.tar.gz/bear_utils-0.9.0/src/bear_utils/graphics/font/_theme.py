ec2249ba68143451de408dbfe7f261e63ca1e056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:10 +0000

    Add package bear-utils/0.9.0/bear_utils-0.9.0.tar.gz

---
+++ packages/bear-utils/bear_utils-0.9.0.tar.gz/bear_utils-0.9.0/src/bear_utils/graphics/font/_theme.py
@@ -0,0 +1,11 @@
+class CyberTheme:
+    """Namespace for cyberpunk color theme constants."""
+
+    primary: str = "bright_magenta"
+    neon_green: str = "bright_green"
+    neon_cyan: str = "bright_cyan"
+    warning: str = "bright_yellow"
+    error: str = "bright_red"
+    credits: str = "bright_yellow"
+    data: str = "bright_blue"
+    system: str = "dim white"

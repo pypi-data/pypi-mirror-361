eef431c2dcfd1a82a88439af2edb11e12a30d2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:36 +0000

    Add package mcpturbo-cli/1.0.0/mcpturbo_cli-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-cli/mcpturbo_cli-1.0.0-py3-none-any.whl/mcpturbo_cli/main.py
@@ -0,0 +1,12 @@
+"""Main module for mcpturbo-cli"""
+
+class McpturboCli:
+    """Main class for mcpturbo-cli"""
+    
+    def __init__(self):
+        self.version = "1.0.0"
+        
+    async def run(self):
+        """Main execution method"""
+        # Implementation will be added
+        pass

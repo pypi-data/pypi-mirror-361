cd73fe812f03bd97406b33ee2d6321544d45a1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:11 +0000

    Add package monacoreport/0.0.6/monacoreport-0.0.6.tar.gz

---
+++ packages/monacoreport/monacoreport-0.0.6.tar.gz/monacoreport-0.0.6/monacoreport/report_builder.py
@@ -0,0 +1,119 @@
+import os
+from monacoreport.parser import parse_time_start_or_end, parse_name_racer_and_brand
+from monacoreport.handlers import lap_time_for_rider, format_lap_time, check_str_type
+
+
+def get_file_paths_from_folder(folder: str) -> tuple[str, str, str]:
+    """Returns full paths to the three log files given a folder path.
+
+    Args:
+        folder (str): Path to the folder with files.
+
+    Returns:
+        tuple:
+            - str: Path to file start.log.
+            - str: Path to file end.log.
+            - str: Path to file abbreviations.txt.
+
+    Raises:
+        TypeError: If the parameter is not a str.
+    """
+    check_str_type(folder)
+    start_path = os.path.join(folder, 'start.log')
+    end_path = os.path.join(folder, 'end.log')
+    abbreviations_path = os.path.join(folder, 'abbreviations.txt')
+    return start_path, end_path, abbreviations_path
+
+
+def build_report(folder: str) -> tuple[list, list]:
+    """
+    Builds a result of lap times for each racer based on parsed log files.
+
+    Reads start and end times from log files, parses racer names and car brands,
+    calculates lap times, separates valid and invalid results.
+
+    Args:
+        folder (str): Path to the folder with files.
+
+    Returns:
+        tuple:
+            - list: Valid results containing racer name, car brand and lap time.
+            - list: Invalid results containing racer name, car brand, and error message.
+
+    Raises:
+        FileNotFoundError: If any of the required files are missing.
+        ValueError: If racer abbreviation from end log is not found in start log.
+    """
+    start_path, end_path, abbreviations_path = get_file_paths_from_folder(folder)
+    try:
+        time_start = parse_time_start_or_end(start_path)
+    except FileNotFoundError:
+        raise FileNotFoundError('File with race start time not found')
+    try:
+        time_end = parse_time_start_or_end(end_path)
+    except FileNotFoundError:
+        raise FileNotFoundError('File with race end time not found')
+    try:
+        name_racer, car_brand = parse_name_racer_and_brand(abbreviations_path)
+    except FileNotFoundError:
+        raise FileNotFoundError('Racing abbreviations file not found')
+    lap_time = lap_time_for_rider(time_start, time_end)
+
+    valid_result = []
+    invalid_result = []
+    for key, value in sorted(lap_time.items(), key=lambda item: item[1]):
+        if value.days < 0:
+            invalid_result.append((name_racer[key], car_brand[key], 'Race time cannot be negative'))
+        else:
+            valid_result.append((name_racer[key], car_brand[key], value))
+    return valid_result, invalid_result
+
+
+def rider_report(valid_result: list, invalid_result: list, name: str) -> None:
+    """Displays the rider's statistics
+
+    Args:
+        valid_result (list): In list - each tuple contains racer name, car brand, and lap time (datetime).
+        invalid_result (list): In list - each tuple contains racer name, car brand, and error message.
+        name (str): Racer's name and surname
+
+    Raises:
+        ValueError: If the rider's name is not in the list of riders.
+    """
+    result = ''
+    for i, idx in enumerate(valid_result, start=1):
+        if name in ' '.join(idx[0:1]):
+            result = name
+            print(f'Racer in {i}st position of leadership, {idx[0]} | {idx[1]} | {format_lap_time(idx[2])}')
+
+    for idx in invalid_result:
+        if name in idx[0]:
+            result = name
+            print(f'{idx[0]} has invalid race data | {idx[1]}')
+    if result == '':
+        raise ValueError(f'{name} not found in rider list')
+
+
+def print_report(valid_result: list, invalid_result: list, sort=True) -> None:
+    """
+    Prints a formatted report of race results.
+
+    Displays the top 15 racers with formatted lap times
+    and the rest after underline,
+    followed by racers with invalid or erroneous data.
+
+    Args:
+        valid_result (list): In list - each tuple contains racer name, car brand, and lap time (datetime).
+        invalid_result (list): In list - each tuple contains racer name, car brand, and error message.
+        sort (bool): Sort by racer - time, False - from high to low
+    """
+    valid_result = valid_result if sort else valid_result[::-1]
+    total_valid_racer = len(valid_result)
+    for i, idx in enumerate(valid_result, start=1):
+        num = i if sort else total_valid_racer - i + 1
+        print(f'{num}. {idx[0]} | {idx[1]} | {format_lap_time(idx[2])}')
+        if i == 15:
+            print('-' * 70)
+
+    for idx in invalid_result:
+        print(f'{idx[0]} | {idx[1]} | {idx[2]}')
---
+++ packages/monacoreport/monacoreport-0.0.6.tar.gz/monacoreport-0.0.6/tests/test_handlers.py
@@ -0,0 +1,65 @@
+import pytest
+from datetime import datetime, timedelta
+from monacoreport.handlers import check_str_type, correct_info_in_file, lap_time_for_rider
+
+LIST_NOT_STR = [451278, None, ['abc', 'def'], {1: 'test'}, 4.5, True, {1, 2}, (1, 2)]
+
+
+class TestCheckStrType:
+    """Tests for check_str_type() function."""
+
+    def test_not_str_input(self):
+        """Should raise TypeError for non-string input."""
+        for not_str_type in LIST_NOT_STR:
+            with pytest.raises(TypeError):
+                check_str_type(not_str_type)
+
+
+class TestCorrectInfoInFile:
+    """Tests for correct_info_in_file() function."""
+
+    def test_correct_info_in_file(self, mocker):
+        """Test of opening and reading a mock file and returns it as a list"""
+        mock_open = mocker.mock_open(read_data='SVF2018-05-24_12:02:58.917\nRGH2018-05-24_12:05:14.511')
+        mocker.patch("builtins.open", mock_open)
+        result = correct_info_in_file('test_file.txt')
+        assert result == ['SVF2018-05-24_12:02:58.917', 'RGH2018-05-24_12:05:14.511']
+        mock_open.assert_called_once_with('test_file.txt', encoding='utf-8')
+
+    def test_not_str_input(self):
+        """Should raise TypeError for non-string input."""
+        for not_str_type in LIST_NOT_STR:
+            with pytest.raises(TypeError):
+                correct_info_in_file(not_str_type)
+
+    def test_file_not_found(self):
+        """Should raise FileNotFoundError for incorrect address or missing file."""
+        with pytest.raises(FileNotFoundError):
+            correct_info_in_file(r' ')
+
+
+class TestLapTimeForRider:
+    """Tests for lap_time_for_rider() function."""
+
+    def test_correct_lap_time(self):
+        """Should return correct timedelta for known abbreviation."""
+        dict_start = {'SVF': datetime(2018, 5, 24, 12, 2, 58, 917000),
+                      'LHM': datetime(2018, 5, 24, 12, 3, 1, 250000)}
+        dict_end = {'SVF': datetime(2018, 5, 24, 12, 5, 58, 917000),
+                    'LHM': datetime(2018, 5, 24, 12, 4, 1, 250000)}
+        result = lap_time_for_rider(dict_start, dict_end)
+        assert result == {'SVF': timedelta(seconds=180), 'LHM': timedelta(seconds=60)}
+
+    def test_negative_lap_time(self):
+        """Should return negative timedelta if end over start."""
+        dict_start = {"SVF": datetime(2018, 5, 24, 12, 2, 58, 917000)}
+        dict_end = {"SVF": datetime(2018, 5, 24, 12, 2, 28, 917000)}
+        result = lap_time_for_rider(dict_start, dict_end)
+        assert result == {"SVF": timedelta(seconds=-30)}
+
+    def test_non_finding_abbreviation(self):
+        """Should raise ValueError if end has key not in start."""
+        dict_start = {"SVF": datetime(2018, 5, 24, 12, 2, 58, 917000)}
+        dict_end = {"SBB": datetime(2018, 5, 24, 12, 2, 28, 917000)}
+        with pytest.raises(ValueError):
+            lap_time_for_rider(dict_start, dict_end)

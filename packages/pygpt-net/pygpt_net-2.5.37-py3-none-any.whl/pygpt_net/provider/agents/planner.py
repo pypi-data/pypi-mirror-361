1fc2ce69ab73d48a5ba8881da5e38d860d336e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:01 +0000

    Add package pygpt-net/2.5.37/pygpt_net-2.5.37-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.5.37-py3-none-any.whl/pygpt_net/provider/agents/planner.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.12.14 22:00:00                  #
+# ================================================== #
+
+from typing import Dict, Any
+
+from llama_index.core.agent import (
+    StructuredPlannerAgent,
+    FunctionCallingAgentWorker,
+    ReActAgentWorker,
+)
+
+from .base import BaseAgent
+
+class PlannerAgent(BaseAgent):
+    def __init__(self, *args, **kwargs):
+        super(PlannerAgent, self).__init__(*args, **kwargs)
+        self.id = "planner"
+        self.mode = "plan"  # step|plan
+
+    def get_agent(self, window, kwargs: Dict[str, Any]):
+        """
+        Return Agent provider instance
+
+        :param window: window instance
+        :param kwargs: keyword arguments
+        :return: Agent provider instance
+        """
+        tools = kwargs.get("tools", [])
+        verbose = kwargs.get("verbose", False)
+        llm = kwargs.get("llm", None)
+        chat_history = kwargs.get("chat_history", [])
+        max_iterations = kwargs.get("max_iterations", 10)
+        worker = FunctionCallingAgentWorker.from_tools(
+            tools=tools,
+            llm=llm,
+            verbose=verbose,
+        )
+        return StructuredPlannerAgent(
+            agent_worker=worker,
+            llm=llm,
+            chat_history=chat_history,
+            tools=tools,
+            verbose=verbose,
+        )

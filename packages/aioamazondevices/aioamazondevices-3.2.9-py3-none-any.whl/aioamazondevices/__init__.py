3b3b8fc9acf62a568375511f2008528a9e3be574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:39 +0000

    Add package aioamazondevices/3.2.9/aioamazondevices-3.2.9-py3-none-any.whl

---
+++ packages/aioamazondevices/aioamazondevices-3.2.9-py3-none-any.whl/aioamazondevices/__init__.py
@@ -0,0 +1,17 @@
+"""aioamazondevices library."""
+
+__version__ = "3.2.9"
+
+
+from .api import AmazonDevice, AmazonEchoApi
+from .exceptions import (
+    CannotAuthenticate,
+    CannotConnect,
+)
+
+__all__ = [
+    "AmazonDevice",
+    "AmazonEchoApi",
+    "CannotAuthenticate",
+    "CannotConnect",
+]

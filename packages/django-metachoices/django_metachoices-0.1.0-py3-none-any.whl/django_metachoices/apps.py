18d3b444ad9d07f9af182e57e3d146dee063aa23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:36 +0000

    Add package django-metachoices/0.1.0/django_metachoices-0.1.0-py3-none-any.whl

---
+++ packages/django-metachoices/django_metachoices-0.1.0-py3-none-any.whl/django_metachoices/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class DjangoMetachoicesConfig(AppConfig):
+    """Configuration for the Django Metachoices app."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_metachoices"

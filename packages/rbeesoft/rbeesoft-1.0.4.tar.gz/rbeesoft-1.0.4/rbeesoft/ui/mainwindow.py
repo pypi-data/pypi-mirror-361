6950a243319333a29bdd2cd1bdd0b8922eae23c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:44 +0000

    Add package rbeesoft/1.0.4/rbeesoft-1.0.4.tar.gz

---
+++ packages/rbeesoft/rbeesoft-1.0.4.tar.gz/rbeesoft-1.0.4/rbeesoft/ui/mainwindow.py
@@ -0,0 +1,66 @@
+import os
+
+from PySide6.QtWidgets import (
+    QMainWindow,
+)
+from PySide6.QtGui import (
+    QGuiApplication,
+    QIcon,
+)
+from PySide6.QtCore import Qt, QByteArray
+
+from rbeesoft.ui.constants import Constants
+from rbeesoft.ui.settings import Settings
+from rbeesoft.ui.utils import resource_path, version
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self._settings = None
+        self.init_window()
+
+    def init_window(self):
+        self.setWindowTitle(f'{Constants.RBEESOFT_WINDOW_TITLE} {version(Constants.RBEESOFT_NAME)}')
+        self.setWindowIcon(QIcon(resource_path(os.path.join(
+            Constants.RBEESOFT_RESOURCES_IMAGES_ICONS_DIR, Constants.RBEESOFT_RESOURCES_ICON))))
+        if not self.load_geometry_and_state():
+            self.set_default_size_and_position()
+
+    # GETTERS
+
+    def settings(self):
+        if not self._settings:
+            self._settings = Settings()
+        return self._settings
+    
+    def closeEvent(self, event):
+        self.save_geometry_and_state()
+        return super().closeEvent(event)
+    
+    # MISCELLANEOUS
+
+    def load_geometry_and_state(self):
+        geometry = self.settings().get(Constants.RBEESOFT_WINDOW_GEOMETRY_KEY)
+        state = self.settings().get(Constants.RBEESOFT_WINDOW_STATE_KEY)
+        if isinstance(geometry, QByteArray) and self.restoreGeometry(geometry):
+            if isinstance(state, QByteArray):
+                self.restoreState(state)
+            return True
+        return False
+
+    def save_geometry_and_state(self):
+        self.settings().set(
+            Constants.RBEESOFT_WINDOW_GEOMETRY_KEY, self.saveGeometry())
+        self.settings().set(
+            Constants.RBEESOFT_WINDOW_STATE_KEY, self.saveState())
+
+    def set_default_size_and_position(self):
+        self.resize(Constants.RBEESOFT_WINDOW_W, Constants.RBEESOFT_WINDOW_H)
+        self.center_window()
+
+    def center_window(self):
+        screen = QGuiApplication.primaryScreen().geometry()
+        x = (screen.width() - self.geometry().width()) / 2
+        y = (screen.height() - self.geometry().height()) / 2
+        self.move(int(x), int(y))

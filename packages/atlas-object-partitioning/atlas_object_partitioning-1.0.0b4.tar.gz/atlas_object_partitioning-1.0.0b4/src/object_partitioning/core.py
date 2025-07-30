54bf669b31d4621d3468e5cc5384f9368441b85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:52 +0000

    Add package atlas-object-partitioning/1.0.0b4/atlas_object_partitioning-1.0.0b4.tar.gz

---
+++ packages/atlas-object-partitioning/atlas_object_partitioning-1.0.0b4.tar.gz/atlas_object_partitioning-1.0.0b4/src/object_partitioning/core.py
@@ -0,0 +1,10 @@
+import warnings
+
+warnings.warn(
+    "The 'object_partitioning' package is deprecated. Use 'atlas_object_partitioning' instead.",
+    DeprecationWarning,
+)
+
+
+def dummy_function():
+    return True

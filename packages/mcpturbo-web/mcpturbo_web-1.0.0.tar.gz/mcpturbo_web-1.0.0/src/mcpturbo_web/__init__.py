141b06de6ea681550f9e385e7f993d28c1029e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:42 +0000

    Add package mcpturbo-web/1.0.0/mcpturbo_web-1.0.0.tar.gz

---
+++ packages/mcpturbo-web/mcpturbo_web-1.0.0.tar.gz/mcpturbo_web-1.0.0/src/mcpturbo_web/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Web
+
+MCPTurbo - Web Dashboard and Visual Editor
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []
---
+++ packages/mcpturbo-web/mcpturbo_web-1.0.0.tar.gz/mcpturbo_web-1.0.0/src/mcpturbo_web/main.py
@@ -0,0 +1,12 @@
+"""Main module for mcpturbo-web"""
+
+class McpturboWeb:
+    """Main class for mcpturbo-web"""
+    
+    def __init__(self):
+        self.version = "1.0.0"
+        
+    async def run(self):
+        """Main execution method"""
+        # Implementation will be added
+        pass

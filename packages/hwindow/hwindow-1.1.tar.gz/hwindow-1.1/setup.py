f07dfcc85478870aa4e142fc46c3b58a6b39e007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:59 +0000

    Add package hwindow/1.1/hwindow-1.1.tar.gz

---
+++ packages/hwindow/hwindow-1.1.tar.gz/hwindow-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hwindow',
+    version='1.1',
+    description='hwindow1',
+    long_description=long_description,
+    author='huang yi yi',
+    author_email='363766687@qq.com',
+    packages=find_packages(),
+    package_dir={'hwindow1': 'hwindow1'},
+    package_data={"hwindow1": ["**"]},
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        "PyQt6",
+    ],
+)

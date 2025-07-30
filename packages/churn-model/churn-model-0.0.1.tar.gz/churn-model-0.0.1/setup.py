6e43042505881acc635dceac04df3acde170caa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package churn-model/0.0.1/churn-model-0.0.1.tar.gz

---
+++ packages/churn-model/churn-model-0.0.1.tar.gz/churn-model-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="churn-model",
+    version="0.0.1",
+    packages=["churn-model"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

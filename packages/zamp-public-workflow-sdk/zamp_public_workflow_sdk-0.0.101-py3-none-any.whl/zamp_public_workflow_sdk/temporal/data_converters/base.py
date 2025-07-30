1b1a106cfc6f62aa353bede8075b87983558fa50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package zamp-public-workflow-sdk/0.0.101/zamp_public_workflow_sdk-0.0.101-py3-none-any.whl

---
+++ packages/zamp-public-workflow-sdk/zamp_public_workflow_sdk-0.0.101-py3-none-any.whl/zamp_public_workflow_sdk/temporal/data_converters/base.py
@@ -0,0 +1,18 @@
+from temporalio.converter import DataConverter, PayloadCodec, PayloadConverter, CompositePayloadConverter
+import dataclasses
+from temporalio.contrib.pydantic import pydantic_data_converter
+from typing import Type
+
+class BaseDataConverter:
+    converter = pydantic_data_converter
+
+    def replace_payload_codec(self, payload_codec: PayloadCodec) -> 'BaseDataConverter':
+        self.converter = dataclasses.replace(self.converter, payload_codec=payload_codec)
+        return self
+    
+    def replace_payload_converter(self, payload_converter_type: Type[PayloadConverter]) -> 'BaseDataConverter':
+        self.converter = dataclasses.replace(self.converter, payload_converter_class=payload_converter_type)
+        return self
+        
+    def get_converter(self) -> DataConverter:
+        return self.converter

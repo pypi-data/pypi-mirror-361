0049dd84fa21b14024cee37fc0577f18181ab27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:04 +0000

    Add package scikit-neuromsi/1.0.2/scikit_neuromsi-1.0.2.tar.gz

---
+++ packages/scikit-neuromsi/scikit_neuromsi-1.0.2.tar.gz/scikit_neuromsi-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Scikit-NeuroMSI Project (https://github.com/renatoparedes/scikit-neuromsi).
+# Copyright (c) 2021-2025, Renato Paredes; Cabral, Juan
+# License: BSD 3-Clause
+# Full Text:
+#     https://github.com/renatoparedes/scikit-neuromsi/blob/main/LICENSE.txt
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""Setup script for the scikit-neuromsi project.
+
+empty for backward compatibility
+
+"""
+
+# =============================================================================
+# IMPORTS and RUN
+# =============================================================================
+
+import setuptools
+
+setuptools.setup()

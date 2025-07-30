7c332ed9785235a4c27bd551ef4d993767028ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:49 +0000

    Add package src-agent/0.0.1/src_agent-0.0.1-py3-none-any.whl

---
+++ packages/src-agent/src_agent-0.0.1-py3-none-any.whl/src_agent/__init__.py
@@ -0,0 +1,3 @@
+# ruff: noqa: F401
+# Configure clean imports for the package
+# See: https://hynek.me/articles/testing-packaging/
---
+++ packages/src-agent/src_agent-0.0.1-py3-none-any.whl/src_agent/entrypoint.py
@@ -0,0 +1,9 @@
+# Resources
+#  - [Using OnepRouter with Python](https://openrouter.ai/docs/quickstart)
+
+def main():
+    print("Welcome to the Source Agent!")
+
+
+if __name__ == "__main__":
+    main()

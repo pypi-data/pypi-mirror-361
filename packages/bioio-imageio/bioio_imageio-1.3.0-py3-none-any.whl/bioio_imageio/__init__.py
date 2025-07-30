5c43c311a3e0655877d12f85f3251da65e4a6b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:26 +0000

    Add package bioio-imageio/1.3.0/bioio_imageio-1.3.0-py3-none-any.whl

---
+++ packages/bioio-imageio/bioio_imageio-1.3.0-py3-none-any.whl/bioio_imageio/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bioio_imageio."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("bioio-imageio")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Sean Meharry"
+__email__ = "seanm@alleninstitute.org"
+
+from .reader import Reader
+from .reader_metadata import ReaderMetadata
+
+__all__ = ["Reader", "ReaderMetadata"]

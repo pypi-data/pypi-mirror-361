b9455680eb674a3f5688a83f2f960461bf46cbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:27 +0000

    Add package exorobot/1.3.0/exorobot-1.3.0-py3-none-any.whl

---
+++ packages/exorobot/exorobot-1.3.0-py3-none-any.whl/my_module/__init__.py
@@ -0,0 +1,5 @@
+from .robot import pyads_connection,fifo_power,stop_do,fifostop
+from .robot import Poly_O,Poly_P,Poly_Q,Poly_R,Poly_S,Poly_T,Poly_U,Poly_V,Poly_B,Poly_C,Poly_D,Poly_E,Poly_G,Poly_H,Poly_I,Poly_M,Poly_N
+from .robot import Poly_NEW1,Poly_NEW2,Poly_NEW3,Poly_NEW4,Poly_NEW5,Poly_NEW6,Poly_NEW7,Poly_NEW8,Poly_NEW9,Poly_NEW10
+from .robot import fifo_insert,fifostart,back_to_zero,Traject_planning
+from .utils import greet

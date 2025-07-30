a18b22dbbe4948ab259d0c1fda7e263fe4dcb42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:47 +0000

    Add package ngff-zarr/0.14.0/ngff_zarr-0.14.0.tar.gz

---
+++ packages/ngff-zarr/ngff_zarr-0.14.0.tar.gz/ngff_zarr-0.14.0/ngff_zarr/multiscales.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple, Union
+
+from .methods import Methods
+from .ngff_image import NgffImage
+from .v04.zarr_metadata import Metadata
+
+
+@dataclass
+class Multiscales:
+    images: List[NgffImage]
+    metadata: Metadata
+    scale_factors: Optional[Sequence[Union[Dict[str, int], int]]] = None
+    method: Optional[Methods] = None
+    chunks: Optional[
+        Union[
+            int,
+            Tuple[int, ...],
+            Tuple[Tuple[int, ...], ...],
+            Mapping[Any, Union[None, int, Tuple[int, ...]]],
+        ]
+    ] = None

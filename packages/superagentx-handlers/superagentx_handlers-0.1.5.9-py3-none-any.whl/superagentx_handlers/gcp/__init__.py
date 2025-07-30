cd44a919d42b890461c792d95bb158160dfd4020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:18 +0000

    Add package superagentx-handlers/0.1.5.9/superagentx_handlers-0.1.5.9-py3-none-any.whl

---
+++ packages/superagentx-handlers/superagentx_handlers-0.1.5.9-py3-none-any.whl/superagentx_handlers/gcp/__init__.py
@@ -0,0 +1,6 @@
+from .calender import CalenderHandler
+from .gmail import GmailHandler
+from .iam import GCPIAMHandler
+from .storage import GCPStorageHandler
+from .cloud_run import GCPCloudRunHandler
+from .loadbalancer import GCPLoadBalancerHandler

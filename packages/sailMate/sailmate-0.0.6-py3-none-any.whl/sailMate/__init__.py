6a052c9921b5a6dd359a1ff146e0bb6158cd35cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:10 +0000

    Add package sailMate/0.0.6/sailmate-0.0.6-py3-none-any.whl

---
+++ packages/sailMate/sailmate-0.0.6-py3-none-any.whl/sailMate/__init__.py
@@ -0,0 +1,16 @@
+from .sailMate import (
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+    Board,
+    FEN,
+    evaluate,
+    possiblePositions, 
+    minimaxImproved, 
+    minimax, 
+    bestMove, 
+    play
+)

ea0015a7dd54a8350bfbd2e6ea656ada2053374c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:21 +0000

    Add package crpt1/1.0.0/crpt1-1.0.0.tar.gz

---
+++ packages/crpt1/crpt1-1.0.0.tar.gz/crpt1-1.0.0/README.md
@@ -0,0 +1,5 @@
+# crpt1
+
+Utilities and helpers for cryptographic and system tasks.
+
+This package provides utility functions for system and cryptographic tasks.
---
+++ packages/crpt1/crpt1-1.0.0.tar.gz/crpt1-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='crpt1',
+    version='1.0.0',
+    author='WKJyUdx960Lr',
+    author_email='Zl8p8X@txBXD.com',
+    description='Utilities and helpers for cryptographic and system tasks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://6pmdJA1F.com',
+    packages=['crpt1'],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

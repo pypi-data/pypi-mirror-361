9c39d35c52decf5b2f05e440a5fe4dbbc5a60d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:54 +0000

    Add package efootprint/10.1.13/efootprint-10.1.13.tar.gz

---
+++ packages/efootprint/efootprint-10.1.13.tar.gz/efootprint-10.1.13/efootprint/constants/units.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+import pint
+from pint import UnitRegistry
+
+from efootprint.constants.files import CUSTOM_UNITS_PATH
+
+u = UnitRegistry()
+u.load_definitions(CUSTOM_UNITS_PATH)
+u.default_locale = 'en_EN'
+pint.set_application_registry(u)
+
+
+@lru_cache(maxsize=None)
+def get_unit(unit_str):
+    return u(unit_str)

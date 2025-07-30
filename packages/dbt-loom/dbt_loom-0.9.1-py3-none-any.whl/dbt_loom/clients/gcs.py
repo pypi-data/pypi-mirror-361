4cbc183490773d8353320af8182e7181d7e0e47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:32 +0000

    Add package dbt-loom/0.9.1/dbt_loom-0.9.1-py3-none-any.whl

---
+++ packages/dbt-loom/dbt_loom-0.9.1-py3-none-any.whl/dbt_loom/clients/gcs.py
@@ -0,0 +1,72 @@
+import json
+import gzip
+from io import BytesIO
+from pathlib import Path
+from typing import Dict, Optional
+
+from pydantic import BaseModel
+
+from dbt_loom.logging import fire_event
+
+
+class GCSReferenceConfig(BaseModel):
+    """Configuration for a GCS reference"""
+
+    project_id: str
+    bucket_name: str
+    object_name: str
+    credentials: Optional[Path] = None
+
+
+class GCSClient:
+    """Client for GCS. Fetches manifest for a given bucket."""
+
+    def __init__(
+        self,
+        project_id: str,
+        bucket_name: str,
+        object_name: str,
+        credentials: Optional[Path] = None,
+    ) -> None:
+        self.project_id = project_id
+        self.bucket_name = bucket_name
+        self.object_name = object_name
+        self.credentials = credentials
+
+    def load_manifest(self) -> Dict:
+        """Load a manifest json from a GCS bucket."""
+
+        try:
+            from google.cloud import storage
+        except ImportError:
+            fire_event(msg="dbt-loom expected google-cloud-storage to be installed.")
+            raise
+
+        client = (
+            storage.Client.from_service_account_json(
+                self.credentials, project=self.project_id
+            )
+            if self.credentials
+            else storage.Client(project=self.project_id)
+        )
+        bucket = client.get_bucket(self.bucket_name)
+        blob = bucket.get_blob(self.object_name)
+        if not blob:
+            raise Exception(
+                f"The object `{self.object_name}` does not exist in bucket "
+                f"`{self.bucket_name}`."
+            )
+
+        if self.object_name.endswith(".gz"):
+            compressed_manifest = blob.download_as_bytes()
+            with gzip.GzipFile(fileobj=BytesIO(compressed_manifest)) as gzip_file:
+                manifest_json = gzip_file.read()
+        else:
+            manifest_json = blob.download_as_text()
+
+        try:
+            return json.loads(manifest_json)
+        except Exception:
+            raise Exception(
+                f"The object `{self.object_name}` does not contain valid JSON."
+            )

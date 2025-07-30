807ef2580cd1e62ffdd8b059b964e7d28a5c7d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:08:00 +0000

    Add package uvkernel/0.2.0/uvkernel-0.2.0.tar.gz

---
+++ packages/uvkernel/uvkernel-0.2.0.tar.gz/uvkernel-0.2.0/src/uvkernel/ipynb_metadata.py
@@ -0,0 +1,47 @@
+import re
+
+import nbformat
+
+REGEX = r"(?m)^# /// (?P<type>[a-zA-Z0-9-]+)$\s(?P<content>(^#(| .*)$\s)+)^# ///$"
+
+
+def find_script_metadata_blocks(source: str) -> list[str]:
+    """Read script metadata from python source
+
+    See https://peps.python.org/pep-0723/#reference-implementation
+    """
+    name = "script"
+    matches = filter(lambda m: m.group("type") == name, re.finditer(REGEX, source))
+    return list(m.string for m in matches)
+
+
+def get_ipynb_script_metadata(file: str) -> str | None:
+    """Get script metadata from ipynb file
+
+    Metadata must be in a code cell. Cannot be split across multiple cells. Only one metadata block is allowed per notebook.
+    """
+    with open(file, "r", encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+
+    code_cells = (cell for cell in notebook.cells if cell.cell_type == "code")
+
+    script_metadata_blocks = [
+        block
+        for cell in code_cells
+        for block in find_script_metadata_blocks(cell.source)
+    ]
+
+    if len(script_metadata_blocks) == 0:
+        return None
+    elif len(script_metadata_blocks) == 1:
+        return script_metadata_blocks[0]
+    else:
+        raise ValueError("Multiple script blocks found")
+
+
+def get_ipynb_lock(file: str) -> str | None:
+    """Get lockfile from notebook metadata if exists"""
+
+    with open(file, "r", encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+        return notebook["metadata"].get("uv.lock", None)

45eef7f6cdbc31017e9a6ec8f490432e80108541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:34 +0000

    Add package aiojito/0.1.1/aiojito-0.1.1-py3-none-any.whl

---
+++ packages/aiojito/aiojito-0.1.1-py3-none-any.whl/aiojito/api/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-#
+# -------------------------------------------------------------------------------
+# Name:         a 
+# Author:       yepeng
+# Date:         2021/10/22 2:44 下午
+# Description: Python scripts in this directory are modified based on the project: https://github.com/MatrixYe/jito-py
+# -------------------------------------------------------------------------------
+from .block_engine import *
+from .searcher import *
+from .tip_payment_account import *
---
+++ packages/aiojito/aiojito-0.1.1-py3-none-any.whl/aiojito/api/searcher.py
@@ -0,0 +1,119 @@
+# Reviewed by Tyler. Safe to use.
+
+from dataclasses import dataclass, field
+from typing import List, Optional, Dict, Any
+
+import requests
+
+
+@dataclass
+class BundleStatus:
+    bundle_id: str
+    transactions: List[str]
+    slot: int
+    confirmation_status: str
+    err: Dict[str, Any]
+
+
+@dataclass
+class BundleStatusesResponse:
+    context_slot: int
+    statuses: List[BundleStatus] = field(default_factory=list)
+
+
+class Searcher:
+    def __init__(self, block_engine_url: str):
+        self.block_engine_url = block_engine_url
+
+    @staticmethod
+    def _extract_result(response: Dict[str, Any], method: str) -> Any:
+        if 'result' in response:
+            return response['result']
+        else:
+            raise Exception(f"Error in {method} response: {response}")
+
+    def _send_rpc_request(self, endpoint: str, method: str, params: Optional[List] = None) -> Dict[str, Any]:
+        """
+        Documentation: https://jito-labs.gitbook.io/mev/searcher-resources/json-rpc-api-reference
+        :param endpoint:
+        :param method:
+        :param params:
+        :return:
+        """
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "jsonrpc": "2.0",  # set to "2.0"
+            "id": 1,  # a unique client-generated identifying integer
+            "method": method,  # a string containing the method to be invoked
+            "params": params or []  # a JSON array of ordered parameter values
+        }
+        try:
+            url = f"{self.block_engine_url}{endpoint}"
+            response = requests.post(url=url, json=payload, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            raise Exception(f"HTTP request failed: {e}")
+        except ValueError as e:
+            raise Exception(f"Invalid JSON response: {e}")
+
+    def get_bundle_statuses(self, bundle_ids: List[str]) -> BundleStatusesResponse:
+        """
+        Returns the status of submitted bundle(s).
+        https://jito-labs.gitbook.io/mev/searcher-resources/json-rpc-api-reference/bundles/getbundlestatuses
+
+        :param bundle_ids: An array of bundle ids to confirm, as base-58 encoded strings (up to a maximum of 5).
+        :return: A BundleStatusesResponse object containing the context slot and a list of BundleStatus objects.
+        """
+        response = self._send_rpc_request("/api/v1/bundles", "getBundleStatuses", [bundle_ids])
+        result = self._extract_result(response, "getBundleStatuses")
+        context_slot = result['context']['slot']
+        statuses = [
+            BundleStatus(
+                bundle_id=status['bundle_id'],
+                transactions=status['transactions'],
+                slot=status['slot'],
+                confirmation_status=status['confirmation_status'],
+                err=status['err']
+            )
+            for status in result['value']
+        ]
+        return BundleStatusesResponse(context_slot=context_slot, statuses=statuses)
+
+    def get_tip_accounts(self) -> List[str]:
+        """
+        Retrieves the tip accounts designated for tip payments for bundles.
+        https://jito-labs.gitbook.io/mev/searcher-resources/json-rpc-api-reference/bundles/gettipaccounts
+
+        :return: Tip accounts as a list of strings.
+        """
+        response = self._send_rpc_request("/api/v1/bundles", "getTipAccounts")
+        return self._extract_result(response, "getTipAccounts")
+
+    def send_bundle(self, transactions: List[str]) -> str:
+        """
+        Submits a bundled list of signed transactions (base-58 encoded strings) to the cluster for processing.
+        https://jito-labs.gitbook.io/mev/searcher-resources/json-rpc-api-reference/bundles/sendbundle
+
+        :param transactions: Fully-signed transactions, as base-58 encoded strings (up to a maximum of 5).
+                             Base-64 encoded transactions are not supported at this time.
+        :return: A bundle ID, used to identify the bundle. This is the SHA-256 hash of the bundle's transaction signatures.
+        """
+        response = self._send_rpc_request("/api/v1/bundles", "sendBundle", [transactions])
+        return self._extract_result(response, "sendBundle")
+
+    def send_transaction(self, transaction: str) -> str:
+        """
+        This method serves as a proxy to the Solana sendTransaction RPC method. It forwards the received transaction as a
+        regular Solana transaction via the Solana RPC method and submits it as a bundle. Jito sponsors the bundling and
+        provides a minimum tip for the bundle. However, please note that this minimum tip might not be sufficient to get
+        the bundle through the auction, especially during high-demand periods. If you set the query parameter bundleOnly=true,
+        the transaction will only be sent out as a bundle and not as a regular transaction via RPC.
+        https://jito-labs.gitbook.io/mev/searcher-resources/json-rpc-api-reference/transactions-endpoint/sendtransaction
+
+        :param transaction: First Transaction Signature embedded in the transaction, as base-58 encoded string.
+        :return: The result will be the same as described in the Solana RPC documentation. If sending as a bundle was
+                 successful, you can get the bundle_id for further querying from the custom header in the response x-bundle-id.
+        """
+        response = self._send_rpc_request("/api/v1/transactions", "sendTransaction", [transaction])
+        return self._extract_result(response, "sendTransaction")

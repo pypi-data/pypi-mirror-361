40f5efa5d4ef5f7b6a518628f586954b57ad0426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:44 +0000

    Add package isc-interop/0.0.1/isc_interop-0.0.1.tar.gz

---
+++ packages/isc-interop/isc_interop-0.0.1.tar.gz/isc_interop-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="isc_interop",       
+    version="0.0.1",           
+    packages=find_packages(),  
+    python_requires=">=3.7",
+)
+

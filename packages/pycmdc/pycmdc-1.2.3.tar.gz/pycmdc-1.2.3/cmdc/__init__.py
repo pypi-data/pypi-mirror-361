a4b3736135bac385d9b6061697007cb72291696e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:36 +0000

    Add package pycmdc/1.2.3/pycmdc-1.2.3.tar.gz

---
+++ packages/pycmdc/pycmdc-1.2.3.tar.gz/pycmdc-1.2.3/cmdc/__init__.py
@@ -0,0 +1,13 @@
+"""
+cmdc - Interactive CLI tool for browsing and selecting files for LLM contexts.
+"""
+
+from importlib.metadata import version as get_version
+
+__version__ = get_version("pycmdc")
+
+ASCII_ART = """\
+     ┓ ╻
+┏┏┳┓┏┫━╋━┏
+┗┛┗┗┗┻ ╹ ┗
+"""

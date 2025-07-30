5fcce95b49c8ecdc577cb59d75d6920a96a5ec8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package proxidize-pt/1.0.6/proxidize_pt-1.0.6-py3-none-any.whl

---
+++ packages/proxidize-pt/proxidize_pt-1.0.6-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Proxidize: Proxy Tester
+A multi-threaded proxy testing tool for HTTP/SOCKS proxies with TXT and CSV output formats
+"""
+
+__version__ = "1.0.6"
+__author__ = "Proxidize"
+__email__ = "support@proxidize.com"
+__description__ = "Proxidize Proxy Tester - A multi-threaded proxy testing tool for HTTP/SOCKS proxies with TXT and CSV output formats"

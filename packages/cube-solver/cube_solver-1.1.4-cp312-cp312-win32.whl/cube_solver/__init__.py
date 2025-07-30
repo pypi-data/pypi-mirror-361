c7dc89cfe0f116cecbe6dba342d14b763c55e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:41 +0000

    Add package cube-solver/1.1.4/cube_solver-1.1.4-cp312-cp312-win32.whl

---
+++ packages/cube-solver/cube_solver-1.1.4-cp312-cp312-win32.whl/cube_solver/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Cube Solver."""
+
+__author__ = """Dave Barragan"""
+__email__ = 'itsdaveba@gmail.com'
+__version__ = '1.1.4'
+
+from .cube import Cube, apply_move, apply_maneuver, Move, Maneuver
+from .solver import BaseSolver, DummySolver, Korf, Thistlethwaite, Kociemba
+
+__all__ = ["Cube", "apply_move", "apply_maneuver", "Move", "Maneuver",
+           "BaseSolver", "DummySolver", "Korf", "Thistlethwaite", "Kociemba"]

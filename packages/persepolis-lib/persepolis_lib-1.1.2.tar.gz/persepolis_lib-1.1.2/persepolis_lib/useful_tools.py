84210e9209318b0e48c3dff74e98b2544d535854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:54 +0000

    Add package persepolis-lib/1.1.2/persepolis_lib-1.1.2.tar.gz

---
+++ packages/persepolis-lib/persepolis_lib-1.1.2.tar.gz/persepolis_lib-1.1.2/persepolis_lib/useful_tools.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+import urllib.parse
+import logging
+import os
+import requests
+# define logging object
+logObj = logging.getLogger("Persepolis")
+logObj.setLevel(logging.INFO)
+
+# don't show log in console
+logObj.propagate = False
+
+log_file = os.path.join(os.path.expanduser('~'), 'persepolis_lib_log.log')
+
+# create a file handler
+handler = logging.FileHandler(log_file)
+handler.setLevel(logging.INFO)
+# create a logging format
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logObj.addHandler(handler)
+
+
+def sendToLog(text="", type="INFO"):
+    if type == "INFO":
+        logObj.info(text)
+    elif type == "ERROR":
+        logObj.error(text)
+    else:
+        logObj.warning(text)
+
+# this function converts second to hour and minute
+
+
+def convertTime(time):
+    minutes = int(time // 60)
+    if minutes == 0:
+        return str(int(time)) + 's'
+    elif minutes < 60:
+        return str(minutes) + 'm'
+    else:
+        hours = minutes // 60
+        minutes = minutes - (hours * 60)
+        return str(hours) + 'h ' + str(minutes) + 'm'
+
+
+# this function converts file_size to KiB or MiB or GiB
+def humanReadableSize(size, input_type='file_size'):
+    labels = ['KiB', 'MiB', 'GiB', 'TiB']
+    i = -1
+    if size < 1024:
+        return size, 'B'
+
+    while size >= 1024:
+        i += 1
+        size = size / 1024
+
+    if input_type == 'speed':
+        j = 0
+    else:
+        j = 1
+
+    if i > j:
+        return round(size, 2), labels[i]
+    else:
+        return round(size, None), labels[i]
+
+
+def convertSize(size, unit):
+    if unit == 'B':
+        converted_size = size
+    elif unit == 'KiB':
+        converted_size = size / 1024
+    elif unit == 'MiB':
+        converted_size = size / 1024**2
+    elif unit == 'GiB':
+        converted_size = size / 1024**3
+    elif unit == 'TiB':
+        converted_size = size / 1024**4
+
+    return round(converted_size, 2)
+
+
+# this method get http header as string and convert it to dictionary
+def convertHeaderToDictionary(headers):
+    dic = {}
+    for line in headers.split("\n"):
+        if line.startswith(("GET", "POST")):
+            continue
+        point_index = line.find(":")
+        dic[line[:point_index].strip()] = line[point_index + 1:].strip()
+    return dic
+
+
+def readCookieJar(load_cookies):
+    jar = None
+    if os.path.isfile(load_cookies):
+        # Open cookie file
+        cookies_txt = open(load_cookies, 'r')
+
+        # Initialize RequestsCookieJar
+        jar = requests.cookies.RequestsCookieJar()
+
+        for line in cookies_txt.readlines():
+            words = line.split()
+
+            # Filter out lines that don't contain cookies
+            if (len(words) == 7) and (words[0] != "#"):
+                # Split cookies into the appropriate parameters
+                jar.set(words[5], words[6], domain=words[0], path=words[2])
+
+        return jar
+
+
+# get file name from link string.
+def getFileNameFromLink(link):
+    link = requests.utils.unquote(link)
+    parsed_linkd = urllib.parse.urlparse(link)
+    file_name = Path(parsed_linkd.path).name
+
+    return file_name
+
+
+# this function checks free space in hard disk.
+def freeSpace(dir):
+    try:
+        import psutil
+    except:
+        print('psutil is not installed!')
+        return None
+
+    try:
+        dir_space = psutil.disk_usage(dir)
+        free_space = dir_space.free
+        return int(free_space)
+
+    except Exception as e:
+        # log in to the log file
+        print('persepolis couldn\'t find free space value:\n' + str(e))
+        return None
+
+
+# Return a new name for the file, if a file with the current name exists.
+def returnNewFileName(folder_path, file_name):
+    i = 1
+    file_path = os.path.join(folder_path, file_name)
+
+    # If file is already exists.
+    while os.path.isfile(file_path):
+        # split file name to file_name + extension
+        file_name_split = list(os.path.splitext(file_name))
+
+        # add _i to the end of file name
+
+        if file_name_split[0][-2] == '_':
+            try:
+                # check the last character of file_name.
+                # If it's integer, add 1 to it.
+                j = file_name_split[0][-1]
+
+                j = int(j)
+                j += 1
+                file_name = file_name_split[0][:-1] + str(j) + file_name_split[-1]
+            except:
+                file_name = file_name_split[0] + '_' + str(i) + file_name_split[-1]
+                i += 1
+        else:
+            file_name = file_name_split[0] + '_' + str(i) + file_name_split[-1]
+            i += 1
+
+        # create new file_path
+        file_path = os.path.join(folder_path, file_name)
+
+    return file_name
---
+++ packages/persepolis-lib/persepolis_lib-1.1.2.tar.gz/persepolis_lib-1.1.2/test/test.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import signal
+import argparse
+import sys
+import os
+import platform
+
+# finding os platform
+os_type = platform.system()
+
+# Don't run persepolis as root!
+if os_type == 'Linux' or os_type == 'FreeBSD' or os_type == 'OpenBSD' or os_type == 'Darwin':
+    uid = os.getuid()
+    if uid == 0:
+        print('Do not run persepolis_lib as root.')
+        sys.exit(1)
+
+
+cwd = os.path.abspath(__file__)
+run_dir = os.path.dirname(cwd)
+# if persepolis run in test folder
+print('persepolis_lib is running from test folder')
+parent_dir = os.path.dirname(run_dir)
+
+sys.path.insert(0, parent_dir)
+
+from persepolis_lib.persepolis_lib import Download
+
+# create  terminal arguments
+parser = argparse.ArgumentParser(
+    description='Persepolis command line download utility')
+parser.add_argument('--version',
+                    action='version', version='Persepolis lib: ' + Download.__Version__())
+parser.add_argument('--link',
+                    action='store', nargs=1,
+                    help='Download link.(Use "" for links)')
+parser.add_argument('--name',
+                    action='store', nargs=1,
+                    help='The  file  name  of  the downloaded file\
+                            with extension.')
+parser.add_argument('--number-of-threads',
+                    action='store', nargs=1, help='Number of threads.')
+parser.add_argument('--download-path', action='store', nargs=1,
+                    help='Set download path.')
+parser.add_argument('--proxy-ip', action='store', nargs=1, help='Proxy IP')
+parser.add_argument('--proxy-port', action='store', nargs=1, help='proxy port')
+parser.add_argument('--proxy-user',
+                    action='store', nargs=1, help='Proxy user name')
+parser.add_argument('--proxy-password', action='store', nargs=1,
+                    help='Proxy pass word')
+parser.add_argument('--proxy-type', action='store', nargs=1,
+                    help='set type proxy. http or socks5')
+parser.add_argument('--download-user', action='store', nargs=1,
+                    help='Download user name')
+parser.add_argument('--download-password', action='store', nargs=1,
+                    help='Download pass word')
+parser.add_argument('--header', action='store', nargs=1,
+                    help='Append HEADER to HTTP request header.')
+parser.add_argument('--user-agent', action='store', nargs=1,
+                    help='Set user agent for HTTP(S) downloads.')
+parser.add_argument('--cookie', action='store', nargs=1, help='Cookies file path')
+parser.add_argument('--referrer', action='store', nargs=1,
+                    help='Set an http referrer')
+parser.add_argument('--chunk-size', action='store', nargs=1,
+                    help='Chunk size in KiB, Default is 200 KiB')
+
+parser.add_argument('--check-certificate', action='store', nargs=1,
+                    help='Use certificate to verify the peers, Default is yes')
+
+args, unknownargs = parser.parse_known_args()
+
+add_link_dictionary = {'link': None,
+                       'out': None,
+                       'download_path': None,
+                       'ip': None,
+                       'port': None,
+                       'proxy_user': None,
+                       'proxy_passwd': None,
+                       'proxy_type': None,
+                       'download_user': None,
+                       'download_passwd': None,
+                       'header': None,
+                       'user_agent': None,
+                       'load_cookies': None,
+                       'referer': None}
+
+
+if args.link:
+    add_link_dictionary['link'] = "".join(args.link)
+
+if args.name:
+    add_link_dictionary['out'] = "".join(args.name)
+
+if args.download_path:
+    add_link_dictionary['download_path'] = "".join(args.download_path)
+
+if args.proxy_ip:
+    add_link_dictionary['ip'] = "".join(args.proxy_ip)
+
+if args.proxy_port:
+    add_link_dictionary['port'] = "".join(args.proxy_port)
+
+if args.proxy_user:
+    add_link_dictionary['proxy_user'] = "".join(args.proxy_user)
+
+if args.proxy_password:
+    add_link_dictionary['proxy_passwd'] = "".join(args.proxy_password)
+
+if args.proxy_type:
+    add_link_dictionary['proxy_type'] = "".join(args.proxy_type)
+
+if args.download_user:
+    add_link_dictionary['download_user'] = "".join(args.download_user)
+
+if args.download_password:
+    add_link_dictionary['download_passwd'] = "".join(args.download_password)
+
+if args.header:
+    add_link_dictionary['header'] = "".join(args.header)
+
+
+if args.user_agent:
+    add_link_dictionary['user_agent'] = "".join(args.user_agent)
+
+if args.cookie:
+    add_link_dictionary['load_cookies'] = "".join(args.cookie)
+
+if args.referrer:
+    add_link_dictionary['referer'] = "".join(args.referrer)
+
+if args.number_of_threads:
+    number_of_threads = "".join(args.number_of_threads)
+else:
+    number_of_threads = 64
+
+if args.check_certificate:
+    check_certificate_out = "".join(args.check_certificate)
+    if check_certificate_out in ['yes', 'true', 'True']:
+
+        add_link_dictionary['check_certificate'] = True
+    else:
+        add_link_dictionary['check_certificate'] = False
+else:
+    add_link_dictionary['check_certificate'] = True
+
+if args.chunk_size:
+    chunk_size = "".join(args.chunk_size)
+else:
+    chunk_size = 200
+
+
+if __name__ == '__main__':
+    # create download object
+    download_item = Download(add_link_dictionary, int(number_of_threads),
+                             int(chunk_size), progress_bar=True, threads_progress_bar=False)
+
+    # capture SIGINT signal
+    signal.signal(signal.SIGINT, download_item.stop)
+
+    # start download
+    download_item.start()

56123b753b9b506bc8fbc5c171d821016f4640f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:21 +0000

    Add package airflow-config/1.10.10/airflow_config-1.10.10.tar.gz

---
+++ packages/airflow-config/airflow_config-1.10.10.tar.gz/airflow_config-1.10.10/airflow_config/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.10.10"
+
+from .configuration import *
+
+try:
+    from .dag import DAG, create_dag, create_dags
+except ImportError:
+    # Allow airflow be removed
+    pass
+from .exceptions import *
+from .utils import generate_dag_id

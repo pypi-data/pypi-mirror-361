92e2d11359bce81a319f5b1593743fbe3bddeac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:03 +0000

    Add package wyrmx-cli/1.0.35/wyrmx_cli-1.0.35.tar.gz

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.35.tar.gz/wyrmx_cli-1.0.35/wyrmx_cli/commands/database_management/make_migration.py
@@ -0,0 +1,11 @@
+
+
+
+
+def make_migration(name: str):
+
+    """
+    Create a new database migration based on your current schemas.
+    """
+
+    
---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.35.tar.gz/wyrmx_cli-1.0.35/wyrmx_cli/utilities/file_utilities/__init__.py
@@ -0,0 +1,3 @@
+from wyrmx_cli.utilities.file_utilities.file_exists import fileExists
+from wyrmx_cli.utilities.file_utilities.insert_line import insertLine, insertLines
+from wyrmx_cli.utilities.file_utilities.replace_line import replaceLine, replaceLines

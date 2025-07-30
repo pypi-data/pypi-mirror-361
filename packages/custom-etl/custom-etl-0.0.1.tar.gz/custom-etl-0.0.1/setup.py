f0c70e81000e2d39430a8c79505ffe704dc6d107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:05 +0000

    Add package custom-etl/0.0.1/custom-etl-0.0.1.tar.gz

---
+++ packages/custom-etl/custom-etl-0.0.1.tar.gz/custom-etl-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="custom-etl",
+    version="0.0.1",
+    packages=["custom-etl"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

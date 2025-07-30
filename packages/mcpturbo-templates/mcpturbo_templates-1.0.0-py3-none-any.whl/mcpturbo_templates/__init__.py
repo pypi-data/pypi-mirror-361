7db22c87f18dbac47344641b4862f8b764944162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-templates/1.0.0/mcpturbo_templates-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-templates/mcpturbo_templates-1.0.0-py3-none-any.whl/mcpturbo_templates/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Templates
+
+MCPTurbo - Template Engine and Collections
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

be0f55e93b3a94e3bdcd75eae774744e04755fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:38 +0000

    Add package mcpturbo-enterprise/1.0.0/mcpturbo_enterprise-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-enterprise/mcpturbo_enterprise-1.0.0-py3-none-any.whl/mcpturbo_enterprise/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Enterprise
+
+MCPTurbo - Enterprise Features and Governance
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

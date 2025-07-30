a3878113164130db407950cf624465c4374eb00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:36 +0000

    Add package qt-dataviewer/0.4.5/qt_dataviewer-0.4.5.tar.gz

---
+++ packages/qt-dataviewer/qt_dataviewer-0.4.5.tar.gz/qt_dataviewer-0.4.5/qt_dataviewer/plot_ui/plots.py
@@ -0,0 +1,65 @@
+from abc import abstractmethod
+
+from PyQt5 import QtWidgets
+from qt_dataviewer.model.plot_model import PlotModel
+
+class BasePlot:
+    def __init__(self, layout: QtWidgets.QLayout, plot_model: PlotModel):
+        self._layout = layout
+        self._plot_model = plot_model
+        self.dims = plot_model.dims
+        self.ndim = plot_model.ndim
+        self.one_d_is_vertical = plot_model.one_d_is_vertical
+        # TODO add title and action buttons
+        self.create()
+
+    @abstractmethod
+    def create(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    def remove(self):
+        layout = self._layout
+        for i in reversed(range(layout.count())):
+            widget = layout.itemAt(i).widget()
+            layout.removeWidget(widget)
+            widget.setParent(None)
+
+
+
+## RESOURCES
+## https://www.pythonguis.com/tutorials/qresource-system/
+
+    # def save(self):
+    #     img = QImage(mywidget.size())
+    #     painter = QPainter(img)
+    #     mywidget.render(painter)
+    #     img.save("/some/file.jpg")
+
+
+# ## https://pyqtgraph.readthedocs.io/en/latest/user_guide/exporting.html
+
+# * With filename, no filename -> dialog, bytes -> QImage
+
+# import pyqtgraph as pg
+# import pyqtgraph.exporters
+
+# # generate something to export
+# plt = pg.plot([1,5,2,4,3])
+
+# # create an exporter instance, as an argument give it
+# # the item you wish to export
+# exporter = pg.exporters.ImageExporter(plt.plotItem)
+
+# # set export parameters if needed
+# exporter.parameters()['width'] = 100   # (note this also affects height parameter)
+
+# # save to file
+# exporter.export('fileName.png')
+
+## https://doc.qt.io/qt-6/qclipboard.html
+
+# QClipboard().setImage(exporter.export(toBytes=True))

c4f8d660174b7b3d240b87825016440fa06ca69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package smb-assistant/0.0.1/smb-assistant-0.0.1.tar.gz

---
+++ packages/smb-assistant/smb-assistant-0.0.1.tar.gz/smb-assistant-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="smb-assistant",
+    version="0.0.1",
+    packages=["smb-assistant"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

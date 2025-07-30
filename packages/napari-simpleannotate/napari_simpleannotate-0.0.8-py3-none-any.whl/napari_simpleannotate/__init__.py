f0cb952f5b354d0b8efc5b8c70bf2862b871e9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:33 +0000

    Add package napari-simpleannotate/0.0.8/napari_simpleannotate-0.0.8-py3-none-any.whl

---
+++ packages/napari-simpleannotate/napari_simpleannotate-0.0.8-py3-none-any.whl/napari_simpleannotate/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.8"
+from ._bbox_widget import BboxQWidget
+from ._labelimg_widget import LabelImgQWidget
+from ._bbox_video_widget import BboxVideoQWidget
+
+__all__ = ("BboxQWidget", "LabelImgQWidget", "BboxVideoQWidget")

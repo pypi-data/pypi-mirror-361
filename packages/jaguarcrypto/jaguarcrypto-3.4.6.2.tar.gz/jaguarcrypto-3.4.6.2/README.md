e3796c467bcc3112d269df6a1f54c50e2281a72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:25 +0000

    Add package jaguarcrypto/3.4.6.2/jaguarcrypto-3.4.6.2.tar.gz

---
+++ packages/jaguarcrypto/jaguarcrypto-3.4.6.2.tar.gz/jaguarcrypto-3.4.6.2/README.md
@@ -0,0 +1,10 @@
+
+How to install:
+pip install jaguarcrypto
+
+
+
+How to use in Python programs:
+
+from jaguarcrypto.encryptor import JaguarEncryptor
+
---
+++ packages/jaguarcrypto/jaguarcrypto-3.4.6.2.tar.gz/jaguarcrypto-3.4.6.2/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="jaguarcrypto",
+    version="3.4.6.2",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=41.0.0"
+    ],
+    author="DataJaguar, Inc.",
+    description="Encryption/decryption library compatible with JaguarDB",
+    url="https://github.com/fserv/jaguar-sdk/encryption/python/jaguarcrypto",
+    license="BSD-3-Clause",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+

10d06696c952a42b4b5f67e693e32fe23c8b5949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:42 +0000

    Add package diskcache-rs/0.1.0/diskcache_rs-0.1.0-cp310-cp310-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/diskcache-rs/diskcache_rs-0.1.0-cp310-cp310-manylinux_2_12_i686.manylinux2010_i686.whl/diskcache_rs/_diskcache_rs.pyi
@@ -0,0 +1,9 @@
+# flake8: noqa: PYI021
+def detect_diskcache_format_py(path):
+    """Python wrapper for detect_diskcache_format"""
+
+def rust_pickle_dumps(obj):
+    """High-performance pickle serialization using Rust"""
+
+def rust_pickle_loads(data):
+    """High-performance pickle deserialization using Rust"""

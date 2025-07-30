1c5e0f387f47ec978cbd006ac3f6164b2eb61e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:50 +0000

    Add package taranis-models/1.2.0.dev2/taranis_models-1.2.0.dev2-py3-none-any.whl

---
+++ packages/taranis-models/taranis_models-1.2.0.dev2-py3-none-any.whl/models/__init__.py
@@ -0,0 +1,19 @@
+from models.admin import *  # noqa: F401
+from models.types import *  # noqa: F401
+from models.base import *  # noqa: F401
+from models.dashboard import *  # noqa: F401
+
+__all__ = [
+    "Job",
+    "Address",
+    "Organization",
+    "Role",
+    "User",
+    "Permissions",
+    "Dashboard",
+    "TrendingTag",
+    "TrendingClusters",
+    "CacheObject",
+    "TaranisBaseModel",
+    "TLPLevel",
+]

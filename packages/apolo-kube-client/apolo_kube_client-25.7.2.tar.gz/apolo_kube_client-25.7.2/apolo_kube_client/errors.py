50db618c6f7b0c655575aa8787a77b171605b5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:50 +0000

    Add package apolo-kube-client/25.7.2/apolo_kube_client-25.7.2.tar.gz

---
+++ packages/apolo-kube-client/apolo_kube_client-25.7.2.tar.gz/apolo_kube_client-25.7.2/apolo_kube_client/errors.py
@@ -0,0 +1,30 @@
+class KubeClientException(Exception):
+    """Base kube client error"""
+
+
+class ResourceNotFound(KubeClientException):
+    pass
+
+
+class ResourceInvalid(KubeClientException):
+    pass
+
+
+class ResourceExists(KubeClientException):
+    pass
+
+
+class ResourceBadRequest(KubeClientException):
+    pass
+
+
+class ResourceGone(KubeClientException):
+    pass
+
+
+class KubeClientUnauthorized(KubeClientException):
+    pass
+
+
+class KubeClientExpired(KubeClientException):
+    pass

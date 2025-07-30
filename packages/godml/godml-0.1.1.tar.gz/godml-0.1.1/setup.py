20c8c6f14ab44d9c2058db1f6d01e3fe2ad4f557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:53 +0000

    Add package godml/0.1.1/godml-0.1.1.tar.gz

---
+++ packages/godml/godml-0.1.1.tar.gz/godml-0.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="godml",
+    version="0.1.1",
+    description="Governed, Observable & Declarative Machine Learning CLI",
+    author="Arturo Gutierrez Rubio Rojas",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "typer[all]",
+        "pydantic",
+        "mlflow",
+        "pandas",
+        "scikit-learn",
+        "xgboost",
+        "lightgbm",
+        "tensorflow",
+    ],
+    entry_points={
+        "console_scripts": [
+            "godml=godml.godml_cli:main",
+        ],
+    },
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)

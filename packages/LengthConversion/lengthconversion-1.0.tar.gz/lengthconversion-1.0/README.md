688d5460402cf62ec4fe8adb802bdec5a2a3d676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:45 +0000

    Add package LengthConversion/1.0/lengthconversion-1.0.tar.gz

---
+++ packages/LengthConversion/lengthconversion-1.0.tar.gz/lengthconversion-1.0/README.md
@@ -0,0 +1,2 @@
+						Open-Source
+Length Unit Conversion Library is a library which helps conversion of length units such as Kilometers, Miles, Millimeters.
---
+++ packages/LengthConversion/lengthconversion-1.0.tar.gz/lengthconversion-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='LengthConversion',
+	version='1.0',
+ 	description="Module That Lets You Convert Units Of Length In Easy Way.",
+	packages=find_packages(),
+	install_requires=[
+	],
+)

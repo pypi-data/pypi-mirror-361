6d36464fb9a0092835b0b55b87a3e75ad539c0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:50 +0000

    Add package pyinterceptor-hotkeys/0.1.2/pyinterceptor_hotkeys-0.1.2.tar.gz

---
+++ packages/pyinterceptor-hotkeys/pyinterceptor_hotkeys-0.1.2.tar.gz/pyinterceptor_hotkeys-0.1.2/pyinterceptor/__init__.py
@@ -0,0 +1,17 @@
+from .core import Interception, HotkeyManager, Keyboard, Mouse, Device
+from .defs import Key, KeyState, KeyStroke, MouseButton, MouseState, MouseFlag, MouseStroke
+
+__all__ = [
+    "Interception",
+    "Device",
+    "HotkeyManager",
+    "Keyboard",
+    "Mouse",
+    "Key",
+    "KeyState",
+    "KeyStroke",
+    "MouseButton",
+    "MouseState",
+    "MouseFlag",
+    "MouseStroke",
+]

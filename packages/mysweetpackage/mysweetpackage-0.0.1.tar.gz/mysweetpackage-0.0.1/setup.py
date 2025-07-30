7d09a676746aea993eabfc87f675268b9d7ba3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:42 +0000

    Add package mysweetpackage/0.0.1/mysweetpackage-0.0.1.tar.gz

---
+++ packages/mysweetpackage/mysweetpackage-0.0.1.tar.gz/mysweetpackage-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mysweetpackage",
+    version="0.0.1",
+    author="CHRISTIAN NGAPGUE",
+    author_email="xiandev25@gmail.com",
+    description="Test-driving Python package creation",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/xiandev25/mysweetpackage",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        # Liste des dépendances
+    ],
+)

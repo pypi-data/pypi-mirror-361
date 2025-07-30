293229beaba4c97339b22bfdcba27b3fac2ead6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:10 +0000

    Add package nonebot-plugin-value/0.0.8/nonebot_plugin_value-0.0.8.tar.gz

---
+++ packages/nonebot-plugin-value/nonebot_plugin_value-0.0.8.tar.gz/nonebot_plugin_value-0.0.8/src/nonebot_plugin_value/uuid_lib.py
@@ -0,0 +1,8 @@
+from uuid import UUID, uuid5
+
+# UUID namespace常量定义。
+NAMESPACE_VALUE = UUID("e6fec076-98df-4979-8618-36ad04dea39f")
+
+
+def to_uuid(s: str) -> str:
+    return uuid5(NAMESPACE_VALUE, s).hex

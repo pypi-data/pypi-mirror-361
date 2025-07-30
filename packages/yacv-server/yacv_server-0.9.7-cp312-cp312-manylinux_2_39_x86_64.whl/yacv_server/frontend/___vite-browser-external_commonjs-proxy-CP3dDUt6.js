d309fb9c7740a8b2dd53748f5d9d6d365c5b16da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:43 +0000

    Add package yacv-server/0.9.7/yacv_server-0.9.7-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/yacv-server/yacv_server-0.9.7-cp312-cp312-manylinux_2_39_x86_64.whl/yacv_server/frontend/___vite-browser-external_commonjs-proxy-CP3dDUt6.js
@@ -0,0 +1,2 @@
+import{bR as e}from"./index-DOwMiaD3.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-CP3dDUt6.js.map

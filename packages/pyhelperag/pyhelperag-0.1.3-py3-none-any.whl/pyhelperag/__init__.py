bf213e176d64327d13deb6edb5a1a267ed1ea14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:54 +0000

    Add package pyhelperag/0.1.3/pyhelperag-0.1.3-py3-none-any.whl

---
+++ packages/pyhelperag/pyhelperag-0.1.3-py3-none-any.whl/pyhelperag/__init__.py
@@ -0,0 +1,11 @@
+
+from .file_utils import *
+from .dict_utils import *
+from .time_utils import *
+from .system_utils import *
+from .secure_utils import *
+from .math_utils import *
+from .dataframe_utils import *
+from .password_utils import *
+from .memory_utils import *
+from .zip_utils import *
---
+++ packages/pyhelperag/pyhelperag-0.1.3-py3-none-any.whl/pyhelperag/zip_utils.py
@@ -0,0 +1,10 @@
+import zipfile
+import os
+
+def zip_folder(folder_path, output_path):
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                full_path = os.path.join(root, file)
+                arcname = os.path.relpath(full_path, folder_path)
+                zipf.write(full_path, arcname)

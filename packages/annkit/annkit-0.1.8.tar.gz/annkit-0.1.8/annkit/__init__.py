7d5113675074aa1b315eaee71de0ceac034adc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:25 +0000

    Add package annkit/0.1.8/annkit-0.1.8.tar.gz

---
+++ packages/annkit/annkit-0.1.8.tar.gz/annkit-0.1.8/annkit/__init__.py
@@ -0,0 +1,11 @@
+"""
+annkit - A neural network library with visualization tools.
+"""
+
+__version__ = "0.1.8"
+
+from annkit.basekit import net,class_optimizer
+
+__all__=['net','class_optimizer']
+
+print("✅ annkit loaded!")

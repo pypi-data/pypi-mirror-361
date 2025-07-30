be7da94eac14fa0b32e38f9360664b8d1bfc870b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package mmanalysis/1.0.1/mmanalysis-1.0.1.tar.gz

---
+++ packages/mmanalysis/mmanalysis-1.0.1.tar.gz/mmanalysis-1.0.1/mmanalysis/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+"""
+Created on Tue Dec 20 11:12:01 2022
+
+@author: Tim Kodalle
+"""
+
+# package info
+__version__ = '1.0.1'
+__date__    = "13 Jun. 2025"
+__author__  = "sutterfellalab"

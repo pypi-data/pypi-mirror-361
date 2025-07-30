53b355611519c0e74bdcb2223555b5bc988fe508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:21 +0000

    Add package samplehc/0.3.6/samplehc-0.3.6-py3-none-any.whl

---
+++ packages/samplehc/samplehc-0.3.6-py3-none-any.whl/samplehc/resources/v2/documents/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .legacy import (
+    LegacyResource,
+    AsyncLegacyResource,
+    LegacyResourceWithRawResponse,
+    AsyncLegacyResourceWithRawResponse,
+    LegacyResourceWithStreamingResponse,
+    AsyncLegacyResourceWithStreamingResponse,
+)
+from .documents import (
+    DocumentsResource,
+    AsyncDocumentsResource,
+    DocumentsResourceWithRawResponse,
+    AsyncDocumentsResourceWithRawResponse,
+    DocumentsResourceWithStreamingResponse,
+    AsyncDocumentsResourceWithStreamingResponse,
+)
+from .templates import (
+    TemplatesResource,
+    AsyncTemplatesResource,
+    TemplatesResourceWithRawResponse,
+    AsyncTemplatesResourceWithRawResponse,
+    TemplatesResourceWithStreamingResponse,
+    AsyncTemplatesResourceWithStreamingResponse,
+)
+from .pdf_template import (
+    PdfTemplateResource,
+    AsyncPdfTemplateResource,
+    PdfTemplateResourceWithRawResponse,
+    AsyncPdfTemplateResourceWithRawResponse,
+    PdfTemplateResourceWithStreamingResponse,
+    AsyncPdfTemplateResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "LegacyResource",
+    "AsyncLegacyResource",
+    "LegacyResourceWithRawResponse",
+    "AsyncLegacyResourceWithRawResponse",
+    "LegacyResourceWithStreamingResponse",
+    "AsyncLegacyResourceWithStreamingResponse",
+    "TemplatesResource",
+    "AsyncTemplatesResource",
+    "TemplatesResourceWithRawResponse",
+    "AsyncTemplatesResourceWithRawResponse",
+    "TemplatesResourceWithStreamingResponse",
+    "AsyncTemplatesResourceWithStreamingResponse",
+    "PdfTemplateResource",
+    "AsyncPdfTemplateResource",
+    "PdfTemplateResourceWithRawResponse",
+    "AsyncPdfTemplateResourceWithRawResponse",
+    "PdfTemplateResourceWithStreamingResponse",
+    "AsyncPdfTemplateResourceWithStreamingResponse",
+    "DocumentsResource",
+    "AsyncDocumentsResource",
+    "DocumentsResourceWithRawResponse",
+    "AsyncDocumentsResourceWithRawResponse",
+    "DocumentsResourceWithStreamingResponse",
+    "AsyncDocumentsResourceWithStreamingResponse",
+]

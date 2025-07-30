b593888552d8f979f7e19ec3f1c3ee607475abaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:36 +0000

    Add package HAPI-SDK/0.2.16/hapi_sdk-0.2.16.tar.gz

---
+++ packages/HAPI-SDK/hapi_sdk-0.2.16.tar.gz/hapi_sdk-0.2.16/dehallucinate_sdk/__init__.py
@@ -0,0 +1,16 @@
+"""
+HAPI SDK - A plug-and-play SDK for de-hallucinating outputs from LLMs
+
+This package provides tools to detect and reduce hallucinations in Large Language Model outputs
+using semantic entropy analysis and trained classifiers.
+"""
+
+from .client import DeHallucinationClient
+
+__version__ = "0.2.16"
+__author__ = "True"
+__email__ = "cyprienseydoux@gmail.com"
+
+# Expose the main class for easy import
+__all__ = ["DeHallucinationClient"]
+

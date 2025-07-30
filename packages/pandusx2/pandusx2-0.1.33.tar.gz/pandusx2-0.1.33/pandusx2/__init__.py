fc8269464c13b7ec3526b9033d3303118b56b655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:41 +0000

    Add package pandusx2/0.1.33/pandusx2-0.1.33.tar.gz

---
+++ packages/pandusx2/pandusx2-0.1.33.tar.gz/pandusx2-0.1.33/pandusx2/__init__.py
@@ -0,0 +1,21 @@
+from .main import (
+    prac_2,
+    prac_3,
+    prac_4,
+    prac_5,
+    prac_7,
+    prac_8,
+    prac_9,
+    prac_10
+)
+
+__all__ = [
+    'prac_2',
+    'prac_3',
+    'prac_4',
+    'prac_5',
+    'prac_7',
+    'prac_8',
+    'prac_9',
+    'prac_10'
+] 
---
+++ packages/pandusx2/pandusx2-0.1.33.tar.gz/pandusx2-0.1.33/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandusx2",
+    version="0.1.33",
+    packages=find_packages(),
+    description="Pandus package for koding sma",
+    author="Your Name",
+    install_requires=[
+        
+    ],
+    python_requires=">=3.6",
+)

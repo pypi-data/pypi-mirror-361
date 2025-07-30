c9972181b7623deca653f7609d696390a597a32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:22 +0000

    Add package 2captcha-python-async/1.5.1/2captcha_python_async-1.5.1.tar.gz

---
+++ packages/2captcha-python-async/2captcha_python_async-1.5.1.tar.gz/2captcha_python_async-1.5.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import re
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def get_version():
+    with open('twocaptcha/__init__.py', 'r', encoding="utf-8") as f:
+        return re.search(r'__version__ = ["\'](.*?)["\']', f.read()).group(1)
+
+
+setup(name='2captcha-python-async',
+      version=get_version(),
+      description='Python module for easy integration with 2Captcha API',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/techinz/2captcha-python-async',
+      install_requires=['requests', 'httpx', 'aiofiles'],
+      author='2Captcha',
+      author_email='info@2captcha.com',
+      packages=find_packages(),
+      include_package_data=True,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Scientific/Engineering :: Image Recognition",
+          "Topic :: Utilities",
+          "Intended Audience :: Developers",
+      ],
+      keywords=[
+          '2captcha', 'captcha', 'api', 'captcha solver', 'reCAPTCHA',
+          'FunCaptcha', 'Geetest', 'image captcha', 'Coordinates', 'Click Captcha',
+          'Geetest V4', 'Lemin captcha', 'Amazon WAF', 'Cloudflare Turnstile',
+          'Capy Puzzle', 'MTCaptcha', 'Friendly Captcha', 'Tencent', 'Cutcaptcha', 'DataDome', 'cybersiara'],
+      python_requires='>=3.6',
+      test_suite='tests')

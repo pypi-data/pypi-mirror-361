84960e71495fca97de2d3d43cab58aaf189c1ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:35 +0000

    Add package crpt1/1.8.0/crpt1-1.8.0.tar.gz

---
+++ packages/crpt1/crpt1-1.8.0.tar.gz/crpt1-1.8.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='crpt1',
+    version='1.8.0',
+    author='EzTNi8cX995m',
+    author_email='ltKHiU@olwBX.com',
+    description='Lightweight system & cryptography utilities with helper functions.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://ZbdcQ5H6.com',
+    packages=['crpt1'],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

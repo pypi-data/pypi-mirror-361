fd63070858f61b051a876aaa03b903442c91e4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:25 +0000

    Add package LXu-leetcode/0.2/LXu_leetcode-0.2.tar.gz

---
+++ packages/LXu-leetcode/LXu_leetcode-0.2.tar.gz/LXu_leetcode-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='LXu_leetcode',
+    version='0.2',
+    packages=find_packages(),
+    description='Reusable algorithm templates for LeetCode problems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Zhihui Xu',
+    author_email='your@email.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

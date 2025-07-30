d6515aa51e15114d01b1598cca4ea0fa25d05948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:05 +0000

    Add package aggregates-yt/0.0.1/aggregates-yt-0.0.1.tar.gz

---
+++ packages/aggregates-yt/aggregates-yt-0.0.1.tar.gz/aggregates-yt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="aggregates-yt",
+    version="0.0.1",
+    packages=["aggregates-yt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

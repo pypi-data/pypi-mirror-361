185741377b8d9914af572a37fb046d367c2de18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:17 +0000

    Add package textcleaner-partha/0.1.7/textcleaner_partha-0.1.7.tar.gz

---
+++ packages/textcleaner-partha/textcleaner_partha-0.1.7.tar.gz/textcleaner_partha-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textcleaner-partha",
+    version="0.1.7",
+    description="Reusable text preprocessing library for NLP tasks",
+    author="Dr. Partha Majumdar",
+    author_email="partha.majumdar@hotmail.com",
+    url="https://github.com/partha6369/textcleaner",
+    packages=['textcleaner_partha'],
+    install_requires=[
+        "spacy>=3.0.0",
+        "autocorrect==0.4.4",
+        "contractions>=0.1.73",
+        "beautifulsoup4>=4.9.3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

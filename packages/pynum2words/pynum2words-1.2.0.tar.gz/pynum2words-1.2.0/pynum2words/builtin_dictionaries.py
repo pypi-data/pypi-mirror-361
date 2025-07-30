50d5486caa57ce06bba459a0ed1af4a36a812c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:17 +0000

    Add package pynum2words/1.2.0/pynum2words-1.2.0.tar.gz

---
+++ packages/pynum2words/pynum2words-1.2.0.tar.gz/pynum2words-1.2.0/pynum2words/builtin_dictionaries.py
@@ -0,0 +1,104 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Licensed under the Apache License, Version 2.0 (the "License");
+
+dictionaries_file_path = "pynum2words/dictionaries/"
+
+def get_dictionary_file_path(dictionary_name):
+    return f"{dictionaries_file_path}{dictionary_name}.n2w"
+
+def afrikaans_dictionary():
+    return get_dictionary_file_path("afrikaans")
+
+def amharic_dictionary():
+    return get_dictionary_file_path("amharic")
+
+def arabic_dictionary():
+    return get_dictionary_file_path("arabic")
+
+def armenian_dictionary():
+    return get_dictionary_file_path("armenian")
+
+def bengali_dictionary():
+    return get_dictionary_file_path("bengali")
+
+def chinese_dictionary():
+    return get_dictionary_file_path("chinese")
+
+def dutch_dictionary():
+    return get_dictionary_file_path("dutch")
+
+def english_dictionary():
+    return get_dictionary_file_path("english")
+
+def finnish_dictionary():
+    return get_dictionary_file_path("finnish")
+
+def french_dictionary():
+    return get_dictionary_file_path("french")
+
+def german_dictionary():
+    return get_dictionary_file_path("german")
+
+def greek_dictionary():
+    return get_dictionary_file_path("greek")
+
+def hebrew_dictionary():
+    return get_dictionary_file_path("hebrew")
+
+def hindi_dictionary():
+    return get_dictionary_file_path("hindi")
+
+def hungarian_dictionary():
+    return get_dictionary_file_path("hungarian")
+
+def italian_dictionary():
+    return get_dictionary_file_path("italian")
+
+def japanese_dictionary():
+    return get_dictionary_file_path("japanese")
+
+def kannada_dictionary():
+    return get_dictionary_file_path("kannada")
+
+def oromo_dictionary():
+    return get_dictionary_file_path("oromo")
+
+def korean_dictionary():
+    return get_dictionary_file_path("korean")
+
+def malay_dictionary():
+    return get_dictionary_file_path("malay")
+
+def persian_dictionary():
+    return get_dictionary_file_path("persian")
+
+def portuguese_dictionary():
+    return get_dictionary_file_path("portuguese")
+
+def romanian_dictionary():
+    return get_dictionary_file_path("romanian")
+
+def russian_dictionary():
+    return get_dictionary_file_path("russian")
+
+def spanish_dictionary():
+    return get_dictionary_file_path("spanish")
+
+def swahili_dictionary():
+    return get_dictionary_file_path("swahili")
+
+def tigrinya_dictionary():
+    return get_dictionary_file_path("tigrinya")
+
+def turkish_dictionary():
+    return get_dictionary_file_path("turkish")

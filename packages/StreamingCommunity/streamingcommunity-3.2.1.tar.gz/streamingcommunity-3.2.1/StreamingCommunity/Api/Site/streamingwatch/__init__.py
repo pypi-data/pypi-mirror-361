651bbd0cb3775c9259f806ccee1e0e1e0ad6f99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:52 +0000

    Add package StreamingCommunity/3.2.1/streamingcommunity-3.2.1.tar.gz

---
+++ packages/StreamingCommunity/streamingcommunity-3.2.1.tar.gz/streamingcommunity-3.2.1/StreamingCommunity/Api/Site/streamingwatch/__init__.py
@@ -0,0 +1,102 @@
+# 29.04.25
+
+# External library
+from rich.console import Console
+from rich.prompt import Prompt
+
+
+# Internal utilities
+from StreamingCommunity.Api.Template import get_select_title
+from StreamingCommunity.Lib.Proxies.proxy import ProxyFinder
+from StreamingCommunity.Api.Template.config_loader import site_constant
+from StreamingCommunity.Api.Template.Class.SearchType import MediaItem
+
+
+# Logic class
+from .site import title_search, table_show_manager, media_search_manager
+from .film import download_film
+from .series import download_series
+
+
+# Variable
+indice = 7
+_useFor = "Film_&_Serie"
+_priority = 0
+_engineDownload = "hls"
+_deprecate = False
+
+msg = Prompt()
+console = Console()
+proxy = None
+
+
+def get_user_input(string_to_search: str = None):
+    """
+    Asks the user to input a search term.
+    Handles both Telegram bot input and direct input.
+    """
+    string_to_search = msg.ask(f"\n[purple]Insert a word to search in [green]{site_constant.SITE_NAME}").strip()
+    return string_to_search
+
+def process_search_result(select_title, selections=None, proxy=None):
+    """
+    Handles the search result and initiates the download for either a film or series.
+    
+    Parameters:
+        select_title (MediaItem): The selected media item
+        selections (dict, optional): Dictionary containing selection inputs that bypass manual input
+                                    {'season': season_selection, 'episode': episode_selection}
+    """
+    if select_title.type == 'tv':
+        season_selection = None
+        episode_selection = None
+        
+        if selections:
+            season_selection = selections.get('season')
+            episode_selection = selections.get('episode')
+
+        download_series(select_title, season_selection, episode_selection, proxy)
+
+    else:
+        download_film(select_title, proxy)
+
+def search(string_to_search: str = None, get_onlyDatabase: bool = False, direct_item: dict = None, selections: dict = None):
+    """
+    Main function of the application for search.
+
+    Parameters:
+        string_to_search (str, optional): String to search for
+        get_onlyDatabase (bool, optional): If True, return only the database object
+        direct_item (dict, optional): Direct item to process (bypass search)
+        selections (dict, optional): Dictionary containing selection inputs that bypass manual input
+                                    {'season': season_selection, 'episode': episode_selection}
+    """
+    if direct_item:
+        select_title = MediaItem(**direct_item)
+        process_search_result(select_title, selections) # DONT SUPPORT PROXY FOR NOW
+        return
+    
+    # Check proxy if not already set
+    finder = ProxyFinder(site_constant.FULL_URL)
+    proxy = finder.find_fast_proxy()
+
+    if string_to_search is None:
+        string_to_search = msg.ask(f"\n[purple]Insert a word to search in [green]{site_constant.SITE_NAME}").strip()
+    
+    # Perform search on the database using the obtained query
+    finder = ProxyFinder(url=f"{site_constant.FULL_URL}/serie/euphoria/")
+    proxy = finder.find_fast_proxy()
+    len_database = title_search(string_to_search, proxy)
+
+    # If only the database is needed, return the manager
+    if get_onlyDatabase:
+        return media_search_manager
+    
+    if len_database > 0:
+        select_title = get_select_title(table_show_manager, media_search_manager,len_database)
+        process_search_result(select_title, selections, proxy)
+    
+    else:
+        # If no results are found, ask again
+        console.print(f"\n[red]Nothing matching was found for[white]: [purple]{string_to_search}")
+        search()

001d32c41f4ce4b43ec43493c4522909309b80f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:08:12 +0000

    Add package permit-fastmcp/0.1.0/permit_fastmcp-0.1.0.tar.gz

---
+++ packages/permit-fastmcp/permit_fastmcp-0.1.0.tar.gz/permit_fastmcp-0.1.0/permit_fastmcp/__init__.py
@@ -0,0 +1,9 @@
+"""
+permit_fastmcp: Permit.io authorization middleware for FastMCP servers.
+
+Provides seamless integration between FastMCP and Permit.io for fine-grained access control.
+"""
+
+from . import middleware
+
+__all__ = ["middleware"]

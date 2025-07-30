5bcffdce3c5db3a2500f623a9e82483ca80b0d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:56 +0000

    Add package image-dataset-converter-all/0.0.12/image_dataset_converter_all-0.0.12.tar.gz

---
+++ packages/image-dataset-converter-all/image_dataset_converter_all-0.0.12.tar.gz/image_dataset_converter_all-0.0.12/README.md
@@ -0,0 +1,25 @@
+# image-dataset-converter-all
+Meta-library that combines all [image-dataset-converter](https://github.com/waikato-datamining/image-dataset-converter) libraries.
+
+
+## Installation
+
+Via PyPI:
+
+```bash
+pip install image_dataset_converter_all
+```
+
+The latest code straight from the repository:
+
+```bash
+pip install git+https://github.com/waikato-datamining/image-dataset-converter-all.git
+```
+
+
+## Docker
+
+[Docker](docker) images are available from:
+
+* Docker hub: [waikatodatamining/image-dataset-converter](https://hub.docker.com/r/waikatodatamining/image-dataset-converter)
+* In-house registry: `public.aml-repo.cms.waikato.ac.nz:443/tools/image-dataset-converter`
---
+++ packages/image-dataset-converter-all/image_dataset_converter_all-0.0.12.tar.gz/image_dataset_converter_all-0.0.12/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="image_dataset_converter_all",
+    description="Meta-library that combines all image_dataset_converter libraries.",
+    long_description=(
+            _read('DESCRIPTION.rst') + b'\n' +
+            _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-datamining/image-dataset-converter-all",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Processing',
+    ],
+    license='MIT License',
+    packages=[],
+    install_requires=[
+        "image_dataset_converter>=0.0.12",
+        "image_dataset_converter_imgaug>=0.0.10",
+        "image_dataset_converter_imgstats>=0.0.2",
+        "image_dataset_converter_imgvis>=0.0.5",
+        "image_dataset_converter_labelme>=0.0.3",
+        "image_dataset_converter_paddle>=0.0.4",
+        "image_dataset_converter_pdf>=0.0.3",
+        "image_dataset_converter_plantcv>=0.0.1",
+        "image_dataset_converter_redis>=0.0.4",
+        "image_dataset_converter_video>=0.0.5",
+    ],
+    version="0.0.12",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+)

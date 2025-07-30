3fc1cd39856e9ab66b423bf3570b33199323b015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:29 +0000

    Add package gsl-331/1.0.0/gsl_331-1.0.0.tar.gz

---
+++ packages/gsl-331/gsl_331-1.0.0.tar.gz/gsl_331-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# Read the contents of README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gsl_331",
+    version="1.0.0",
+    author="CHICH",
+    author_email="cbe05@mail.aub.edu",  
+    description="A comprehensive Python library for data structures and algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # No external dependencies - uses only Python standard library
+    ],
+    keywords="data-structures algorithms queue stack graph priority-queue linked-list bfs dfs eece 331",
+)

5c0b6bcfd77cea7a2e00257d315076987ded914c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:00 +0000

    Add package xianb/1.1.5/xianb-1.1.5-py3-none-any.whl

---
+++ packages/xianb/xianb-1.1.5-py3-none-any.whl/xianbao/__init__.py
@@ -0,0 +1,7 @@
+
+from xianbao.core.Task import TaskManager
+from xianbao.core.DictData import DictManager
+import xianbao.utils.FileScanner as FileScanner
+from xianbao.core.BlockDeque import SQLiteProcessQueue
+
+__all__ = ['TaskManager', 'DictManager', 'FileScanner', 'SQLiteProcessQueue']

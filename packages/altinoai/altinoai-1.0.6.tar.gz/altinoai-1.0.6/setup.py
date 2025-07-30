373ddcc00af3e276fcfa61b36c5ab4aaa2068356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:09 +0000

    Add package altinoai/1.0.6/altinoai-1.0.6.tar.gz

---
+++ packages/altinoai/altinoai-1.0.6.tar.gz/altinoai-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='altinoai',
+    version='1.0.6',
+    packages=find_packages(),  # Automatically finds the package directories
+    install_requires=[
+        # List your package dependencies here
+    ],
+)

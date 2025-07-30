7ea8feae2eb2f14f994bceef3c524519fde1e0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:25 +0000

    Add package rinexmod/3.7.6/rinexmod-3.7.6.tar.gz

---
+++ packages/rinexmod/rinexmod-3.7.6.tar.gz/rinexmod-3.7.6/rinexmod/__init__.py
@@ -0,0 +1,9 @@
+from . import logger
+from . import rinexfile
+from . import metadata
+from . import gamit_meta
+from . import rinexmod_api
+from . import get_m3g
+
+__version__='3.7.6' #  changed automaticcaly with bump-my-version
+

a528e93186e44d3a0d13d843338cf44848bbe449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:31 +0000

    Add package kreuzberg/3.8.0/kreuzberg-3.8.0-py3-none-any.whl

---
+++ packages/kreuzberg/kreuzberg-3.8.0-py3-none-any.whl/kreuzberg/_multiprocessing/__init__.py
@@ -0,0 +1,5 @@
+"""Multiprocessing utilities for kreuzberg."""
+
+from .gmft_isolated import extract_tables_isolated, extract_tables_isolated_async
+
+__all__ = ["extract_tables_isolated", "extract_tables_isolated_async"]

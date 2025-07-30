b078456c816fe83fc5678b8342c9fbdfa5d58e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:03 +0000

    Add package openai-async-functions/0.2.5/openai_async_functions-0.2.5-py3-none-any.whl

---
+++ packages/openai-async-functions/openai_async_functions-0.2.5-py3-none-any.whl/openai_proxy/settings/__init__.py
@@ -0,0 +1,15 @@
+from openai_proxy.settings.openai_settings import (
+    DeepseekOpenAISettings,
+    OfficialOpenAISettings,
+    OpenAISettings,
+)
+from openai_proxy.settings.proxy_client_settings import (
+    OpenAIProxyClientSettings,
+)
+
+__all__ = [
+    "DeepseekOpenAISettings",
+    "OfficialOpenAISettings",
+    "OpenAIProxyClientSettings",
+    "OpenAISettings",
+]

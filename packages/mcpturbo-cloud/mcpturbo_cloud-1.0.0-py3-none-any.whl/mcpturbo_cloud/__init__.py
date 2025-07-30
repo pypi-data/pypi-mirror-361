1b3ce6f8f18f59e8cd1b98018252b6b99e8fd2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:36 +0000

    Add package mcpturbo-cloud/1.0.0/mcpturbo_cloud-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-cloud/mcpturbo_cloud-1.0.0-py3-none-any.whl/mcpturbo_cloud/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Cloud
+
+MCPTurbo - Cloud Deployment and Infrastructure
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

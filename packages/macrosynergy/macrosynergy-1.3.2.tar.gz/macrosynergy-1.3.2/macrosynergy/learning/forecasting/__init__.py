cc678071d2155a8f06a57c1c051e525536c5993c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:10 +0000

    Add package macrosynergy/1.3.2/macrosynergy-1.3.2.tar.gz

---
+++ packages/macrosynergy/macrosynergy-1.3.2.tar.gz/macrosynergy-1.3.2/macrosynergy/learning/forecasting/__init__.py
@@ -0,0 +1,52 @@
+from .linear_model import (
+    LADRegressor,
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+    ModifiedLinearRegression,
+    ModifiedSignWeightedLinearRegression,
+    ModifiedTimeWeightedLinearRegression,
+)
+
+from .model_systems import (
+    CorrelationVolatilitySystem,
+    LADRegressionSystem,
+    LinearRegressionSystem,
+    RidgeRegressionSystem,
+)
+
+from .naive_predictors import (
+    NaiveRegressor,
+)
+
+from .neighbors.nearest_neighbors import KNNClassifier
+
+from .meta_estimators import ProbabilityEstimator, FIExtractor, DataFrameTransformer
+
+from .ensemble import (
+    VotingClassifier,
+    VotingRegressor,
+)
+
+__all__ = [
+    "LADRegressor",
+    "KNNClassifier",
+    "SignWeightedLADRegressor",
+    "TimeWeightedLADRegressor",
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+    "NaiveRegressor",
+    "CorrelationVolatilitySystem",
+    "LADRegressionSystem",
+    "LinearRegressionSystem",
+    "RidgeRegressionSystem",
+    "ModifiedLinearRegression",
+    "ModifiedSignWeightedLinearRegression",
+    "ModifiedTimeWeightedLinearRegression",
+    "ProbabilityEstimator",
+    "VotingClassifier",
+    "VotingRegressor",
+    "FIExtractor",
+    "DataFrameTransformer",
+]

654b60422474afce4d833cd618dc29a361523ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:47 +0000

    Add package EstateEdgePy/0.7.0/estateedgepy-0.7.0.tar.gz

---
+++ packages/EstateEdgePy/estateedgepy-0.7.0.tar.gz/estateedgepy-0.7.0/EstateEdgePy/__init__.py
@@ -0,0 +1,7 @@
+from EstateEdgePy.metadata.__version__ import __version__
+from EstateEdgePy.src._client import EstateEdgeClient
+from EstateEdgePy.core.storage import PropertyService
+from EstateEdgePy.filters import EntityFilter, PriceRangeFilter, DateRangeFilter
+
+
+VERSION = __version__

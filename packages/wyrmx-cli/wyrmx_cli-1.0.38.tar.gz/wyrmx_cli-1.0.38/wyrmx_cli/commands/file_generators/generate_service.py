f8a8992de4de93d2ebddd7d85a6a4eb1dea8d4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:38 +0000

    Add package wyrmx-cli/1.0.38/wyrmx_cli-1.0.38.tar.gz

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.38.tar.gz/wyrmx_cli-1.0.38/wyrmx_cli/commands/file_generators/generate_service.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from wyrmx_cli.utilities.string_utilities import *
+from wyrmx_cli.utilities.file_utilities import *
+
+import typer
+
+
+def generate_service(name: str):
+
+    """
+    Generate a new service. (shortcut: gs)
+    """
+
+    serviceName = pascalcase(name, "Service")
+    serviceFilename = snakecase(name, "_service")
+
+
+    template = (
+        f"from wyrmx_core import service\n\n"
+        f"@service\n"
+        f"class {serviceName}:\n\n"
+        f"    def __init__(self):\n"
+        f"        pass\n\n"
+        f"    # Add your methods here\n"
+    )
+
+    serviceFolder = Path().cwd() / "src" / "services"
+    serviceFolder.mkdir(parents=True, exist_ok=True)
+
+    service = serviceFolder / f"{serviceFilename}.py"
+    fileExists(service, serviceFilename, "Service")
+
+    service.write_text(template)
+    typer.echo(f"✅ Created service: {service.resolve()}")

5248592fbd06ef7996bf7b4220386e378261531b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:17 +0000

    Add package streamlithelpers/0.4.0/streamlithelpers-0.4.0.tar.gz

---
+++ packages/streamlithelpers/streamlithelpers-0.4.0.tar.gz/streamlithelpers-0.4.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='streamlithelpers',
+    packages=['streamlithelpers'],
+    version='0.4.0',
+    license='MIT',
+    description='Simple utilities to help streamlit app development',
+    author='Andrew Robertson',
+    author_email='',
+    url='https://github.com/andehr/streamlit-helpers',
+    download_url='https://github.com/andehr/streamlit-helpers/archive/refs/tags/v0.2.1.tar.gz',
+    keywords=['streamlit', 'utility'],
+    install_requires=[
+        'pandas',
+        'streamlit',
+        'streamlit-extras',
+        'streamlit-ace'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)

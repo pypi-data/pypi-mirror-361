fdbb2ed22f1575ae1474d6365d8f8aea6d9df7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:12 +0000

    Add package ob-metaflow-stubs/6.0.3.188rc4/ob-metaflow-stubs-6.0.3.188rc4.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.188rc4.tar.gz/ob-metaflow-stubs-6.0.3.188rc4/metaflow-stubs/metadata_provider/util.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-10T23:14:33.206608                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def copy_tree(src, dst, update = False):
+    ...
+
+def sync_local_metadata_to_datastore(metadata_local_dir, task_ds):
+    ...
+
+def sync_local_metadata_from_datastore(metadata_local_dir, task_ds):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.188rc4.tar.gz/ob-metaflow-stubs-6.0.3.188rc4/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/datastore/utils.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-10T23:14:33.263928                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def is_json_serializable(value):
+    """
+    Check if value is JSON serializable.
+    """
+    ...
+
+def safe_serialize(data):
+    ...
+

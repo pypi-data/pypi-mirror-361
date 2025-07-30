ae1ab1405d281b1147b6df5dd60e75a4c1c22376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:09:34 +0000

    Add package hasnainkk-07/1.0/hasnainkk_07-1.0.tar.gz

---
+++ packages/hasnainkk-07/hasnainkk_07-1.0.tar.gz/hasnainkk_07-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hasnainkk-07",
+    version="1.0",
+    author="Endtrz",
+    author_email="endtrz@gmail.com",
+    description="An enhanced Pyrogram-like filter and handler system using Kurigram.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Endtrz/hasnainkk",  # Change to your repo URL if any
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "kurigram>=2.2.0", 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

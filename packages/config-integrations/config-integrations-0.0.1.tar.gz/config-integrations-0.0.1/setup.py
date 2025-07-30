33ffaf56207d0869ea0103c88060289acd9fdba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:32 +0000

    Add package config-integrations/0.0.1/config-integrations-0.0.1.tar.gz

---
+++ packages/config-integrations/config-integrations-0.0.1.tar.gz/config-integrations-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="config-integrations",
+    version="0.0.1",
+    packages=["config-integrations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

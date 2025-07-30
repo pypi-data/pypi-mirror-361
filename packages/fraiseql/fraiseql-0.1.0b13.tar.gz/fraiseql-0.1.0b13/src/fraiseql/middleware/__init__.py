3076a7b7570c86caf629811f5f87df0ef404c848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:04 +0000

    Add package fraiseql/0.1.0b13/fraiseql-0.1.0b13.tar.gz

---
+++ packages/fraiseql/fraiseql-0.1.0b13.tar.gz/fraiseql-0.1.0b13/src/fraiseql/middleware/__init__.py
@@ -0,0 +1,37 @@
+"""Middleware components for FraiseQL."""
+
+# Import non-Redis classes first
+from .rate_limiter import (
+    InMemoryRateLimiter,
+    RateLimitConfig,
+    RateLimiterMiddleware,
+    RateLimitExceeded,
+    RateLimitInfo,
+    SlidingWindowRateLimiter,
+)
+
+# Lazy import Redis-dependent classes
+try:
+    from .rate_limiter import RedisRateLimiter
+
+    _HAS_REDIS = True
+except ImportError:
+    _HAS_REDIS = False
+
+    class RedisRateLimiter:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "Redis is required for RedisRateLimiter. "
+                "Install it with: pip install fraiseql[redis]"
+            )
+
+
+__all__ = [
+    "InMemoryRateLimiter",
+    "RateLimitConfig",
+    "RateLimitExceeded",
+    "RateLimitInfo",
+    "RateLimiterMiddleware",
+    "RedisRateLimiter",
+    "SlidingWindowRateLimiter",
+]

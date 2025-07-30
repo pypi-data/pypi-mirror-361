197e2c2fd9f5bfb37ff55c913a67075a69e95f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:23 +0000

    Add package HAPI-SDK/0.1.10/hapi_sdk-0.1.10.tar.gz

---
+++ packages/HAPI-SDK/hapi_sdk-0.1.10.tar.gz/hapi_sdk-0.1.10/dehallucinate_sdk/SE/SemanticEntropy/SE_api_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the directory containing the Flask script
+# cd /Users/aidan/audit/True/models/SE/semantic_uncertainty
+
+# Set the FLASK_APP environment variable
+export FLASK_APP=SE_api.py
+
+# Run the Flask application
+flask run

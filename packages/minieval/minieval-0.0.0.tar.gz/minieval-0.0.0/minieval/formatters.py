3200473a95c8bab469910dc2c9bbed31c3458e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:31 +0000

    Add package minieval/0.0.0/minieval-0.0.0.tar.gz

---
+++ packages/minieval/minieval-0.0.0.tar.gz/minieval-0.0.0/minieval/formatters.py
@@ -0,0 +1,45 @@
+from minieval.datatypes import Instance
+
+
+def base_model_chat_template(messages, tokenize=False):
+    assert not tokenize
+    assert len(messages) == 1
+    message = messages[0]["content"]
+    prompt = f"Question: {message}\nAnswer:"
+    return prompt
+
+
+class Formatter:
+    def __init__(self, template_func):
+        self.template_func = template_func
+
+    def build_requests(self, instances):
+        return list(map(self._build_request, instances))
+
+    def _build_request(self, instance: Instance) -> str:
+        messages = self._build_message(instance)
+        request = self.template_func(messages, tokenize=False)
+        return request
+
+    def _build_message(self):
+        raise NotImplementedError()
+
+
+class CoT(Formatter):
+    def _build_message(self, instance: Instance):
+        messages = [{"role": "user", "content": instance.question}]
+        return messages
+
+
+class MC(Formatter):
+    def _build_message(self, instance: Instance):
+        question_text = f"Question: {instance.question}\nAnswer: "
+        messages = [{"role": "user", "content": question_text}]
+        return messages
+
+
+class RC(Formatter):
+    def _build_message(self, instance: Instance):
+        question_text = f"Question: {instance.question}\nAnswer: "
+        messages = [{"role": "user", "content": question_text}]
+        return messages

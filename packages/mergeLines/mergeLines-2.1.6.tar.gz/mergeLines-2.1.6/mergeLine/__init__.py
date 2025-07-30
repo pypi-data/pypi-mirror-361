6cb2f4cfaa8e3675f28a5a54a3f6dcede0532520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:30 +0000

    Add package mergeLines/2.1.6/mergeLines-2.1.6.tar.gz

---
+++ packages/mergeLines/mergeLines-2.1.6.tar.gz/mergeLines-2.1.6/mergeLine/__init__.py
@@ -0,0 +1,1 @@
+from .tools import merge_lines
---
+++ packages/mergeLines/mergeLines-2.1.6.tar.gz/mergeLines-2.1.6/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Anaconda, Inc
+# SPDX-License-Identifier: BSD-3-Clause
+
+from setuptools import setup
+
+setup(
+    name='mergeLines',
+    packages=['mergeLine'],
+    description="merge lines after houghlinesp",
+    version='2.1.6',
+    long_description=open('readme.md').read(),
+    long_description_content_type= 'text/markdown',
+)

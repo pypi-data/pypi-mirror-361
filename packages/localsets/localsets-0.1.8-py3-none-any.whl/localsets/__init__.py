84cccf6daf735b4d71781d811d07cb97b987ba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:47 +0000

    Add package localsets/0.1.8/localsets-0.1.8-py3-none-any.whl

---
+++ packages/localsets/localsets-0.1.8-py3-none-any.whl/localsets/__init__.py
@@ -0,0 +1,27 @@
+"""
+Pokemon Random Battle Data Package
+
+A Python package providing offline access to Pokemon random battle data 
+with automatic updates from the official source repository.
+"""
+
+__version__ = "0.1.8"
+__author__ = "Pokemon RandBats Team"
+
+from .core import PokemonData, RandBatsData
+from .formats import (
+    get_pokemon, update_data, list_pokemon,
+    get_smogon_sets, list_smogon_pokemon
+)
+from .smogon.sets import SmogonSets
+
+__all__ = [
+    'PokemonData',
+    'RandBatsData',
+    'SmogonSets',
+    'get_pokemon', 
+    'update_data',
+    'list_pokemon',
+    'get_smogon_sets',
+    'list_smogon_pokemon',
+] 

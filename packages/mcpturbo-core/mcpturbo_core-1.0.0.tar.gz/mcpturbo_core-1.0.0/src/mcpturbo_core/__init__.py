d7745180e727bb53909eda4594f53b7df26df116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:40 +0000

    Add package mcpturbo-core/1.0.0/mcpturbo_core-1.0.0.tar.gz

---
+++ packages/mcpturbo-core/mcpturbo_core-1.0.0.tar.gz/mcpturbo_core-1.0.0/src/mcpturbo_core/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Core
+
+Multi-agent Communication Protocol - Core Components
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []
---
+++ packages/mcpturbo-core/mcpturbo_core-1.0.0.tar.gz/mcpturbo_core-1.0.0/src/mcpturbo_core/protocol.py
@@ -0,0 +1,12 @@
+"""MCP Protocol implementation"""
+
+class MCPProtocol:
+    """Multi-agent Communication Protocol"""
+    
+    def __init__(self):
+        self.version = "1.0.0"
+        
+    async def send_message(self, message):
+        """Send message between agents"""
+        # Implementation will be added
+        pass

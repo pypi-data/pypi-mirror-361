bf5772c7d8e40fb27fef29b564707a95ad696942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:53 +0000

    Add package endstone-ip-tracker/0.1.0/endstone_ip_tracker-0.1.0-py2.py3-none-any.whl

---
+++ packages/endstone-ip-tracker/endstone_ip_tracker-0.1.0-py2.py3-none-any.whl/endstone_ip_tracker/__init__.py
@@ -0,0 +1,9 @@
+"""
+Endstone IP Tracker Plugin
+A plugin for tracking player IP addresses in Endstone servers
+"""
+
+from .plugin import IPTrackerPlugin
+
+__version__ = "0.1.0"
+__all__ = ["IPTrackerPlugin"]

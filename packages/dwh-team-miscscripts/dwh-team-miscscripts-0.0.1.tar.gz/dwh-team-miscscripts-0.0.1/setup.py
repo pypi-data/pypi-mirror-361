7d35a3d738e9cdb49f0ddf0037f52cf2664af133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package dwh-team-miscscripts/0.0.1/dwh-team-miscscripts-0.0.1.tar.gz

---
+++ packages/dwh-team-miscscripts/dwh-team-miscscripts-0.0.1.tar.gz/dwh-team-miscscripts-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh-team-miscscripts",
+    version="0.0.1",
+    packages=["dwh-team-miscscripts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

ac2cb06f92ae97461e1c22505b970daf161d7b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package django-prose-editor/0.16.0/django_prose_editor-0.16.0.tar.gz

---
+++ packages/django-prose-editor/django_prose_editor-0.16.0.tar.gz/django_prose_editor-0.16.0/django_prose_editor/sanitized.py
@@ -0,0 +1,20 @@
+from copy import deepcopy
+
+from django_prose_editor.fields import ProseEditorField, _actually_empty
+
+
+def _nh3_sanitizer():
+    import nh3
+
+    attributes = deepcopy(nh3.ALLOWED_ATTRIBUTES)
+    attributes["a"].add("target")
+    attributes["ol"] |= {"start", "type"}
+
+    return lambda x: _actually_empty(nh3.clean(x, attributes=attributes))
+
+
+class SanitizedProseEditorField(ProseEditorField):
+    def __init__(self, *args, **kwargs):
+        if "sanitize" not in kwargs:
+            kwargs["sanitize"] = _nh3_sanitizer()
+        super().__init__(*args, **kwargs)

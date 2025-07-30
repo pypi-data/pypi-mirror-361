44e1c06873faf925fb1a750fca7c8869b1a5f609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:11 +0000

    Add package sqlite-database/0.7.5/sqlite_database-0.7.5-py3-none-any.whl

---
+++ packages/sqlite-database/sqlite_database-0.7.5-py3-none-any.whl/sqlite_database/__init__.py
@@ -0,0 +1,39 @@
+"""Database"""
+
+from .models import BaseModel, model, Foreign, Primary, Unique, hook, validate
+from .database import Database
+from ._utils import null, Row, Null
+from .column import Column, text, integer, blob, real
+from .signature import op
+from .operators import this
+from .table import Table
+
+
+def test_installed():
+    """Is the module installed?"""
+    return True
+
+
+__version__ = "0.7.5"
+__all__ = [
+    "Database",
+    "Table",
+    "this",
+    "op",
+    "Column",
+    "null",
+    "Null",
+    "Row",
+    "text",
+    "integer",
+    "real",
+    "blob",
+    "BaseModel",
+    "models",
+    "Foreign",
+    "Primary",
+    "Unique",
+    "model",
+    "hook",
+    "validate"
+]

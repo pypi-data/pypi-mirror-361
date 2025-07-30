c7866abff5e1b63e1927a61caa51b66d4bcfed7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:14 +0000

    Add package senzing-core/0.3.17/senzing_core-0.3.17-py3-none-any.whl

---
+++ packages/senzing-core/senzing_core-0.3.17-py3-none-any.whl/senzing_core/szabstractfactory.py
@@ -0,0 +1,210 @@
+"""
+``senzing_core.szabstractfactory.SzAbstractFactoryCore`` is an implementation
+of the `senzing.szabstractfactory.SzAbstractFactory`_ interface that communicates with the Senzing binaries.
+
+.. _senzing.szabstractfactory.SzAbstractFactory: https://garage.senzing.com/sz-sdk-python/senzing.html#module-senzing.szabstractfactory
+"""
+
+# pylint: disable=E1101
+
+from types import TracebackType
+from typing import Any, Dict, Type, TypedDict, Union
+
+from senzing import (
+    SzAbstractFactory,
+    SzConfigManager,
+    SzDiagnostic,
+    SzEngine,
+    SzProduct,
+)
+
+from .szconfigmanager import SzConfigManagerCore
+from .szdiagnostic import SzDiagnosticCore
+from .szengine import SzEngineCore
+from .szproduct import SzProductCore
+
+# Metadata
+
+__all__ = ["SzAbstractFactoryCore", "SzAbstractFactoryParametersCore"]
+__version__ = "0.0.1"  # See https://www.python.org/dev/peps/pep-0396/
+__date__ = "2024-10-21"
+__updated__ = "2025-01-28"
+
+
+# -----------------------------------------------------------------------------
+# SzAbstractFactoryParametersCore class
+# -----------------------------------------------------------------------------
+
+
+class SzAbstractFactoryParametersCore(TypedDict, total=False):
+    """
+    SzAbstractFactoryParametersCore is used to create a dictionary that can be unpacked when creating an
+    SzAbstractFactory.
+    """
+
+    instance_name: str
+    settings: Union[str, Dict[Any, Any]]
+    config_id: int
+    verbose_logging: int
+
+
+# -----------------------------------------------------------------------------
+# SzAbstractFactoryCore class
+# -----------------------------------------------------------------------------
+
+
+class SzAbstractFactoryCore(SzAbstractFactory):
+    """
+    SzAbstractFactoryCore is a factory pattern for accessing Senzing.
+    """
+
+    # -------------------------------------------------------------------------
+    # Python dunder/magic methods
+    # -------------------------------------------------------------------------
+
+    def __init__(
+        self,
+        instance_name: str = "",
+        settings: Union[str, Dict[Any, Any]] = "",
+        config_id: int = 0,
+        verbose_logging: int = 0,
+    ) -> None:
+        """
+        Initializer.
+
+        Args:
+            instance_name (str): A name to distinguish the instances of engine objects.
+            settings (Union[str, Dict[Any, Any]]): A JSON document defining runtime configuration.
+            config_id (int, optional): Initialize with a specific configuration ID. Defaults to 0 which uses the current system DEFAULTCONFIGID.
+            verbose_logging (int, optional): Send debug statements to STDOUT. Defaults to 0.
+        """
+        self._instance_name = instance_name
+        self._settings = settings
+        self._config_id = config_id
+        self._verbose_logging = verbose_logging
+        self._is_szconfigmanager_initialized = False
+        self._is_szdiagnostic_initialized = False
+        self._is_szengine_initialized = False
+        self._is_szproduct_initialized = False
+
+    def __enter__(
+        self,
+    ) -> Any:
+        """Context Manager method."""
+        return self
+
+    def __del__(self) -> None:
+        """Destructor"""
+        self._destroy()
+
+    def __exit__(
+        self,
+        exc_type: Union[Type[BaseException], None],
+        exc_val: Union[BaseException, None],
+        exc_tb: Union[TracebackType, None],
+    ) -> None:
+        """Context Manager method."""
+        self._destroy()
+
+    @property
+    def instance_name(self) -> str:
+        """Get the instance name the abstract factory was instantiated with."""
+        return self._instance_name
+
+    @property
+    def settings(self) -> Union[str, Dict[Any, Any]]:
+        """Get the settings the abstract factory was instantiated with."""
+        return self._settings
+
+    @property
+    def config_id(self) -> int:
+        """Get the config ID the abstract factory was instantiated with. If this is 0 no config ID was specified and
+        the current system DEFAULTCONFIGID was used."""
+        return self._config_id
+
+    @property
+    def verbose_logging(self) -> int:
+        """Get the verbose logging value the abstract factory was instantiated with."""
+        return self._verbose_logging
+
+    # -------------------------------------------------------------------------
+    # SzAbstractFactory methods
+    # -------------------------------------------------------------------------
+
+    def create_configmanager(self) -> SzConfigManager:
+        result = SzConfigManagerCore()
+        if not self._is_szconfigmanager_initialized:
+            result.initialize(  # pylint: disable=W0212
+                instance_name=self._instance_name,
+                settings=self._settings,
+                verbose_logging=self._verbose_logging,
+            )
+            self._is_szconfigmanager_initialized = True
+        return result
+
+    def create_diagnostic(self) -> SzDiagnostic:
+        result = SzDiagnosticCore()
+        if not self._is_szdiagnostic_initialized:
+            result.initialize(  # pylint: disable=W0212
+                instance_name=self._instance_name,
+                settings=self._settings,
+                config_id=self._config_id,
+                verbose_logging=self._verbose_logging,
+            )
+            self._is_szdiagnostic_initialized = True
+        return result
+
+    def create_engine(self) -> SzEngine:
+        result = SzEngineCore()
+        if not self._is_szengine_initialized:
+            result.initialize(  # pylint: disable=W0212
+                instance_name=self._instance_name,
+                settings=self._settings,
+                config_id=self._config_id,
+                verbose_logging=self._verbose_logging,
+            )
+            self._is_szengine_initialized = True
+        return result
+
+    def create_product(self) -> SzProduct:
+        result = SzProductCore()
+        if not self._is_szproduct_initialized:
+            result.initialize(  # pylint: disable=W0212
+                instance_name=self._instance_name,
+                settings=self._settings,
+                verbose_logging=self._verbose_logging,
+            )
+            self._is_szproduct_initialized = True
+        return result
+
+    def _destroy(self) -> None:
+        if self._is_szconfigmanager_initialized:
+            self._is_szconfigmanager_initialized = False
+            sz_configmanager = SzConfigManagerCore()
+            sz_configmanager._destroy()  # pylint: disable=W0212
+
+        if self._is_szdiagnostic_initialized:
+            self._is_szdiagnostic_initialized = False
+            sz_diagnostic = SzDiagnosticCore()
+            sz_diagnostic._destroy()  # pylint: disable=W0212
+
+        if self._is_szengine_initialized:
+            self._is_szengine_initialized = False
+            sz_engine = SzEngineCore()
+            sz_engine._destroy()  # pylint: disable=W0212
+
+        if self._is_szproduct_initialized:
+            self._is_szproduct_initialized = False
+            sz_product = SzProductCore()
+            sz_product._destroy()  # pylint: disable=W0212
+
+    def reinitialize(self, config_id: int) -> None:
+        self._config_id = config_id
+
+        if self._is_szengine_initialized:
+            sz_engine = SzEngineCore()
+            sz_engine.reinitialize(config_id=config_id)  # pylint: disable=W0212
+
+        if self._is_szdiagnostic_initialized:
+            sz_diagnostic = SzDiagnosticCore()
+            sz_diagnostic.reinitialize(config_id=config_id)  # pylint: disable=W0212
---
+++ packages/senzing-core/senzing_core-0.3.17-py3-none-any.whl/senzing_core/szdiagnostic.py
@@ -0,0 +1,211 @@
+"""
+``senzing_core.szdiagnostic.SzDiagnosticCore`` is an implementation
+of the `senzing.szdiagnostic.SzDiagnostic`_ interface that communicates with the Senzing binaries.
+
+To use szdiagnostic,
+the **LD_LIBRARY_PATH** environment variable must include a path to Senzing's libraries.
+
+Example:
+
+.. code-block:: bash
+
+    export LD_LIBRARY_PATH=/opt/senzing/er/lib
+
+.. _senzing.szdiagnostic.SzDiagnostic: https://garage.senzing.com/sz-sdk-python/senzing.html#module-senzing.szdiagnostic
+"""
+
+# pylint: disable=R0903
+
+from ctypes import POINTER, Structure, c_char, c_char_p, c_int, c_longlong, c_void_p
+from functools import partial
+from typing import Any, Dict, Union
+
+from senzing import SzDiagnostic
+
+from ._helpers import (
+    FreeCResources,
+    as_c_char_p,
+    as_python_str,
+    as_str,
+    catch_sdk_exceptions,
+    check_result_rc,
+    load_sz_library,
+)
+
+# Metadata
+
+__all__ = ["SzDiagnosticCore"]
+__version__ = "0.0.1"  # See https://www.python.org/dev/peps/pep-0396/
+__date__ = "2023-10-30"
+__updated__ = "2025-01-28"
+
+
+# -----------------------------------------------------------------------------
+# Classes that are result structures from calls to Senzing
+# -----------------------------------------------------------------------------
+
+
+class SzResponseReturnCodeResult(Structure):
+    """Simple response, return_code structure"""
+
+    _fields_ = [
+        ("response", POINTER(c_char)),
+        ("return_code", c_longlong),
+    ]
+
+
+class SzDiagnosticCheckRepositoryPerformanceResult(SzResponseReturnCodeResult):
+    """In SzLang_helpers.h SzDiagnostic_checkRepositoryPerformance_result"""
+
+
+class SzDiagnosticGetFeatureResult(SzResponseReturnCodeResult):
+    """In SzLang_helpers.h SzDiagnostic_getFeature_result"""
+
+
+class SzDiagnosticGetRepositoryInfoResult(SzResponseReturnCodeResult):
+    """In SzLang_helpers.h SzDiagnostic_getRepositoryInfo_result"""
+
+
+# -----------------------------------------------------------------------------
+# SzDiagnosticCore class
+# -----------------------------------------------------------------------------
+
+
+class SzDiagnosticCore(SzDiagnostic):
+    """
+    Use SzAbstractFactoryCore.create_diagnostic() to create an SzDiagnostic object.
+    The SzDiagnostic object uses the arguments provided to SzAbstractFactoryCore().
+
+    Example:
+
+    .. code-block:: python
+
+        from senzing_core import SzAbstractFactoryCore
+
+        sz_abstract_factory = SzAbstractFactoryCore(instance_name, settings)
+        sz_diagnostic = sz_abstract_factory.create_diagnostic()
+
+    Args:
+
+    Raises:
+
+    """
+
+    # -------------------------------------------------------------------------
+    # Python dunder/magic methods
+    # -------------------------------------------------------------------------
+
+    def __init__(self, **kwargs: Any) -> None:
+        """Initializer"""
+
+        _ = kwargs
+
+        self._library_handle = load_sz_library()
+
+        # Partial function to use this modules self._library_handle for exception handling
+        self._check_result = partial(
+            check_result_rc,
+            self._library_handle.SzDiagnostic_getLastException,
+            self._library_handle.SzDiagnostic_clearLastException,
+            self._library_handle.SzDiagnostic_getLastExceptionCode,
+        )
+
+        # Initialize C function input parameters and results.
+        # Must be synchronized with er/sdk/c/libSzDiagnostic.h
+        self._library_handle.SzDiagnostic_checkRepositoryPerformance_helper.argtypes = [c_longlong]
+        self._library_handle.SzDiagnostic_checkRepositoryPerformance_helper.restype = (
+            SzDiagnosticCheckRepositoryPerformanceResult
+        )
+        self._library_handle.SzDiagnostic_destroy.argtypes = []
+        self._library_handle.SzDiagnostic_destroy.restype = c_longlong
+        self._library_handle.SzDiagnostic_getRepositoryInfo_helper.argtypes = []
+        self._library_handle.SzDiagnostic_getFeature_helper.restype = SzDiagnosticGetFeatureResult
+        self._library_handle.SzDiagnostic_getRepositoryInfo_helper.restype = SzDiagnosticGetRepositoryInfoResult
+        self._library_handle.SzDiagnostic_getFeature_helper.argtypes = [c_longlong]
+        self._library_handle.SzDiagnostic_init.argtypes = [c_char_p, c_char_p, c_int]
+        self._library_handle.SzDiagnostic_init.restype = c_longlong
+        self._library_handle.SzDiagnostic_initWithConfigID.argtypes = [
+            c_char_p,
+            c_char_p,
+            c_longlong,
+            c_longlong,
+        ]
+        self._library_handle.SzDiagnostic_initWithConfigID.restype = c_longlong
+        self._library_handle.SzDiagnostic_reinit.argtypes = [c_longlong]
+        self._library_handle.SzDiagnostic_reinit.restype = c_longlong
+        self._library_handle.SzHelper_free.argtypes = [c_void_p]
+
+    # -------------------------------------------------------------------------
+    # SzDiagnostic methods
+    # -------------------------------------------------------------------------
+
+    @catch_sdk_exceptions
+    def check_repository_performance(self, seconds_to_run: int) -> str:
+        result = self._library_handle.SzDiagnostic_checkRepositoryPerformance_helper(seconds_to_run)
+        with FreeCResources(self._library_handle, result.response):
+            self._check_result(result.return_code)
+            return as_python_str(result.response)
+
+    def _destroy(self) -> None:
+        _ = self._library_handle.SzDiagnostic_destroy()
+
+    @catch_sdk_exceptions
+    def get_feature(self, feature_id: int) -> str:
+        result = self._library_handle.SzDiagnostic_getFeature_helper(feature_id)
+        with FreeCResources(self._library_handle, result.response):
+            self._check_result(result.return_code)
+            return as_python_str(result.response)
+
+    def get_repository_info(self) -> str:
+        result = self._library_handle.SzDiagnostic_getRepositoryInfo_helper()
+        with FreeCResources(self._library_handle, result.response):
+            self._check_result(result.return_code)
+            return as_python_str(result.response)
+
+    @catch_sdk_exceptions
+    def initialize(
+        self,
+        instance_name: str,
+        settings: Union[str, Dict[Any, Any]],
+        config_id: int = 0,
+        verbose_logging: int = 0,
+    ) -> None:
+        """
+        Initialize the C-based Senzing SzDiagnostic.
+
+        Args:
+            instance_name (str): A name to distinguish this instance of the SzDiagnostic.
+            settings (Union[str, Dict[Any, Any]]): A JSON document defining runtime configuration.
+            config_id (int, optional): Initialize with a specific configuration ID. Defaults to current system DEFAULTCONFIGID.
+            verbose_logging (int, optional): Send debug statements to STDOUT. Defaults to 0.
+        """
+        if config_id == 0:
+            result = self._library_handle.SzDiagnostic_init(
+                as_c_char_p(instance_name),
+                as_c_char_p(as_str(settings)),
+                verbose_logging,
+            )
+            self._check_result(result)
+            return
+
+        result = self._library_handle.SzDiagnostic_initWithConfigID(
+            as_c_char_p(instance_name),
+            as_c_char_p(as_str(settings)),
+            config_id,
+            verbose_logging,
+        )
+        self._check_result(result)
+
+    def purge_repository(self) -> None:
+        result = self._library_handle.SzDiagnostic_purgeRepository()
+        self._check_result(result)
+
+    @catch_sdk_exceptions
+    def reinitialize(self, config_id: int) -> None:
+        """
+        The `reinitialize` method reinitializes the Senzing object using a specific configuration
+        identifier. A list of available configuration identifiers can be retrieved using
+        `szconfigmanager.get_config_registry`.
+        """
+        result = self._library_handle.SzDiagnostic_reinit(config_id)
+        self._check_result(result)

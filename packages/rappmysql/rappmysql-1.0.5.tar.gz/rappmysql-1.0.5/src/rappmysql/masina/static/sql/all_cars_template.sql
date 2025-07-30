42ff71686292a4fea04ac5232d0d2fa9dc559bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:06 +0000

    Add package rappmysql/1.0.5/rappmysql-1.0.5.tar.gz

---
+++ packages/rappmysql/rappmysql-1.0.5.tar.gz/rappmysql-1.0.5/src/rappmysql/masina/static/sql/all_cars_template.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `all_cars` (
+  `id` int NOT NULL,
+  `id_users` int NOT NULL,
+  `tstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cartype` varchar(50) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `all_cars`
+  ADD PRIMARY KEY (`id`);
+

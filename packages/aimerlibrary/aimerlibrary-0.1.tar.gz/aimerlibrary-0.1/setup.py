08a65f394df66ce4f8e8faf61466c5c3236ad820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:39 +0000

    Add package aimerlibrary/0.1/aimerlibrary-0.1.tar.gz

---
+++ packages/aimerlibrary/aimerlibrary-0.1.tar.gz/aimerlibrary-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aimerlibrary',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple library management system',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/YourUsername/mylibrary',  # لینک گیت‌هاب
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # یا لایسنس دیگه
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

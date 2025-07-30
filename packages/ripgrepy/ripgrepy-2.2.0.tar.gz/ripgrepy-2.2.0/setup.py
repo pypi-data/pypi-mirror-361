59ad548ee845feeb861d198028009dff0807b380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:55 +0000

    Add package ripgrepy/2.2.0/ripgrepy-2.2.0.tar.gz

---
+++ packages/ripgrepy/ripgrepy-2.2.0.tar.gz/ripgrepy-2.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = '2.2.0'
+__author__ = 'Hapsida @securisec'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name="ripgrepy",
+    version=__version__,
+    author=__author__,
+    url='https://github.com/securisec/ripgrepy',
+    project_urls={
+        'Documentation': 'https://ripgrepy.readthedocs.io/',
+    },
+    packages=find_packages(),
+    install_requires = [
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ]
+)

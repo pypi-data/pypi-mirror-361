9de28fa6bf898e7016e2f66164294ae1dc1de5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:27 +0000

    Add package monacoreport/0.0.1/monacoreport-0.0.1.tar.gz

---
+++ packages/monacoreport/monacoreport-0.0.1.tar.gz/monacoreport-0.0.1/src/main.py
@@ -0,0 +1,5 @@
+from src.report_builder import build_report, print_report
+
+if __name__ == '__main__':
+    valid_result, invalid_result = build_report('data')
+    print_report(valid_result, invalid_result)
---
+++ packages/monacoreport/monacoreport-0.0.1.tar.gz/monacoreport-0.0.1/src/parser.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from src.handlers import correct_info_in_file
+
+
+def parse_time_start_or_end(filepath: str) -> dict:
+    """Reads a text file and returns dictionary with abbreviations and correct time format.
+
+    Args:
+        filepath (str): Path to the file.
+
+    Returns:
+        dict: Dictionary mapping abbreviation to correct time format (datetime).
+
+    Raises:
+        FileNotFoundError: If the file is not found.
+        Exception: If any other error except FileNotFoundError.
+    """
+    result = {}
+    file_lines = correct_info_in_file(filepath)
+    for line in file_lines:
+        abbreviations = line[:3]
+        time_start_or_end = line[3:].replace('_', " ").strip()
+        time_start_or_end += '000'
+        correct_date_time = datetime.strptime(time_start_or_end, "%Y-%m-%d %H:%M:%S.%f")
+        result[abbreviations] = correct_date_time
+    return result
+
+
+def parse_name_racer_and_brand(filepath: str) -> tuple[dict, dict]:
+    """Parses racer abbreviation file and returns two dictionaries:
+        - Abbreviation to full racer name
+        - Abbreviation to car brand
+
+    Args:
+        filepath (str): Path to the file.
+
+    Returns:
+        tuple[dict, dict]:
+        - Dictionary mapping abbreviation to full racer name.
+        - Dictionary mapping abbreviation to car brand.
+
+    Raises:
+        FileNotFoundError: If the file is not found.
+        Exception: If any other error except FileNotFoundError.
+    """
+    abbreviation_name_racer = {}
+    abbreviation_car_brand = {}
+    file_lines = correct_info_in_file(filepath)
+    for line in file_lines:
+        list_info = line.replace('_', " ").split(' ')
+        abbreviation = list_info[0]
+        first_and_last_name = f'{list_info[1]} {list_info[2]}'
+        car_brand = ' '.join(list_info[3:])
+        abbreviation_name_racer[abbreviation] = first_and_last_name
+        abbreviation_car_brand[abbreviation] = car_brand
+    return abbreviation_name_racer, abbreviation_car_brand

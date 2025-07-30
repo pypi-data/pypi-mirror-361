92a46ac2f492b46754eb4bd24a664f69f0e40fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:31 +0000

    Add package smrtpsg/0.0.2/smrtpsg-0.0.2-py3-none-any.whl

---
+++ packages/smrtpsg/smrtpsg-0.0.2-py3-none-any.whl/smartpassgen/__init__.py
@@ -0,0 +1,17 @@
+from .generator import PasswordGenerator
+"""
+smartpassgen: A powerful and customizable password and passphrase generator.
+
+Features:
+- Strong password generation with symbol/number ratios.
+- Custom character sets.
+- OTP/password patterns.
+- Passphrases using word lists.
+- Random PINs and hexadecimal strings.
+
+Usage:
+    from smartpassgen import PasswordGenerator
+
+    gen = PasswordGenerator()
+    print(gen.generate_strong_password())
+"""

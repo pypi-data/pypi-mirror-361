6b4beb3ab8a0db282205f818dafbae7515a8426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:20 +0000

    Add package cube-dify-plugin/1.0.1/cube_dify_plugin-1.0.1.tar.gz

---
+++ packages/cube-dify-plugin/cube_dify_plugin-1.0.1.tar.gz/cube_dify_plugin-1.0.1/dify_plugin/core/server/__base/writer_entities.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Event(Enum):
+    LOG = "log"
+    ERROR = "error"
+    SESSION = "session"
+    HEARTBEAT = "heartbeat"
+
+
+class StreamOutputMessage(BaseModel):
+    event: Event
+    session_id: Optional[str]
+    data: Optional[dict | BaseModel]

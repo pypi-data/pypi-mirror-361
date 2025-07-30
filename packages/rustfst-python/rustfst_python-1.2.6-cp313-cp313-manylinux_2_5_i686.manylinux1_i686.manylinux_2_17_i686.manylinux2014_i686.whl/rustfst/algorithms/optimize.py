6368f66d04dbabd8320ed9512abeec95d12e1202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:32 +0000

    Add package rustfst-python/1.2.6/rustfst_python-1.2.6-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/rustfst-python/rustfst_python-1.2.6-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/rustfst/algorithms/optimize.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import ctypes
+
+from rustfst.ffi_utils import (
+    lib,
+    check_ffi_error,
+)
+
+from rustfst.fst.vector_fst import VectorFst
+
+
+def optimize(fst: VectorFst) -> VectorFst:
+    """
+    Optimize an fst in-place
+    Args:
+        fst: Fst to optimize.
+    """
+    ret_code = lib.fst_optimize(fst.ptr)
+    err_msg = "Error during optimize"
+    check_ffi_error(ret_code, err_msg)
+    return fst
+
+
+def optimize_in_log(fst: VectorFst) -> VectorFst:
+    """
+    Optimize an fst in-place in the log semiring.
+    Args:
+        fst: Fst to optimize.
+    """
+    ret_code = lib.fst_optimize_in_log(ctypes.byref(fst.ptr))
+    err_msg = "Error during optimize_in_log"
+    check_ffi_error(ret_code, err_msg)
+    return fst
---
+++ packages/rustfst-python/rustfst_python-1.2.6-cp313-cp313-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/rustfst/fst/const_fst.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+import ctypes
+from rustfst.ffi_utils import (
+    lib,
+    check_ffi_error,
+)
+
+from rustfst.fst import Fst
+from rustfst.fst.vector_fst import VectorFst
+from rustfst.symbol_table import SymbolTable
+from rustfst.drawing_config import DrawingConfig
+from typing import Optional, Union
+from pathlib import Path
+
+
+class ConstFst(Fst):
+    def __init__(self, ptr=None):
+        if ptr:
+            self.ptr = ptr
+        else:
+            raise ValueError(
+                "Const fst should be init with a pointer or loaded from a file"
+            )
+        super().__init__(self.ptr)
+
+    def draw(
+        self,
+        filename: str,
+        isymbols: Optional[SymbolTable] = None,
+        osymbols: Optional[SymbolTable] = None,
+        drawing_config: DrawingConfig = DrawingConfig(),
+    ):
+        """
+        Writes out the FST in Graphviz text format.
+        This method writes out the FST in the dot graph description language. The
+        graph can be rendered using the `dot` executable provided by Graphviz.
+        Args:
+          filename: The string location of the output dot/Graphviz file.
+          isymbols: An optional symbol table used to label input symbols.
+          osymbols: An optional symbol table used to label output symbols.
+          drawing_config: Drawing configuration to use.
+        See also: `text`.
+        """
+
+        isymbols_ptr = isymbols.ptr if isymbols is not None else None
+        osymbols_ptr = osymbols.ptr if osymbols is not None else None
+
+        if drawing_config.width is None:
+            width = ctypes.c_float(-1.0)
+        else:
+            width = ctypes.c_float(drawing_config.width)
+
+        if drawing_config.height is None:
+            height = ctypes.c_float(-1.0)
+        else:
+            height = ctypes.c_float(drawing_config.height)
+
+        if drawing_config.ranksep is None:
+            ranksep = ctypes.c_float(-1.0)
+        else:
+            ranksep = ctypes.c_float(drawing_config.ranksep)
+
+        if drawing_config.nodesep is None:
+            nodesep = ctypes.c_float(-1.0)
+        else:
+            nodesep = ctypes.c_float(drawing_config.nodesep)
+
+        ret_code = lib.const_fst_draw(
+            self.ptr,
+            isymbols_ptr,
+            osymbols_ptr,
+            filename.encode("utf-8"),
+            drawing_config.title.encode("utf-8"),
+            ctypes.c_size_t(drawing_config.acceptor),
+            width,
+            height,
+            ctypes.c_size_t(drawing_config.portrait),
+            ctypes.c_size_t(drawing_config.vertical),
+            ranksep,
+            nodesep,
+            ctypes.c_size_t(drawing_config.fontsize),
+            ctypes.c_size_t(drawing_config.show_weight_one),
+            ctypes.c_size_t(drawing_config.print_weight),
+        )
+
+        err_msg = "fst draw failed"
+        check_ffi_error(ret_code, err_msg)
+
+    @classmethod
+    def read(cls, filename: Union[str, Path]) -> ConstFst:
+        """
+        Read a Fst at a given path.
+        Args:
+          filename: The string location of the input file.
+        Returns:
+          An FST.
+        Raises:
+          ValueError: Read failed.
+        """
+        fst = ctypes.pointer(ctypes.c_void_p())
+        ret_code = lib.const_fst_from_path(
+            ctypes.byref(fst), str(filename).encode("utf-8")
+        )
+        err_msg = f"Read failed. file: {filename}"
+        check_ffi_error(ret_code, err_msg)
+
+        return cls(ptr=fst)
+
+    @classmethod
+    def from_vector_fst(cls, fst: VectorFst) -> ConstFst:
+        """
+        Converts a given `VectorFst` to `ConstFst`
+        Args:
+          fst: The `VectorFst` that should be converted
+        Returns:
+          A `ConstFst`
+        Raises:
+          ValueError: Conversion failed
+        """
+        const_fst = ctypes.pointer(ctypes.c_void_p())
+        ret_code = lib.const_fst_from_vec_fst(fst.ptr, ctypes.byref(const_fst))
+        err_msg = "Failed to convert VectorFST to ConstFST"
+        check_ffi_error(ret_code, err_msg)
+
+        return cls(ptr=const_fst)
+
+    def write(self, filename: Union[str, Path]):
+        """
+        Serializes FST to a file.
+        This method writes the FST to a file in binary format.
+        Args:
+          filename: The string location of the output file.
+        Raises:
+          ValueError: Write failed.
+        """
+        ret_code = lib.const_fst_write_file(self.ptr, str(filename).encode("utf-8"))
+        err_msg = f"Write failed. file: {filename}"
+        check_ffi_error(ret_code, err_msg)
+
+    def equals(self, other: Fst) -> bool:
+        """
+        Check if this Fst is equal to the other
+        Args :
+            other: Fst instance
+        Returns:
+             bool
+        """
+        is_equal = ctypes.c_size_t()
+
+        ret_code = lib.const_fst_equals(self.ptr, other.ptr, ctypes.byref(is_equal))
+        err_msg = "Error checking equality"
+        check_ffi_error(ret_code, err_msg)
+
+        return bool(is_equal.value)
+
+    def copy(self) -> ConstFst:
+        """
+        Returns :
+            Deepcopy of the Fst.
+        """
+        cloned_fst = ctypes.c_size_t()
+        ret_code = lib.const_fst_copy(self.ptr, ctypes.byref(cloned_fst))
+        err_msg = "Error copying fst"
+        check_ffi_error(ret_code, err_msg)
+
+        return ConstFst(cloned_fst)
+
+    def __str__(self):
+        s = ctypes.c_void_p()
+        ret_code = lib.const_fst_display(self.ptr, ctypes.byref(s))
+        err_msg = "Error displaying ConstFst"
+        check_ffi_error(ret_code, err_msg)
+
+        return ctypes.string_at(s).decode("utf8")

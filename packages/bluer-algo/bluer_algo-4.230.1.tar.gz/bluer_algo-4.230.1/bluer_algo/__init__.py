e6e4ec2737b49ddd06560b3a6af0d51d735a8308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:33 +0000

    Add package bluer-algo/4.230.1/bluer_algo-4.230.1.tar.gz

---
+++ packages/bluer-algo/bluer_algo-4.230.1.tar.gz/bluer_algo-4.230.1/bluer_algo/__init__.py
@@ -0,0 +1,17 @@
+NAME = "bluer_algo"
+
+ICON = "🪄"
+
+DESCRIPTION = f"{ICON} AI Algo."
+
+VERSION = "4.230.1"
+
+REPO_NAME = "bluer-algo"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
+
+ALIAS = "@algo"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"

8f31a89870a14987405ccd39956b33f149ca1f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:22 +0000

    Add package sapiens-tokenizer/1.0.1/sapiens_tokenizer-1.0.1.tar.gz

---
+++ packages/sapiens-tokenizer/sapiens_tokenizer-1.0.1.tar.gz/sapiens_tokenizer-1.0.1/setup.py
@@ -0,0 +1,14 @@
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'sapiens_tokenizer'
+version = '1.0.1'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=['tiktoken'],
+    url='https://github.com/sapiens-technology/SapiensTokenizer',
+    license='Proprietary Software'
+)
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------

805d58be13283c6f8b35bb461fc1d444d0759c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:47 +0000

    Add package viper-python/1.0.4/viper-python-1.0.4.tar.gz

---
+++ packages/viper-python/viper-python-1.0.4.tar.gz/viper-python-1.0.4/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from distutils.core import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='viper-python',
+    packages=['viper'],
+    version='1.0.4',
+    license='MIT',
+    description='Common utility functions.',
+    author='Siddhant Kushwaha',
+    author_email='k16.siddhant@gmail.com',
+    url='https://github.com/siddhantkushwaha/viper',
+    download_url='https://github.com/siddhantkushwaha/viper/archive/1.0.2.tar.gz',
+    keywords=['DOWNLOAD', 'PARALLEL', 'CONCURRENT', 'LOGGING'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    include_package_data=True
+)

967f3898fab02fb30b8298616b00debb0b730b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:33 +0000

    Add package sec-eagle/0.1.1/sec_eagle-0.1.1.tar.gz

---
+++ packages/sec-eagle/sec_eagle-0.1.1.tar.gz/sec_eagle-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+
+setup(
+    name='sec_eagle',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'beautifulsoup4',
+        'numpy'
+    ],
+    python_requires='>=3.6',
+    description='A Python package for parsing SEC data with XML and web scraping tools',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/secpy',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)

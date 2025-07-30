dad75c2289f59b19e32412d5b0a618a73c2a8752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:43 +0000

    Add package fovus/2.0.17/fovus-2.0.17-py3-none-any.whl

---
+++ packages/fovus/fovus-2.0.17-py3-none-any.whl/fovus/commands/storage/storage_command.py
@@ -0,0 +1,21 @@
+import click
+
+from fovus.commands.storage.commands.mount.storage_mount_command import (
+    storage_mount_command,
+)
+from fovus.commands.storage.commands.unmount.storage_unmount_command import (
+    storage_unmount_command,
+)
+from fovus.commands.storage.commands.upload.storage_upload_command import (
+    storage_upload_command,
+)
+
+
+@click.group("storage")
+def storage_command():
+    """Contains commands related to storage."""
+
+
+storage_command.add_command(storage_mount_command)
+storage_command.add_command(storage_unmount_command)
+storage_command.add_command(storage_upload_command)

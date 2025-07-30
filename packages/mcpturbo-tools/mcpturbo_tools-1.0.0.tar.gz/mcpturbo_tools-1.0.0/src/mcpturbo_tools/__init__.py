0731e62c9b7bdc8c8e4ac376c4b6a6ffeb3850d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-tools/1.0.0/mcpturbo_tools-1.0.0.tar.gz

---
+++ packages/mcpturbo-tools/mcpturbo_tools-1.0.0.tar.gz/mcpturbo_tools-1.0.0/src/mcpturbo_tools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Tools
+
+MCPTurbo - Development Tools and Utilities
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

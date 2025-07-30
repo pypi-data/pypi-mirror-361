69f0df13282e5fa14ee3b80fd0c4c54b412c8405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:51 +0000

    Add package python-bugger/0.5.1/python_bugger-0.5.1.tar.gz

---
+++ packages/python-bugger/python_bugger-0.5.1.tar.gz/python_bugger-0.5.1/src/py_bugger/cli/config.py
@@ -0,0 +1,24 @@
+"""Config object to collect CLI options."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+SUPPORTED_EXCEPTION_TYPES = [
+    "ModuleNotFoundError",
+    "AttributeError",
+    "IndentationError",
+]
+
+
+@dataclass
+class PBConfig:
+    exception_type: str = ""
+    target_dir: Path = ""
+    target_file: Path = ""
+    num_bugs: int = 1
+    ignore_git_status: bool = False
+    verbose: bool = True
+
+
+pb_config = PBConfig()

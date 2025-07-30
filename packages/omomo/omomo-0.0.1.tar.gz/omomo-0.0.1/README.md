3b10d628a1f0e00a3ca5ea4750ab697941e6a09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:08:40 +0000

    Add package omomo/0.0.1/omomo-0.0.1.tar.gz

---
+++ packages/omomo/omomo-0.0.1.tar.gz/omomo-0.0.1/README.md
@@ -0,0 +1,22 @@
+# Omomo
+
+## Description
+Omomo, https://omomo.dev, is an experimental open-source tool to support core optimization techniques in machine learning.
+
+## For users:
+1. Create conda environment:  
+`conda create -n omomo python=3.10`
+2. Install pip package:  
+`pip install omomo`
+
+## Examples
+1. Basic type conversion: `python ex1-types.py`
+2. Quantize a CNN model: `python ex2-model-conversion.py`
+
+## For developers:
+1. Create conda environment:  
+`conda create -n omomo python=3.10`  
+A. To install library:  
+`poetry install`  
+B. To build package:  
+`poetry build`
---
+++ packages/omomo/omomo-0.0.1.tar.gz/omomo-0.0.1/src/omomo/core/quantization/quantizer_kmeans.py
@@ -0,0 +1,107 @@
+import torch
+from collections import namedtuple
+from fast_pytorch_kmeans import KMeans
+import torch.nn as nn
+from torch.nn import parameter
+
+Codebook = namedtuple('Codebook', ['centroids', 'labels'])
+
+def k_means_quantize(fp32_tensor: torch.Tensor, bitwidth=4, codebook=None):
+    """
+    quantize tensor using k-means clustering
+    :param fp32_tensor:
+    :param bitwidth: [int] quantization bit width, default=4
+    :param codebook: [Codebook] (the cluster centroids, the cluster label tensor)
+    :return:
+        [Codebook = (centroids, labels)]
+            centroids: [torch.(cuda.)FloatTensor] the cluster centroids
+            labels: [torch.(cuda.)LongTensor] cluster label tensor
+    """
+    if codebook is None:
+        ############### YOUR CODE STARTS HERE ###############
+        # get number of clusters based on the quantization precision
+        # hint: one line of code
+        n_clusters = 2**bitwidth
+        ############### YOUR CODE ENDS HERE #################
+        # use k-means to get the quantization centroids
+        kmeans = KMeans(n_clusters=n_clusters, mode='euclidean', verbose=0)
+
+        # flatten all numbers in the weight to [N_ele, 1]
+        labels = kmeans.fit_predict(fp32_tensor.view(-1, 1)).to(torch.long)
+
+        centroids = kmeans.centroids.to(torch.float).view(-1)
+        codebook = Codebook(centroids, labels)
+    ############### YOUR CODE STARTS HERE ###############
+    # decode the codebook into k-means quantized tensor for inference
+    # hint: one line of code
+    quantized_tensor = codebook.centroids[codebook.labels]
+    # print(f"{fp32_tensor.shape=}, {codebook.centroids.shape=}, {codebook.labels.shape=}")
+    ############### YOUR CODE ENDS HERE #################
+    fp32_tensor.set_(quantized_tensor.view_as(fp32_tensor))
+    return codebook
+
+
+def update_codebook(fp32_tensor: torch.Tensor, codebook: Codebook):
+    """
+    update the centroids in the codebook using updated fp32_tensor
+    :param fp32_tensor: [torch.(cuda.)Tensor]
+    :param codebook: [Codebook] (the cluster centroids, the cluster label tensor)
+    """
+    n_clusters = codebook.centroids.numel()
+    fp32_tensor = fp32_tensor.view(-1)
+    for k in range(n_clusters):
+    ############### YOUR CODE STARTS HERE ###############
+        # hint: one line of code
+        codebook.centroids[k] = torch.mean(fp32_tensor[codebook.labels==k])
+    ############### YOUR CODE ENDS HERE #################
+
+
+
+class KMeansQuantizer:
+    def __init__(self, model : nn.Module, bitwidth=4):
+        self.codebook = KMeansQuantizer.quantize(model, bitwidth)
+
+    @torch.no_grad()
+    def apply(self, model, update_centroids):
+        for name, param in model.named_parameters():
+            if name in self.codebook:
+                if update_centroids:
+                    update_codebook(param, codebook=self.codebook[name])
+                self.codebook[name] = k_means_quantize(
+                    param, codebook=self.codebook[name])
+
+    @staticmethod
+    @torch.no_grad()
+    def quantize(model: nn.Module, bitwidth=4):
+        codebook = dict()
+        if isinstance(bitwidth, dict):
+            for name, param in model.named_parameters():
+                if name in bitwidth:
+                    codebook[name] = k_means_quantize(param, bitwidth=bitwidth[name])
+        else:
+            for name, param in model.named_parameters():
+                # print(f"{name=}: {param.shape}")
+                # only quantize weight, not bias
+                if param.dim() > 1:
+                    codebook[name] = k_means_quantize(param, bitwidth=bitwidth)
+        return codebook
+    
+
+def quantize_tensor_example():
+    bitwidth=3
+    test_tensor=torch.tensor([
+            [-0.3747,  0.0874,  0.3200, -0.4868,  0.4404],
+            [-0.0402,  0.2322, -0.2024, -0.4986,  0.1814],
+            [ 0.3102, -0.3942, -0.2030,  0.0883, -0.4741],
+            [-0.1592, -0.0777, -0.3946, -0.2128,  0.2675],
+            [ 0.0611, -0.1933, -0.4350,  0.2928, -0.1087]])
+
+    print(test_tensor)
+    num_unique_values_before_quantization = test_tensor.unique().numel()
+    k_means_quantize(test_tensor, bitwidth=bitwidth)
+    num_unique_values_after_quantization = test_tensor.unique().numel()
+
+    print(f'    target bitwidth: {bitwidth} bits')
+    print(f'        num unique values before k-means quantization: {num_unique_values_before_quantization}')
+    print(f'        num unique values after  k-means quantization: {num_unique_values_after_quantization}')
+    print(test_tensor)

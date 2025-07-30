1d9d78ba5f77785bf9f47ec18973ac5d2166e72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-orchestrator/1.0.0/mcpturbo_orchestrator-1.0.0.tar.gz

---
+++ packages/mcpturbo-orchestrator/mcpturbo_orchestrator-1.0.0.tar.gz/mcpturbo_orchestrator-1.0.0/src/mcpturbo_orchestrator/orchestrator.py
@@ -0,0 +1,12 @@
+"""Project orchestration and coordination"""
+
+class ProjectOrchestrator:
+    """Coordinates multiple agents for project generation"""
+    
+    def __init__(self):
+        self.agents = {}
+        
+    async def coordinate_project(self, project_spec):
+        """Coordinate agents to generate project"""
+        # Implementation will be added
+        pass

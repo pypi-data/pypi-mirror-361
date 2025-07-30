c23164c25645d2fa4a46de5f55ef31ed5a7a8e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:40 +0000

    Add package DePass/0.0.10/depass-0.0.10.tar.gz

---
+++ packages/DePass/depass-0.0.10.tar.gz/depass-0.0.10/setup.py
@@ -0,0 +1,45 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='DePass',
+#     version='0.0.8',
+#     description='DePass: a dual-enhanced graph learning framework for paired data integration',
+#     packages=find_packages(),
+#     install_requires=[
+#         'anndata==0.9.2',
+#         'hnswlib==0.8.0',
+#         'matplotlib==3.7.1',
+#         'numpy==1.23.5',
+#         'pandas==1.5.3',
+#         'scanpy==1.9.8',
+#         'scikit-learn==1.3.0',
+#         'scipy==1.10.1',
+#         'seaborn>=0.12.2,<0.14.0',
+#         'tqdm==4.65.0',
+#         'jupyter',
+#     ],
+#     python_requires='==3.8.20',
+# )
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DePass',
+    version='0.0.10',
+    description='DePass: a dual-enhanced graph learning framework for paired data integration',
+    packages=find_packages(),
+    install_requires=[
+        'anndata>=0.9.2',           # 向下兼容
+        'hnswlib>=0.8.0',
+        'matplotlib>=3.7.1,<4.0.0',  # 允许补丁更新
+        'numpy>=1.23.5,<2.0.0',      # 主版本兼容
+        'pandas>=1.5.3,<2.0.0',
+        'scanpy>=1.9.8,<2.0.0',
+        'scikit-learn>=1.3.0,<2.0.0',
+        'scipy>=1.10.1,<2.0.0',
+        'seaborn>=0.12.2,<0.14.0',
+        'tqdm>=4.65.0,<5.0.0',
+        'jupyter',                   # 通常不锁定版本
+    ],
+    python_requires='>=3.8,<3.9',   # 允许3.8.x全系列
+)

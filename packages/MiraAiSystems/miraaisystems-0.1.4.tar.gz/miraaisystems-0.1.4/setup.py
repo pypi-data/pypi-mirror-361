d45dfc72148cb1658c90d22391517bbbe16d3686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:38 +0000

    Add package MiraAiSystems/0.1.4/miraaisystems-0.1.4.tar.gz

---
+++ packages/MiraAiSystems/miraaisystems-0.1.4.tar.gz/miraaisystems-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MiraAiSystems",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "python-dotenv",
+        "google-generativeai",
+    ],
+    author="MoonTech",
+    description="An AI system that using Gemini AI to do various tasks",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)

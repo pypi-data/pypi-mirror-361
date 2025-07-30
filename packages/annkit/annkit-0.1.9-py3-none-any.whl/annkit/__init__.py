130a1446c284ae0e70554aee94a9cffdfe101aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:26 +0000

    Add package annkit/0.1.9/annkit-0.1.9-py3-none-any.whl

---
+++ packages/annkit/annkit-0.1.9-py3-none-any.whl/annkit/__init__.py
@@ -0,0 +1,11 @@
+"""
+annkit - A neural network library with visualization tools.
+"""
+
+__version__ = "0.1.9"
+
+from annkit.basekit import net,class_optimizer
+
+__all__=['net','class_optimizer']
+
+print("✅ annkit loaded!")

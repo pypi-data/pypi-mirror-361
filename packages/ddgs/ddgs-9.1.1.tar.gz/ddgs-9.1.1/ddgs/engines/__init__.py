381a6385b06ad052727bb2372d9ad866b6a50680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:53 +0000

    Add package ddgs/9.1.1/ddgs-9.1.1.tar.gz

---
+++ packages/ddgs/ddgs-9.1.1.tar.gz/ddgs-9.1.1/ddgs/engines/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from ..base import BaseSearchEngine
+from .bing import Bing
+from .duckduckgo import Duckduckgo
+from .duckduckgo_images import DuckduckgoImages
+from .duckduckgo_news import DuckduckgoNews
+from .duckduckgo_videos import DuckduckgoVideos
+from .google import Google
+from .wikipedia import Wikipedia
+from .yahoo import Yahoo
+from .yandex import Yandex
+
+ENGINES: dict[str, dict[str, type[BaseSearchEngine]]] = {
+    "text": {
+        "wikipedia": Wikipedia,
+        "google": Google,
+        "bing": Bing,
+        "yahoo": Yahoo,
+        "yandex": Yandex,
+        "duckduckgo": Duckduckgo,
+    },
+    "images": {
+        "duckduckgo": DuckduckgoImages,
+    },
+    "news": {
+        "duckduckgo": DuckduckgoNews,
+    },
+    "videos": {
+        "duckduckgo": DuckduckgoVideos,
+    },
+}

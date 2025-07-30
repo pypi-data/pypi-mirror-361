fe0420a4b3d3a30f608558a4ec3724965b511ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:51 +0000

    Add package jpcm/0.1.27/jpcm-0.1.27.tar.gz

---
+++ packages/jpcm/jpcm-0.1.27.tar.gz/jpcm-0.1.27/src/jpcm/load/load.py
@@ -0,0 +1,22 @@
+import matplotlib
+from matplotlib.colors import ListedColormap as LCM
+import matplotlib as mpl
+import numpy as np
+import json as js
+
+rev = "_r"
+
+def jpcm_load(datafile):
+
+    with open(datafile,'r') as file:
+        mapdata = js.load(file)
+
+    return {key: (np.array(mapdata[key]),key) for key in mapdata.keys()}
+
+def jpcm_register(jpcm_cmaps):
+    for key in jpcm_cmaps.keys():
+        if key not in matplotlib.pyplot.colormaps():
+            mpl.colormaps.register(LCM(jpcm_cmaps[key]),name = key)
+            mpl.colormaps.register(np.flip(LCM(jpcm_cmaps[key])),name = key+rev)
+
+

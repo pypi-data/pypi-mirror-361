7961a721531df4ef707529074cf7b3769858225e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:45 +0000

    Add package pyCapsid/0.6.1/pycapsid-0.6.1-py3-none-any.whl

---
+++ packages/pyCapsid/pycapsid-0.6.1-py3-none-any.whl/pyCapsid/NMA.py
@@ -0,0 +1,299 @@
+"""Module with functions for calculating the normal modes and frequencies of a given hessian. Eigenvalue/vector functions.
+Also contains functions for calculating mechanical properties from NMA results. I.e. squared fluctuations, distance
+fluctuations, etc."""
+import numba as nb
+import numpy as np
+
+
+def modeCalc(hess, n_modes=200, eigen_method='eigsh', is3d=True, shift_invert=True, save_modes=True, save_mode_path='./'):
+    """Calculate the 'n_modes' lowest frequency modes of the system by calculating the smallest eigenvalues and eigenvectors
+    of the hessian matrix.
+
+    :param hess: Sparse hessian matrix
+    :param n_modes: Integer number of low-frequency modes to calculate.
+    :param eigen_method: Choice of method for solving the eigenvalue problem.
+    :returns:
+    """
+    from timeit import default_timer as timer
+    NMA_start = timer()
+    print('Calculating Normal Modes')
+
+    n_dim = hess.shape[0]
+
+    # if useMass:
+    #     from scipy.sparse import diags
+    #     if model == 'anm':
+    #         masses = np.repeat(masses, 3)
+    #
+    #     mass = np.tile(masses, 60)
+    #     print('mass variance: ', np.std(mass))
+    #     print('mass mean: ', np.mean(mass))
+    #     print('mass min: ', np.min(mass))
+    #     print('mass max: ', np.max(mass))
+    #     Mass = diags(mass)
+    # else:
+
+
+    # from scipy.sparse import identity
+    # Mass = identity(n_dim)
+
+    if eigen_method == 'eigsh':
+        from scipy.sparse.linalg import eigsh
+        if shift_invert:
+            print('Using shift-invert for increased performance with increased memory usage.')
+            evals, evecs = eigsh(hess, k=n_modes, sigma=1e-10, which='LA')
+        else:
+            evals, evecs = eigsh(hess, k=n_modes, which='SA')
+            evals = evals[6:]
+            evecs = evecs[:, 6:]
+    elif eigen_method == 'lobpcg':
+        from scipy.sparse.linalg import lobpcg
+
+        epredict = np.random.rand(n_dim, n_modes + 6)
+        evals, evecs = lobpcg(hess, epredict, largest=False, tol=0, maxiter=n_dim)
+        evals = evals[6:]
+        evecs = evecs[:, 6:]
+    elif eigen_method == 'lobcuda':
+        import cupy as cp
+        from cupyx.scipy.sparse.linalg import lobpcg as clobpcg
+        from cupyx.scipy.sparse.linalg import LinearOperator, spilu
+
+        sparse_gpu = cp.sparse.csr_matrix(hess.astype(cp.float32))
+        epredict = cp.random.rand(n_dim, n_modes + 6, dtype=cp.float32)
+
+        lu = spilu(sparse_gpu, fill_factor=50)  # LU decomposition
+        M = LinearOperator(hess.shape, lu.solve)
+        print('gpu eigen')
+
+        evals, evecs = clobpcg(sparse_gpu, epredict, M=M, largest=False, tol=0, verbosityLevel=0)
+        if is3d:
+            evals = cp.asnumpy(evals[6:])
+            evecs = cp.asnumpy(evecs[:, 6:])
+        else:
+            evals = cp.asnumpy(evals[1:])
+            evecs = cp.asnumpy(evecs[:, 1:])
+
+    NMA_time = timer() - NMA_start
+    print('NMA time: ', NMA_time)
+
+    if save_modes:
+        file = save_mode_path + 'modes.npz'
+        print('Saving NMA results in' + file)
+        np.savez_compressed(file, eigen_vals=evals, eigen_vecs = evecs)
+
+    return evals, evecs
+
+
+def calcCovMat(evals, evecs, n_modes, coords, fluct_cutoff, is3d=True):
+    """Calculates a sparse covariance matrix from the low frequency modes.
+
+    :param evals:
+    :param evecs:
+    :param n_modes:
+    :param coords:
+    :param fluct_cutoff:
+    :param is3d:
+    :return:
+    """
+    from scipy import sparse
+    from sklearn.neighbors import radius_neighbors_graph
+
+    print('Calculating sparse covariance matrix')
+
+    adj = radius_neighbors_graph(coords, fluct_cutoff, mode='connectivity', n_jobs=-1).tocoo()
+    adj.setdiag(1)
+    row, col = adj.row, adj.col
+
+    if is3d:
+        data = con_c(evals, evecs, row, col)
+        covariance = sparse.coo_matrix((data, (row, col)), shape=adj.shape)
+    else:
+        data = gCon_c(evals, evecs, row, col)
+        covariance = sparse.coo_matrix((data, (row, col)), shape=adj.shape)
+
+    return covariance
+
+
+def calcDistFlucts(evals, evecs, coords, fluct_cutoff=7.5, is3d=True, **kwargs):
+    """Calculates a sparse covariance matrix from the low frequency modes.
+
+    :param evals:
+    :param evecs:
+    :param n_modes:
+    :param coords:
+    :param fluct_cutoff:
+    :param is3d:
+    :return:
+    """
+    from scipy import sparse
+
+    n_modes = evals.shape[0]
+    covariance = calcCovMat(evals, evecs, n_modes, coords, fluct_cutoff, is3d)
+    c_diag = covariance.diagonal()
+    row, col, c_data = (covariance.row, covariance.col, covariance.data)
+
+    print('Calculating sparse distance fluctuation matrix from covariance matrix')
+    fluct_data = distFluctFromCov(c_diag, c_data, row, col)
+
+    dist_flucts = sparse.coo_matrix((fluct_data, (row, col)), shape=covariance.shape)
+
+    return dist_flucts
+
+
+@nb.njit()
+def con_c(evals, evecs, row, col):
+    """
+
+    :param evals:
+    :param evecs:
+    :param row:
+    :param col:
+    :return:
+    """
+    data = []
+    for k in range(row.shape[0]):
+        i, j = (row[k], col[k])
+        data.append(cov(evals, evecs, i, j))
+    return np.array(data)
+
+
+#@nb.njit()
+def gCon_c(evals, evecs, row, col):
+    """
+
+    :param evals:
+    :param evecs:
+    :param row:
+    :param col:
+    :return:
+    """
+    data = []
+    for k in range(row.shape[0]):
+        i, j = (row[k], col[k])
+        data.append(gCov(evals, evecs, i, j))
+    return np.array(data)
+
+
+@nb.njit(parallel=False)
+def cov(evals, evecs, i, j):
+    """
+
+    :param evals:
+    :param evecs:
+    :param i:
+    :param j:
+    :return:
+    """
+    # Calculates the covariance between two residues in ANM. Takes the trace of block so no anisotropy info.
+    # Commented section would compute normalized covariances
+    n_e = evals.shape[0]
+    # n_d = evecs.shape[1]
+    tr1 = 0
+    # tr2 = 0
+    # tr3 = 0
+    for n in range(n_e):
+        l = evals[n]
+        tr1 += 1 / l * (evecs[3 * i, n] * evecs[3 * j, n] + evecs[3 * i + 1, n] * evecs[3 * j + 1, n] + evecs[
+            3 * i + 2, n] * evecs[3 * j + 2, n])
+        # tr2 += 1 / l * (evecs[3 * i, n] * evecs[3 * i, n] + evecs[3 * i + 1, n] * evecs[3 * i + 1, n] + evecs[
+        #     3 * i + 2, n] * evecs[3 * i + 2, n])
+        # tr3 += 1 / l * (evecs[3 * j, n] * evecs[3 * j, n] + evecs[3 * j + 1, n] * evecs[3 * j + 1, n] + evecs[
+        #     3 * j + 2, n] * evecs[3 * j + 2, n])
+    cov = tr1  # / np.sqrt(tr2 * tr3)
+    return cov
+
+
+@nb.njit(parallel=False)
+def gCov(evals, evecs, i, j):
+    """
+
+    :param evals:
+    :param evecs:
+    :param i:
+    :param j:
+    :return:
+    """
+    # Calculates the covariance between two residues in GNM
+    n_e = evals.shape[0]
+    c = 0
+    for n in range(n_e):
+        l = evals[n]
+        c += 1 / l * (evecs[i, n] * evecs[j, n])
+    return c
+
+
+@nb.njit()
+def distFluctFromCov(c_diag, c_data, row, col):
+    """
+
+    :param c_diag:
+    :param c_data:
+    :param row:
+    :param col:
+    :return:
+    """
+    d_data = []
+    for k in range(row.shape[0]):
+        i, j = (row[k], col[k])
+        d = np.abs(c_diag[i] + c_diag[j] - 2 * c_data[k])
+        d_data.append(d)
+    return np.array(d_data)
+
+
+def fluctPlot(d, title, pdb):
+    """
+
+    :param d:
+    :param title:
+    :param pdb:
+    """
+    import matplotlib.pyplot as plt
+    print('Plotting Fluctuation Histogram')
+    import matplotlib
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    font = {'family': 'sans-serif',
+            'weight': 'normal',
+            'size': 11}
+    matplotlib.rc('font', **font)
+    print('Plotting')
+    ax.set_ylabel('Density', fontsize=12)
+    ax.set_xlabel('$A^{2}$', fontsize=12)
+    ax.tick_params(axis='y', labelsize=8)
+    ax.tick_params(axis='x', labelsize=8)
+    ax.legend()
+    fig.suptitle(
+        'Histogram Of Pairwise Fluctuations: ' + title + ' (' + pdb + ')', fontsize=12)
+
+    ax.hist(d.data, bins='fd', histtype='stepfilled', density=True)
+    fig.tight_layout()
+    plt.show()
+
+
+# This function calls the relevant function from bfactorfit.py
+def fitCompareBfactors(evals, evecs, bfactors, pdb, is3d=True, fit_modes=True, plot_modes=True, force_ico=False,
+                       ico_tol=0.002, is_ico=True, save_bfactors=True, save_bfactors_path='./'):
+    """
+
+    :param is_ico:
+    :param evals:
+    :param evecs:
+    :param bfactors:
+    :param pdb:
+    :param is3d:
+    :param fit_modes:
+    :param plot_modes:
+    :param force_ico:
+    :param ico_tol:
+    :return:
+    """
+    from .bfactorfit import fitPlotBfactors
+    from timeit import default_timer as timer
+    bfactor_start = timer()
+    cc, gamma,n_modes = fitPlotBfactors(evals, evecs, bfactors, pdb, is3d=is3d, fitModes=fit_modes, plotModes=plot_modes,
+                                        forceIco=force_ico, icotol=ico_tol, isIco=is_ico, save_path=save_bfactors_path)
+
+    bfactor_time = timer() - bfactor_start
+    print('bfactor fitting time: ', bfactor_time)
+    r_evals = evals[:n_modes]*gamma
+    r_evecs = evecs[:, :n_modes]
+    return r_evals, r_evecs, cc, gamma, n_modes
---
+++ packages/pyCapsid/pycapsid-0.6.1-py3-none-any.whl/pyCapsid/VIS.py
@@ -0,0 +1,475 @@
+"""Functions for visualizing results in chimeraX
+
+"""
+
+
+def visualizeResults(pdb, capsid, labels, method='chimerax', **kwargs):
+    if method == 'chimerax':
+        chimeraxViz(labels, pdb, **kwargs)
+    elif method == 'nglview':
+        return view_pdb_ngl(pdb, capsid, labels, **kwargs)
+    else:
+        print('method must be chimerax or nglview')
+
+def chimeraxViz(labels, pdb, remote=True, chimerax_path=None, pdb_path='.', save_path='.',
+                rwb_scale=False, clust_mask=None, **kwargs):
+    """Launches ChimeraX and runs a script that visualizes the results.
+
+    :param labels:
+    :param pdb:
+    :param remote:
+    :param chimerax_path:
+    :param pdb_path:
+    :param save_path:
+    :param script_path:
+    :param labels_path:
+    :return:
+    """
+
+    import os
+    import platform
+    if chimerax_path is None:
+        print('No chimerax path specified, checking in default locations for your OS')
+        if platform.system()=='Linux':
+            chimerax_path = 'usr/bin/chimerax'
+        elif platform.system()=='Windows':
+            chimerax_path = 'C:\\Program Files\\ChimeraX\\bin\\ChimeraX.exe'
+        elif platform.system()=='Darwin':
+            chimerax_path = '/Applications/ChimeraX'
+        else:
+            print('No chimerax path is given and cannot check default locations')
+            chimerax_path = ''
+
+    import matplotlib as mpl
+    norm = mpl.colors.Normalize(vmin=np.min(labels), vmax=np.max(labels))
+
+    if rwb_scale:
+        import matplotlib.pyplot as plt
+        cmap = plt.get_cmap('coolwarm_r')
+        rgba = cmap(norm(labels)) * 255
+    else:
+        cmap = generate_colormap(int(np.max(labels)))
+        rgba = cmap(norm(labels)) * 255
+
+    if clust_mask is not None:
+        rgba = addUnclusteredColor(rgba, clust_mask)
+
+    from numpy import save
+    from tempfile import NamedTemporaryFile
+    with NamedTemporaryFile(suffix='.npy', delete=False) as temp_file:
+        save(temp_file, rgba)
+        rgba_path = os.path.normpath(temp_file.name)
+        rgba_path = rgba_path.replace('\\', '/') #os.path.abspath(temp_file.name)
+        print(rgba_path)
+
+    # get path to chimerax script
+    import pyCapsid.scripts as scpath
+    import os
+
+    script_path = os.path.abspath(scpath.__file__)
+    script_path = script_path.replace('__init__', 'chimerax_script')
+
+    chimerax_exe = chimerax_path #+ '\\ChimeraX.exe'
+    if platform.system()=='Windows':
+        cmd_string = f'""{chimerax_exe}" --script "{script_path} {rgba_path} {save_path} {pdb_path} {str(remote)} {pdb} {str(rwb_scale)}""'
+    elif platform.system()=='Linux':
+        cmd_string = f'{chimerax_exe} --script "{script_path} {rgba_path} {save_path} {pdb_path} {str(remote)} {pdb} {str(rwb_scale)}"'
+    else:
+        cmd_string = f'{chimerax_exe} --script "{script_path} {rgba_path} {save_path} {pdb_path} {str(remote)} {pdb} {str(rwb_scale)}"'
+    print(cmd_string)
+    os.system(cmd_string)
+    temp_file.close()
+    os.unlink(temp_file.name)
+
+def visualizeSavedResults(pdb, results_file, n_cluster=None, method='chimerax', **kwargs):
+    import numpy as np
+    results = np.load(results_file)
+    labels = results['labels']
+
+    nc_range = results['nc_range']
+    scores = results['score']
+
+    if n_cluster is None:
+        print('Defaulting to highest score clustering')
+        ind = np.argmax(scores)
+        n_c = nc_range[ind]
+    else:
+        ind = np.argwhere(nc_range == n_cluster)[0][0]
+        n_c = n_cluster
+
+    print(f'Visualizing cluster results of {pdb} for {n_c} clusters')
+    labels = labels[ind]
+    print(labels)
+    if method=='chimerax':
+        chimeraxViz(labels, pdb, **kwargs)
+    elif method=='nglview':
+        view = createCapsidView(pdb)
+        return labels, view
+    else:
+        print('Method must be one of chimerax or nglview')
+
+
+def addUnclusteredColor(rgba, clust_mask):
+    from numpy import insert
+    print(rgba.shape)
+    print(rgba)
+    black = np.array([0,0,0,255])
+    for i,val in enumerate(clust_mask):
+        if not val:
+            rgba = insert(rgba, i, black, axis=0)
+    print(rgba.shape)
+    print(rgba)
+    return rgba
+
+
+
+# Adapted from py3dmol tutorial
+# https://william-dawson.github.io/using-py3dmol.html
+class Atom(dict):
+    def __init__(self, line):
+        self["type"] = line[0:6].strip()
+        self["idx"] = line[6:11].strip()
+        self["name"] = line[12:16].strip()
+        self["resname"] = line[17:20].strip()
+        self["resid"] = int(int(line[22:26]))
+        self["x"] = float(line[30:38])
+        self["y"] = float(line[38:46])
+        self["z"] = float(line[46:54])
+        self["sym"] = line[76:78].strip()
+
+    def __str__(self):
+        line = list(" " * 80)
+
+        line[0:6] = self["type"].ljust(6)
+        line[6:11] = self["idx"].ljust(5)
+        line[12:16] = self["name"].ljust(4)
+        line[17:20] = self["resname"].ljust(3)
+        line[22:26] = str(self["resid"]).ljust(4)
+        line[30:38] = str(self["x"]).rjust(8)
+        line[38:46] = str(self["y"]).rjust(8)
+        line[46:54] = str(self["z"]).rjust(8)
+        line[76:78] = self["sym"].rjust(2)
+        return "".join(line) + "\n"
+
+
+class Molecule(list):
+    def __init__(self, file):
+        for line in file:
+            if "ATOM" in line or "HETATM" in line:
+                self.append(Atom(line))
+
+    def __str__(self):
+        outstr = ""
+        for at in self:
+            outstr += str(at)
+
+        return outstr
+    
+# Colormap generation taken from stackexchange
+# https://stackoverflow.com/questions/42697933/colormap-with-maximum-distinguishable-colours
+def generate_colormap(number_of_distinct_colors: int = 80):
+    import math
+
+    import numpy as np
+    from matplotlib.colors import ListedColormap
+    from matplotlib.cm import hsv
+
+    if number_of_distinct_colors == 0:
+        number_of_distinct_colors = 80
+
+    number_of_shades = 7
+    if number_of_distinct_colors < (number_of_shades-1):
+        number_of_shades = number_of_distinct_colors - 2
+
+    number_of_distinct_colors_with_multiply_of_shades = int(math.ceil(number_of_distinct_colors / number_of_shades) * number_of_shades)
+
+    # Create an array with uniformly drawn floats taken from <0, 1) partition
+    linearly_distributed_nums = np.arange(number_of_distinct_colors_with_multiply_of_shades) / number_of_distinct_colors_with_multiply_of_shades
+
+    # We are going to reorganise monotonically growing numbers in such way that there will be single array with saw-like pattern
+    #     but each saw tooth is slightly higher than the one before
+    # First divide linearly_distributed_nums into number_of_shades sub-arrays containing linearly distributed numbers
+    arr_by_shade_rows = linearly_distributed_nums.reshape(number_of_shades, number_of_distinct_colors_with_multiply_of_shades // number_of_shades)
+
+    # Transpose the above matrix (columns become rows) - as a result each row contains saw tooth with values slightly higher than row above
+    arr_by_shade_columns = arr_by_shade_rows.T
+    
+     # Keep number of saw teeth for later
+    number_of_partitions = arr_by_shade_columns.shape[0]
+
+    # Flatten the above matrix - join each row into single array
+    nums_distributed_like_rising_saw = arr_by_shade_columns.reshape(-1)
+
+    # HSV colour map is cyclic (https://matplotlib.org/tutorials/colors/colormaps.html#cyclic), we'll use this property
+    initial_cm = hsv(nums_distributed_like_rising_saw)
+
+    lower_partitions_half = number_of_partitions // 2
+    upper_partitions_half = number_of_partitions - lower_partitions_half
+
+    # Modify lower half in such way that colours towards beginning of partition are darker
+    # First colours are affected more, colours closer to the middle are affected less
+    lower_half = lower_partitions_half * number_of_shades
+    for i in range(3):
+        initial_cm[0:lower_half, i] *= np.arange(0.2, 1, 0.8/lower_half)
+
+    # Modify second half in such way that colours towards end of partition are less intense and brighter
+    # Colours closer to the middle are affected less, colours closer to the end are affected more
+    for i in range(3):
+        for j in range(upper_partitions_half):
+            modifier = np.ones(number_of_shades) - initial_cm[lower_half + j * number_of_shades: lower_half + (j + 1) * number_of_shades, i]
+            modifier = j * modifier / upper_partitions_half
+            initial_cm[lower_half + j * number_of_shades: lower_half + (j + 1) * number_of_shades, i] += modifier
+
+    return ListedColormap(initial_cm)
+  
+
+import numpy as np
+def open_pdb(pdb):
+    with open(pdb + "_capsid.pdb") as ifile:
+        mol = Molecule(ifile)
+    return mol
+
+  
+def clusters_colormap_hexcolor(clusters, rwb_scale):
+    import matplotlib as mpl
+    norm = mpl.colors.Normalize(vmin=np.min(clusters), vmax=np.max(clusters))
+
+    if rwb_scale:
+        cmap = mpl.pyplot.get_cmap('coolwarm_r')
+        rgba = cmap(norm(clusters))
+    else:
+        cmap = generate_colormap(int(np.max(clusters)))
+        rgba = cmap(norm(clusters))
+    hexcolor = []
+    for c in rgba:
+      hexcolor.append(mpl.colors.rgb2hex(c))
+
+    return hexcolor, cmap
+      
+def cluster_scheme(mol, hexcolor, clusters):
+  r0 = mol[0]['resid']
+  c0 = hexcolor[0]
+  clust_scheme = []
+  select = '@'
+  i = 1
+  j = 0
+  for at in mol:
+      r = at['resid']
+      if r == r0:
+          select += str(i) + ','
+          i += 1
+      else:
+          clust_scheme.append([c0, select[:-1]])
+          select = '@' + str(i) + ','
+          r0 = r
+          j +=1
+          i +=1
+          c0 = hexcolor[j]
+          l0 = clusters[j]
+  clust_scheme.append([c0, select[:-1]])
+
+  return clust_scheme
+  
+
+def view_pdb_ngl(pdb, capsid, labels, rwb_scale=False):
+    import biotite.structure.io as strucio
+    strucio.save_structure(pdb + '_capsid.pdb', capsid, hybrid36=True)
+
+    mol = open_pdb(pdb)
+    hexcolor, cmap = clusters_colormap_hexcolor(labels, rwb_scale)
+    clust_scheme = cluster_scheme(mol, hexcolor, labels)
+
+    import nglview as ngl
+    color_scheme = ngl.color._ColorScheme(clust_scheme, label="scheme_regions")
+    view = ngl.show_structure_file(pdb + '_capsid.pdb', gui=False, default_representation=False)
+    view._remote_call("setSize", target='Widget', args=['800px', '800px'])
+    view.add_representation("spacefill", color=color_scheme)
+
+
+    if rwb_scale:
+        print('Each atom in this structure is colored according to the clustering quality score of its residue.')
+        import matplotlib.colorbar as colorbar
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(figsize=(10,0.5))
+        cb = colorbar.ColorbarBase(ax, orientation='horizontal',
+                                   cmap=cmap, norm=plt.Normalize(np.min(labels), np.max(labels)))
+        plt.show()
+        code = """
+                    var $text = $("<div></div>")
+                                .css("position", "absolute")
+                                .css("top", "3%")
+                                .css("left", "30%")
+                                .css("padding", "2px 5px 2px 5px")
+                                .css("opacity", "1.0")
+                                .css("font-size", "30px")
+                                .css("color", "black")
+                                .appendTo(this.$container);
+
+                    $text.text("{0} residue cluster scores")
+                    """
+        view._execute_js_code(code.format(pdb))
+    else:
+        print('Each atom in this structure has the same color as other atoms in the same cluster.')
+        code = """
+            var $text = $("<div></div>")
+                        .css("position", "absolute")
+                        .css("top", "3%")
+                        .css("left", "40%")
+                        .css("padding", "2px 5px 2px 5px")
+                        .css("opacity", "1.0")
+                        .css("font-size", "30px")
+                        .css("color", "black")
+                        .appendTo(this.$container);
+
+            $text.text("{0} ({1} clusters)")
+            """
+        n_clusters = str(int(np.max(labels) + 1))
+        view._execute_js_code(code.format(pdb, n_clusters))
+
+
+    view.center()
+    print('rendering')
+    view.render_image()
+
+    return view
+
+def createCapsidView(pdb, capsid=None):
+
+    if capsid is None:
+        print('No capsid structure provided, getting capsid.')
+        from pyCapsid.PDB import getCapsid
+        capsid, _, _, _, _, _, _ = getCapsid(pdb)
+
+    import biotite.structure.io as strucio
+    strucio.save_structure(pdb + '_capsid.pdb', capsid, hybrid36=True)
+
+    import nglview as ngl
+    view = ngl.NGLWidget()
+    from nglview.adaptor import FileStructure
+
+    view.add_component(FileStructure(pdb + '_capsid.pdb'))
+    #view.clear_representations()
+
+    return view
+
+def showColorBar(labels):
+    print('Each atom in this structure is colored according to the clustering quality score of its residue.')
+    import matplotlib.colorbar as colorbar
+    import matplotlib.pyplot as plt
+    hexcolor, cmap = clusters_colormap_hexcolor(labels, rwb_scale=True)
+    fig, ax = plt.subplots(figsize=(10, 0.5))
+    cb = colorbar.ColorbarBase(ax, orientation='horizontal',
+                               cmap=cmap, norm=plt.Normalize(np.min(labels), np.max(labels)))
+    plt.show()
+
+def createClusterRepresentation(pdb, labels, view, rwb_scale=False, rep_type='cartoon'):
+
+    mol = open_pdb(pdb)
+    hexcolor, cmap = clusters_colormap_hexcolor(labels, rwb_scale)
+    clust_scheme = cluster_scheme(mol, hexcolor, labels)
+
+    n_components = view.n_components
+    for i in range(n_components):
+        view.clear_representations(component=i)
+
+    import nglview as ngl
+    color_scheme = ngl.color._ColorScheme(clust_scheme, label="scheme_regions")
+    view._remote_call("setSize", target='Widget', args=['800px', '800px'])
+    view.add_representation(rep_type, color=color_scheme)
+
+    if rwb_scale:
+        print('Each atom in this structure is colored according to the clustering quality score of its residue.')
+        import matplotlib.colorbar as colorbar
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(figsize=(10, 0.5))
+        cb = colorbar.ColorbarBase(ax, orientation='horizontal',
+                                   cmap=cmap, norm=plt.Normalize(np.min(labels), np.max(labels)))
+        plt.show()
+        code = """
+                        var $text = $("<div></div>")
+                                    .css("position", "absolute")
+                                    .css("top", "3%")
+                                    .css("left", "30%")
+                                    .css("padding", "2px 5px 2px 5px")
+                                    .css("opacity", "1.0")
+                                    .css("font-size", "30px")
+                                    .css("color", "black")
+                                    .appendTo(this.$container);
+
+                        $text.text("{0} residue cluster scores")
+                        """
+        view._execute_js_code(code.format(pdb))
+    else:
+        print('Each atom in this structure has the same color as other atoms in the same cluster.')
+        code = """
+                var $text = $("<div></div>")
+                            .css("position", "absolute")
+                            .css("top", "3%")
+                            .css("left", "40%")
+                            .css("padding", "2px 5px 2px 5px")
+                            .css("opacity", "1.0")
+                            .css("font-size", "30px")
+                            .css("color", "black")
+                            .appendTo(this.$container);
+
+                $text.text("{0} ({1} clusters)")
+                """
+        n_clusters = str(int(np.max(labels) + 1))
+        view._execute_js_code(code.format(pdb, n_clusters))
+
+    view.center()
+    print('rendering')
+    #view.render_image()
+
+
+def chimeraxReportScriptVis(report_path, remote=True, chimerax_path=None, **kwargs):
+    """Launches ChimeraX and runs a script that visualizes the results.
+
+    :param labels:
+    :param pdb:
+    :param remote:
+    :param chimerax_path:
+    :param pdb_path:
+    :param save_path:
+    :param script_path:
+    :param labels_path:
+    :return:
+    """
+
+    import os
+    import platform
+
+    if 'offscreen' in kwargs:
+        offscreen = kwargs['offscreen']
+    else:
+        offscreen = False
+
+    if chimerax_path is None:
+        print('No chimerax path specified, checking in default locations for your OS')
+        if platform.system() == 'Linux':
+            chimerax_path = 'usr/bin/chimerax'
+        elif platform.system() == 'Windows':
+            chimerax_path = 'C:\\Program Files\\ChimeraX\\bin\\ChimeraX.exe'
+        elif platform.system() == 'Darwin':
+            chimerax_path = '/Applications/ChimeraX'
+        else:
+            print('No chimerax path is given and cannot check default locations')
+            chimerax_path = ''
+
+    # get path to chimerax script
+    import os
+
+    script_path = f'{report_path}/chimerax/chimerax_script_colab.py'
+
+    chimerax_exe = chimerax_path  # + '\\ChimeraX.exe'
+    if platform.system() == 'Windows':
+        cmd_string = f'""{chimerax_exe}" --script "{script_path}""'
+    elif platform.system() == 'Linux':
+        cmd_string = f'{chimerax_exe} --script "{script_path}"'
+    else:
+        cmd_string = f'{chimerax_exe} --script "{script_path}"'
+
+    if offscreen:
+        cmd_string = cmd_string + ' --offscreen'
+    print(cmd_string)
+    os.system(cmd_string)

39ad69e7f4d3302f5ea0334e4d3b4065107cde57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:09:35 +0000

    Add package crud-mysql/0.1.3/crud_mysql-0.1.3.tar.gz

---
+++ packages/crud-mysql/crud_mysql-0.1.3.tar.gz/crud_mysql-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='crud-mysql',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=['mysql-connector-python', 'mysql-database', 'flask'],
+    author='hanna',
+    author_email='channashosh@gmail.com',
+    description='crud defiend by json',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ms-Shoshany/crud-mysql',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)

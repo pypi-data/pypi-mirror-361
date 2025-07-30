8b058fa3ad96e16413a8fa24d2e69d9646565fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:57 +0000

    Add package square-database/3.2.1/square_database-3.2.1.tar.gz

---
+++ packages/square-database/square_database-3.2.1.tar.gz/square_database-3.2.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+package_name = "square_database"
+
+setup(
+    name=package_name,
+    version="3.2.1",
+    packages=find_packages(),
+    package_data={
+        package_name: ["data/*", "pydantic_models/*"],
+    },
+    install_requires=[
+        "sqlalchemy>=2.0.23",
+        "uvicorn>=0.24.0.post1",
+        "fastapi>=0.104.1",
+        "python-multipart>=0.0.6",
+        "websockets>=12.0",
+        "httpx>=0.26.0",
+        "pytest>=8.0.0",
+        "square_logger>=2.0.0",
+        "square_commons>=2.1.0",
+        "pydantic>=2.9.2",
+    ],
+    extras_require={
+        "all": [
+            "square_database_structure>=2.5.8",
+        ],
+    },
+    author="thePmSquare",
+    author_email="thepmsquare@gmail.com",
+    description="database layer for my personal server.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/thepmsquare/{package_name}",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
---
+++ packages/square-database/square_database-3.2.1.tar.gz/square_database-3.2.1/tests/test_main.py
@@ -0,0 +1,70 @@
+from square_commons import get_api_output_in_standard_format
+
+
+def test_database_creation(create_client_and_cleanup):
+    pass
+
+
+def test_read_main(get_patched_configuration, create_client_and_cleanup):
+
+    client = create_client_and_cleanup
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == get_api_output_in_standard_format(
+        log=get_patched_configuration.config_str_module_name
+    )
+
+
+# test insert_rows
+def test_insert_rows(create_client_and_cleanup, fixture_insert_rows):
+    client = create_client_and_cleanup
+    response = client.post("/insert_rows/v0", json=fixture_insert_rows)
+    assert response.status_code == 201
+    response_json = response.json()
+    assert "data" in response_json
+    assert "main" in response_json["data"]
+    assert "affected_count" in response_json["data"]
+    assert len(response_json["data"]["main"]) == 1
+    assert response_json["data"]["main"][0]["test_text"] == "example"
+    # assert response_json["data"]["main"][0]["test_id"] == 1
+    assert response_json["data"]["affected_count"] == 1
+    assert "log" in response_json
+    assert "message" in response_json
+
+
+def test_get_rows(create_client_and_cleanup, fixture_get_rows):
+    client = create_client_and_cleanup
+
+    response = client.post(
+        "/get_rows/v0",
+        json=fixture_get_rows,
+    )
+
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "data" in response_json
+    assert "main" in response_json["data"]
+    assert "total_count" in response_json["data"]
+    assert len(response_json["data"]["main"]) == 1
+    assert response_json["data"]["main"][0]["test_text"] == "example"
+    # assert response_json["data"]["main"][0]["test_id"] == 1
+    assert response_json["data"]["total_count"] == 1
+    assert "log" in response_json
+    assert "message" in response_json
+
+
+def test_duplicate_insert_rows(
+    create_client_and_cleanup, fixture_duplicate_insert_rows
+):
+    client = create_client_and_cleanup
+    response = client.post("/insert_rows/v0", json=fixture_duplicate_insert_rows)
+    assert response.status_code == 201
+    response_json = response.json()
+    assert "data" in response_json
+    assert "main" in response_json["data"]
+    assert "affected_count" in response_json["data"]
+    assert len(response_json["data"]["main"]) == 1
+    assert response_json["data"]["main"][0]["test_text"] == "example"
+    assert response_json["data"]["affected_count"] == 1
+    assert "log" in response_json
+    assert "message" in response_json

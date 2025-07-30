3e434fa75094bb8c9cdf4ce5a6050d90817cff2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:23 +0000

    Add package aiogram-blueprint/0.0.0b7/aiogram_blueprint-0.0.0b7.tar.gz

---
+++ packages/aiogram-blueprint/aiogram_blueprint-0.0.0b7.tar.gz/aiogram_blueprint-0.0.0b7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiogram-blueprint",
+    version="0.0.0b7",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "Jinja2",
+        "InquirerPy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "aiogram-blueprint=aiogram_blueprint.__main__:cli",
+        ],
+    },
+    package_data={
+        "aiogram_blueprint": [
+            "template/**/*",
+            "template/.env.j2",
+            "template/data/**/*",
+        ],
+    },
+)

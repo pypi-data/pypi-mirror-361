bfb1912ba609e9f5c373f1b25b592ea6e387faad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:02 +0000

    Add package shapepy/1.1.3/shapepy-1.1.3.tar.gz

---
+++ packages/shapepy/shapepy-1.1.3.tar.gz/shapepy-1.1.3/src/shapepy/__init__.py
@@ -0,0 +1,27 @@
+"""
+This module analyse sections of beams by using the boundary element method
+It uses mainly curves as boundary to compute the elements.
+For the moment, it only uses polygon shapes:
+    retangular is a 4-side shape while a circle is a polygon with many sides
+"""
+
+import importlib
+
+from shapepy.bool2d.primitive import Primitive
+from shapepy.bool2d.shape import (
+    ConnectedShape,
+    DisjointShape,
+    EmptyShape,
+    IntegrateShape,
+    SimpleShape,
+    WholeShape,
+)
+from shapepy.geometry.curve import IntegratePlanar, PlanarCurve
+from shapepy.geometry.jordancurve import IntegrateJordan, JordanCurve
+from shapepy.geometry.polygon import Point2D
+from shapepy.plot.plot import ShapePloter
+
+__version__ = importlib.metadata.version("shapepy")
+
+if __name__ == "__main__":
+    pass

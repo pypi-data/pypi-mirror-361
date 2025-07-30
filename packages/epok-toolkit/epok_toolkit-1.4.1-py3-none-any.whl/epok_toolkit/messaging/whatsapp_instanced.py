e539fe239dc83d73ac43e92e19f9b9a15cf197c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:43 +0000

    Add package epok-toolkit/1.4.1/epok_toolkit-1.4.1-py3-none-any.whl

---
+++ packages/epok-toolkit/epok_toolkit-1.4.1-py3-none-any.whl/epok_toolkit/messaging/whatsapp_instanced.py
@@ -0,0 +1,26 @@
+from .whatsapp import WhatsappClient
+from django.conf import settings
+from celery import shared_task
+
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(ConnectionError, TimeoutError),
+    retry_backoff=True,       # 2, 4, 8, 16… s
+    retry_jitter=True,        # +- aleatorio
+    max_retries=3,
+    ignore_result=True,       # <-- ¡importante!
+)
+def send_whatsapp_message_async(number: str, message: str):
+    from colorstreak import log
+    log.debug(f"Enviando mensaje a {number}: '{message}'")
+    API_KEY = settings.API_KEY
+    INSTANCE = settings.INSTANCE
+    SERVER_URL = settings.SERVER_URL
+
+    client = WhatsappClient(api_key=API_KEY, server_url=SERVER_URL, instance_name=INSTANCE)
+    return client.send_text(number, message)
+
+def send_text(number: str, message: str):
+    send_whatsapp_message_async(number, message)

8a7b5dfe4ec5497fe83eb54d19869ef238d6fd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:59 +0000

    Add package odoo14-addon-ssi-liquidity-transfer-work-log/14.0.1.0.0/odoo14_addon_ssi_liquidity_transfer_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-liquidity-transfer-work-log/odoo14_addon_ssi_liquidity_transfer_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_liquidity_transfer_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Liquidity Transfer - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_liquidity_transfer",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}

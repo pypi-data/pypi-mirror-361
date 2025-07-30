cbeb9d934a16b3917976893a97191990e5fc67d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-orchestrator/1.0.0/mcpturbo_orchestrator-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-orchestrator/mcpturbo_orchestrator-1.0.0-py3-none-any.whl/mcpturbo_orchestrator/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Orchestrator
+
+MCPTurbo - Intelligent Agent Orchestration & Coordination
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []

29d7e0e968f871d688a4638614abf9556b71c1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:05 +0000

    Add package dhv/0.3.0/dhv-0.3.0.tar.gz

---
+++ packages/dhv/dhv-0.3.0.tar.gz/dhv-0.3.0/ChangeLog.md
@@ -0,0 +1,29 @@
+# DHV ChangeLog
+
+## v0.3.0
+
+**Released: 2025-07-12**
+
+- Added support for viewing the AST of the Python code.
+  ([#8](https://github.com/davep/dhv/pull/8))
+
+## v0.2.0
+
+**Released: 2025-07-09**
+
+- Added the ability to pass a filename on the command line.
+  ([#5](https://github.com/davep/dhv/pull/5))
+
+## v0.1.1
+
+**Released: 2025-07-08**
+
+- Initial release.
+
+## v0.1.0
+
+**Released: 2025-07-01**
+
+- Initial placeholder package to test that the name is available in PyPI.
+
+[//]: # (ChangeLog.md ends here)
---
+++ packages/dhv/dhv-0.3.0.tar.gz/dhv-0.3.0/src/dhv/commands/main.py
@@ -0,0 +1,54 @@
+"""Provides the main commands for the application."""
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.commands import Command
+
+
+##############################################################################
+class NewCode(Command):
+    """Empty the editor ready to enter some new code."""
+
+    BINDING_KEY = "ctrl+n"
+    SHOW_IN_FOOTER = True
+    FOOTER_TEXT = "New"
+
+
+##############################################################################
+class LoadFile(Command):
+    """Load the content of a Python source file."""
+
+    BINDING_KEY = "ctrl+l"
+    SHOW_IN_FOOTER = True
+    FOOTER_TEXT = "Load"
+
+
+##############################################################################
+class SwitchLayout(Command):
+    """Switch the screen layout between horizontal and vertical."""
+
+    BINDING_KEY = "f2"
+
+
+##############################################################################
+class ShowDisassemblyOnly(Command):
+    """Only show the disassembly of the source code."""
+
+    BINDING_KEY = "ctrl+b"
+
+
+##############################################################################
+class ShowASTOnly(Command):
+    """Only show the AST of the source code."""
+
+    BINDING_KEY = "ctrl+t"
+
+
+##############################################################################
+class ShowDisassemblyAndAST(Command):
+    """Show both the disassembly and the AST of the source code."""
+
+    BINDING_KEY = "ctrl+o"
+
+
+### main.py ends here

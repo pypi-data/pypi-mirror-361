da914d40e64e27ab1e2598c8f16b13088da1709b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:52 +0000

    Add package business-tips/0.0.1/business-tips-0.0.1.tar.gz

---
+++ packages/business-tips/business-tips-0.0.1.tar.gz/business-tips-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="business-tips",
+    version="0.0.1",
+    packages=["business-tips"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

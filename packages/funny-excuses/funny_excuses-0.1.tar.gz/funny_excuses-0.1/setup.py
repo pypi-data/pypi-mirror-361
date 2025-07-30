9fab6ae45b4415a4c20fc978b0b2d719bb950604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:53 +0000

    Add package funny-excuses/0.1/funny_excuses-0.1.tar.gz

---
+++ packages/funny-excuses/funny_excuses-0.1.tar.gz/funny_excuses-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="funny_excuses",
+    version="0.1",
+    packages=find_packages(),
+    description="A funny excuse generator for everyday life.",
+    author="Kamran Ahmed",
+    author_email="your.email@example.com",
+    keywords=["funny", "excuses", "humor", "generator"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

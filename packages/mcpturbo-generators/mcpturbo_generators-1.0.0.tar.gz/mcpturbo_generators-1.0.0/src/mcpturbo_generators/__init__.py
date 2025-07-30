7c763658238934d6793be92e391ca71531f95d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-generators/1.0.0/mcpturbo_generators-1.0.0.tar.gz

---
+++ packages/mcpturbo-generators/mcpturbo_generators-1.0.0.tar.gz/mcpturbo_generators-1.0.0/src/mcpturbo_generators/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Generators
+
+MCPTurbo - Code Generation Agents (Backend, Frontend, DevOps)
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []
---
+++ packages/mcpturbo-generators/mcpturbo_generators-1.0.0.tar.gz/mcpturbo_generators-1.0.0/src/mcpturbo_generators/architect.py
@@ -0,0 +1,14 @@
+"""Architect agent for system design"""
+
+from mcpturbo_agents import BaseAgent
+
+class ArchitectAgent(BaseAgent):
+    """Agent responsible for system architecture design"""
+    
+    def __init__(self):
+        super().__init__("architect", "ArchitectAgent")
+        
+    async def design_architecture(self, requirements):
+        """Design system architecture based on requirements"""
+        # Implementation will be added
+        pass

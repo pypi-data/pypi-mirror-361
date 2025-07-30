45a09526045dcacc40ee9f0ea8f1999f32e8e8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:40 +0000

    Add package youdl-jyan/0.6.5.7/youdl-jyan-0.6.5.7.tar.gz

---
+++ packages/youdl-jyan/youdl-jyan-0.6.5.7.tar.gz/youdl-jyan-0.6.5.7/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup,find_packages
+import re
+def version():
+    filename = "youdl-jyan/__init__.py"
+    with open(filename) as f:
+        match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""", f.read(), re.M)
+    if not match:
+        raise RuntimeError("{} doesn't contain __version__".format(filename))
+    version = match.groups()[0]
+    return version
+with open("README.md", encoding="utf8") as readme:
+    long_desc = readme.read()
+    
+
+
+# Setting up
+setup(
+    name="youdl-jyan",
+    version=version(),
+    author="Yasir | TeamProBots",
+    author_email="yasircpr@gmail.com",
+    description="python api hub | youdl-jyan",
+    long_description_content_type="text/markdown",
+    long_description=long_desc,
+    packages=find_packages(),
+    license="MIT",
+    url="https://github.com/TeamProBots/youdl-jyan",
+    download_url="https://github.com/TeamProBots/youdl-jyan/blob/main/README.md",
+    install_requires=["pytz>=2023.3","requests-html","pillow","lxml_html_clean"],
+    keywords=['python', "youdl-jyan","flask"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    
+    project_urls={
+        "Tracker": "https://github.com/TeamProBots/youdl-jyan/issues",
+        "Community": "https://t.me/ZeoXpro",
+        "Source": "https://github.com/TeamProBots/youdl-jyan",
+    },
+    python_requires="~=3.7",
+)

63c752904235919025097b45929fcb7449440982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:24 +0000

    Add package adviz/0.0.25/adviz-0.0.25.tar.gz

---
+++ packages/adviz/adviz-0.0.25.tar.gz/adviz-0.0.25/adviz/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.0.25"
+
+from dash_bootstrap_templates import load_figure_template
+
+from .ecdf import ecdf
+from .flag import country_code_flag, flag
+from .racing_chart import racing_chart
+from .serp_heatmap import serp_heatmap
+from .status_codes import status_codes
+from .style_table import style_table
+from .url_structure import url_structure
+from .value_counts import value_counts
+from .value_counts_plus import value_counts_plus
+
+load_figure_template("all")

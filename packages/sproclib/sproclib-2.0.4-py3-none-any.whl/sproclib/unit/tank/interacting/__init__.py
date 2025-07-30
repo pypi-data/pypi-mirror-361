72e17aaf929c3a7811b79038bbca32ca8b19c486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:58 +0000

    Add package sproclib/2.0.4/sproclib-2.0.4-py3-none-any.whl

---
+++ packages/sproclib/sproclib-2.0.4-py3-none-any.whl/sproclib/unit/tank/interacting/__init__.py
@@ -0,0 +1,9 @@
+"""
+InteractingTanks module for SPROCLIB - Standard Process Control Library
+
+This module imports the InteractingTanks class from InteractingTanks.py.
+"""
+
+from ..InteractingTanks import InteractingTanks
+
+__all__ = ['InteractingTanks']

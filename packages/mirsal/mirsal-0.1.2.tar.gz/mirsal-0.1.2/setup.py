f76a4e3951d492499fc5d8ae85f5e46a041172a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:09 +0000

    Add package mirsal/0.1.2/mirsal-0.1.2.tar.gz

---
+++ packages/mirsal/mirsal-0.1.2.tar.gz/mirsal-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mirsal",
+    version="0.1.2",  # ← bump this every time you re-upload
+    packages=find_packages(),
+    install_requires=[
+        "flask",
+        "pyngrok",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mirsal=mirsal.cli:main",
+        ],
+    },
+    author="Bashar T.",
+    description="📄 Live log viewer with Ngrok + Flask",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bashartalafha/mirsal",  # ← GitHub main page
+    project_urls={
+        "Source Code": "https://github.com/bashartalafha/mirsal",
+        "Bug Tracker": "https://github.com/bashartalafha/mirsal/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
+

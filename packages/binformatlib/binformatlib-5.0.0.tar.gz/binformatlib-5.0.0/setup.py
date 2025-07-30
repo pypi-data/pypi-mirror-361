395d28328063965df4e21cf745a008c209dae490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:56 +0000

    Add package binformatlib/5.0.0/binformatlib-5.0.0.tar.gz

---
+++ packages/binformatlib/binformatlib-5.0.0.tar.gz/binformatlib-5.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binformatlib',
+    version='5.0.0',
+    description='A beginner-friendly binary format packing/unpacking module with full hex and byte control.',
+    author='jony eldry',
+    author_email='umayashinderu@gmail.com',
+    py_modules=['binformatlib'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: File Formats',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.6',
+)

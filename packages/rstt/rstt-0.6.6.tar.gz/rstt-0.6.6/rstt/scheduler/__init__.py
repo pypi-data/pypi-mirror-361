cf9d74378cfbed7c33d296b5dc7ca0798b0d9925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:16 +0000

    Add package rstt/0.6.6/rstt-0.6.6.tar.gz

---
+++ packages/rstt/rstt-0.6.6.tar.gz/rstt-0.6.6/rstt/scheduler/__init__.py
@@ -0,0 +1,7 @@
+"""Package for automated matches generation procedure
+
+RSTT consider scheduler in competition as a large notion covering both sport tournaments and live-matchmaking.
+"""
+
+from .tournament import Competition
+from .tournament import RoundRobin, SwissRound, RandomRound, SwissBracket, SingleEliminationBracket, DoubleEliminationBracket, Snake

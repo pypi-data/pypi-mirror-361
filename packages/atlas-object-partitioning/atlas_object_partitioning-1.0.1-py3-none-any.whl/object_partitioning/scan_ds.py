acfa390b79db31b4b5925e9ce49805e6f4ba8ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:31 +0000

    Add package atlas-object-partitioning/1.0.1/atlas_object_partitioning-1.0.1-py3-none-any.whl

---
+++ packages/atlas-object-partitioning/atlas_object_partitioning-1.0.1-py3-none-any.whl/object_partitioning/scan_ds.py
@@ -0,0 +1,10 @@
+import warnings
+
+warnings.warn(
+    "The 'object_partitioning' package is deprecated. Use 'atlas_object_partitioning' instead.",
+    DeprecationWarning,
+)
+
+
+def collect_object_counts(ds_name: str, n_files: int = 1):
+    pass

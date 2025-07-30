c6e5c6cdf6025db00157f2c03913b2d2865e4e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:20 +0000

    Add package ob-metaflow-stubs/6.0.3.188/ob-metaflow-stubs-6.0.3.188.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.188.tar.gz/ob-metaflow-stubs-6.0.3.188/metaflow-stubs/info_file.pyi
@@ -0,0 +1,17 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-11T16:11:55.022565                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+CURRENT_DIRECTORY: str
+
+INFO_FILE: str
+
+def read_info_file():
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.188.tar.gz/ob-metaflow-stubs-6.0.3.188/metaflow-stubs/ob_internal.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-11T16:11:55.022388                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .mf_extensions.outerbounds.plugins.kubernetes.pod_killer import PodKiller as PodKiller
+

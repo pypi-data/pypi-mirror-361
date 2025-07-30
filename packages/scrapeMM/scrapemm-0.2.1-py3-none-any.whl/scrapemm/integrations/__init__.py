2f4e0f97a780f9c3836f1d15adab7426cd2e1889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:07 +0000

    Add package scrapeMM/0.2.1/scrapemm-0.2.1-py3-none-any.whl

---
+++ packages/scrapeMM/scrapemm-0.2.1-py3-none-any.whl/scrapemm/integrations/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+logger = logging.getLogger("scrapeMM")
+logger.debug("Starting integrations...")
+
+from typing import Optional
+
+import aiohttp
+from ezmm import MultimodalSequence
+
+from scrapemm.util import get_domain
+from .bluesky import Bluesky
+from .telegram import Telegram
+from .tiktok import TikTok
+from .x import X
+
+RETRIEVAL_INTEGRATIONS = [X(), Telegram(), Bluesky(), TikTok()]
+DOMAIN_TO_INTEGRATION = {domain: integration
+                         for integration in RETRIEVAL_INTEGRATIONS
+                         for domain in integration.domains
+                         if integration.connected}
+
+
+async def retrieve_via_integration(url: str, session: aiohttp.ClientSession) -> Optional[MultimodalSequence]:
+    domain = get_domain(url)
+    if domain in DOMAIN_TO_INTEGRATION:
+        integration = DOMAIN_TO_INTEGRATION[domain]
+        return await integration.get(url, session)

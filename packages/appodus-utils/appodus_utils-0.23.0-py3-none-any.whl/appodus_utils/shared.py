d8904d5e72618ded2aa0d031d47c7e920551d0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:16 +0000

    Add package appodus-utils/0.23.0/appodus_utils-0.23.0-py3-none-any.whl

---
+++ packages/appodus-utils/appodus_utils-0.23.0-py3-none-any.whl/appodus_utils/shared.py
@@ -0,0 +1,9 @@
+# Configure the logger
+import logging
+
+logger = logging.getLogger("Appodus Utils")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(levelname)s] %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)

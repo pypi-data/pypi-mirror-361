837e7777afff3ddad6f6bf17f57ea93f86f1627c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:23 +0000

    Add package zhiqing/0.0.7/zhiqing-0.0.7.tar.gz

---
+++ packages/zhiqing/zhiqing-0.0.7.tar.gz/zhiqing-0.0.7/src/zhiqing/__init__.py
@@ -0,0 +1,22 @@
+import copy
+from importlib.metadata import version
+
+__version__ = version("zhiqing")
+
+
+def min_repetend_length(inputs):
+    count = len(inputs)
+    for i in range(1, count):
+        for j in range(i+1, count):
+            if not (inputs[j-i] == inputs[j]):
+                break
+        else:
+            return i
+    return count
+
+
+def merge_dicts(arg, *args):
+    results = copy.copy(arg)
+    for a in args:
+        results.update(a)
+    return results

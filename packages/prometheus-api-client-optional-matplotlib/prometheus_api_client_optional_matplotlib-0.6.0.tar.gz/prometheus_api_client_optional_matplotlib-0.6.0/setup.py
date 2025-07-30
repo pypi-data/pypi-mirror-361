f29848d28a6f448e96216d5783f16f6015c3cd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:15 +0000

    Add package prometheus-api-client-optional-matplotlib/0.6.0/prometheus_api_client_optional_matplotlib-0.6.0.tar.gz

---
+++ packages/prometheus-api-client-optional-matplotlib/prometheus_api_client_optional_matplotlib-0.6.0.tar.gz/prometheus_api_client_optional_matplotlib-0.6.0/setup.py
@@ -0,0 +1,47 @@
+"""Setup for Prometheus Api Client module."""
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def get_install_requires():
+    """Get requirements from requirements.txt."""
+    with open("requirements.txt", "r") as requirements_file:
+        res = requirements_file.readlines()
+        return [req.split(" ", maxsplit=1)[0] for req in res if req]
+
+
+def get_version():
+    """Get package version."""
+    with open(os.path.join("prometheus_api_client", "__init__.py")) as f:
+        content = f.readlines()
+
+    for line in content:
+        if line.startswith("__version__ ="):
+            # dirty, remove trailing and leading chars
+            return line.split(" = ")[1][1:-2]
+    raise ValueError("No version identifier found")
+
+
+VERSION = get_version()
+setuptools.setup(
+    name="prometheus-api-client-optional-matplotlib",
+    version=VERSION,
+    author="Anand Sanmukhani",
+    author_email="asanmukhani@microsoft.com",
+    description="A Python API client for Prometheus with optional matplotlib dependency",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/4n4nd/prometheus-api-client-python",
+    install_requires=get_install_requires(),
+    packages=setuptools.find_packages(),
+    package_data={"prometheus-api-client": ["py.typed"]},
+    tests_require=["httmock"],
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

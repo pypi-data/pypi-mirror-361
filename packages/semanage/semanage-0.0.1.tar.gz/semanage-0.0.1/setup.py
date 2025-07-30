8cce0d70c130ede8eae4dc3356fce09fc75ed50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:35 +0000

    Add package semanage/0.0.1/semanage-0.0.1.tar.gz

---
+++ packages/semanage/semanage-0.0.1.tar.gz/semanage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="semanage",
+    version="0.0.1",
+    packages=["semanage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

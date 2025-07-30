d01d9313aea271a84d4a58f591991b2827dd3f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:05 +0000

    Add package spyt-transformations/0.0.1/spyt-transformations-0.0.1.tar.gz

---
+++ packages/spyt-transformations/spyt-transformations-0.0.1.tar.gz/spyt-transformations-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spyt-transformations",
+    version="0.0.1",
+    packages=["spyt-transformations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

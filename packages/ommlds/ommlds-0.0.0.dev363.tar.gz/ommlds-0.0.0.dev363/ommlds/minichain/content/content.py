b5a6d20c51b0667bcae35463b6bb739436d42cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:06 +0000

    Add package ommlds/0.0.0.dev363/ommlds-0.0.0.dev363.tar.gz

---
+++ packages/ommlds/ommlds-0.0.0.dev363.tar.gz/ommlds-0.0.0.dev363/ommlds/minichain/content/content.py
@@ -0,0 +1,25 @@
+import typing as ta
+
+from omlish import lang
+
+
+##
+
+
+class ExtendedContent(lang.Abstract, lang.PackageSealed):
+    pass
+
+
+##
+
+
+SingleContent: ta.TypeAlias = ta.Union[  # noqa
+    str,
+    ExtendedContent,
+]
+
+
+Content: ta.TypeAlias = ta.Union[  # noqa
+    ta.Sequence['Content'],
+    SingleContent,
+]

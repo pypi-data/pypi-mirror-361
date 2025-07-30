fff19ddb25213e4773c1c525eab678b50a58f160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:00 +0000

    Add package hanifx/8.0.2/hanifx-8.0.2.tar.gz

---
+++ packages/hanifx/hanifx-8.0.2.tar.gz/hanifx-8.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hanifx",
+    version="8.0.2",
+    author="HanifX Team",
+    author_email="sajim4653@gmail.com",
+    description="🔥 HANIFX — Encode, Stealth & Runtime Trap System for API, Token & Sensitive Data Protection",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hanifx-540/hanifx",
+    project_urls={
+        "Documentation": "https://github.com/hanifx-540/hanifx/wiki",
+        "Source": "https://github.com/hanifx-540/hanifx",
+        "Bug Tracker": "https://github.com/hanifx-540/hanifx/issues",
+    },
+    packages=find_packages(include=["hanifx", "hanifx.*"]),
+    include_package_data=True,
+    keywords=[
+        "hanifx", "encode", "obfuscate", "token-protection", "api-encode",
+        "anti-clone", "anti-scan", "traplogic", "cybersec", "uid-lock"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Security :: Cryptography",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license="MIT"
+)

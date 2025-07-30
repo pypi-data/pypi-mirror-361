952ae9e0ff0684ab88dc6c13de409b03404f3cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:50 +0000

    Add package icestream/0.1.4/icestream-0.1.4.tar.gz

---
+++ packages/icestream/icestream-0.1.4.tar.gz/icestream-0.1.4/src/icestream/kafkaserver/messages.py
@@ -0,0 +1,59 @@
+from kio.schema.api_versions.v4.request import (
+    ApiVersionsRequest,
+)
+from kio.schema.api_versions.v4.request import (
+    RequestHeader as ApiVersionsRequestHeader,
+)
+from kio.schema.api_versions.v4.response import (
+    ApiVersion,
+    ApiVersionsResponse,
+)
+from kio.schema.api_versions.v4.response import (
+    ResponseHeader as ApiVersionsResponseHeader,
+)
+from kio.schema.create_topics.v7.request import (
+    CreateTopicsRequest,
+)
+from kio.schema.create_topics.v7.request import (
+    RequestHeader as CreateTopicsRequestHeader,
+)
+from kio.schema.create_topics.v7.response import (
+    CreatableTopicResult,
+    CreateTopicsResponse,
+)
+from kio.schema.create_topics.v7.response import (
+    ResponseHeader as CreateTopicsResponseHeader,
+)
+from kio.schema.metadata.v12.request import (
+    MetadataRequest,
+    MetadataRequestTopic,
+    TopicName,
+)
+from kio.schema.metadata.v12.request import (
+    RequestHeader as MetadataRequestHeader,
+)
+from kio.schema.metadata.v12.response import (
+    MetadataResponse,
+    MetadataResponseBroker,
+    MetadataResponsePartition,
+    MetadataResponseTopic,
+)
+from kio.schema.metadata.v12.response import (
+    ResponseHeader as MetadataResponseHeader,
+)
+from kio.schema.produce.v9.request import (
+    PartitionProduceData,
+    ProduceRequest,
+    TopicProduceData,
+)
+from kio.schema.produce.v9.request import (
+    RequestHeader as ProduceRequestHeader,
+)
+from kio.schema.produce.v9.response import (
+    PartitionProduceResponse,
+    ProduceResponse,
+    TopicProduceResponse,
+)
+from kio.schema.produce.v9.response import (
+    ResponseHeader as ProduceResponseHeader,
+)

983029b91792fccc819ad3aff884e653e0629518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:43 +0000

    Add package ymrp/0.4.0/ymrp-0.4.0-py3-none-any.whl

---
+++ packages/ymrp/ymrp-0.4.0-py3-none-any.whl/ymrp/constants.py
@@ -0,0 +1,26 @@
+# Date constants
+months = {
+    'января': '01',
+    'февраля': '02',
+    'марта': '03',
+    'апреля': '04',
+    'мая': '05',
+    'июня': '06',
+    'июля': '07',
+    'августа': '08',
+    'сентября': '09',
+    'октября': '10',
+    'ноября': '11',
+    'декабря': '12',
+}
+
+# Time constants
+VERY_SMALL_TIMEOUT = 1_000  # 1 seconds
+SMALL_TIMEOUT = 4_000  # 4 seconds
+MEDIUM_TIMEOUT = 8_000  # 8 seconds
+BIG_TIMEOUT = 12_000  # 12 seconds
+
+# Selector constants
+REVIEWS_CONTAINER = '.business-reviews-card-view__reviews-container'
+REVIEW = '.business-reviews-card-view__review'
+REVIEW_VIEW_EXPAND = '.business-review-view__expand[aria-hidden="false"]'

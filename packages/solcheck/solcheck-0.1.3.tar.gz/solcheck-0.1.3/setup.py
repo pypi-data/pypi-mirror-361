b363513971d1fe7fdf6ff257bc78fc2d9ce2cf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:19 +0000

    Add package solcheck/0.1.3/solcheck-0.1.3.tar.gz

---
+++ packages/solcheck/solcheck-0.1.3.tar.gz/solcheck-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solcheck",                    
+    version="0.1.3",                            
+    author="Faiz Qowy",
+    author_email="faizislamicqowy@gmail.com",
+    description="A smart contract auditing tools",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/faizqowy/solcheck", 
+    packages=find_packages(),                   
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "slither-analyzer",
+        "py-solc-x"
+    ],
+)

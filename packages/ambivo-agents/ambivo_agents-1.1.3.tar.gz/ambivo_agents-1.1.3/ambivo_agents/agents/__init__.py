07d8b6271758d9870c5137254c7eb56c1dd735cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:39 +0000

    Add package ambivo-agents/1.1.3/ambivo_agents-1.1.3.tar.gz

---
+++ packages/ambivo-agents/ambivo_agents-1.1.3.tar.gz/ambivo_agents-1.1.3/ambivo_agents/agents/__init__.py
@@ -0,0 +1,21 @@
+# ambivo_agents/agents/__init__.py
+from .assistant import AssistantAgent
+from .code_executor import CodeExecutorAgent
+from .knowledge_base import KnowledgeBaseAgent
+from .web_search import WebSearchAgent
+from .web_scraper import WebScraperAgent
+from .media_editor import MediaEditorAgent
+from .youtube_download import YouTubeDownloadAgent
+from .moderator import ModeratorAgent
+
+__all__ = [
+    "AssistantAgent",
+    "CodeExecutorAgent",
+    "KnowledgeBaseAgent",
+    "WebSearchAgent",
+    "WebScraperAgent",
+    "MediaEditorAgent",
+    "YouTubeDownloadAgent",
+    "ModeratorAgent"
+]
+

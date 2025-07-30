c446ce4a046e494aedad76b17bcbd4581a2e72f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:08:21 +0000

    Add package chromolog/0.2.5.dev0/chromolog-0.2.5.dev0.tar.gz

---
+++ packages/chromolog/chromolog-0.2.5.dev0.tar.gz/chromolog-0.2.5.dev0/chromolog/__init__.py
@@ -0,0 +1,5 @@
+from .chromolog import Print, __author__, __version__
+from . import chromolog
+
+__doc__ = chromolog.__doc__
+__all__ = ["Print", "__author__", "__version__"]
---
+++ packages/chromolog/chromolog-0.2.5.dev0.tar.gz/chromolog-0.2.5.dev0/tests/test.py
@@ -0,0 +1,10 @@
+# pip install chromolog
+
+# Prueba con chromolog instalado desde Pypi - pip install
+# Importar módulo
+from chromolog import Print
+
+# Crear instancia
+pt = Print()
+# Usar prueba del módulo
+pt.test()

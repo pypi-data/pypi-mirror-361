d95ca44de62eb33cac5ac9940e61b63d5003f71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:06 +0000

    Add package yt-documentation/0.0.1/yt-documentation-0.0.1.tar.gz

---
+++ packages/yt-documentation/yt-documentation-0.0.1.tar.gz/yt-documentation-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yt-documentation",
+    version="0.0.1",
+    packages=["yt-documentation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)

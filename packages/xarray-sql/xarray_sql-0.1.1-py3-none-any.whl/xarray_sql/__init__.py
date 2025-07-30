ff746bfeaa4db7ab1c3ac81a59273baaed5f2d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:53 +0000

    Add package xarray-sql/0.1.1/xarray_sql-0.1.1-py3-none-any.whl

---
+++ packages/xarray-sql/xarray_sql-0.1.1-py3-none-any.whl/xarray_sql/__init__.py
@@ -0,0 +1,2 @@
+from .df import read_xarray, from_map
+from .sql import XarrayContext
---
+++ packages/xarray-sql/xarray_sql-0.1.1-py3-none-any.whl/xarray_sql/sql.py
@@ -0,0 +1,17 @@
+import xarray as xr
+from datafusion import SessionContext
+
+from .df import read_xarray, Chunks
+
+
+class XarrayContext(SessionContext):
+  """A datafusion `SessionContext` that also supports `xarray.Dataset`s."""
+
+  def from_dataset(
+      self,
+      table_name: str,
+      input_table: xr.Dataset,
+      chunks: Chunks = None,
+  ):
+    arrow_table = read_xarray(input_table, chunks)
+    return self.from_arrow(arrow_table, table_name)

44744efc1662a1d1af0106ded1cf9bc94d070485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:25 +0000

    Add package matrix-sdk/1.0.1/matrix_sdk-1.0.1-py3-none-any.whl

---
+++ packages/matrix-sdk/matrix_sdk-1.0.1-py3-none-any.whl/matrix/client/server.py
@@ -0,0 +1,5 @@
+SERVER_URI = "https://api.matrix-ai.app"
+UPLOAD_FILES_URI = "/upload/files/"
+MODEL_REQUEST_URI = "/repo/model/"
+REQUEST_RESULT_URI = "/repo/results/"
+REPO_LIST_URI = "/repo/list/"

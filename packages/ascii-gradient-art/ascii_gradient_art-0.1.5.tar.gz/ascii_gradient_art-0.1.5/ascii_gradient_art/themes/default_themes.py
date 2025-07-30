4be8f75d3ce652d5fd52e53a72443d69f0336bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:23 +0000

    Add package ascii-gradient-art/0.1.5/ascii_gradient_art-0.1.5.tar.gz

---
+++ packages/ascii-gradient-art/ascii_gradient_art-0.1.5.tar.gz/ascii_gradient_art-0.1.5/ascii_gradient_art/themes/default_themes.py
@@ -0,0 +1,83 @@
+
+COLOR_THEMES = {
+    'rainbow': [
+        {'pos': 0.0, 'color': (255, 0, 0)},   # Red
+        {'pos': 0.16, 'color': (255, 127, 0)}, # Orange
+        {'pos': 0.33, 'color': (255, 255, 0)}, # Yellow
+        {'pos': 0.5, 'color': (0, 255, 0)},   # Green
+        {'pos': 0.66, 'color': (0, 0, 255)},   # Blue
+        {'pos': 0.83, 'color': (75, 0, 130)},  # Indigo
+        {'pos': 1.0, 'color': (148, 0, 211)}   # Violet
+    ],
+    'ocean': [
+        {'pos': 0.0, 'color': (0, 0, 128)},   # Navy
+        {'pos': 0.5, 'color': (0, 191, 255)}, # Deep Sky Blue
+        {'pos': 1.0, 'color': (135, 206, 235)} # Sky Blue
+    ],
+    'fire': [
+        {'pos': 0.0, 'color': (0, 0, 0)},     # Black
+        {'pos': 0.2, 'color': (139, 0, 0)},   # Dark Red
+        {'pos': 0.4, 'color': (255, 69, 0)},  # Orange Red
+        {'pos': 0.7, 'color': (255, 140, 0)}, # Dark Orange
+        {'pos': 1.0, 'color': (255, 255, 0)}  # Yellow
+    ],
+    'forest': [
+        {'pos': 0.0, 'color': (34, 139, 34)}, # Forest Green
+        {'pos': 0.5, 'color': (85, 107, 47)}, # Dark Olive Green
+        {'pos': 1.0, 'color': (107, 142, 35)} # Olive Drab
+    ],
+    'sunset': [
+        {'pos': 0.0, 'color': (255, 165, 0)}, # Orange
+        {'pos': 0.5, 'color': (255, 69, 0)},  # Orange Red
+        {'pos': 1.0, 'color': (139, 0, 139)}  # Dark Magenta
+    ],
+    'pastel': [
+        {'pos': 0.0, 'color': (255, 204, 204)}, # Light Pink
+        {'pos': 0.3, 'color': (204, 255, 204)}, # Light Green
+        {'pos': 0.6, 'color': (204, 204, 255)}, # Light Blue
+        {'pos': 1.0, 'color': (255, 255, 204)}  # Light Yellow
+    ],
+    'grayscale': [
+        {'pos': 0.0, 'color': (0, 0, 0)},     # Black
+        {'pos': 0.5, 'color': (128, 128, 128)}, # Gray
+        {'pos': 1.0, 'color': (255, 255, 255)}  # White
+    ],
+    'neon': [
+        {'pos': 0.0, 'color': (0, 255, 255)}, # Cyan
+        {'pos': 0.3, 'color': (255, 0, 255)}, # Magenta
+        {'pos': 0.6, 'color': (255, 255, 0)}, # Yellow
+        {'pos': 1.0, 'color': (0, 255, 0)}    # Green
+    ],
+    'candy': [
+        {'pos': 0.0, 'color': (255, 192, 203)}, # Pink
+        {'pos': 0.5, 'color': (173, 216, 230)}, # Light Blue
+        {'pos': 1.0, 'color': (255, 255, 0)}    # Yellow
+    ],
+    'winter': [
+        {'pos': 0.0, 'color': (0, 0, 128)},   # Navy
+        {'pos': 0.5, 'color': (173, 216, 230)}, # Light Blue
+        {'pos': 1.0, 'color': (255, 250, 250)}  # Snow
+    ],
+    'autumn': [
+        {'pos': 0.0, 'color': (139, 69, 19)},  # Saddle Brown
+        {'pos': 0.5, 'color': (255, 140, 0)}, # Dark Orange
+        {'pos': 1.0, 'color': (255, 215, 0)}  # Gold
+    ],
+    'spring': [
+        {'pos': 0.0, 'color': (152, 251, 152)}, # Pale Green
+        {'pos': 0.5, 'color': (255, 228, 196)}, # Bisque
+        {'pos': 1.0, 'color': (255, 192, 203)}  # Pink
+    ],
+    'vaporwave': [
+        {'pos': 0.0, 'color': (128, 0, 128)}, # Purple
+        {'pos': 0.5, 'color': (0, 255, 255)}, # Cyan
+        {'pos': 1.0, 'color': (255, 105, 180)} # Hot Pink
+    ],
+    'custom': [
+        {'pos': 0.0, 'color': (0, 0, 0)}, # Placeholder for custom theme
+        {'pos': 1.0, 'color': (255, 255, 255)}
+    ]
+}
+
+
+

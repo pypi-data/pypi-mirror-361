3b0b8e914419b1c57a7116b4aba58d7374c58e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:44 +0000

    Add package bsmutility/0.3.9/bsmutility-0.3.9-py3-none-any.whl

---
+++ packages/bsmutility/bsmutility-0.3.9-py3-none-any.whl/bsmutility/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'bsmutility'
+
+VER_MAJOR        = 0
+VER_MINOR        = 3
+VER_RELEASE      = 9
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'

3131515f98b787884b7b02b52aea63a3c5b4ca8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:06 +0000

    Add package palettize/0.0.1a2/palettize-0.0.1a2-py3-none-any.whl

---
+++ packages/palettize/palettize-0.0.1a2-py3-none-any.whl/palettize/__init__.py
@@ -0,0 +1,7 @@
+"""Palettize: Python utility and CLI tool for generating and exporting color map objects."""
+
+__version__ = "0.0.1.dev0"  # Initial development version
+
+
+def hello() -> str:
+    return "Hello from palettize!"

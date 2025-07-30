bd0a5c4ceac50d14cceeb2daa19b524c54c926b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package mcpturbo-docs/1.0.0/mcpturbo_docs-1.0.0-py3-none-any.whl

---
+++ packages/mcpturbo-docs/mcpturbo_docs-1.0.0-py3-none-any.whl/mcpturbo_docs/__init__.py
@@ -0,0 +1,11 @@
+"""
+Mcpturbo Docs
+
+MCPTurbo - Documentation and Examples
+"""
+
+__version__ = "1.0.0"
+__author__ = "Federico Monfasani"
+
+# Main exports will be added here
+__all__ = []
---
+++ packages/mcpturbo-docs/mcpturbo_docs-1.0.0-py3-none-any.whl/mcpturbo_docs/main.py
@@ -0,0 +1,12 @@
+"""Main module for mcpturbo-docs"""
+
+class McpturboDocs:
+    """Main class for mcpturbo-docs"""
+    
+    def __init__(self):
+        self.version = "1.0.0"
+        
+    async def run(self):
+        """Main execution method"""
+        # Implementation will be added
+        pass

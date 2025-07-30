c56523971ca4508b5727d28fcef3a202ec98aa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:01 +0000

    Add package OTP-python/0.0.1/otp_python-0.0.1.tar.gz

---
+++ packages/OTP-python/otp_python-0.0.1.tar.gz/otp_python-0.0.1/README.md
@@ -0,0 +1,8 @@
+**readme**\
+我不会英文,只能用中文写了\
+这个代码可以生成随机OTP密钥,
+加密和解密OTP\
+使用generate_the_otp_key()生成密钥,\
+使用encrypt()加密OTP,\
+使用declassify()解密OTP\
+(只能使用大写字母)
---
+++ packages/OTP-python/otp_python-0.0.1.tar.gz/otp_python-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools  # 导入setuptools打包工具
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OTP-python",
+    version="0.0.1",
+    author="zhx",
+    author_email="zhx_13306079573@outlook.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',  # 至少我用的是3.11(>▽<)
+)

265490f74d1bcc7ca53117e1425820b71798ac97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:59 +0000

    Add package fintoc/2.10.0/fintoc-2.10.0.tar.gz

---
+++ packages/fintoc/fintoc-2.10.0.tar.gz/fintoc-2.10.0/fintoc/resources/v2/__init__.py
@@ -0,0 +1,7 @@
+"""Init file for the v2 resources module of the SDK."""
+
+from .account import Account
+from .account_number import AccountNumber
+from .account_verification import AccountVerification
+from .entity import Entity
+from .transfer import Transfer

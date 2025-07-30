c158a5b18767e6caa67098e14cad014ec76ccf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:48 +0000

    Add package llm-dataset-converter-all/0.0.6/llm_dataset_converter_all-0.0.6.tar.gz

---
+++ packages/llm-dataset-converter-all/llm_dataset_converter_all-0.0.6.tar.gz/llm_dataset_converter_all-0.0.6/CHANGES.rst
@@ -0,0 +1,57 @@
+Changelog
+=========
+
+0.0.6 (2025-07-11)
+------------------
+
+- upgraded llm_dataset_converter to 0.2.7
+- upgraded ldc_doc to 0.0.5
+- upgraded ldc_docx to 0.0.4
+- upgraded ldc_faster_whisper to 0.0.3
+- upgraded ldc_google to 0.0.2
+- upgraded ldc_openai to 0.0.2
+- upgraded ldc_pdf to 0.0.4
+- upgraded ldc_tint to 0.0.3
+
+
+0.0.5 (2025-03-14)
+------------------
+
+- upgraded llm_dataset_converter to 0.2.6
+- upgraded ldc_doc to 0.0.4
+- upgraded ldc_docx to 0.0.3
+- upgraded ldc_faster_whisper to 0.0.2
+- upgraded ldc_gitingest to 0.0.2
+- upgraded ldc_html to 0.0.3
+- upgraded ldc_pdf to 0.0.3
+
+
+0.0.4 (2025-01-07)
+------------------
+
+- upgraded llm_dataset_converter to 0.2.5
+- upgraded ldc_doc to 0.0.3
+- added ldc_gitingest 0.0.1
+
+
+0.0.3 (2024-09-25)
+------------------
+
+- added ldc-tint as dependency
+
+
+0.0.2 (2024-07-05)
+------------------
+
+- upgraded llm-dataset-converter to 0.2.4
+- upgraded ldc-doc to 0.0.2
+- upgraded ldc-docx to 0.0.2
+- upgraded ldc-html to 0.0.2
+- upgraded ldc-pdf to 0.0.2
+
+
+0.0.1 (2024-06-06)
+------------------
+
+- initial release
+
---
+++ packages/llm-dataset-converter-all/llm_dataset_converter_all-0.0.6.tar.gz/llm_dataset_converter_all-0.0.6/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="llm_dataset_converter_all",
+    description="Meta-library that combines all llm-dataset-converter libraries.",
+    long_description=(
+            _read('DESCRIPTION.rst') + b'\n' +
+            _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-llm/audio-dataset-converter-all",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ],
+    license='MIT License',
+    install_requires=[
+        "llm_dataset_converter>=0.2.7",
+        "ldc_doc>=0.0.5",
+        "ldc_docx>=0.0.4",
+        "ldc_faster-whisper>=0.0.3",
+        "ldc_gitingest>=0.0.2",
+        "ldc_google>=0.0.2",
+        "ldc_html>=0.0.3",
+        "ldc_openai>=0.0.2",
+        "ldc_pdf>=0.0.4",
+        "ldc_tint>=0.0.3",
+    ],
+    version="0.0.6",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+)

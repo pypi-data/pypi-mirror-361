5b01c78db15021f8768461ca065063a36f49ca7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:32 +0000

    Add package multipyline/0.1.0/multipyline-0.1.0-py3-none-any.whl

---
+++ packages/multipyline/multipyline-0.1.0-py3-none-any.whl/multipyline/__init__.py
@@ -0,0 +1,9 @@
+from .multipyline import multipyline
+from .multipyline_inner import multipyline_inner
+from .multipyline_format import multipyline_format
+
+__all__ = [
+    "multipyline",
+    "multipyline_inner",
+    "multipyline_format",
+]

a76fc8ffde8deb21755547168970933c378319ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package veluna/1.0.0/veluna-1.0.0.tar.gz

---
+++ packages/veluna/veluna-1.0.0.tar.gz/veluna-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='veluna',
+    version='1.0.0',
+    packages=find_packages(),  # Automatically finds the package directories
+    install_requires=[
+        # List your package dependencies here
+    ],
+)

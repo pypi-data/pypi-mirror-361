c811d16468c55296c356e42b080b12ee02c1d2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:15 +0000

    Add package omlish/0.0.0.dev360/omlish-0.0.0.dev360.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev360.tar.gz/omlish-0.0.0.dev360/omlish/dataclasses/processing/priority.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class ProcessorPriority(enum.IntEnum):
+    BOOTSTRAP = enum.auto()
+
+    PRE_GENERATION = enum.auto()
+    GENERATION = enum.auto()
+    POST_GENERATION = enum.auto()
+
+    PRE_SLOTS = enum.auto()
+    SLOTS = enum.auto()
+    POST_SLOTS = enum.auto()

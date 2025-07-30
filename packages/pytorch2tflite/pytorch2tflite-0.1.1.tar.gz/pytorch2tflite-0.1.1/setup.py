7b993944f0b470497e4c6f3414aed623acf0968e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:59 +0000

    Add package pytorch2tflite/0.1.1/pytorch2tflite-0.1.1.tar.gz

---
+++ packages/pytorch2tflite/pytorch2tflite-0.1.1.tar.gz/pytorch2tflite-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch2tflite',
+    version='0.1.1',
+    description='Convert PyTorch (.pt) models to TFLite format for mobile deployment.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Md.Hamid Hosen',
+    author_email='mdhamidhosen4@gmail.com',
+    url='https://github.com/hamidhosen42/pytorch2tflite',
+    packages=find_packages(),
+    install_requires=[
+        'ultralytics',
+        'torch',
+        'onnx',
+        'onnx-tf',
+        'tensorflow'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
